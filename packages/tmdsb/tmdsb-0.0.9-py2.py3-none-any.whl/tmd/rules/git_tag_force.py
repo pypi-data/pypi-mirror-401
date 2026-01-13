c47b2f3a600206858f98196e7f6369d51d3a2675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:47 +0000

    Add package tmdsb/0.0.9/tmdsb-0.0.9-py2.py3-none-any.whl

---
+++ packages/tmdsb/tmdsb-0.0.9-py2.py3-none-any.whl/tmd/rules/git_tag_force.py
@@ -0,0 +1,13 @@
+from tmd.utils import replace_argument
+from tmd.specific.git import git_support
+
+
+@git_support
+def match(command):
+    return ('tag' in command.script_parts
+            and 'already exists' in command.output)
+
+
+@git_support
+def get_new_command(command):
+    return replace_argument(command.script, 'tag', 'tag --force')
