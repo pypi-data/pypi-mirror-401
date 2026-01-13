cc5d32703bb97e70327cd5a52119e0adc1176f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:19 +0000

    Add package pysdenv/1.0.0/pysdenv-1.0.0.tar.gz

---
+++ packages/pysdenv/pysdenv-1.0.0.tar.gz/pysdenv-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pysdenv",
+    version="1.0.0",
+    description="Python wrapper for sdenv - 瑞数VMP补环境框架",
+    py_modules=["sdenv"],
+    python_requires=">=3.7",
+)
