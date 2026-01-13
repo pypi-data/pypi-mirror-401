bbd0ad15e16709412a04e869b4264836d3234b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:25 +0000

    Add package tyc-engine/0.2.2/tyc_engine-0.2.2-py3-none-any.whl

---
+++ packages/tyc-engine/tyc_engine-0.2.2-py3-none-any.whl/TYC_ENGINE/render/window.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+def render(w, h, name):
+    screen = pygame.display.set_mode((w, h))
+    pygame.display.set_caption(name)
+    return screen
+
+def run(running):
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
