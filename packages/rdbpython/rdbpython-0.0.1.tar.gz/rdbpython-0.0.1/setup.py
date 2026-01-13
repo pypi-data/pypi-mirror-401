b4f64b4d34c2f2ad60e57d64f91c736dd9a3650b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:19 +0000

    Add package rdbpython/0.0.1/rdbpython-0.0.1.tar.gz

---
+++ packages/rdbpython/rdbpython-0.0.1.tar.gz/rdbpython-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="rdbpython",
+    version="0.0.1",
+    description="Package name reserved for future use",
+    long_description="This package name is reserved for future development.",
+    long_description_content_type="text/plain",
+    author="Gor Arakelyan",
+    author_email="arakelyangor10@gmail.com",
+    url="https://github.com/gorarakelyan/rdbpython",
+    py_modules=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
