2538416c8155b96b39521cab9f69e987478ef8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:03 +0000

    Add package flytekitplugins-onnxscikitlearn/1.16.12/flytekitplugins_onnxscikitlearn-1.16.12.tar.gz

---
+++ packages/flytekitplugins-onnxscikitlearn/flytekitplugins_onnxscikitlearn-1.16.12.tar.gz/flytekitplugins_onnxscikitlearn-1.16.12/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+PLUGIN_NAME = "onnxscikitlearn"
+
+microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
+
+plugin_requires = ["flytekit", "skl2onnx>=1.10.3,<1.19.0", "networkx<3.2; python_version<'3.9'", "onnx<1.18.0"]
+
+__version__ = "1.16.12"
+
+setup(
+    title="ONNX ScikitLearn",
+    title_expanded="Flytekit ONNX ScikitLearn Plugin",
+    name=f"flytekitplugins-{PLUGIN_NAME}",
+    version=__version__,
+    author="flyteorg",
+    author_email="admin@flyte.org",
+    description="ONNX ScikitLearn Plugin for Flytekit",
+    namespace_packages=["flytekitplugins"],
+    packages=[f"flytekitplugins.{PLUGIN_NAME}"],
+    install_requires=plugin_requires,
+    license="apache2",
+    python_requires=">=3.9",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
