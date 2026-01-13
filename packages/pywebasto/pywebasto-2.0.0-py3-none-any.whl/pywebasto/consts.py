556fc735e3cf152224a20aa21fe5476ab3471981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 15:07:57 +0000

    Add package pywebasto/2.0.0/pywebasto-2.0.0-py3-none-any.whl

---
+++ packages/pywebasto/pywebasto-2.0.0-py3-none-any.whl/pywebasto/consts.py
@@ -0,0 +1,15 @@
+"""Constants used for Webasto."""
+
+API_URL = "https://my.webastoconnect.com/webapi"
+
+CMD_HEATER_ON = "OUT H ON"
+CMD_HEATER_OFF = "OUT H OFF"
+
+CMD_VENTILATION_ON = "OUT V ON"
+CMD_VENTILATION_OFF = "OUT V OFF"
+
+CMD_AUX1_ON = "OUT 1 ON"
+CMD_AUX1_OFF = "OUT 1 OFF"
+
+CMD_AUX2_ON = "OUT 2 ON"
+CMD_AUX2_OFF = "OUT 2 OFF"
