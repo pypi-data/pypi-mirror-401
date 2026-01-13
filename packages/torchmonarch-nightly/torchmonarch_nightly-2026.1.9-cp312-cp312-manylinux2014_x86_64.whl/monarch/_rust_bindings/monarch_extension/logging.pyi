247e8e949cf402f5fc1415f352a745f8f586a274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:25 +0000

    Add package torchmonarch-nightly/2026.1.9/torchmonarch_nightly-2026.1.9-cp312-cp312-manylinux2014_x86_64.whl

---
+++ packages/torchmonarch-nightly/torchmonarch_nightly-2026.1.9-cp312-cp312-manylinux2014_x86_64.whl/monarch/_rust_bindings/monarch_extension/logging.pyi
@@ -0,0 +1,32 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from typing import final
+
+from monarch._rust_bindings.monarch_hyperactor.context import Instance
+
+from monarch._rust_bindings.monarch_hyperactor.proc_mesh import ProcMesh
+from monarch._rust_bindings.monarch_hyperactor.pytokio import PythonTask
+
+@final
+class LoggingMeshClient:
+    """
+    Python binding for the Rust LoggingMeshClient.
+    """
+    @staticmethod
+    def spawn(
+        instance: Instance, proc_mesh: ProcMesh
+    ) -> PythonTask[LoggingMeshClient]: ...
+    def set_mode(
+        self,
+        instance: Instance,
+        stream_to_client: bool,
+        aggregate_window_sec: int | None,
+        level: int,
+    ) -> None: ...
+    def flush(self, instance: Instance) -> PythonTask[None]: ...
---
+++ packages/torchmonarch-nightly/torchmonarch_nightly-2026.1.9-cp312-cp312-manylinux2014_x86_64.whl/tests/test_alloc.py
@@ -0,0 +1,25 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from unittest import IsolatedAsyncioTestCase
+
+from monarch._rust_bindings.monarch_hyperactor.alloc import (  # @manual=//monarch/monarch_extension:monarch_extension
+    AllocConstraints,
+    AllocSpec,
+)
+from monarch._src.actor.allocator import ProcessAllocator
+
+
+class TestAlloc(IsolatedAsyncioTestCase):
+    async def test_basic(self) -> None:
+        cmd = "echo hello"
+        allocator = ProcessAllocator(cmd)
+        spec = AllocSpec(AllocConstraints(), replica=2)
+        alloc = allocator.allocate(spec)
+
+        print(alloc)
