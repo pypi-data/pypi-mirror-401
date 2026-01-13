0f87cc2224bedf96b0335889694a37add46baa07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:19 +0000

    Add package authutils/7.2.1/authutils-7.2.1.tar.gz

---
+++ packages/authutils/authutils-7.2.1.tar.gz/authutils-7.2.1/src/authutils/errors.py
@@ -0,0 +1,31 @@
+from cdiserrors import AuthNError as CDISAuthNError
+
+
+class AuthError(CDISAuthNError):
+
+    pass
+
+
+class JWTError(AuthError):
+
+    pass
+
+
+class JWTExpiredError(AuthError):
+
+    pass
+
+
+class JWTPurposeError(JWTError):
+
+    pass
+
+
+class JWTAudienceError(JWTError):
+
+    pass
+
+
+class JWTScopeError(JWTError):
+
+    pass
