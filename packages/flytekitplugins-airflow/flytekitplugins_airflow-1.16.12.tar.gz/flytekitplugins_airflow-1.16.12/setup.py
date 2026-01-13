0bf1919e9c12302e7a0a80fee2181f05b691810f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:00 +0000

    Add package flytekitplugins-airflow/1.16.12/flytekitplugins_airflow-1.16.12.tar.gz

---
+++ packages/flytekitplugins-airflow/flytekitplugins_airflow-1.16.12.tar.gz/flytekitplugins_airflow-1.16.12/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+PLUGIN_NAME = "airflow"
+
+microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
+
+plugin_requires = [
+    "apache-airflow<3.0.0",
+    "apache-airflow-providers-google<12.0.0",
+    "flytekit>1.10.7",
+    "flyteidl>1.10.7",
+    "sqlalchemy-spanner<1.12.0",
+]
+
+__version__ = "1.16.12"
+
+setup(
+    title="Apache Airflow",
+    title_expanded="Flytekit Airflow Plugin",
+    name=microlib_name,
+    version=__version__,
+    author="flyteorg",
+    author_email="admin@flyte.org",
+    description="This package holds the Airflow plugins for flytekit",
+    namespace_packages=["flytekitplugins"],
+    packages=[f"flytekitplugins.{PLUGIN_NAME}"],
+    install_requires=plugin_requires,
+    license="apache2",
+    python_requires=">=3.9",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    entry_points={"flytekit.plugins": [f"{PLUGIN_NAME}=flytekitplugins.{PLUGIN_NAME}"]},
+)
