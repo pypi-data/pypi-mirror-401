84637a03c72651a7ee11a160f54135a7f95f5df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:06:41 +0000

    Add package bosing/2.6.1/bosing-2.6.1-cp313-cp313t-win_arm64.whl

---
+++ packages/bosing/bosing-2.6.1-cp313-cp313t-win_arm64.whl/bosing/__init__.py
@@ -0,0 +1,65 @@
+r"""Generates microwave pulses for superconducting quantum computing experiments.
+
+.. caution::
+
+    The unit of phase is number of cycles, not radians. For example, a phase
+    of :math:`0.5` means a phase shift of :math:`\pi` radians.
+"""
+
+from __future__ import annotations
+
+from ._bosing import (
+    Absolute,
+    AbsoluteEntry,
+    Alignment,
+    Barrier,
+    Channel,
+    Direction,
+    Element,
+    Grid,
+    GridEntry,
+    GridLength,
+    GridLengthUnit,
+    Hann,
+    Interp,
+    OscState,
+    Play,
+    Repeat,
+    SetFreq,
+    SetPhase,
+    Shape,
+    ShiftFreq,
+    ShiftPhase,
+    Stack,
+    SwapPhase,
+    generate_waveforms,
+    generate_waveforms_with_states,
+)
+
+__all__ = [
+    "Absolute",
+    "AbsoluteEntry",
+    "Alignment",
+    "Barrier",
+    "Channel",
+    "Direction",
+    "Element",
+    "Grid",
+    "GridEntry",
+    "GridLength",
+    "GridLengthUnit",
+    "Hann",
+    "Interp",
+    "OscState",
+    "Play",
+    "Repeat",
+    "SetFreq",
+    "SetPhase",
+    "Shape",
+    "ShiftFreq",
+    "ShiftPhase",
+    "Stack",
+    "SwapPhase",
+    "generate_waveforms",
+    "generate_waveforms_with_states",
+]
