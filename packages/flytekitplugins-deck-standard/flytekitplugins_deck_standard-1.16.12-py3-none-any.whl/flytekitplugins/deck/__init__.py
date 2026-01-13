faa5ddf829d9527bcb6827c06789c9560be85692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:05:57 +0000

    Add package flytekitplugins-deck-standard/1.16.12/flytekitplugins_deck_standard-1.16.12-py3-none-any.whl

---
+++ packages/flytekitplugins-deck-standard/flytekitplugins_deck_standard-1.16.12-py3-none-any.whl/flytekitplugins/deck/__init__.py
@@ -0,0 +1,27 @@
+"""
+.. currentmodule:: flytekitplugins.deck
+
+This package contains things that are useful when extending Flytekit.
+
+.. autosummary::
+   :template: custom.rst
+   :toctree: generated/
+
+   BoxRenderer
+   FrameProfilingRenderer
+   GanttChartRenderer
+   ImageRenderer
+   MarkdownRenderer
+   SourceCodeRenderer
+   TableRenderer
+"""
+
+from .renderer import (
+    BoxRenderer,
+    FrameProfilingRenderer,
+    GanttChartRenderer,
+    ImageRenderer,
+    MarkdownRenderer,
+    SourceCodeRenderer,
+    TableRenderer,
+)
