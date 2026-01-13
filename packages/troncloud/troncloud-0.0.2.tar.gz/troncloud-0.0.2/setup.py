72b4b07d2875165cf6f2b41df220134dd8823f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:04:40 +0000

    Add package troncloud/0.0.2/troncloud-0.0.2.tar.gz

---
+++ packages/troncloud/troncloud-0.0.2.tar.gz/troncloud-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text() #Gets the long description from Readme file
+
+setup(
+    name='troncloud',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=[
+         'requests',
+    ],
+    include_package_data=True,
+    package_data={
+        "troncloud": ["pyarmor_runtime_000000/*"],
+    },
+    zip_safe=False,
+    author='alimserg',
+    author_email='d.ig.o.sch.r.istiann@gmail.com',
+    description='troncloud library for Python',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+     project_urls={
+           'Source Repository': 'https://github.com/sergeychernyakov' #replace with your github source
+    }
+)
