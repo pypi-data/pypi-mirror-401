baefa0f05907f94c9503b27187beda2bbe3e11af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:06:46 +0000

    Add package py-me/3.3.3/py_me-3.3.3.tar.gz

---
+++ packages/py-me/py_me-3.3.3.tar.gz/py_me-3.3.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py_me',
+    version='3.3.3',
+    packages=find_packages(),
+    description='Biblioteca com módulos musicais, gráficos e de criação de projetos',
+    author='isaias da silva nobrega',
+    author_email='isaiasdasilvanobrega@gmail.com',
+)
