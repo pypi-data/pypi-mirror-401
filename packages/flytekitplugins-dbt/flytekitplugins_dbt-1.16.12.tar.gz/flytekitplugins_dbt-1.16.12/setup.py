ab90fbfee77e1aa6fa8c2da52a4b82eb7ea20dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:00 +0000

    Add package flytekitplugins-dbt/1.16.12/flytekitplugins_dbt-1.16.12.tar.gz

---
+++ packages/flytekitplugins-dbt/flytekitplugins_dbt-1.16.12.tar.gz/flytekitplugins_dbt-1.16.12/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+PLUGIN_NAME = "dbt"
+
+microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
+
+plugin_requires = ["flytekit>=1.3.0b2", "dbt-core>=1.6.0,<1.8.0", "networkx>=2.5"]
+
+__version__ = "1.16.12"
+
+setup(
+    title="DBT",
+    title_expanded="Flytekit dbt plugin",
+    name=microlib_name,
+    version=__version__,
+    author="flyteorg",
+    author_email="admin@flyte.org",
+    description="DBT Plugin for Flytekit",
+    url="https://github.com/flyteorg/flytekit/tree/master/plugins/flytekit-dbt",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    namespace_packages=["flytekitplugins"],
+    packages=[f"flytekitplugins.{PLUGIN_NAME}"],
+    install_requires=plugin_requires,
+    license="apache2",
+    python_requires=">=3.9",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
