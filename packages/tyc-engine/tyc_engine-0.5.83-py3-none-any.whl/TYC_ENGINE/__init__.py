2a722b4fd3fb7e1136732acebeb2da55e3b1a6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:35 +0000

    Add package tyc-engine/0.5.83/tyc_engine-0.5.83-py3-none-any.whl

---
+++ packages/tyc-engine/tyc_engine-0.5.83-py3-none-any.whl/TYC_ENGINE/__init__.py
@@ -0,0 +1,10 @@
+from src.TYC_ENGINE.assets.lib_assets.py_assets.core import render, run
+from src.TYC_ENGINE.assets.lib_assets.py_assets.loader import loading_app
+from src.TYC_ENGINE.assets.lib_assets.py_assets.engine import Engine
+
+__all__ = [
+    "render",
+    "run",
+    "loading_app",
+    "Engine",
+]
