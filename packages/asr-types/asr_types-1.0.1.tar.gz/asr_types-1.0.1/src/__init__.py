488c8dbfc9a7493a813981447db0d6c57264e8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:06:54 +0000

    Add package asr-types/1.0.1/asr_types-1.0.1.tar.gz

---
+++ packages/asr-types/asr_types-1.0.1.tar.gz/asr_types-1.0.1/src/__init__.py
@@ -0,0 +1,13 @@
+"""
+ASR Types - Type definitions for ASR data structures.
+"""
+
+from .definitions import ASRRawData, ASRSegment, ASRWord
+
+__version__ = "1.0.1"
+
+__all__ = [
+    "ASRWord",
+    "ASRSegment",
+    "ASRRawData",
+]
