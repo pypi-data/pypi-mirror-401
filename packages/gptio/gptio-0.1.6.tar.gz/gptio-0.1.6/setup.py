69d2e7f3e96fc8b7b1ab066fec8ca68b7b81fd48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:09:27 +0000

    Add package gptio/0.1.6/gptio-0.1.6.tar.gz

---
+++ packages/gptio/gptio-0.1.6.tar.gz/gptio-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="gptio",               # your module name
+    version="0.1.6",
+    author="wirnty",
+author_email="skedovichusjdj@gmail.com.com",
+    description="A simple Python module to interact with ChatGPT via OpenAI API.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    py_modules=["gptio"],       # <-- points to your single .py file
+    python_requires=">=3.12",
+    install_requires=[
+        "requests>=2.31.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Intended Audience :: Developers"
+    ],
+)
