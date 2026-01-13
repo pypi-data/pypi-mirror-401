b9206588e9586f845d361810d2cca48a81355d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:04:33 +0000

    Add package esnodePy/0.1.0/esnodepy-0.1.0-py3-none-any.whl

---
+++ packages/esnodePy/esnodepy-0.1.0-py3-none-any.whl/esnodepy/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2024 ESTIMATEDSTOCKS AB & KHAJAMODDIN SHAIK. All Rights Reserved.
+#
+# This software is released under the ESNODE COMMUNITY LICENSE 1.0.
+# See the LICENSE file in the root directory for full terms and conditions.
+
+__version__ = "0.1.0"
