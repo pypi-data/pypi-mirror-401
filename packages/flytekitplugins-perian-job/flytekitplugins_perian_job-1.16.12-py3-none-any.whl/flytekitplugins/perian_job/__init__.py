d5ba10e9a2dc8d87f22431af4100adb05fca43bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:05:59 +0000

    Add package flytekitplugins-perian-job/1.16.12/flytekitplugins_perian_job-1.16.12-py3-none-any.whl

---
+++ packages/flytekitplugins-perian-job/flytekitplugins_perian_job-1.16.12-py3-none-any.whl/flytekitplugins/perian_job/__init__.py
@@ -0,0 +1,14 @@
+"""
+.. currentmodule:: flytekitplugins.perian_job
+
+.. autosummary::
+   :template: custom.rst
+   :toctree: generated/
+
+   PerianConnector
+   PerianConfig
+   PerianTask
+"""
+
+from .connector import PerianConnector
+from .task import PerianConfig, PerianContainerTask, PerianTask
