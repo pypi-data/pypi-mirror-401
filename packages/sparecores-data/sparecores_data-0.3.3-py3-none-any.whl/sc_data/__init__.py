1c8d81865e79f716fb55218c20991de0c97d6999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:08:48 +0000

    Add package sparecores-data/0.3.3/sparecores_data-0.3.3-py3-none-any.whl

---
+++ packages/sparecores-data/sparecores_data-0.3.3-py3-none-any.whl/sc_data/__init__.py
@@ -0,0 +1,28 @@
+"""
+Hold/update the data file for Spare Cores.
+
+Usage:
+
+```
+import sc_data
+sc_data.db.path  # this will hold the actual data file's path (either the built-in or a temporary file when update is not disabled
+sc_data.db.hash  # an SHA256 hash of the DB, can be used to track changes and reopen the database if needed
+```
+
+The module accepts the following parameters (must be set before importing):
+    - builtins.sc_data_no_update / SC_DATA_NO_UPDATE - don't do updates if set
+    - builtins.sc_data_db_path / SC_DATA_DB_PATH - the initial database path
+    - builtins.sc_data_db_url / SC_DATA_DB_URL - DB URL to fetch updates
+    - builtins.sc_data_http_timeout / SC_DATA_HTTP_TIMEOUT - HTTP timeout in seconds
+    - builtins.sc_data_db_refresh_seconds / SC_DATA_DB_REFRESH_SECONDS - update database after this has passed
+"""
+
+import safe_exit
+
+from .data import Data, close_tmpfiles, get_parameter
+
+db = Data(name="remote_update")
+db.start()
+if not get_parameter("no_update"):
+    safe_exit.register(close_tmpfiles, db.tmpfiles)
+db.updated.wait(float(get_parameter("http_timeout")) + 1)
---
+++ packages/sparecores-data/sparecores_data-0.3.3-py3-none-any.whl/sc_data/data.py
@@ -0,0 +1,112 @@
+import builtins
+import bz2
+import collections
+import logging
+import os
+import shutil
+import tempfile
+import threading
+import time
+
+import requests
+
+from . import constants
+
+logger = logging.getLogger(__name__)
+
+
+def get_parameter(name):
+    """Get a parameter either from builtins, envvars or the constants module."""
+    return (
+        getattr(builtins, f"sc_data_{name}", None)
+        or os.environ.get(f"SC_DATA_{name.upper()}")
+        or getattr(constants, name.upper(), None)
+    )
+
+
+def close_tmpfiles(tmpfiles):
+    """Close/remove temporary files."""
+    for tmpfile in tmpfiles.copy():
+        try:
+            os.unlink(tmpfile.name)
+        except FileNotFoundError:
+            pass
+        tmpfile.close()
+        tmpfiles.remove(tmpfile)
+
+
+def handle(f, url=None):
+    """Return the original or wrapped file handle depending on the file name."""
+    if url.endswith("bz2"):
+        return bz2.BZ2File(f, mode="rb")
+    else:
+        return f
+
+
+class Data(threading.Thread):
+    daemon = True
+
+    def __init__(self, *args, **kwargs):
+        self.tmpfiles = collections.deque()
+        self.updated = threading.Event()
+        self.lock = threading.Lock()
+        self.actual_db_path = get_parameter("db_path")
+        # initialize with embedded DB hash
+        self.actual_db_hash = constants.DB_HASH
+        super().__init__(*args, **kwargs)
+
+    @property
+    def path(self):
+        with self.lock:
+            return self.actual_db_path
+
+    @property
+    def hash(self):
+        with self.lock:
+            return self.actual_db_hash
+
+    def update(self):
+        """Update the database file if necessary."""
+
+        # initiate a streaming GET, to receive the headers, but halt the content
+        r = requests.get(
+            get_parameter("db_url"),
+            timeout=float(get_parameter("http_timeout")),
+            stream=True,
+        )
+        # fetch the file if necessary (200 status and etag is missing or different than previous)
+        if (
+            200 <= r.status_code < 300
+            and (db_hash := r.headers.get("x-amz-meta-hash", time.time()))
+            != self.actual_db_hash
+        ):
+            # delete=False due to Windows support
+            # https://stackoverflow.com/questions/15588314/cant-access-temporary-files-created-with-tempfile/15590253#15590253
+            logger.debug("Downloading new SQLite database")
+            tmpfile = tempfile.NamedTemporaryFile(delete=False)
+            # use the original, or a decompressor-wrapped file handle
+            fh = handle(r.raw, url=get_parameter("db_url"))
+            shutil.copyfileobj(fh, tmpfile)
+            tmpfile.flush()
+            with self.lock:
+                self.actual_db_path = tmpfile.name
+                self.actual_db_hash = db_hash
+            close_tmpfiles(self.tmpfiles)
+            self.tmpfiles.append(tmpfile)
+            logger.debug("Updated database to hash %s", db_hash)
+        else:
+            logger.debug("No need to update database")
+
+    def run(self):
+        """Start the update thread if no_update is not set."""
+        if get_parameter("no_update"):
+            logger.warn("Automated database refresh is disabled.")
+            self.updated.set()
+            return
+        while True:
+            try:
+                self.update()
+            except Exception:
+                logger.exception("Failed to update the database")
+            self.updated.set()
+            time.sleep(int(get_parameter("db_refresh_seconds")))
