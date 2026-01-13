5a867cdfa6dda4518f7c68d89e83b6da1ccc1cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:45 +0000

    Add package tmdsb/0.0.3/tmdsb-0.0.3.tar.gz

---
+++ packages/tmdsb/tmdsb-0.0.3.tar.gz/tmdsb-0.0.3/tmd/rules/apt_list_upgradable.py
@@ -0,0 +1,16 @@
+from tmd.specific.apt import apt_available
+from tmd.specific.sudo import sudo_support
+from tmd.utils import for_app
+
+enabled_by_default = apt_available
+
+
+@sudo_support
+@for_app('apt')
+def match(command):
+    return 'apt list --upgradable' in command.output
+
+
+@sudo_support
+def get_new_command(command):
+    return 'apt list --upgradable'
