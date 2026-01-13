6d058ffeeb83e85c8d4943f18036f8030a08a111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:04:48 +0000

    Add package musi/0.4.0/musi-0.4.0.tar.gz

---
+++ packages/musi/musi-0.4.0.tar.gz/musi-0.4.0/musi/__init__.py
@@ -0,0 +1,30 @@
+﻿"""
+Musi - A music package based on twelve-tone equal temperament
+"""
+
+__version__ = \"0.4.0\"
+__author__ = \"piacode\"
+
+# 导入 main.py 中的所有内容
+from .main import *
+
+# 显式导出所有重要函数和类
+__all__ = [
+    'Musi',
+    'musi',
+    'calculate_frequency',
+    'note',
+    'scale',
+    'chord',
+    'play_note',
+    'play_scale',
+    'play_chord',
+    'export_scale',
+    'export_chord',
+    'InvalidNoteError',
+    'InvalidScaleTypeError',
+    'InvalidChordTypeError',
+    'InvalidNoteDurationError',
+    'InvalidBPMError',
+    'InvalidDirectionError'
+]
