88c17294a0371314893b7eea933a097186a2549b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:08:31 +0000

    Add package neuralib-imaging/0.7.0/neuralib_imaging-0.7.0-py3-none-any.whl

---
+++ packages/neuralib-imaging/neuralib_imaging-0.7.0-py3-none-any.whl/neuralib/widefield/main_app.py
@@ -0,0 +1,30 @@
+from argclz.commands import parse_command_args
+from neuralib.widefield.align import NapariAlignmentOptions
+
+from .fft_view import WideFieldFFTViewOption
+
+
+def main():
+    parsers = dict(
+        align=NapariAlignmentOptions,
+        fft=WideFieldFFTViewOption
+    )
+
+    parse_command_args(
+        parsers=parsers,
+        description='widefield tools',
+        usage="""
+        Usage Examples:
+
+        View HSV map in FFT:
+        $ neuralib_widefield fft <FILE>
+        
+        Alignment napari for image sequence
+        $ neuralib_widefield align <FILE>
+        
+        """
+    )
+
+
+if __name__ == '__main__':
+    main()
