701fc2b92066bd8b65f9c27ade0bda8fef7f69e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:32 +0000

    Add package django-clickthroughs/0.1/django_clickthroughs-0.1.tar.gz

---
+++ packages/django-clickthroughs/django_clickthroughs-0.1.tar.gz/django_clickthroughs-0.1/clickthroughs/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponseRedirect
+from django.views import View
+
+from .models import Clickthrough
+
+
+class ClickthroughView(View):
+    def get(self, request):
+
+        # TODO: switch to get() throughout
+        ip_address = request.META['REMOTE_ADDR']
+        session_id = request.session.session_key
+        user_agent = request.META['HTTP_USER_AGENT']
+        url_to = request.GET.get('to')
+        hostname = request.META['HTTP_HOST']
+        path_from = request.META['HTTP_REFERER']
+
+        click = Clickthrough()
+        click.ip_address = ip_address
+        click.session_id = session_id
+        click.user_agent = user_agent
+        click.url_to = url_to
+        click.hostname = hostname
+        click.path_from = path_from
+        click.save()
+
+        return HttpResponseRedirect(url_to)
