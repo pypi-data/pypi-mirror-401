5de6c99d85b2c0126734d67469d9b5cb4c481b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:10 +0000

    Add package crongateway/0.1.0/crongateway-0.1.0.tar.gz

---
+++ packages/crongateway/crongateway-0.1.0.tar.gz/crongateway-0.1.0/crongateway/__init__.py
@@ -0,0 +1,10 @@
+"""
+CronGateway Python SDK
+Official Python client for the CronGateway AI Platform.
+"""
+
+from .client import CronGatewayClient
+
+__version__ = "0.1.0"
+__all__ = ["CronGatewayClient"]
+
