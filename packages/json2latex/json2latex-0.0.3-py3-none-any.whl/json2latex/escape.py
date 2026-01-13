26eba3906aa3efd0f59f432feaebe3a2e30a48aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:29 +0000

    Add package json2latex/0.0.3/json2latex-0.0.3-py3-none-any.whl

---
+++ packages/json2latex/json2latex-0.0.3-py3-none-any.whl/json2latex/escape.py
@@ -0,0 +1,31 @@
+_replacements = {
+    "&": r"\&",
+    "%": r"\%",
+    "$": r"\$",
+    "#": r"\#",
+    "_": r"\_",
+    "{": r"\{",
+    "}": r"\}",
+    "~": r"\textasciitilde{}",
+    "^": r"\^{}",
+    "\\": r"\textbackslash{}",
+    "\n": "\\newline%\n",
+    "-": r"{-}",
+    "\xA0": "~",  # Non-breaking space
+    "[": r"{[}",
+    "]": r"{]}",
+}
+_replacement_lookup = str.maketrans(_replacements)
+
+
+def escape(string):
+    """
+    Replace special characters with their equivalent LaTeX macros.
+
+    Args:
+        string (str): The string to process.
+
+    Returns:
+        str: The string with special characters replaced with macros.
+    """
+    return string.translate(_replacement_lookup)
