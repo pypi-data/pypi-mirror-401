7ed29b7b89ac2d54f369d1b489dca1c1d1d4cff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 15:07:33 +0000

    Add package momcodwiz/0.1.0/momcodwiz-0.1.0.tar.gz

---
+++ packages/momcodwiz/momcodwiz-0.1.0.tar.gz/momcodwiz-0.1.0/momcodwiz/__init__.py
@@ -0,0 +1,2 @@
+# __init__.py
+from .generator import fuckcodwiz,niggerfathercodwiz,shitoncodwiz,burnmotherwiz,fuckwizandhismom
---
+++ packages/momcodwiz/momcodwiz-0.1.0.tar.gz/momcodwiz-0.1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+setup(
+    name="momcodwiz",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
