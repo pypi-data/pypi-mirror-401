efca1cf89eefc701e26597fbc063e412d47ba23a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:25 +0000

    Add package redbot/2.3.5/redbot-2.3.5.tar.gz

---
+++ packages/redbot/redbot-2.3.5.tar.gz/redbot-2.3.5/redbot/translations/extra_strings.py
@@ -0,0 +1,15 @@
+from redbot.i18n import _
+
+# These are extra strings from httplint that need to be translated
+
+
+def extra_strings() -> None:
+    # httplint categories
+    _("General")
+    _("Browser Security")
+    _("Cookies")
+    _("Content Negotiation")
+    _("Caching")
+    _("Validation")
+    _("Connection")
+    _("Partial Content")
