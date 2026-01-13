5889fe3320598ed9318b1d53530c55ccfb1a2e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:19 +0000

    Add package dhapi/4.2.1/dhapi-4.2.1.tar.gz

---
+++ packages/dhapi/dhapi-4.2.1.tar.gz/dhapi-4.2.1/setup.py
@@ -0,0 +1,48 @@
+import pathlib
+
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent.resolve()
+
+
+def _get_dependencies():
+    with open(HERE / "requirements.txt") as f:
+        return f.read().splitlines()
+
+
+setup(
+    name="dhapi",
+    description="동행복권 비공식 API",
+    use_scm_version=True,
+
+    long_description=(HERE / "README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/roeniss/dhlottery-api",
+    project_urls={
+        "Bug Reports": "https://github.com/roeniss/dhlottery-api/issues",
+        "Source": "https://github.com/roeniss/dhlottery-api/",
+    },
+    author="Roeniss Moon",
+    author_email="roeniss2@gmail.com",
+
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+    ],
+    keywords="donghaeng,lottery,lotto645,api,korean",
+
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.9, <4",
+
+    entry_points={
+        "console_scripts": [
+            "dhapi=dhapi.main:main",
+        ],
+    },
+
+    install_requires=_get_dependencies(),
+)
