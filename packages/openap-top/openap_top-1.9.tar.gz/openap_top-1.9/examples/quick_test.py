b70433bc3fc6ac6673a05957d690ba6bb5007f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:07:47 +0000

    Add package openap-top/1.9/openap_top-1.9.tar.gz

---
+++ packages/openap-top/openap_top-1.9.tar.gz/openap_top-1.9/examples/quick_test.py
@@ -0,0 +1,32 @@
+# %%
+import time
+import warnings
+
+from openap import top
+
+# %%
+warnings.filterwarnings("ignore")
+
+actype = "A320"
+origin = "EHAM"
+destination = "LGAV"
+m0 = 0.85
+
+optimizer = top.CompleteFlight(actype, origin, destination, m0)
+# optimizer = top.MultiPhase(actype, origin, destination, m0)
+# optimizer = top.Cruise(actype, origin, destination, m0)
+# optimizer = top.Climb(actype, origin, destination, m0)
+# optimizer = top.Descent(actype, origin, destination, m0)
+
+start = time.time()
+
+flight = optimizer.trajectory(objective="fuel")
+# flight = optimizer.trajectory(objective="ci:30")
+# flight = optimizer.trajectory(objective="gwp100")
+# flight = optimizer.trajectory(objective="gtp100")
+# flight = optimizer.trajectory(objective=("ci:90", "ci:10", "ci:20"))  # Multiphase
+
+
+print(flight)
+
+print(f"\nOptimal trajectory was generated in {round(time.time() - start)} seconds.\n")
