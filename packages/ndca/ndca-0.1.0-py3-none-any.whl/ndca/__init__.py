4f72dbf19c105eeafe47201f3a2cf01d86f84795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:21 +0000

    Add package ndca/0.1.0/ndca-0.1.0-py3-none-any.whl

---
+++ packages/ndca/ndca-0.1.0-py3-none-any.whl/ndca/__init__.py
@@ -0,0 +1,3 @@
+from .core import NDCA, file, get, write, delete, wipe, load, save, dump, keys, exists, merge, append, remove_from_list, loads, dumps, version
+__all__ = ['NDCA','file','get','write','delete','wipe','load','save','dump','keys','exists','merge','append','remove_from_list','loads','dumps','version']
+version = "0.1.0"
