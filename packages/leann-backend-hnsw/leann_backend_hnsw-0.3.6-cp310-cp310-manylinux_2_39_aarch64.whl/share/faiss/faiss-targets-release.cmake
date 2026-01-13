32999abd727694536c9c42492372ef7337a1f61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:33 +0000

    Add package leann-backend-hnsw/0.3.6/leann_backend_hnsw-0.3.6-cp310-cp310-manylinux_2_39_aarch64.whl

---
+++ packages/leann-backend-hnsw/leann_backend_hnsw-0.3.6-cp310-cp310-manylinux_2_39_aarch64.whl/share/faiss/faiss-targets-release.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "faiss" for configuration "Release"
+set_property(TARGET faiss APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(faiss PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libfaiss.a"
+  )
+
+list(APPEND _cmake_import_check_targets faiss )
+list(APPEND _cmake_import_check_files_for_faiss "${_IMPORT_PREFIX}/lib/libfaiss.a" )
+
+# Import target "faiss_sve" for configuration "Release"
+set_property(TARGET faiss_sve APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(faiss_sve PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libfaiss_sve.a"
+  )
+
+list(APPEND _cmake_import_check_targets faiss_sve )
+list(APPEND _cmake_import_check_files_for_faiss_sve "${_IMPORT_PREFIX}/lib/libfaiss_sve.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
