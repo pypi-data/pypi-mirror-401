09b84f5257172c2c70a2124fe4acc14e21a4822d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:48 +0000

    Add package auradata/1.0.0/auradata-1.0.0.tar.gz

---
+++ packages/auradata/auradata-1.0.0.tar.gz/auradata-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='auradata',
+    version='1.0.0',
+    author='Abdul Mofique Siddiqui',
+    author_email='mofique7860@gmail.com',
+    description='AuraData: A data-centric auditing and diagnostics engine for machine learning datasets, designed to detect noise, duplicates, label issues, and subgroup risks with transparent reporting.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Luckyy0311',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'numpy>=1.20',
+        'pandas>=1.0',
+        'scikit-learn>=1.0'
+    ],
+)
