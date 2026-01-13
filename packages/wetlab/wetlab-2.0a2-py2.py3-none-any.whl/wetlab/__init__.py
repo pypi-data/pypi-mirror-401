ce7d3a5e8f1f57ca6addbff29fc8e7098cbd72f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:28 +0000

    Add package wetlab/2.0a2/wetlab-2.0a2-py2.py3-none-any.whl

---
+++ packages/wetlab/wetlab-2.0a2-py2.py3-none-any.whl/wetlab/__init__.py
@@ -0,0 +1,76 @@
+"""Basic wetlab entities [`source <https://github.com/laminlabs/wetlab/blob/main/wetlab/models.py>`__].
+
+Install and mount `wetlab` in a new instance:
+
+>>> pip install wetlab
+>>> lamin init --storage ./test-wetlab --modules bionty,wetlab
+
+Import the package:
+
+>>> import wetlab as wl
+
+Create records:
+
+>>> biologic = wl.Biologic(name="IFNG").save()
+
+Perturbation registries:
+
+.. autosummary::
+   :toctree: .
+
+   GeneticPerturbation
+   Biologic
+   Compound
+   CompoundPerturbation
+   EnvironmentalPerturbation
+   CombinationPerturbation
+
+Auxiliary registries:
+
+.. autosummary::
+   :toctree: .
+
+   PerturbationTarget
+
+Types:
+
+.. autosummary::
+   :toctree: .
+
+   GeneticPerturbationSystem
+   BiologicType
+
+"""
+
+__version__ = "2.0a2"
+
+from lamindb_setup import _check_instance_setup
+
+from .types import BiologicType, GeneticPerturbationSystem
+
+_check_instance_setup(from_module="wetlab")
+
+
+from .models import (
+    Biologic,
+    CombinationPerturbation,
+    Compound,
+    CompoundPerturbation,
+    EnvironmentalPerturbation,
+    GeneticPerturbation,
+    PerturbationTarget,
+)
+
+__all__ = [
+    # registries
+    "Biologic",
+    "CombinationPerturbation",
+    "Compound",
+    "CompoundPerturbation",
+    "EnvironmentalPerturbation",
+    "GeneticPerturbation",
+    "PerturbationTarget",
+    # helper types
+    "BiologicType",
+    "GeneticPerturbationSystem",
+]
