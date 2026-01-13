19e859c1696fda3387e944868ac0eaacb1aae85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:03 +0000

    Add package flytekitplugins-mlflow/1.16.12/flytekitplugins_mlflow-1.16.12.tar.gz

---
+++ packages/flytekitplugins-mlflow/flytekitplugins_mlflow-1.16.12.tar.gz/flytekitplugins_mlflow-1.16.12/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+PLUGIN_NAME = "mlflow"
+
+microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
+
+plugin_requires = ["flytekit>=1.1.0,<2.0.0", "plotly", "mlflow>=2.10.0", "pandas"]
+
+__version__ = "1.16.12"
+
+setup(
+    title="MLflow",
+    title_expanded="Flytekit MLflow Plugin",
+    name=microlib_name,
+    version=__version__,
+    author="flyteorg",
+    author_email="admin@flyte.org",
+    description="This package enables seamless use of MLFlow within Flyte",
+    namespace_packages=["flytekitplugins"],
+    packages=[f"flytekitplugins.{PLUGIN_NAME}"],
+    install_requires=plugin_requires,
+    license="apache2",
+    python_requires=">=3.9",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
