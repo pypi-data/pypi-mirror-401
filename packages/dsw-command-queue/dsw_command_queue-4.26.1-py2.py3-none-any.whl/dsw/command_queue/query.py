0d07f2245000d98f405202497e01ba3aa6989e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:09:33 +0000

    Add package dsw-command-queue/4.26.1/dsw_command_queue-4.26.1-py2.py3-none-any.whl

---
+++ packages/dsw-command-queue/dsw_command_queue-4.26.1-py2.py3-none-any.whl/dsw/command_queue/query.py
@@ -0,0 +1,75 @@
+import enum
+
+
+class CommandState(enum.Enum):
+    NEW = 'NewPersistentCommandState'
+    DONE = 'DonePersistentCommandState'
+    ERROR = 'ErrorPersistentCommandState'
+    IGNORE = 'IgnorePersistentCommandState'
+
+
+class CommandQueries:
+
+    def __init__(self, channel: str, component: str):
+        self.channel = channel
+        self.component = component
+
+    def query_listen(self) -> str:
+        return f'LISTEN persistent_command_channel__{self.channel};'
+
+    def query_get_command(self, exp=2, interval='1 min') -> str:
+        return f"""
+            SELECT *
+            FROM persistent_command
+            WHERE component = '{self.component}'
+              AND attempts < max_attempts
+              AND state != '{CommandState.DONE.value}'
+              AND state != '{CommandState.IGNORE.value}'
+              AND (created_at AT TIME ZONE 'UTC')
+                    <
+                  (%(now)s - ({exp} ^ attempts - 1) * INTERVAL '{interval}')
+            ORDER BY attempts ASC, updated_at DESC
+            LIMIT 1 FOR UPDATE SKIP LOCKED;
+        """
+
+    @staticmethod
+    def query_command_error() -> str:
+        return f"""
+            UPDATE persistent_command
+            SET attempts = %(attempts)s,
+                last_error_message = %(error_message)s,
+                state = '{CommandState.ERROR.value}',
+                updated_at = %(updated_at)s
+            WHERE uuid = %(uuid)s;
+        """
+
+    @staticmethod
+    def query_command_error_stop() -> str:
+        return f"""
+            UPDATE persistent_command
+            SET attempts = %(attempts)s,
+                max_attempts = %(attempts)s,
+                last_error_message = %(error_message)s,
+                state = '{CommandState.ERROR}',
+                updated_at = %(updated_at)s
+            WHERE uuid = %(uuid)s;
+        """
+
+    @staticmethod
+    def query_command_done() -> str:
+        return f"""
+            UPDATE persistent_command
+            SET attempts = %(attempts)s,
+                state = '{CommandState.DONE.value}',
+                updated_at = %(updated_at)s
+            WHERE uuid = %(uuid)s;
+        """
+
+    @staticmethod
+    def query_command_start() -> str:
+        return """
+            UPDATE persistent_command
+            SET attempts = %(attempts)s,
+                updated_at = %(updated_at)s
+            WHERE uuid = %(uuid)s;
+        """
