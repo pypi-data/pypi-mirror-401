5b60487aa4e927ddb8db505c535f5140935dd21f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 15:08:05 +0000

    Add package abstractvision/0.1.0/abstractvision-0.1.0-py3-none-any.whl

---
+++ packages/abstractvision/abstractvision-0.1.0-py3-none-any.whl/abstractvision/__init__.py
@@ -0,0 +1,7 @@
+"""abstractvision: Generative vision capabilities for abstractcore.ai."""
+
+__version__ = "0.1.0"
+__author__ = "Laurent-Philippe Albou"
+__email__ = "contact@abstractcore.ai"
+
+# Core exports will be added as the project develops
