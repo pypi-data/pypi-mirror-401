98b8a14219a47e8ca99ccd13907ecef8fbdd4910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:08:09 +0000

    Add package pyridescence/1.0.1/pyridescence-1.0.1-cp312-cp312-win_amd64.whl

---
+++ packages/pyridescence/pyridescence-1.0.1-cp312-cp312-win_amd64.whl/lib/cmake/iridescence/iridescence-targets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "Iridescence::iridescence" for configuration "Release"
+set_property(TARGET Iridescence::iridescence APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(Iridescence::iridescence PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/iridescence.lib"
+  )
+
+list(APPEND _cmake_import_check_targets Iridescence::iridescence )
+list(APPEND _cmake_import_check_files_for_Iridescence::iridescence "${_IMPORT_PREFIX}/lib/iridescence.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
