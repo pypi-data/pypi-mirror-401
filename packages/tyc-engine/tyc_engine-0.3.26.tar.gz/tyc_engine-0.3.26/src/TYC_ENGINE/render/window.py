3879cdbda7e4b5691e85719d152d7736cee99f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:32 +0000

    Add package tyc-engine/0.3.26/tyc_engine-0.3.26.tar.gz

---
+++ packages/tyc-engine/tyc_engine-0.3.26.tar.gz/tyc_engine-0.3.26/src/TYC_ENGINE/render/window.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+def render(w=200, h=200, name='tyce', icon_path='res.ico.png'):
+    screen = pygame.display.set_mode((w, h))
+    pygame.display.set_caption(name)
+    if icon_path:  # load icon if provided
+        icon = pygame.image.load(icon_path)
+        pygame.display.set_icon(icon)
+    return screen
+
+def run(running):
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
