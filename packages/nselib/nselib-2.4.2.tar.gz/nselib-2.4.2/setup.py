a63bb08061bf42fb420d91d8fe73c36673b3db10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:04:55 +0000

    Add package nselib/2.4.2/nselib-2.4.2.tar.gz

---
+++ packages/nselib/nselib-2.4.2.tar.gz/nselib-2.4.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='nselib',
+    packages=setuptools.find_packages(),
+    version='2.4.2',
+    include_package_data=True,
+    description='library to get NSE India data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Ruchi Tanmay, Amlan Mishra',
+    author_email='ruchitanmay@gmail.com',
+    url='https://github.com/RuchiTanmay/nselib',
+    install_requires=['requests', 'pandas', 'scipy', 'pandas_market_calendars'],
+    keywords=['nseindia', 'nse', 'nse data', 'stock data', 'python', 'nse daily data', 'stock markets',
+              'nse library', 'nse python', 'nse daily reports'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
