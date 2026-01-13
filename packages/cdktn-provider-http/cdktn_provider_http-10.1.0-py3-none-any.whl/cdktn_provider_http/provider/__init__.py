65379922fa2996428184d0ed451fadc662d3aed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:04:45 +0000

    Add package cdktn-provider-http/10.1.0/cdktn_provider_http-10.1.0-py3-none-any.whl

---
+++ packages/cdktn-provider-http/cdktn_provider_http-10.1.0-py3-none-any.whl/cdktn_provider_http/provider/__init__.py
@@ -0,0 +1,203 @@
+r'''
+# `provider`
+
+Refer to the Terraform Registry for docs: [`http`](https://registry.terraform.io/providers/hashicorp/http/3.5.0/docs).
+'''
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import typeguard
+from importlib.metadata import version as _metadata_package_version
+TYPEGUARD_MAJOR_VERSION = int(_metadata_package_version('typeguard').split('.')[0])
+
+def check_type(argname: str, value: object, expected_type: typing.Any) -> typing.Any:
+    if TYPEGUARD_MAJOR_VERSION <= 2:
+        return typeguard.check_type(argname=argname, value=value, expected_type=expected_type) # type:ignore
+    else:
+        if isinstance(value, jsii._reference_map.InterfaceDynamicProxy): # pyright: ignore [reportAttributeAccessIssue]
+           pass
+        else:
+            if TYPEGUARD_MAJOR_VERSION == 3:
+                typeguard.config.collection_check_strategy = typeguard.CollectionCheckStrategy.ALL_ITEMS # type:ignore
+                typeguard.check_type(value=value, expected_type=expected_type) # type:ignore
+            else:
+                typeguard.check_type(value=value, expected_type=expected_type, collection_check_strategy=typeguard.CollectionCheckStrategy.ALL_ITEMS) # type:ignore
+
+from .._jsii import *
+
+import cdktf as _cdktf_9a9027ec
+import constructs as _constructs_77d1e7e8
+
+
+class HttpProvider(
+    _cdktf_9a9027ec.TerraformProvider,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@cdktn/provider-http.provider.HttpProvider",
+):
+    '''Represents a {@link https://registry.terraform.io/providers/hashicorp/http/3.5.0/docs http}.'''
+
+    def __init__(
+        self,
+        scope: _constructs_77d1e7e8.Construct,
+        id: builtins.str,
+        *,
+        alias: typing.Optional[builtins.str] = None,
+    ) -> None:
+        '''Create a new {@link https://registry.terraform.io/providers/hashicorp/http/3.5.0/docs http} Resource.
+
+        :param scope: The scope in which to define this construct.
+        :param id: The scoped construct ID. Must be unique amongst siblings in the same scope
+        :param alias: Alias name. Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.5.0/docs#alias HttpProvider#alias}
+        '''
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__d52a0c32056151d53a02c0a763000fbeefd7d69471f71809ee38b28bd03e3ee5)
+            check_type(argname="argument scope", value=scope, expected_type=type_hints["scope"])
+            check_type(argname="argument id", value=id, expected_type=type_hints["id"])
+        config = HttpProviderConfig(alias=alias)
+
+        jsii.create(self.__class__, self, [scope, id, config])
+
+    @jsii.member(jsii_name="generateConfigForImport")
+    @builtins.classmethod
+    def generate_config_for_import(
+        cls,
+        scope: _constructs_77d1e7e8.Construct,
+        import_to_id: builtins.str,
+        import_from_id: builtins.str,
+        provider: typing.Optional[_cdktf_9a9027ec.TerraformProvider] = None,
+    ) -> _cdktf_9a9027ec.ImportableResource:
+        '''Generates CDKTF code for importing a HttpProvider resource upon running "cdktf plan ".
+
+        :param scope: The scope in which to define this construct.
+        :param import_to_id: The construct id used in the generated config for the HttpProvider to import.
+        :param import_from_id: The id of the existing HttpProvider that should be imported. Refer to the {@link https://registry.terraform.io/providers/hashicorp/http/3.5.0/docs#import import section} in the documentation of this resource for the id to use
+        :param provider: ? Optional instance of the provider where the HttpProvider to import is found.
+        '''
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__d2b1401f700d130102fe90033617efaf0b24b3a66283edba22379d74714ffb73)
+            check_type(argname="argument scope", value=scope, expected_type=type_hints["scope"])
+            check_type(argname="argument import_to_id", value=import_to_id, expected_type=type_hints["import_to_id"])
+            check_type(argname="argument import_from_id", value=import_from_id, expected_type=type_hints["import_from_id"])
+            check_type(argname="argument provider", value=provider, expected_type=type_hints["provider"])
+        return typing.cast(_cdktf_9a9027ec.ImportableResource, jsii.sinvoke(cls, "generateConfigForImport", [scope, import_to_id, import_from_id, provider]))
+
+    @jsii.member(jsii_name="resetAlias")
+    def reset_alias(self) -> None:
+        return typing.cast(None, jsii.invoke(self, "resetAlias", []))
+
+    @jsii.member(jsii_name="synthesizeAttributes")
+    def _synthesize_attributes(self) -> typing.Mapping[builtins.str, typing.Any]:
+        return typing.cast(typing.Mapping[builtins.str, typing.Any], jsii.invoke(self, "synthesizeAttributes", []))
+
+    @jsii.member(jsii_name="synthesizeHclAttributes")
+    def _synthesize_hcl_attributes(self) -> typing.Mapping[builtins.str, typing.Any]:
+        return typing.cast(typing.Mapping[builtins.str, typing.Any], jsii.invoke(self, "synthesizeHclAttributes", []))
+
+    @jsii.python.classproperty
+    @jsii.member(jsii_name="tfResourceType")
+    def TF_RESOURCE_TYPE(cls) -> builtins.str:
+        return typing.cast(builtins.str, jsii.sget(cls, "tfResourceType"))
+
+    @builtins.property
+    @jsii.member(jsii_name="aliasInput")
+    def alias_input(self) -> typing.Optional[builtins.str]:
+        return typing.cast(typing.Optional[builtins.str], jsii.get(self, "aliasInput"))
+
+    @builtins.property
+    @jsii.member(jsii_name="alias")
+    def alias(self) -> typing.Optional[builtins.str]:
+        return typing.cast(typing.Optional[builtins.str], jsii.get(self, "alias"))
+
+    @alias.setter
+    def alias(self, value: typing.Optional[builtins.str]) -> None:
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__01aaa8d111ec8346130878936e24639367600eee0cfc19e8653e307b509532a5)
+            check_type(argname="argument value", value=value, expected_type=type_hints["value"])
+        jsii.set(self, "alias", value) # pyright: ignore[reportArgumentType]
+
+
+@jsii.data_type(
+    jsii_type="@cdktn/provider-http.provider.HttpProviderConfig",
+    jsii_struct_bases=[],
+    name_mapping={"alias": "alias"},
+)
+class HttpProviderConfig:
+    def __init__(self, *, alias: typing.Optional[builtins.str] = None) -> None:
+        '''
+        :param alias: Alias name. Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.5.0/docs#alias HttpProvider#alias}
+        '''
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__ed5d99b099997f2f0325646c206f11e20cdd561f40d23e431bec39f32552798f)
+            check_type(argname="argument alias", value=alias, expected_type=type_hints["alias"])
+        self._values: typing.Dict[builtins.str, typing.Any] = {}
+        if alias is not None:
+            self._values["alias"] = alias
+
+    @builtins.property
+    def alias(self) -> typing.Optional[builtins.str]:
+        '''Alias name.
+
+        Docs at Terraform Registry: {@link https://registry.terraform.io/providers/hashicorp/http/3.5.0/docs#alias HttpProvider#alias}
+        '''
+        result = self._values.get("alias")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "HttpProviderConfig(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "HttpProvider",
+    "HttpProviderConfig",
+]
+
+publication.publish()
+
+def _typecheckingstub__d52a0c32056151d53a02c0a763000fbeefd7d69471f71809ee38b28bd03e3ee5(
+    scope: _constructs_77d1e7e8.Construct,
+    id: builtins.str,
+    *,
+    alias: typing.Optional[builtins.str] = None,
+) -> None:
+    """Type checking stubs"""
+    pass
+
+def _typecheckingstub__d2b1401f700d130102fe90033617efaf0b24b3a66283edba22379d74714ffb73(
+    scope: _constructs_77d1e7e8.Construct,
+    import_to_id: builtins.str,
+    import_from_id: builtins.str,
+    provider: typing.Optional[_cdktf_9a9027ec.TerraformProvider] = None,
+) -> None:
+    """Type checking stubs"""
+    pass
+
+def _typecheckingstub__01aaa8d111ec8346130878936e24639367600eee0cfc19e8653e307b509532a5(
+    value: typing.Optional[builtins.str],
+) -> None:
+    """Type checking stubs"""
+    pass
+
+def _typecheckingstub__ed5d99b099997f2f0325646c206f11e20cdd561f40d23e431bec39f32552798f(
+    *,
+    alias: typing.Optional[builtins.str] = None,
+) -> None:
+    """Type checking stubs"""
+    pass
