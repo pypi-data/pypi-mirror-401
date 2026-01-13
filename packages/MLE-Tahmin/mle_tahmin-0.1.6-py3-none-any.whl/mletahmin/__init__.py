651967fab5a802eec0f467e762ef5982281445fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:07:02 +0000

    Add package MLE-Tahmin/0.1.6/mle_tahmin-0.1.6-py3-none-any.whl

---
+++ packages/MLE-Tahmin/mle_tahmin-0.1.6-py3-none-any.whl/mletahmin/__init__.py
@@ -0,0 +1,7 @@
+# Fonksiyonları dışa aktar
+from .gamma import gamma_MLE
+from .pareto import pareto_MLE
+from .lomax import lomax_MLE
+
+# Sınıfı dışa aktar (Bunu eklemeniz şart)
+from .mle import DagilimAnalizcisi
