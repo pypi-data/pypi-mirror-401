bc8d4a00683cd87624246ef344c655a33e9773a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:03 +0000

    Add package moocore/0.2.0/moocore-0.2.0-cp310-abi3-musllinux_1_2_aarch64.whl

---
+++ packages/moocore/moocore-0.2.0-cp310-abi3-musllinux_1_2_aarch64.whl/moocore/libmoocore/hvapprox.h
@@ -0,0 +1,26 @@
+/*************************************************************************
+
+ hv_approx.h
+
+*************************************************************************/
+#ifndef HV_APPROX_H_
+#define HV_APPROX_H_
+
+#include <stdbool.h>
+#include <stdint.h> // uint_fast32_t, uint32_t
+#include "libmoocore-config.h"
+
+// C++ needs to know that types and declarations are C, not C++.
+BEGIN_C_DECLS
+
+MOOCORE_API double hv_approx_hua_wang(
+    const double * restrict data, size_t npoints, dimension_t nobjs,
+    const double * restrict ref, const bool * restrict maximise,
+    uint_fast32_t nsamples);
+MOOCORE_API double hv_approx_normal(
+    const double * restrict data, size_t npoints, dimension_t nobjs,
+    const double * restrict ref, const bool * restrict maximise,
+    uint_fast32_t nsamples, uint32_t random_seed);
+
+END_C_DECLS
+#endif // HV_APPROX_H_
