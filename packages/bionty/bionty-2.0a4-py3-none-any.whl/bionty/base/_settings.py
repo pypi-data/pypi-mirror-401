f55ea3c1748f11710f3bf7ec4095095b925c8bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:06:42 +0000

    Add package bionty/2.0a4/bionty-2.0a4-py3-none-any.whl

---
+++ packages/bionty/bionty-2.0a4-py3-none-any.whl/bionty/base/_settings.py
@@ -0,0 +1,59 @@
+from functools import wraps
+from pathlib import Path
+
+HOME_DIR = Path(f"{Path.home()}/.lamin/bionty").resolve()
+ROOT_DIR = Path(__file__).parent.resolve()
+
+
+def check_datasetdir_exists(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        settings.datasetdir.mkdir(exist_ok=True)
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def check_dynamicdir_exists(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        settings.dynamicdir.mkdir(exist_ok=True)
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+class Settings:
+    def __init__(
+        self,
+        datasetdir: str | Path = ROOT_DIR / "data/",
+        dynamicdir: str | Path = ROOT_DIR / "_dynamic/",
+    ):
+        # setters convert to Path and resolve:
+        self.datasetdir = datasetdir
+        self.dynamicdir = dynamicdir
+
+    @property
+    def datasetdir(self):
+        """Directory for datasets."""
+        return self._datasetdir
+
+    @datasetdir.setter
+    def datasetdir(self, datasetdir: str | Path):
+        self._datasetdir = Path(datasetdir).resolve()
+
+    @property
+    def dynamicdir(self):
+        """Directory for datasets."""
+        return self._dynamicdir
+
+    @dynamicdir.setter
+    def dynamicdir(self, dynamicdir: str | Path):
+        self._dynamicdir = Path(dynamicdir).resolve()
+
+    @property
+    def public_sources(self):
+        return ROOT_DIR / "sources.yaml"
+
+
+settings = Settings()
