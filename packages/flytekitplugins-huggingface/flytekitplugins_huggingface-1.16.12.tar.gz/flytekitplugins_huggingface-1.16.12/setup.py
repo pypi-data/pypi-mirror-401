c6cf338b0579a1eb139d921b679c6b939b35f11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:02 +0000

    Add package flytekitplugins-huggingface/1.16.12/flytekitplugins_huggingface-1.16.12.tar.gz

---
+++ packages/flytekitplugins-huggingface/flytekitplugins_huggingface-1.16.12.tar.gz/flytekitplugins_huggingface-1.16.12/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+PLUGIN_NAME = "huggingface"
+
+microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
+
+plugin_requires = ["flytekit>=1.3.0b2", "datasets>=2.4.0"]
+
+__version__ = "1.16.12"
+
+setup(
+    title="Hugging Face",
+    title_expanded="Flytekit Hugging Face Plugin",
+    name=microlib_name,
+    version=__version__,
+    author="Evan Sadler",
+    description="Hugging Face plugin for flytekit",
+    url="https://github.com/flyteorg/flytekit/tree/master/plugins/flytekit-huggingface",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    namespace_packages=["flytekitplugins"],
+    packages=[f"flytekitplugins.{PLUGIN_NAME}"],
+    install_requires=plugin_requires,
+    license="apache2",
+    python_requires=">=3.9",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    entry_points={"flytekit.plugins": [f"{PLUGIN_NAME}=flytekitplugins.{PLUGIN_NAME}"]},
+)
