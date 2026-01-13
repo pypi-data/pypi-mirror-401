4fb2855f037972cf60e16bd93fd29b1573104ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:04:35 +0000

    Add package torchfont/0.7.0/torchfont-0.7.0-cp38-abi3-musllinux_1_2_aarch64.whl

---
+++ packages/torchfont/torchfont-0.7.0-cp38-abi3-musllinux_1_2_aarch64.whl/torchfont/datasets/__init__.py
@@ -0,0 +1,20 @@
+"""Dataset utilities that turn font files into model-ready glyph samples.
+
+Examples:
+    Mirror the Google Fonts index into a training dataset::
+
+        from torchfont.datasets import GoogleFonts
+
+        ds = GoogleFonts(root="data/google/fonts", ref="main", download=True)
+
+"""
+
+from torchfont.datasets.folder import FontFolder
+from torchfont.datasets.google_fonts import GoogleFonts
+from torchfont.datasets.repo import FontRepo
+
+__all__ = [
+    "FontFolder",
+    "FontRepo",
+    "GoogleFonts",
+]
