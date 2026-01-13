b2d54a071e0e71ff70f00fa333998c2434ea32bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:47 +0000

    Add package cbpi4-iSpindle/1.0.3/cbpi4_ispindle-1.0.3.tar.gz

---
+++ packages/cbpi4-iSpindle/cbpi4_ispindle-1.0.3.tar.gz/cbpi4_ispindle-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='cbpi4-iSpindle',
+      version='1.0.3',
+      description='CraftBeerPi4 iSpindle Sensor Plugin',
+      author='Alexander Vollkopf',
+      author_email='avollkopf@web.de',
+      url='https://github.com/avollkopf/cbpi4-iSpindle',
+      license='GPLv 3.0',
+      include_package_data=True,
+      package_data={
+        # If any package contains *.txt or *.rst files, include them:
+      '': ['*.txt', '*.rst', '*.yaml'],
+      'cbpi4-iSpindle': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['cbpi4-iSpindle'],
+      keywords=['globalsettings'],
+      install_requires=['mysql-connector-python'],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+      )
