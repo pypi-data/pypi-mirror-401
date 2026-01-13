45a70f88a3d441ab11a4e8c302b4c6635de11ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:06:49 +0000

    Add package kyodo/0.9.3/kyodo-0.9.3.tar.gz

---
+++ packages/kyodo/kyodo-0.9.3.tar.gz/kyodo-0.9.3/README.md
@@ -0,0 +1,24 @@
+<body>
+ 	<h1 align="center">
+		<img src="https://github.com/alx0rr/kyodo/blob/main/docs/res/banner.png" alt="kyodo api">
+	</h1>
+	<p align="center">
+	    <a href="https://github.com/alx0rr/kyodo/releases"><img src="https://img.shields.io/github/v/release/alx0rr/kyodo" alt="GitHub release" />
+	    <a href="https://pypi.org/project/kyodo/"><img src="https://img.shields.io/pypi/v/kyodo.svg" alt="Pypi version" />
+	    <img src="https://img.shields.io/pypi/dm/kyodo"/>
+	    <a href="https://github.com/alx0rr/kyodo/blob/main/LICENSE"><img src="https://img.shields.io/badge/License-MIT-yellow.svg" alt="license" /></a>
+	    <a href="https://github.com/alx0rr/kyodo/blob/main/docs/index.md"><img src="https://img.shields.io/website?down_message=failing&label=docs&up_color=green&up_message=passing&url=https://github.com/alx0rr/kyodo/blob/main/docs/index.md" alt="docs" /></a>
+	</p>
+	<br>
+<div align="center">
+	
+# Kyodo – Python library for creating bots on the social network "[Hi Kyodo](https://hi.kyodo.app/)"
+
+</div>
+
+<div align="center">
+	<a href="https://github.com/alx0rr/kyodo/blob/main/docs/index.md">
+		<img src="https://readme-typing-svg.demolab.com?font=Fira+Code&size=14&duration=1&pause=31&color=3DACF7&random=false&width=195&lines=Read+the+documentation" alt="Read the documentation"/>
+	</a>
+</div>
+</body>
---
+++ packages/kyodo/kyodo-0.9.3.tar.gz/kyodo-0.9.3/kyodo/utils/exceptions.py
@@ -0,0 +1,114 @@
+from orjson import loads, JSONDecodeError
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++=
+
+
+class KyodoError(Exception):
+	"""
+	Base class for all kyodo-related errors.
+	"""
+	def __init__(*args, **kwargs):
+		Exception.__init__(*args, **kwargs)
+
+
+class LibraryError(Exception):
+	"""
+	Base class for all library-related errors.
+	"""
+	def __init__(*args, **kwargs):
+		Exception.__init__(*args, **kwargs)
+#++++++++++++++++++++++++++++++++++++++++++++++++++++=
+
+
+
+
+class UnknownError(LibraryError):
+	"""
+	An unknown error occurred.
+	"""
+
+class NeedAuthError(LibraryError):
+	"""
+	Called when an attempt is made to perform an action that requires authorization.
+	"""
+
+
+class UnsupportedArgumentType(LibraryError):
+	"""
+	Called when you pass an unsupported argument type.
+	"""
+
+
+class UnsupportedFileType(LibraryError):
+	"""
+	Called when you pass an unsupported file type.
+	"""
+
+class ArgumentNeeded(LibraryError):
+	"""
+	Called when no arguments are passed or a required argument is missing.
+	"""
+
+class NoDataError(LibraryError):
+	"""
+	Called when the final data for a request is empty (all arguments are None).
+	"""
+
+class ContentTypeError(LibraryError):
+    """
+	ContentType found is not valid.
+	"""
+
+#=====================================================
+
+
+
+class NotFoundError(KyodoError):
+	"""
+	Called if the resource is not found.
+	"""
+
+
+class ForbiddenError(KyodoError):
+	"""
+	Called when the server denies an action.
+	"""
+
+
+class TooManyRequestsError(KyodoError):
+	"""
+	Called when you send too many requests in a short period of time (just put a sleep for 2 seconds)
+	"""
+
+
+class AccessRestricted(KyodoError):
+	"""
+	Called when there is insufficient permission to execute the request.
+	"""
+
+
+class VersionOutOfDate(KyodoError):
+	"""
+	Called when an invalid request is sent. Often associated with incorrect data or updating security systems in the application.
+	"""
+
+
+errors = {
+	"0:404": NotFoundError,
+	"0:403": ForbiddenError,
+	"0:419": AccessRestricted,
+	"0:429": TooManyRequestsError,
+	"0:453": VersionOutOfDate
+}
+
+def checkException(data):
+	try:
+		data = loads(data)
+		apiCode = data.get("apiCode")
+		code = data.get("code")
+		_ = f"{apiCode}:{code}"
+	except JSONDecodeError:
+		raise UnknownError(data)
+	if _ in errors: raise errors[_](data)
+	else:raise UnknownError(data)
