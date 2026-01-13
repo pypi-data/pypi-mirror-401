953c78eeddd09c05f75550b7f494f95742e04602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:06:33 +0000

    Add package python-ember-mug/1.3.0b5/python_ember_mug-1.3.0b5-py3-none-any.whl

---
+++ packages/python-ember-mug/python_ember_mug-1.3.0b5-py3-none-any.whl/ember_mug/formatting.py
@@ -0,0 +1,36 @@
+"""Helpers for formatting values for display."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .consts import TemperatureUnit
+    from .data import Colour
+
+
+def format_temp(temp: float, unit: TemperatureUnit | None = None) -> str:
+    """Format temperature with the correct unit."""
+    value = f"{temp:.2f}"
+    return f"{value}{unit.value}" if unit else value
+
+
+def format_capacity(capacity: int | None, metric: bool | None = None) -> str:
+    """Format capacity for display."""
+    if capacity is None:
+        return "Unknown"
+    if metric is False:
+        # Convert to fahrenheit
+        capacity = round(capacity * 0.033814)
+        return f"{capacity}oz"
+    return f"{capacity}ml"
+
+
+def format_led_colour(led_colour: Colour) -> str:
+    """Return colour as hex value."""
+    return led_colour.as_hex()
+
+
+def format_liquid_level(liquid_level: int) -> str:
+    """Human readable liquid level."""
+    return f"{(liquid_level / 30 * 100):.2f}%"
