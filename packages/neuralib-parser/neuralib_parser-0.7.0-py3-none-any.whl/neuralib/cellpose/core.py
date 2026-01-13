b654600768fb0b01fa4274f08e9c0ec418b94dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:06:33 +0000

    Add package neuralib-parser/0.7.0/neuralib_parser-0.7.0-py3-none-any.whl

---
+++ packages/neuralib-parser/neuralib_parser-0.7.0-py3-none-any.whl/neuralib/cellpose/core.py
@@ -0,0 +1,190 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import numpy as np
+from typing import Self
+
+from neuralib.typing import PathLike
+
+__all__ = [
+    'read_cellpose',
+    'cellpose_point_roi_helper',
+    'CellposeSegmentation'
+]
+
+
+def read_cellpose(file: PathLike) -> CellposeSegmentation:
+    """
+    Read a cellpose segmentation result file
+
+    :param file: cellpose segmentation result ``.npy`` file
+    :return: :class:`CellposeSegmentation`
+    """
+    return CellposeSegmentation.load(file)
+
+
+def cellpose_point_roi_helper(file: PathLike, output: PathLike) -> None:
+    """
+    Read a cellpose segmentation result and convert the segmentation result to point coordinates
+
+    :param file: cellpose segmentation result ``.npy`` file
+    :param output: ``*.roi`` output file path
+    """
+    CellposeSegmentation.load(file).to_roi(output)
+
+
+class CellposeSegmentation:
+    """`Cellpose <https://github.com/MouseLand/cellpose>`_ segmentation results
+
+    `Dimension parameters`:
+
+        N = Number of segmented cells
+
+        W = Image width
+
+        H = Image height
+
+    .. seealso::
+
+        `Cellpose Native Doc <https://cellpose.readthedocs.io/en/latest/outputs.html#seg-npy-output>`_
+
+    """
+
+    def __init__(self, outlines, masks, chan_choose, ismanual, flows, diameter, filename):
+        self._outlines = outlines
+        self._masks = masks
+        self._chan_choose = chan_choose
+        self._is_manual = ismanual
+        self._flows = flows
+        self._diameter = diameter
+
+        self._filename = filename
+
+    @classmethod
+    def load(cls, file: PathLike) -> Self:
+        """
+        Load a cellpose segmentation result
+
+        :param file: cellpose segmentation result ``.npy`` file
+        :return: :class:`CellposeSegmentation`
+        """
+        dat = np.load(file, allow_pickle=True).item()
+        return cls(**dat)
+
+    @property
+    def n_segmentation(self) -> int:
+        """number of segmented cells"""
+        return len(self._is_manual)
+
+    @property
+    def width(self):
+        """image width"""
+        return self._outlines.shape[1]
+
+    @property
+    def height(self):
+        """image height"""
+        return self._outlines.shape[0]
+
+    @property
+    def filename(self) -> Path:
+        """filepath of image"""
+        return Path(self._filename)
+
+    @property
+    def outlines(self) -> np.ndarray:
+        """outlines of ROIs (0 = NO outline; 1,2,… = outline labels). `Array[uint16, [H, W]]`"""
+        return self._outlines
+
+    @property
+    def masks(self) -> np.ndarray:
+        """each pixel in the image is assigned to an ROI (0 = NO ROI; 1,2,… = ROI labels). `Array[uint16, [H, W]]` """
+        return self._masks
+
+    @property
+    def chan_choose(self) -> list[int]:
+        """channels that you chose in GUI (0=gray/none, 1=red, 2=green, 3=blue)"""
+        return self._chan_choose
+
+    @property
+    def flows(self) -> list[np.ndarray]:
+        """
+        flows[0] is XY flow in RGB
+
+        flows[1] is the cell probability in range 0-255 instead of -10.0 to 10.0
+
+        flows[2] is Z flow in range 0-255 (if it exists, otherwise zeros),
+
+        flows[3] is [dY, dX, cellprob] (or [dZ, dY, dX, cellprob] for 3D), flows[4] is pixel destinations (for internal use)
+        """
+        return self._flows
+
+    @property
+    def diameter(self) -> float:
+        """cell body diameter"""
+        return self._diameter
+
+    @property
+    def is_manual(self) -> np.ndarray:
+        """whether or not mask k was manually drawn or computed by the cellpose algorithm. `Array[bool, N]`"""
+        return self._is_manual
+
+    @property
+    def nan_masks(self) -> np.ndarray:
+        """value 0 in :attr:`CellposeSegmentation.masks` to nan"""
+        masks = self.masks.copy().astype(np.float_)
+        masks[masks == 0] = np.nan
+
+        return masks
+
+    @property
+    def nan_outlines(self) -> np.ndarray:
+        """value 0 in :attr:`CellposeSegmentation.outlines` to nan"""
+        outlines = self.outlines.copy().astype(np.float_)
+        outlines[outlines == 0] = np.nan
+
+        return outlines
+
+    @property
+    def points(self) -> np.ndarray:
+        """Calculate center of each segmented area in XY pixel. `Array[int, [N, 2]]`"""
+        centers = self._calculate_centers()
+        return np.round(centers).astype(int)
+
+    # noinspection PyTypeChecker
+    def to_roi(self, output_file: PathLike):
+        """
+        Covert segmented roi to point roi, and save it as ``.roi`` for imageJ.
+
+        :param output_file: ``*.roi`` output file path
+        """
+        from roifile import ImagejRoi, ROI_TYPE, ROI_OPTIONS
+
+        if Path(output_file).suffix != '.roi':
+            raise ValueError('output file must have .roi extension')
+
+        points = np.fliplr(self.points)  # XY rotate in .roi format
+        roi = ImagejRoi(
+            roitype=ROI_TYPE.POINT,
+            options=ROI_OPTIONS.PROMPT_BEFORE_DELETING | ROI_OPTIONS.SUB_PIXEL_RESOLUTION,
+            n_coordinates=self.points.shape[0],
+            integer_coordinates=points,
+            subpixel_coordinates=points
+        )
+
+        roi.tofile(output_file)
+
+    def _calculate_centers(self):
+        """calculate center of each segmented area in XY pixel"""
+        labels = np.unique(self.masks)
+        labels = labels[labels != 0]  # remove background
+
+        n_neurons = len(labels)
+        centers = np.zeros((n_neurons, 2))
+        for i, label in enumerate(labels):
+            segment_coords = np.argwhere(self.masks == label)
+            center = segment_coords.mean(axis=0)
+            centers[i] = center
+
+        return centers
---
+++ packages/neuralib-parser/neuralib_parser-0.7.0-py3-none-any.whl/neuralib/facemap/core.py
@@ -0,0 +1,313 @@
+from __future__ import annotations
+
+import h5py
+import numpy as np
+import pickle
+import polars as pl
+from typing import Self
+from typing import TypedDict
+
+from neuralib.typing import PathLike
+from neuralib.util.dataframe import DataFrameWrapper
+from neuralib.util.utils import uglob
+
+__all__ = [
+    'read_facemap',
+    'KeyPoint',
+    'FaceMapResult',
+    'KeyPointDataFrame',
+    'SVDVariables',
+    'KeyPointsMeta',
+    'PupilDict',
+    'RoiDict'
+]
+
+
+def read_facemap(directory: PathLike) -> FaceMapResult:
+    """loading facemap result
+
+    :param directory: facemap output directory
+    """
+    return FaceMapResult.from_directory(directory)
+
+
+KeyPoint = str
+"""keypoint name"""
+
+
+class FaceMapResult:
+    """Facemap result container"""
+
+    def __init__(self, svd: SVDVariables | None,
+                 meta: KeyPointsMeta | None,
+                 data: h5py.Group | None,
+                 with_keypoints: bool):
+        """
+        :param svd: attr:`~neuralib.tracking.facemap.core.SVDVariables`
+        :param meta: attr:`~neuralib.tracking.facemap.core.KeyPointsMeta`
+        :param data: facemap data result
+        :param with_keypoints: whether it has keypoint tracking
+        """
+        self.svd = svd
+        self.meta = meta
+        self.data = data
+
+        self._with_keypoints = with_keypoints
+
+    @classmethod
+    def from_directory(cls, directory: PathLike) -> Self:
+        """
+        init class loading from a directory
+
+        :param directory: Facemap output directory
+        """
+        # svd
+        try:
+            svd_path = uglob(directory, '*.npy')
+        except FileNotFoundError:
+            svd = None
+        else:
+            svd = np.load(svd_path, allow_pickle=True).item()
+
+        # meta
+        try:
+            meta_path = uglob(directory, '*.pkl')
+        except FileNotFoundError:
+            meta = None
+            data = None
+            keypoints = False
+        else:
+            with open(meta_path, 'rb') as f:
+                meta = pickle.load(f)
+
+            data_path = uglob(directory, '*.h5')
+            data = h5py.File(data_path)['Facemap']
+            keypoints = True
+
+        return cls(svd, meta, data, keypoints)
+
+    @property
+    def with_keypoint(self) -> bool:
+        return self._with_keypoints
+
+    # ============== #
+    # Pupil Tracking #
+    # ============== #
+
+    def get_pupil(self) -> PupilDict:
+        """pupil tracking result
+
+        :raises RuntimeError: If no pupil data is available.
+        """
+        try:
+            pupil: list[PupilDict] = self.svd['pupil']
+        except KeyError:
+            raise RuntimeError('no pupil data found')
+        else:
+            return pupil[0]
+
+    def get_pupil_area(self) -> np.ndarray:
+        """pupil area. `Array[float, F]`"""
+        return self.get_pupil()['area_smooth']
+
+    def get_pupil_center_of_mass(self) -> np.ndarray:
+        """center of mass of pupil tracking. `Array[float, [F, 2]]`"""
+        return self.get_pupil()['com_smooth']
+
+    def get_pupil_location_movement(self) -> np.ndarray:
+        """Calculate the Euclidean distance from the origin for each point in a 2D array. `Array[float, F]`"""
+        com = self.get_pupil_center_of_mass()
+        return np.sqrt(np.sum(com ** 2, axis=1))
+
+    def get_blink(self) -> np.ndarray:
+        """eye blinking array. `Array[float, F]`
+
+        :raises RuntimeError: If no blink data is available.
+        """
+        try:
+            ret = self.svd['blink']
+        except KeyError:
+            raise RuntimeError('no blink data found')
+        else:
+            return ret[0]
+
+    # ========= #
+    # Keypoints #
+    # ========= #
+
+    @property
+    def keypoints(self) -> list[KeyPoint]:
+        """list of all keypoint name"""
+        return list(self.data.keys())
+
+    def get(self, *keypoint) -> KeyPointDataFrame:
+        """get keypoint(s) dataframe"""
+        if len(keypoint) == 1:
+            return self._get(keypoint[0])
+        else:
+            ret = [self._get(k).dataframe() for k in keypoint]
+            return KeyPointDataFrame(pl.concat(ret))
+
+    def _get(self, keypoint: KeyPoint) -> KeyPointDataFrame:
+        x = np.array(self.data[keypoint]['x'])
+        y = np.array(self.data[keypoint]['y'])
+        llh = np.array(self.data[keypoint]['likelihood'])
+        df = pl.DataFrame({'x': x, 'y': y, 'likelihood': llh}).with_columns(pl.lit(keypoint).alias('keypoint'))
+        return KeyPointDataFrame(df)
+
+
+class KeyPointDataFrame(DataFrameWrapper):
+    """
+    Dataframe with ``x``, ``y``, ``likelihood`` and ``keypoint`` columns ::
+
+        ┌────────────┬────────────┬────────────┬───────────┐
+        │ x          ┆ y          ┆ likelihood ┆ keypoint  │
+        │ ---        ┆ ---        ┆ ---        ┆ ---       │
+        │ f32        ┆ f32        ┆ f32        ┆ str       │
+        ╞════════════╪════════════╪════════════╪═══════════╡
+        │ 374.102081 ┆ 199.159668 ┆ 0.777443   ┆ eye(back) │
+        │ 373.785919 ┆ 199.425873 ┆ 0.787424   ┆ eye(back) │
+        │ 374.075867 ┆ 199.507111 ┆ 0.779713   ┆ eye(back) │
+        │ 374.028473 ┆ 199.359955 ┆ 0.761724   ┆ eye(back) │
+        │ 374.222382 ┆ 199.777466 ┆ 0.770329   ┆ eye(back) │
+        │ …          ┆ …          ┆ …          ┆ …         │
+        │ 317.318756 ┆ 285.396912 ┆ 0.596486   ┆ mouth     │
+        │ 318.163696 ┆ 285.492676 ┆ 0.589684   ┆ mouth     │
+        │ 317.758606 ┆ 285.560425 ┆ 0.603126   ┆ mouth     │
+        │ 317.453491 ┆ 285.572235 ┆ 0.573179   ┆ mouth     │
+        │ 317.976196 ┆ 285.477051 ┆ 0.58359    ┆ mouth     │
+        └────────────┴────────────┴────────────┴───────────┘
+
+    """
+
+    def __init__(self, df: pl.DataFrame):
+        self._df = df
+
+    def __repr__(self):
+        return repr(self.dataframe())
+
+    def dataframe(self, dataframe: pl.DataFrame = None, may_inplace=True):
+        if dataframe is None:
+            return self._df
+        else:
+            return KeyPointDataFrame(dataframe)
+
+    def to_zscore(self) -> Self:
+        """
+        xy to zscore
+
+        :return:
+        """
+        return self.with_columns([
+            ((pl.col('x') - pl.col('x').mean()) / pl.col('x').std()).alias('x'),
+            ((pl.col('y') - pl.col('y').mean()) / pl.col('y').std()).alias('y'),
+        ])
+
+    def with_outlier_filter(self, filter_window: int = 15,
+                            baseline_window: int = 50,
+                            max_spike: int = 25,
+                            max_diff: int = 25) -> Self:
+        """
+        with outlier filter
+
+        :param filter_window:
+        :param baseline_window:
+        :param max_spike:
+        :param max_diff:
+        :return:
+        """
+        from .util import filter_outliers
+        x, y = filter_outliers(np.array(self['x']), np.array(self['y']), filter_window, baseline_window, max_spike, max_diff)
+
+        return self.dataframe(pl.DataFrame({
+            'x': x,
+            'y': y,
+            'likelihood': self['likelihood'],
+            'keypoint': self['keypoint']
+        }))
+
+
+class PupilDict(TypedDict):
+    """
+    Pupil data dict
+
+    `Dimension parameters`:
+
+        F: number pf frames
+    """
+    area: np.ndarray
+    """`Array[float, F]`"""
+    com: np.ndarray
+    """center of maze in XY. `Array[float, [F, 2]]`"""
+    axdir: np.ndarray
+    """`Array[float, [F, 2, 2]]`"""
+    axlen: np.ndarray
+    """`Array[float, [F, 2]]`"""
+    area_smooth: np.ndarray
+    """`Array[float, F]`"""
+    com_smooth: np.ndarray
+    """`Array[float, [F, 2]]`"""
+
+
+class RoiDict(TypedDict, total=False):
+    """Roi Dict"""
+    rind: int
+    rtype: str
+    iROI: int
+    ivid: int
+    color: tuple[float, float, float]
+    yrange: np.ndarray
+    xrange: np.ndarray
+    saturation: float
+    pupil_sigma: float
+    ellipse: np.ndarray
+    yrange_bin: np.ndarray
+    xrange_bin: np.ndarray
+
+
+class SVDVariables(TypedDict, total=False):
+    """SVD output from facemap
+    .. seealso:: `<http://facemap.readthedocs.io/en/stable/outputs.html#roi-and-svd-processing>`_"""
+    filenames: list[str]
+    save_path: str
+    Ly: list[int]
+    Lx: list[int]
+    sbin: int
+    fullSVD: bool
+    save_mat: bool
+    Lybin: np.ndarray
+    Lxbin: np.ndarray
+    sybin: np.ndarray
+    sxbin: np.ndarray
+    LYbin: int
+    LXbin: int
+    avgframe: list[np.ndarray]
+    avgmotion: list[np.ndarray]
+    avgframe_reshape: np.ndarray
+    avgmotion_reshape: np.ndarray
+    motion: list[np.ndarray]
+    motSv: list[np.ndarray]
+    movSv: list[np.ndarray]
+    motMask: list[int]
+    movMask: list[int]
+    motMask_reshape: list[int]
+    movMask_reshape: list[int]
+    motSVD: list[np.ndarray]
+    movSVD: list[np.ndarray]
+    pupil: list[PupilDict]
+    running: list[np.ndarray]
+    blink: list[np.ndarray]
+    rois: list[RoiDict]
+    sy: np.ndarray
+    sx: np.ndarray
+
+
+class KeyPointsMeta(TypedDict):
+    """ Keypoint meta
+    .. seealso:: `<https://facemap.readthedocs.io/en/stable/outputs.html#keypoints-processing>`_"""
+    batch_size: int
+    image_size: tuple[list[int], ...]
+    bbox: tuple[int, ...]
+    total_frames: int
+    bodyparts: list[str]
+    inference_speed: float
