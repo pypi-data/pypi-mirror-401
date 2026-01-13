88f781097b262ff54bc9fe6f57c2eb554754c18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:29 +0000

    Add package vrchatapi/1.20.8.dev4/vrchatapi-1.20.8.dev4.tar.gz

---
+++ packages/vrchatapi/vrchatapi-1.20.8.dev4.tar.gz/vrchatapi-1.20.8.dev4/vrchatapi/api/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from vrchatapi.api.authentication_api import AuthenticationApi
+from vrchatapi.api.avatars_api import AvatarsApi
+from vrchatapi.api.calendar_api import CalendarApi
+from vrchatapi.api.economy_api import EconomyApi
+from vrchatapi.api.favorites_api import FavoritesApi
+from vrchatapi.api.files_api import FilesApi
+from vrchatapi.api.friends_api import FriendsApi
+from vrchatapi.api.groups_api import GroupsApi
+from vrchatapi.api.instances_api import InstancesApi
+from vrchatapi.api.inventory_api import InventoryApi
+from vrchatapi.api.invite_api import InviteApi
+from vrchatapi.api.jams_api import JamsApi
+from vrchatapi.api.miscellaneous_api import MiscellaneousApi
+from vrchatapi.api.notifications_api import NotificationsApi
+from vrchatapi.api.playermoderation_api import PlayermoderationApi
+from vrchatapi.api.prints_api import PrintsApi
+from vrchatapi.api.props_api import PropsApi
+from vrchatapi.api.users_api import UsersApi
+from vrchatapi.api.worlds_api import WorldsApi
