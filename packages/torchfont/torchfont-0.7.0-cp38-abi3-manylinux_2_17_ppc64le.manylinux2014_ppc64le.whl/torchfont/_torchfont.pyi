d51b8ac137d3ed9d0354b1fe737d2d448e62d785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:04:35 +0000

    Add package torchfont/0.7.0/torchfont-0.7.0-cp38-abi3-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/torchfont/torchfont-0.7.0-cp38-abi3-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/torchfont/_torchfont.pyi
@@ -0,0 +1,24 @@
+from collections.abc import Sequence
+
+class FontDataset:
+    def __init__(
+        self,
+        root: str,
+        codepoint_filter: Sequence[int] | None = ...,
+        patterns: Sequence[str] | None = ...,
+    ) -> None: ...
+
+    sample_count: int
+
+    content_classes: list[int]
+
+    style_classes: list[str]
+
+    def item(
+        self,
+        idx: int,
+    ) -> tuple[list[int], list[float], int, int]: ...
+    def locate(
+        self,
+        idx: int,
+    ) -> tuple[int, int | None, int, int, int]: ...
