f04ea309bd1ad6cde0ee5fe1f77d194fbf6711cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:11 +0000

    Add package tag-render/0.1.1/tag_render-0.1.1-cp312-cp312-manylinux_2_35_x86_64.whl

---
+++ packages/tag-render/tag_render-0.1.1-cp312-cp312-manylinux_2_35_x86_64.whl/tag_render/__init__.py
@@ -0,0 +1,5 @@
+from .tag_render import *
+
+__doc__ = tag_render.__doc__
+if hasattr(tag_render, "__all__"):
+    __all__ = tag_render.__all__
---
+++ packages/tag-render/tag_render-0.1.1-cp312-cp312-manylinux_2_35_x86_64.whl/tag_render/__init__.pyi
@@ -0,0 +1,10 @@
+class ImagePixels:
+    data: bytes
+
+
+class ModelRender:
+    def __init__():
+        ...
+
+    def image(self, path: str, dimensions: tuple[int, int]) -> ImagePixels:
+        ...
