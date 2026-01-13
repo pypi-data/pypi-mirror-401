026a17460194238c52e999cf445132d00ed7c752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:41 +0000

    Add package flyteplugins-spark/2.0.0b48/flyteplugins_spark-2.0.0b48-py3-none-any.whl

---
+++ packages/flyteplugins-spark/flyteplugins_spark-2.0.0b48-py3-none-any.whl/flyteplugins/spark/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["ParquetToSparkDecoder", "Spark", "SparkToParquetEncoder"]
+
+from flyteplugins.spark.df_transformer import ParquetToSparkDecoder, SparkToParquetEncoder
+from flyteplugins.spark.task import Spark
