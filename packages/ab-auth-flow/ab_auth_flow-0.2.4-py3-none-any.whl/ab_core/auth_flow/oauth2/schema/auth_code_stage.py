87ec35ac247370c9d40f9be13411384da02e83e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 19:06:49 +0000

    Add package ab-auth-flow/0.2.4/ab_auth_flow-0.2.4-py3-none-any.whl

---
+++ packages/ab-auth-flow/ab_auth_flow-0.2.4-py3-none-any.whl/ab_core/auth_flow/oauth2/schema/auth_code_stage.py
@@ -0,0 +1,32 @@
+from abc import ABC
+from enum import StrEnum
+from typing import Annotated, Literal
+
+from pydantic import BaseModel, Discriminator
+
+
+class AuthCodeStage(StrEnum):
+    BEGIN_LOGIN = "BEGIN_LOGIN"  # redirect the user to begin the login
+    DONE = "DONE"  # the auth code has been retrieved
+
+
+class AuthCodeStageInfoBase(BaseModel, ABC): ...
+
+
+class AuthCodeStageInfoBeginLogin(AuthCodeStageInfoBase):
+    stage: Literal[AuthCodeStage.BEGIN_LOGIN] = AuthCodeStage.BEGIN_LOGIN
+
+    ws_url: str
+    gui_url: str | None = None
+
+
+class AuthCodeStageInfoDone(AuthCodeStageInfoBase):
+    stage: Literal[AuthCodeStage.DONE] = AuthCodeStage.DONE
+
+    auth_code: str
+
+
+AuthCodeStageInfo = Annotated[
+    AuthCodeStageInfoBeginLogin | AuthCodeStageInfoDone,
+    Discriminator("stage"),
+]
