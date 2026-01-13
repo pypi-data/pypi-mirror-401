5fe2f3fbf3a0d33bf20e38be9d286fb42dfac558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:26 +0000

    Add package torchmonarch-nightly/2026.1.9/torchmonarch_nightly-2026.1.9-cp313-cp313-manylinux2014_x86_64.whl

---
+++ packages/torchmonarch-nightly/torchmonarch_nightly-2026.1.9-cp313-cp313-manylinux2014_x86_64.whl/monarch/_rust_bindings/monarch_extension/mesh_controller.pyi
@@ -0,0 +1,51 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-unsafe
+
+from traceback import FrameSummary
+from typing import List, NamedTuple, Sequence, Tuple, Union
+
+from monarch._rust_bindings.monarch_extension import client
+from monarch._rust_bindings.monarch_hyperactor.context import Instance
+from monarch._rust_bindings.monarch_hyperactor.mailbox import PortId
+from monarch._rust_bindings.monarch_hyperactor.proc import ActorId
+from monarch._rust_bindings.monarch_hyperactor.proc_mesh import ProcMesh as ProcMeshV0
+
+from monarch._rust_bindings.monarch_hyperactor.shape import Slice as NDSlice
+from monarch._rust_bindings.monarch_hyperactor.v1.proc_mesh import (
+    ProcMesh as ProcMeshV1,
+)
+
+class _Controller:
+    def __new__(self, proc_mesh: Union[ProcMeshV0, ProcMeshV1]) -> None: ...
+    def node(
+        self,
+        seq: int,
+        defs: Sequence[object],
+        uses: Sequence[object],
+        port: Tuple[PortId, NDSlice] | None,
+        tracebacks: List[List[FrameSummary]],
+    ) -> None: ...
+    def drop_refs(self, refs: Sequence[object]) -> None: ...
+    def send(
+        self,
+        ranks: Union[NDSlice, List[NDSlice]],
+        msg: NamedTuple,
+    ) -> None: ...
+    def _drain_and_stop(
+        self, instance: Instance
+    ) -> List[client.LogMessage | client.WorkerResponse | client.DebuggerMessage]: ...
+    def sync_at_exit(self, port: PortId) -> None:
+        """
+        Controller waits until all nodes that were added are complete, then replies on the
+        given port. The port will get an exception if there was a known error that was not reported
+        to any future.
+        """
+        ...
+
+    @property
+    def broker_id(self) -> Tuple[str, int]: ...
