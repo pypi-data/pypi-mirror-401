d8f1591e458f5523ab43d4d1a5371442f7ea6a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:08:48 +0000

    Add package sparecores-data/0.3.3/sparecores_data-0.3.3.tar.gz

---
+++ packages/sparecores-data/sparecores_data-0.3.3.tar.gz/sparecores_data-0.3.3/src/sc_data/constants.py
@@ -0,0 +1,13 @@
+import importlib.resources
+
+PKG_DB_FILE = "data/sc-data-priceless.db"
+PKG_DB_HASH = "data/db_hash"
+DB_PATH = importlib.resources.files(__name__.split(".")[0]).joinpath(PKG_DB_FILE)
+DB_HASH = (
+    open(importlib.resources.files(__name__.split(".")[0]).joinpath(PKG_DB_HASH), "r")
+    .read()
+    .strip()
+)
+DB_URL = "https://sc-data-public-40e9d310.s3.amazonaws.com/sc-data-all.db.bz2"
+HTTP_TIMEOUT = 30
+DB_REFRESH_SECONDS = 600
