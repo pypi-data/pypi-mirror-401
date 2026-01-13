98162108bfe246d72dd63e41871b42ca07389ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:08:23 +0000

    Add package nonebot-plugin-rikka/0.3.9/nonebot_plugin_rikka-0.3.9-py3-none-any.whl

---
+++ packages/nonebot-plugin-rikka/nonebot_plugin_rikka-0.3.9-py3-none-any.whl/nonebot_plugin_rikka/database/__init__.py
@@ -0,0 +1,4 @@
+from .crud import MaiSongAliasORM, MaiSongORM, UserBindInfoORM
+from .orm_models import MaiSongAlias, UserBindInfo
+
+__all__ = ["UserBindInfoORM", "UserBindInfo", "MaiSongORM", "MaiSongAliasORM", "MaiSongAlias"]
