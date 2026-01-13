aef398eb8cd6a738edb1d79e43398a85c3561122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:45 +0000

    Add package coffeeregister/0.1.0/coffeeregister-0.1.0.tar.gz

---
+++ packages/coffeeregister/coffeeregister-0.1.0.tar.gz/coffeeregister-0.1.0/README.md
@@ -0,0 +1,5 @@
+## Установка
+
+Базовая установка (только код):
+```bash
+pip install chill-coffee-cash
---
+++ packages/coffeeregister/coffeeregister-0.1.0.tar.gz/coffeeregister-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="coffeeregister",
+    version="0.1.0",
+    author="quzaory",
+    author_email="zHIjsBlKlHF7oCZ@proton.me",
+    description="Coffee shop cash register",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["PySide6>=6.5.0"],
+    entry_points={
+        "console_scripts": [
+            "coffee-cash=chill_coffee_cash.main:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
