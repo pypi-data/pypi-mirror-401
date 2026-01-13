2679b8aefab741d247477e86509b029d935dfda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:27 +0000

    Add package django-crontask/1.1.2/django_crontask-1.1.2-py3-none-any.whl

---
+++ packages/django-crontask/django_crontask-1.1.2-py3-none-any.whl/crontask/conf.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from django.conf import settings
+
+
+def get_settings():
+    return type(
+        "Settings",
+        (),
+        {
+            "REDIS_URL": None,
+            "LOCK_REFRESH_INTERVAL": 5,
+            "LOCK_TIMEOUT": 10,
+            "LOCK_BLOCKING_TIMEOUT": 15,
+            **getattr(settings, "CRONTASK", {}),
+        },
+    )
