a1a34c28b1de188cf705341861adeeee58ef9219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:09:08 +0000

    Add package oarepo/14.0.23/oarepo-14.0.23.tar.gz

---
+++ packages/oarepo/oarepo-14.0.23.tar.gz/oarepo-14.0.23/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of OArepo.
+# Copyright (C) 2023 CESNET.
+#
+# OArepo is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""CESNET, UCT Prague and NTK wrapper around Invenio """
+
+from setuptools import setup
+
+setup()
