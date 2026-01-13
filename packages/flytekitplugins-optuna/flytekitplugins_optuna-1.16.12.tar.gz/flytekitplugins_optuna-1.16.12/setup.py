ba701db090538f2722430c5040805fdb1f80dc15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:03 +0000

    Add package flytekitplugins-optuna/1.16.12/flytekitplugins_optuna-1.16.12.tar.gz

---
+++ packages/flytekitplugins-optuna/flytekitplugins_optuna-1.16.12.tar.gz/flytekitplugins_optuna-1.16.12/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+PLUGIN_NAME = "optuna"
+
+microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
+
+plugin_requires = ["flytekit>=1.15.0", "optuna>=4.0.0,<5.0.0", "typing-extensions>=4.10,<5.0"]
+
+__version__ = "1.16.12"
+
+setup(
+    title="Optuna (wrapper)",
+    title_expanded="Fully Parallelized Wrapper Around Optuna Using Flyte",
+    name=microlib_name,
+    version=__version__,
+    author="flyteorg",
+    author_email="admin@flyte.org",
+    description="Optuna plugin for flytekit",
+    namespace_packages=["flytekitplugins"],
+    packages=[f"flytekitplugins.{PLUGIN_NAME}"],
+    install_requires=plugin_requires,
+    license="apache2",
+    python_requires=">=3.9",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    entry_points={"flytekit.plugins": [f"{PLUGIN_NAME}=flytekitplugins.{PLUGIN_NAME}"]},
+)
