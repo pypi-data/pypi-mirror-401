3ddbb5c03ae2cbaebba90ce3ced5fa92998e09b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:06:43 +0000

    Add package piepdf/0.1.0/piepdf-0.1.0-py3-none-any.whl

---
+++ packages/piepdf/piepdf-0.1.0-py3-none-any.whl/piepdf/__main__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from PyQt5 import QtCore, QtWidgets
+
+from piepdf import adjustUI
+from piepdf import layout as wlayout
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    # Set Qt attributes BEFORE creating QApplication
+    os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1.5"
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
+    app = QtWidgets.QApplication(argv)
+    Mainwindow = wlayout.Ui_MainWindow()
+    Mainwindow.setupUi()
+    _ = adjustUI.Adjust_UI(Mainwindow)
+    Mainwindow.show()
+    app.aboutToQuit.connect(Mainwindow.saveSession)
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
