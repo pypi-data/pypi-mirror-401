a2159aea22fc7b141bacd2093c6d596c21561059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:03 +0000

    Add package flytekitplugins-onnxpytorch/1.16.12/flytekitplugins_onnxpytorch-1.16.12.tar.gz

---
+++ packages/flytekitplugins-onnxpytorch/flytekitplugins_onnxpytorch-1.16.12.tar.gz/flytekitplugins_onnxpytorch-1.16.12/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+PLUGIN_NAME = "onnxpytorch"
+
+microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
+
+plugin_requires = ["flytekit>=1.3.0b2,<2.0.0", "torch>=1.11.0", "onnx-pytorch", "networkx<3.2; python_version<'3.9'"]
+
+__version__ = "1.16.12"
+
+setup(
+    title="ONNX PyTorch",
+    title_expanded="Flytekit ONNX PyTorch Plugin",
+    name=f"flytekitplugins-{PLUGIN_NAME}",
+    version=__version__,
+    author="flyteorg",
+    author_email="admin@flyte.org",
+    description="ONNX PyTorch Plugin for Flytekit",
+    namespace_packages=["flytekitplugins"],
+    packages=[f"flytekitplugins.{PLUGIN_NAME}"],
+    install_requires=plugin_requires,
+    license="apache2",
+    python_requires=">=3.9",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
