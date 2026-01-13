ecab8ac17e0f686b141da824e98cf8884d0df134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:23 +0000

    Add package robotpy-ctre/2026.1.0/robotpy_ctre-2026.1.0-cp313-cp313-macosx_13_0_x86_64.whl

---
+++ packages/robotpy-ctre/robotpy_ctre-2026.1.0-cp313-cp313-macosx_13_0_x86_64.whl/phoenix5/include/ctre/phoenix/motion/SetValueMotionProfile.h
@@ -0,0 +1,14 @@
+/* Copyright (C) Cross The Road Electronics 2024 */
+#pragma once
+
+namespace ctre {
+namespace phoenix {
+namespace motion {
+
+enum SetValueMotionProfile {
+	Disable = 0, Enable = 1, Hold = 2,
+};
+
+} // namespace motion
+} // namespace phoenix
+} // namespace ctre
