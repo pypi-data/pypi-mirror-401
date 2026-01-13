22eab73278859bf203d4f258313585f762d601b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:09:42 +0000

    Add package publish-lib-ghp/0.2.8/publish_lib_ghp-0.2.8.tar.gz

---
+++ packages/publish-lib-ghp/publish_lib_ghp-0.2.8.tar.gz/publish_lib_ghp-0.2.8/src/publish_lib_ghp/operations.py
@@ -0,0 +1,8 @@
+class Operations:
+    """Class for performing operations in the publish_lib_ghp package."""
+    def __init__(self):
+        pass
+
+    def add(self, a: int, b: int) -> int:
+        """Returns the sum of two integers."""
+        return a + b
