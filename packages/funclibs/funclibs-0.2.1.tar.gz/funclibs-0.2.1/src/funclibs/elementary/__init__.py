dac486cf079cda16efaa1a5143e44c10469858b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:25 +0000

    Add package funclibs/0.2.1/funclibs-0.2.1.tar.gz

---
+++ packages/funclibs/funclibs-0.2.1.tar.gz/funclibs-0.2.1/src/funclibs/elementary/__init__.py
@@ -0,0 +1,1 @@
+from funclibs.elementary import Const, identity
---
+++ packages/funclibs/funclibs-0.2.1.tar.gz/funclibs-0.2.1/src/funclibs/elementary/identity.py
@@ -0,0 +1,8 @@
+from typing import *
+
+__all__ = ["identity"]
+
+
+def identity(value: Any, /) -> Any:
+    "This function returns the value given to it."
+    return value
