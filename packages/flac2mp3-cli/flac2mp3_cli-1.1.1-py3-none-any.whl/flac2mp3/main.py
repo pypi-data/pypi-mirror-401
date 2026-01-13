bc8ec0d53028e20d45a42b0c85f26b23e8990e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:09:08 +0000

    Add package flac2mp3-cli/1.1.1/flac2mp3_cli-1.1.1-py3-none-any.whl

---
+++ packages/flac2mp3-cli/flac2mp3_cli-1.1.1-py3-none-any.whl/flac2mp3/main.py
@@ -0,0 +1,111 @@
+from . import custom_functions as cf
+#import custom_functions as cf
+import os, shutil, time, sys
+
+from concurrent.futures import ThreadPoolExecutor # Multithreading
+from alive_progress import alive_bar # Progress bar
+
+
+def single_file_conversion(file_path, output_dir, bitrate, bar):
+    # convert single file to mp3
+    
+    if file_path.endswith(".flac"):  # if file is a flac file, convert it to mp3
+        output_path = output_dir + "/" + os.path.basename(file_path).replace(".flac", ".mp3")
+        cf.flac_mp3(file_path, output_path, bitrate)
+        flac_count, file_count = [1], [0]
+        bar()
+
+    else:
+        print("File is not a flac file")
+        sys.exit(1)
+
+    return flac_count, file_count
+
+def flac_conversion(input_file_path, output_file_path, bitrate, flac_count, bar):
+    # convert flac file to mp3
+    cf.flac_mp3(input_file_path, output_file_path, bitrate)
+    flac_count[0] += 1
+    bar()
+
+
+def file_copy(input_file_path, output_file_path, file_count, bar):
+    # copy file to output directory
+    shutil.copy(input_file_path, output_file_path)
+    file_count[0] += 1
+    bar()
+
+
+def flac_thread(flac_list, input_dir, output_dir, bitrate, flac_count, theads_count, overwrite, bar):
+        with ThreadPoolExecutor(max_workers=theads_count) as executor:
+            futures = []
+            for file_path in flac_list:
+            
+                output_file_path = file_path.replace(input_dir, output_dir).replace(".flac", ".mp3")
+                os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+                
+                if overwrite or not os.path.isfile(output_file_path):
+                    # if overwrite is True or file does not exist in output directory, convert file
+                    futures.append(executor.submit(flac_conversion, file_path, output_file_path, bitrate, flac_count, bar))
+            
+            for future in futures:
+                future.result()
+
+
+def file_thread(file_list, input_dir, output_dir, file_count, theads_count, overwrite, bar):
+        with ThreadPoolExecutor(max_workers=theads_count) as executor:
+            futures = []
+            for file_path in file_list:
+                
+                output_file_path = file_path.replace(input_dir, output_dir)
+                os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+                
+                if overwrite or not os.path.isfile(output_file_path):
+                    # if overwrite is True or file does not exist in output directory, copy file
+                    futures.append(executor.submit(file_copy, file_path, output_file_path, file_count, bar))
+
+            for future in futures:
+                future.result()
+
+
+def check_ffmpeg_installed():
+    """
+    Checks if ffmpeg is available in the system PATH.
+    """
+    if shutil.which("ffmpeg") is None:
+        print("Error: FFmpeg is not installed or not in the system PATH.")
+        print("Please install FFmpeg to use this tool, see: Note: `ffmpeg` client must be installed on the machine, see: https://ffmpeg.org/download.html.") 
+        sys.exit(1) # Stop the program immediately
+
+def main():
+    check_ffmpeg_installed() # Check if ffmpeg is installed, otherwise exit
+
+    input_path, thread_count, output_path, bitrate, overwrite = cf.collect_args()
+    input_path = os.path.abspath(input_path)
+    output_path = os.path.abspath(output_path)
+    start_time = time.time()
+
+    if os.path.isfile(input_path):
+        with alive_bar(title="Processing single file:", bar=False, spinner_length=5, receipt=False, elapsed=False, stats=None, monitor=False) as bar:
+            flac_count, file_count = single_file_conversion(input_path, output_path, bitrate, bar)
+
+    else:
+        flac_count, file_count = [0], [0]
+        flac_total, flac_list, file_total, file_list = cf.dir_scan(input_path, overwrite)
+ 
+        os.makedirs(output_path, exist_ok=True)
+
+        with alive_bar(flac_total, title="Processing flac files:", bar="notes", spinner_length=5, receipt=False, elapsed=False, stats=None) as bar:
+            flac_thread(flac_list, input_path, output_path, bitrate, flac_count, thread_count, overwrite, bar)
+
+        with alive_bar(file_total, title="Coping other files:", bar="smooth", spinner_length=5, receipt=False, elapsed=False, stats=None) as bar:
+            file_thread(file_list, input_path, output_path, file_count, thread_count, overwrite, bar)
+
+    # print summary
+    total_time = time.time() - start_time
+    minutes = int(total_time // 60)
+    seconds = int(round(total_time % 60))
+    print(f"Done! Converted {flac_count[0]} flac files and copied {file_count[0]} files in {minutes}.{seconds} minutes.")
+
+
+if __name__ == "__main__":
+    main()
