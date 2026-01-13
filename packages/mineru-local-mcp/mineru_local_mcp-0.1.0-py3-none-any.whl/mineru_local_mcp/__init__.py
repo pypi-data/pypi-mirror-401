3579556736157d3a9e3b617221d5120ef1b76375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:48 +0000

    Add package mineru-local-mcp/0.1.0/mineru_local_mcp-0.1.0-py3-none-any.whl

---
+++ packages/mineru-local-mcp/mineru_local_mcp-0.1.0-py3-none-any.whl/mineru_local_mcp/__init__.py
@@ -0,0 +1,10 @@
+from .servers import mineru_mcp
+
+__version__ = "0.1.0"
+__authors__ = [
+    {"name": "SongTan", "email": "sepinetam@gmail.com"}
+]
+
+
+if __name__ == "__main__":
+    mineru_mcp.run()
