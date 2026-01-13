8f5865b8759ba08277fab8fb5fd7a8ca68bf8109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 15:07:50 +0000

    Add package pyautotrace/0.0.7/pyautotrace-0.0.7-cp313-cp313-win_amd64.whl

---
+++ packages/pyautotrace/pyautotrace-0.0.7-cp313-cp313-win_amd64.whl/autotrace/__init__.py
@@ -0,0 +1,53 @@
+"""Python bindings for AutoTrace.
+
+## Usage
+
+```python
+import numpy as np
+from autotrace import Bitmap, VectorFormat
+from PIL import Image
+
+# Load an image.
+image = np.array(Image.open("image.jpeg").convert("RGB"))
+
+# Create a bitmap.
+bitmap = Bitmap(image)
+
+# Trace the bitmap.
+vector = bitmap.trace()
+
+# Save the vector as an SVG.
+vector.save("image.svg")
+
+# Get an SVG as a byte string.
+svg = vector.encode(VectorFormat.SVG)
+```
+"""
+
+from importlib.metadata import version as _version
+
+from .autotrace import (
+    Bitmap,
+    Color,
+    Path,
+    Point,
+    PolynomialDegree,
+    Spline,
+    TraceOptions,
+    Vector,
+    VectorFormat,
+)
+
+__all__ = (
+    "Bitmap",
+    "Color",
+    "Path",
+    "Point",
+    "PolynomialDegree",
+    "Spline",
+    "TraceOptions",
+    "Vector",
+    "VectorFormat",
+    "__version__",
+)
+__version__ = _version("pyautotrace")
