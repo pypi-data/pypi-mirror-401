8ae551627ae5da6080339e7c6c5523f269d97b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 17:09:16 +0000

    Add package rdbpython/0.2.1/rdbpython-0.2.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/rdbpython/rdbpython-0.2.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/rdbpy/lib_utils.py
@@ -0,0 +1,52 @@
+import ctypes
+import os
+import platform
+from glob import glob
+
+
+def get_include_dir():
+    dirname = os.path.dirname(__file__)
+    abs_dirname = os.path.abspath(dirname)
+    path = os.path.join(abs_dirname, "include")
+    return path
+
+
+def get_lib_dir():
+    dirname = os.path.dirname(__file__)
+    abs_dirname = os.path.abspath(dirname)
+    path = abs_dirname
+    return path
+
+
+def get_libs():
+    return [
+        "rocksdb",
+    ]
+
+
+def get_lib_filename(name, lib_dir):
+    if platform.system() == "Darwin":
+        pattern = f"lib{name}*.dylib"
+    else:
+        pattern = f"lib{name}*.so*"
+
+    files = glob(os.path.join(lib_dir, pattern))
+    assert len(files) == 1
+    return os.path.basename(files[0])
+
+
+def get_lib_filenames():
+    local_lib_dir = get_lib_dir()
+    return [get_lib_filename(name, local_lib_dir) for name in get_libs()]
+
+
+def load_lib(filename) -> None:
+    local_lib_dir = get_lib_dir()
+    path = os.path.join(local_lib_dir, filename)
+    print(f"Loading runtime library from {path}")
+    ctypes.CDLL(path)
+
+
+def load_libs() -> None:
+    for filename in get_lib_filenames():
+        load_lib(filename)
