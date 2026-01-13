300c3045ad63f4090d888fe027e5295441d2af79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 16:06:22 +0000

    Add package robotpy-ctre/2026.1.0/robotpy_ctre-2026.1.0-cp310-cp310-macosx_13_0_x86_64.whl

---
+++ packages/robotpy-ctre/robotpy_ctre-2026.1.0-cp310-cp310-macosx_13_0_x86_64.whl/phoenix5/include/ctre/phoenix/platform/DeviceType.hpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) Cross The Road Electronics.  All rights reserved.
+ * License information can be found in CTRE_LICENSE.txt
+ * For support and suggestions contact support@ctr-electronics.com or file
+ * an issue tracker at https://github.com/CrossTheRoadElec/Phoenix-Releases
+ */
+#pragma once
+
+namespace ctre {
+namespace phoenix {
+namespace platform {
+
+    /** List of all supported device types */
+    #define kDeviceTypeListInitializer	\
+        TalonSRXType,	\
+        VictorSPXType,	\
+        PigeonIMUType,	\
+        RibbonPigeonIMUType,	\
+        P6_TalonFXType,	\
+        P6_CANcoderType,	\
+        P6_Pigeon2Type,	\
+        P6_TalonFXSType,	\
+        P6_CANrangeType,	\
+        P6_CANdiType,	\
+        P6_CANdleType
+
+    /** Enumeration of all supported device types. */
+    enum DeviceType {kDeviceTypeListInitializer};
+
+}
+}
+}
