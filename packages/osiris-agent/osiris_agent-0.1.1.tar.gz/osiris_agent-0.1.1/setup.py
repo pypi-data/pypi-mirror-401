e81607eb51a04ccb6f350e958bcf0c7e75f81458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:05:51 +0000

    Add package osiris-agent/0.1.1/osiris_agent-0.1.1.tar.gz

---
+++ packages/osiris-agent/osiris_agent-0.1.1.tar.gz/osiris_agent-0.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+long_description = (HERE / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='osiris_agent',
+    version='0.1.1',
+    description='OSIRIS agent for ROS2/Humble',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/nicolaselielll/osiris_agent',
+    author='Nicolas Tuomaala',
+    author_email='nicolas.tuomaala00@gmail.com',
+    packages=find_packages(),
+    license='Apache-2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: OS Independent',
+    ],
+    keywords='ros2 humble agent',
+    install_requires=[
+        'websockets',
+        'psutil',
+    ],
+    extras_require={
+        'ros': ['rclpy'],
+    },
+    entry_points={
+        'console_scripts': [
+            'agent_node = osiris_agent.agent_node:main',
+        ],
+    },
+)
