0568b66d199dcd01ac58180d6bec94ace508f25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 20:05:02 +0000

    Add package kalpeshcalculator/0.1/kalpeshcalculator-0.1.tar.gz

---
+++ packages/kalpeshcalculator/kalpeshcalculator-0.1.tar.gz/kalpeshcalculator-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kalpeshcalculator",
+    version="0.1",
+    packages=find_packages(),
+    author="Kalpesh Deshmukh",
+    description="Simple calculator Python library",
+)
