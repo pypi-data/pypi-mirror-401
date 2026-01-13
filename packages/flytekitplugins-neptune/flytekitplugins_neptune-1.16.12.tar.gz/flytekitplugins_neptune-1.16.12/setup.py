121dcb37d1c132535e62b783bec4b9c3bff3885b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:03 +0000

    Add package flytekitplugins-neptune/1.16.12/flytekitplugins_neptune-1.16.12.tar.gz

---
+++ packages/flytekitplugins-neptune/flytekitplugins_neptune-1.16.12.tar.gz/flytekitplugins_neptune-1.16.12/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+PLUGIN_NAME = "neptune"
+
+microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
+
+plugin_requires = ["flytekit>=1.16.0", "neptune-scale>=0.13.0"]
+
+__version__ = "1.16.12"
+
+setup(
+    title="Neptune",
+    title_expanded="Flytekit Neptune Plugin",
+    name=microlib_name,
+    version=__version__,
+    author="flyteorg",
+    author_email="admin@flyte.org",
+    description="This package enables seamless use of Neptune within Flyte",
+    namespace_packages=["flytekitplugins"],
+    packages=[f"flytekitplugins.{PLUGIN_NAME}"],
+    install_requires=plugin_requires,
+    license="apache2",
+    python_requires=">=3.9",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    extras_require={"legacy": ["neptune>=1.10.4"]},
+)
