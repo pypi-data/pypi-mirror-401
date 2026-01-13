f8924afca32a3988bb27be9744d00adf93337e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 14:06:18 +0000

    Add package bmi-arcgis-restapi/2.4.15/bmi_arcgis_restapi-2.4.15.tar.gz

---
+++ packages/bmi-arcgis-restapi/bmi_arcgis_restapi-2.4.15.tar.gz/bmi_arcgis_restapi-2.4.15/restapi/geometry-helper/precache-manifest.b62ded22fc8a9700861c815d7e20acbd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3d5886b0e41c3a6147",
+    "url": "css/app.615af0e5.css"
+  },
+  {
+    "revision": "450f6bb2519883e30a2c",
+    "url": "css/chunk-26ed89da.8fb689c1.css"
+  },
+  {
+    "revision": "85908b9ac0d7560c0765",
+    "url": "css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "381babb0d16d4066aca0603e0caec6b0",
+    "url": "index.html"
+  },
+  {
+    "revision": "8e3d5886b0e41c3a6147",
+    "url": "js/app.dbaf9f92.js"
+  },
+  {
+    "revision": "450f6bb2519883e30a2c",
+    "url": "js/chunk-26ed89da.db4a4cda.js"
+  },
+  {
+    "revision": "85908b9ac0d7560c0765",
+    "url": "js/chunk-vendors.20697101.js"
+  },
+  {
+    "revision": "17999de1069f5bbaa59c130a544665c6",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
