5f77c7e2a4e25ae358cdb02be3ef013dce7167eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 18:08:31 +0000

    Add package neuralib-imaging/0.7.0/neuralib_imaging-0.7.0.tar.gz

---
+++ packages/neuralib-imaging/neuralib_imaging-0.7.0.tar.gz/neuralib_imaging-0.7.0/test/test_wfield.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch
+
+import numpy as np
+import pytest
+from tifffile import tifffile
+
+from neuralib.widefield import plot_retinotopic_maps, compute_singular_vector
+from neuralib.io.dataset import load_example_retinotopic_data
+
+
+@pytest.fixture(scope='module')
+def sequence() -> np.ndarray:
+    seq_path = load_example_retinotopic_data()
+    return tifffile.imread(seq_path)
+
+
+@patch('matplotlib.pyplot.show')
+def test_plot_map(mock, sequence: np.ndarray):
+    plot_retinotopic_maps(sequence)
+
+
+def test_compute_svd(sequence: np.ndarray):
+    compute_singular_vector(sequence)
