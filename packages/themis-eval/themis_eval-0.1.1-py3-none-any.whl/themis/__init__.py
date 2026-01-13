1f1a4549277166b2b3fa4a73406fd42c13525538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-13 21:04:42 +0000

    Add package themis-eval/0.1.1/themis_eval-0.1.1-py3-none-any.whl

---
+++ packages/themis-eval/themis_eval-0.1.1-py3-none-any.whl/themis/__init__.py
@@ -0,0 +1,14 @@
+"""Themis experiment platform."""
+
+from themis import config, core, evaluation, experiment, generation, project
+from themis._version import __version__
+
+__all__ = [
+    "config",
+    "core",
+    "evaluation",
+    "experiment",
+    "generation",
+    "project",
+    "__version__",
+]
