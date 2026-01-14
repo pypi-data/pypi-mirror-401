71fddc290533688753c97b1d7186604c1b53f51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 20:05:38 +0000

    Add package viiv/1.0.7/viiv-1.0.7-py3-none-any.whl

---
+++ packages/viiv/viiv-1.0.7-py3-none-any.whl/viiv/__init__.py
@@ -0,0 +1,9 @@
+"""ViiV - VSCode theme generator.
+
+This module provides functionality to generate customized VSCode themes with
+random or configured colors for tokens and workbench elements.
+"""
+
+from viiv.viiv import main
+
+__all__ = ["main"]
