dfb4cd4de69aaf60a0b77126f3a3b9c6524ce73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:06:54 +0000

    Add package vidhash/0.3.2/vidhash-0.3.2.tar.gz

---
+++ packages/vidhash/vidhash-0.3.2.tar.gz/vidhash-0.3.2/vidhash/__init__.py
@@ -0,0 +1,6 @@
+from vidhash.func import check_match, hash_video
+from vidhash.hash_options import HashOptions, HashSettings
+from vidhash.match_options import MatchOptions
+from vidhash.video_hash import VideoHash
+
+__all__ = ["hash_video", "check_match", "HashSettings", "HashOptions", "MatchOptions", "VideoHash"]
