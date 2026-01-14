214c889f403c280582c57a9e379a9701be57618c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:07:15 +0000

    Add package pkgmgr-kunrunic/0.1.0.dev8/pkgmgr_kunrunic-0.1.0.dev8.tar.gz

---
+++ packages/pkgmgr-kunrunic/pkgmgr_kunrunic-0.1.0.dev8.tar.gz/pkgmgr_kunrunic-0.1.0.dev8/pkgmgr/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+"""
+Modernized pkg manager scaffold.
+This package will grow to replace the legacy single-file script.
+"""
+
+__all__ = [
+    "cli",
+    "config",
+    "snapshot",
+    "release",
+    "gitcollect",
+    "watch",
+    "points",
+]
+
+__version__ = "0.1.0.dev8"
