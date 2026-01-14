639a8a597d6f137d232cbb88b2c8aef64b75f67f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 18:05:21 +0000

    Add package geon/0.1.3/geon-0.1.3-cp312-cp312-macosx_10_9_x86_64.whl

---
+++ packages/geon/geon-0.1.3-cp312-cp312-macosx_10_9_x86_64.whl/geon/config/theme.py
@@ -0,0 +1,31 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPalette, QColor
+
+from enum import Enum
+
+DEFAULT_OBJ_COLOR: tuple[float,float,float] = (0.6,0.6,0.6)
+DEFAULT_SEGMENTATION_COLOR: tuple[int,int,int] = (204, 204, 204)
+DEFAULT_RENDERER_BACKGROUND: tuple[float,float,float] = (.1,.1,.1)
+
+
+class UIStyle(Enum):
+    TYPE_LABEL = "color: rgba(128, 128, 128, 128);"
+
+
+def set_dark_palette(app):
+    dark_palette = QPalette()
+    dark_palette.setColor(QPalette.ColorRole.Window, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.white)
+    dark_palette.setColor(QPalette.ColorRole.Base, QColor(25, 25, 25))
+    dark_palette.setColor(QPalette.ColorRole.AlternateBase, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ColorRole.ToolTipBase, QColor(42, 42, 42))
+    dark_palette.setColor(QPalette.ColorRole.ToolTipText, Qt.GlobalColor.white)
+    dark_palette.setColor(QPalette.ColorRole.Text, Qt.GlobalColor.white)
+    dark_palette.setColor(QPalette.ColorRole.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ColorRole.ButtonText, Qt.GlobalColor.white)
+    dark_palette.setColor(QPalette.ColorRole.BrightText, Qt.GlobalColor.red)
+    dark_palette.setColor(QPalette.ColorRole.Link, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.ColorRole.Highlight, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.ColorRole.HighlightedText, Qt.GlobalColor.black)
+    app.setPalette(dark_palette)
+
