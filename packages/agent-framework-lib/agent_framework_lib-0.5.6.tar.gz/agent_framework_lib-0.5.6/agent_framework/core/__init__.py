9f1fcb64f6cb251073470af9575f9250d5ed0dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 19:06:32 +0000

    Add package agent-framework-lib/0.5.6/agent_framework_lib-0.5.6.tar.gz

---
+++ packages/agent-framework-lib/agent_framework_lib-0.5.6.tar.gz/agent_framework_lib-0.5.6/agent_framework/core/__init__.py
@@ -0,0 +1,5 @@
+"""Core components of the agent framework."""
+
+from agent_framework.core.elasticsearch_config_provider import ElasticsearchConfigProvider
+
+__all__ = ["ElasticsearchConfigProvider"]
