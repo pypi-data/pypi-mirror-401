b499cf46f3c17bf0cee89d67a113363552a47da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 20:05:43 +0000

    Add package tsp/1.8.3/tsp-1.8.3.tar.gz

---
+++ packages/tsp/tsp-1.8.3.tar.gz/tsp-1.8.3/tsp/metadata_ref.py
@@ -0,0 +1,6 @@
+{'_latitude': 'latitude of the site', 
+ '_longitude': 'longitude of the site', 
+ '_site_id': 'identifier for the site',
+ '_source_file': 'path to the source data file',
+ 'CF': 'dictionary of CF-compliant metadata, able to be used in netCDF files',
+ }
