a6a1e8b61f68848b0f9bc991ac0bff265b027120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 18:05:58 +0000

    Add package biostarPython/0.6.0.0/biostarpython-0.6.0.0.tar.gz

---
+++ packages/biostarPython/biostarpython-0.6.0.0.tar.gz/biostarpython-0.6.0.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='biostarPython',
+    version='0.6.0.0',
+    author_email = 'gcartlidge@supremainc.com',
+    author = 'SupremaUK',
+    description = 'Python client for Suprema GSDK with included callable classes.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/gcartlidge/biostarPython',
+    packages=['biostarPython','biostarPython.service','biostarPython.proto'],
+    python_requires='>3.5.2',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+    package_data={
+        "": ["*.py", "*.proto", "*.json"],
+        "proto": ["*.proto"],
+        "service": ["*.py"],
+    },
+    install_requires=[
+        'grpcio',
+        'protobuf',
+    ],
+)
