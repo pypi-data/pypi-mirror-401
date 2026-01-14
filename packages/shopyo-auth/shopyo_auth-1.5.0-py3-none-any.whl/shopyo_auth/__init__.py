161856357684fbf56ecd435584e53184f86071c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:06:49 +0000

    Add package shopyo-auth/1.5.0/shopyo_auth-1.5.0-py3-none-any.whl

---
+++ packages/shopyo-auth/shopyo_auth-1.5.0-py3-none-any.whl/shopyo_auth/__init__.py
@@ -0,0 +1,44 @@
+from typing import Any
+import os
+import json
+
+from flask import Flask
+from flask import current_app
+from .view import module_blueprint
+from .upload import upload
+
+__version__ = "1.5.0"
+
+info = {}
+with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + "info.json") as f:
+    info = json.load(f)
+
+
+default_config = {
+    "SHOPYO_AUTH_URL": "/shopyo-auth",
+    "SHOPYO_AUTH_REGISTER": True,
+    "SHOPYO_AUTH_LOGIN_FORGET_PASSWORD": True,
+}
+
+
+class ShopyoAuth:
+    def __init__(self, app: Any = None) -> None:
+        if app is not None:
+            self.init_app(app)
+        self.upload = upload
+
+    def init_app(self, app: Flask) -> None:
+        if not hasattr(app, "extensions"):
+            app.extensions = {}
+
+        for key, value in default_config.items():
+            app.config.setdefault(key, value)
+
+        app.extensions["shopyo_auth"] = self
+        bp = module_blueprint
+        app.register_blueprint(bp, url_prefix=app.config["SHOPYO_AUTH_URL"])
+        app.jinja_env.globals["shopyo_auth"] = self
+
+    def get_info(self):
+        info.update({"url_prefix": current_app.config["SHOPYO_AUTH_URL"]})
+        return info
