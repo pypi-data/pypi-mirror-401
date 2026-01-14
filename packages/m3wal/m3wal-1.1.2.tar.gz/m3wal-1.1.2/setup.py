b9f745486bb45188559d0b4558d04958a7727d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:07:07 +0000

    Add package m3wal/1.1.2/m3wal-1.1.2.tar.gz

---
+++ packages/m3wal/m3wal-1.1.2.tar.gz/m3wal-1.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="m3wal",
+    version="1.1.2",
+    packages=find_packages(),
+    package_data={
+        'm3wal': ['templates/*.template'],  # Include templates
+    },
+    install_requires=[
+        "material-color-utilities",
+        "Pillow",
+    ],
+    entry_points={
+        'console_scripts': [
+            'm3wal=m3wal.m3wal:main',
+        ],
+    },
+    author="Diaz",
+    description="Material 3 wallpaper color scheme generator",
+    url="https://github.com/MDiaznf23/m3wal",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
