a1977eebfe6dd8e6bb7b160c87cb00e16f2981bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 19:06:41 +0000

    Add package vimseo/0.1.4/vimseo-0.1.4-py3-none-any.whl

---
+++ packages/vimseo/vimseo-0.1.4-py3-none-any.whl/vimseo/lib_vimseo/__init__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 IRT Saint Exupéry, https://www.irt-saintexupery.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""Place here sources which have specific constraints like old Python versions.
+The Python files of this module are excluded from pre-commit hook checks.
+
+Concerning the Fortran user subroutines, their path relative to the library directory
+must be provided in file ``subroutines.json``."""
