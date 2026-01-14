de9acdab4ddc4952ff920643f0727fa51988af69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 20:05:38 +0000

    Add package faster-eth-utils/5.3.21/faster_eth_utils-5.3.21-cp314-cp314-manylinux1_i686.manylinux_2_28_i686.manylinux_2_5_i686.whl

---
+++ packages/faster-eth-utils/faster_eth_utils-5.3.21-cp314-cp314-manylinux1_i686.manylinux_2_28_i686.manylinux_2_5_i686.whl/faster_eth_utils/exceptions.py
@@ -0,0 +1,11 @@
+"""
+faster-eth-utils exceptions always inherit from eth-utils exceptions, so porting to faster-eth-utils
+does not require any change to your existing exception handlers. They will continue to work.
+"""
+
+import eth_utils.exceptions
+
+class ValidationError(eth_utils.exceptions.ValidationError):  # type: ignore[misc]
+    """
+    Raised when something does not pass a validation check.
+    """
