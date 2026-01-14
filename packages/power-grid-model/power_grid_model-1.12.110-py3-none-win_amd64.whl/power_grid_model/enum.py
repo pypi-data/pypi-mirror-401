ae462857a9b037c11aae9e7300a6f2f2d52884dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 19:07:25 +0000

    Add package power-grid-model/1.12.110/power_grid_model-1.12.110-py3-none-win_amd64.whl

---
+++ packages/power-grid-model/power_grid_model-1.12.110-py3-none-win_amd64.whl/power_grid_model/enum.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+"""
+Common enumerations used by the power-grid-model library.
+
+Note: these enumeration match the C++ arithmetic core, so don't change the values unless you change them in C++ as well
+
+"""
+
+from power_grid_model._core.enum import (  # noqa: F401
+    AngleMeasurementType,
+    Branch3Side,
+    BranchSide,
+    CalculationMethod,
+    CalculationType,
+    ComponentAttributeFilterOptions,
+    FaultPhase,
+    FaultType,
+    LoadGenType,
+    MeasuredTerminalType,
+    ShortCircuitVoltageScaling,
+    TapChangingStrategy,
+    WindingType,
+    _ExperimentalFeatures,
+)
