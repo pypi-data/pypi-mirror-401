853e12b6948d3f7bdbfb7ace23451f70f5addf28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 17:11:01 +0000

    Add package eth-portfolio/0.5.7/eth_portfolio-0.5.7-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/eth-portfolio/eth_portfolio-0.5.7-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/eth_portfolio/_decimal.py
@@ -0,0 +1,154 @@
+import decimal
+import logging
+
+from evmspec.data import Wei
+from typing_extensions import Self
+
+logger = logging.getLogger(__name__)
+
+# we need to enlarge the precision for Decimal objects so we don't get errors when doing math
+decimal.getcontext().prec = 100
+
+
+class Decimal(decimal.Decimal):
+    """
+    A subclass of :class:`decimal.Decimal` with additional functionality for JSON serialization.
+
+    This class extends the :class:`decimal.Decimal` class to provide an additional
+    method for JSON serialization. It also overrides arithmetic operations to ensure
+    that the result is of type :class:`Decimal`.
+
+    See Also:
+        - :class:`decimal.Decimal`
+    """
+
+    def jsonify(self) -> str | int:
+        """
+        Converts the :class:`Decimal` to a JSON-friendly format.
+
+        This method attempts to represent the :class:`Decimal` in the most compact form
+        possible for JSON serialization. It returns an integer if the :class:`Decimal`
+        is exactly equal to an integer, otherwise it returns a string in either
+        standard or scientific notation, depending on which is shorter.
+
+        If the integer representation is exactly equal to the :class:`Decimal`,
+        the integer is returned. Otherwise, the method returns the shorter of the
+        standard string representation or the scientific notation.
+
+        Examples:
+            >>> Decimal('123.4500').jsonify()
+            '123.45'
+            >>> Decimal('123000').jsonify()
+            123000
+            >>> Decimal('0.000123').jsonify()
+            '1.23E-4'
+            >>> Decimal('1000000').jsonify()
+            1000000
+        """
+        string = str(self)
+        integer = int(self)
+
+        if integer == self:
+            scientific_notation = "%E" % self
+            return (
+                integer
+                if len(str(integer)) <= len(scientific_notation) + 2
+                else scientific_notation
+            )
+
+        if "E" in string:
+            return string
+
+        scientific_notation = "%E" % self
+        while string[-1] == "0":
+            string = string[:-1]
+
+        if type(self)(scientific_notation) == self and len(scientific_notation) < len(string):
+            return scientific_notation
+
+        return string
+
+    def __add__(self, other) -> Self:
+        try:
+            return type(self)(super().__add__(other))
+        except TypeError as e:
+            raise TypeError(str(e), other) from e.__cause__
+
+    def __radd__(self, other) -> Self:
+        try:
+            return type(self)(super().__radd__(other))
+        except TypeError as e:
+            raise TypeError(str(e), other) from e.__cause__
+
+    def __sub__(self, other) -> Self:
+        try:
+            return type(self)(super().__sub__(other))
+        except TypeError as e:
+            raise TypeError(str(e), other) from e.__cause__
+
+    def __rsub__(self, other) -> Self:
+        try:
+            return type(self)(super().__rsub__(other))
+        except TypeError as e:
+            raise TypeError(str(e), other) from e.__cause__
+
+    def __mul__(self, other) -> Self:
+        try:
+            return type(self)(super().__mul__(other))
+        except TypeError as e:
+            raise TypeError(str(e), other) from e.__cause__
+
+    def __rmul__(self, other) -> Self:
+        try:
+            return type(self)(super().__rmul__(other))
+        except TypeError as e:
+            raise TypeError(str(e), other) from e.__cause__
+
+    def __truediv__(self, other) -> Self:
+        try:
+            return type(self)(super().__truediv__(other))
+        except TypeError as e:
+            raise TypeError(str(e), other) from e.__cause__
+
+    def __rtruediv__(self, other) -> Self:
+        try:
+            return type(self)(super().__rtruediv__(other))
+        except TypeError as e:
+            raise TypeError(str(e), other) from e.__cause__
+
+    def __floordiv__(self, other) -> Self:
+        try:
+            return type(self)(super().__floordiv__(other))
+        except TypeError as e:
+            raise TypeError(str(e), other) from e.__cause__
+
+    def __rfloordiv__(self, other) -> Self:
+        try:
+            return type(self)(super().__rfloordiv__(other))
+        except TypeError as e:
+            raise TypeError(str(e), other) from e.__cause__
+
+
+class Gwei(Decimal):
+    """
+    A subclass of :class:`Decimal` representing Gwei values.
+
+    This class provides a property to convert Gwei to Wei.
+
+    See Also:
+        - :class:`Decimal`
+        - :class:`evmspec.data.Wei`
+    """
+
+    @property
+    def as_wei(self) -> Wei:
+        """
+        Converts the Gwei value to Wei.
+
+        This property multiplies the Gwei value by 10^9 to convert it to Wei.
+
+        Examples:
+            >>> Gwei('1').as_wei
+            Wei(1000000000)
+        """
+        return Wei(self * 10**9)
---
+++ packages/eth-portfolio/eth_portfolio-0.5.7-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/eth_portfolio_scripts/docker/check.py
@@ -0,0 +1,66 @@
+from functools import lru_cache
+from subprocess import CalledProcessError, check_output
+
+
+def check_docker() -> None:
+    """
+    Check that docker is installed on the user's system.
+
+    Raises:
+        RuntimeError: If docker is not installed.
+    """
+    print(" 🔍 checking your computer for docker")
+    try:
+        check_output(["docker", "--version"])
+    except (CalledProcessError, FileNotFoundError):
+        raise RuntimeError(
+            "Docker is not installed. You must install Docker before using dao-treasury."
+        ) from None
+    else:
+        print(" ✔️ eth-portfolio found docker!")
+
+
+def check_docker_compose() -> list[str]:
+    """
+    Check that either `docker-compose` or `docker compose` is installed on the user's system.
+
+    Returns:
+        A valid compose command.
+
+    Raises:
+        RuntimeError: If docker-compose is not installed.
+    """
+    for cmd in ["docker-compose", "docker compose"]:
+        print(f" 🔍 checking your computer for {cmd}")
+
+        try:
+            check_output([*cmd.split(" "), "--version"])
+        except (CalledProcessError, FileNotFoundError):
+            print(f" ❌ {cmd} not found")
+            continue
+        else:
+            print(f" ✔️ eth-portfolio found {cmd}!")
+            return cmd.split(" ")
+
+    raise RuntimeError(
+        "Docker Compose is not installed. You must install Docker Compose before using dao-treasury."
+    ) from None
+
+
+@lru_cache(maxsize=None)
+def check_system() -> list[str]:
+    """
+    Check that docker and docker-compose is installed on the user's system.
+
+    Returns:
+        A valid compose command.
+
+    Raises:
+        RuntimeError: If docker-compose is not installed.
+    """
+    print("eth-portfolio is checking for the required docker dependencies...")
+    check_docker()
+    return check_docker_compose()
+
+
+__all__ = ["check_docker", "check_docker_compose", "check_system"]
