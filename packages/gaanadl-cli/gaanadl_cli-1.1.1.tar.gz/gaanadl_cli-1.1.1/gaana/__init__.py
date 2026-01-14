fcb31cd21ab03a5bab42c05de86ff8345431f499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 19:06:38 +0000

    Add package gaanadl-cli/1.1.1/gaanadl_cli-1.1.1.tar.gz

---
+++ packages/gaanadl-cli/gaanadl_cli-1.1.1.tar.gz/gaanadl_cli-1.1.1/gaana/__init__.py
@@ -0,0 +1,22 @@
+"""
+gaana-cli: Download music from Gaana with metadata and synced lyrics.
+"""
+
+__version__ = "1.1.1"
+__author__ = "notdelta_xd"
+
+from .api import GaanaAPI
+from .downloader import HLSDownloader
+from .converter import AudioConverter
+from .metadata import MetadataHandler
+from .lyrics import LyricsProvider
+from .main import GaanaDownloader
+
+__all__ = [
+    "GaanaAPI",
+    "HLSDownloader",
+    "AudioConverter",
+    "MetadataHandler",
+    "LyricsProvider",
+    "GaanaDownloader",
+]
