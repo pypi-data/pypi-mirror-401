a4f00db7583c64160aad205f52b53d96f3407c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:08:01 +0000

    Add package pyggle/1.2.0/pyggle-1.2.0.tar.gz

---
+++ packages/pyggle/pyggle-1.2.0.tar.gz/pyggle-1.2.0/pyggle/functions.py
@@ -0,0 +1,20 @@
+from .boggle import Boggle
+
+# functions for pythonic function calls
+def solve(boggle: Boggle) -> dict[str, list[tuple[int]]]:
+    return boggle.solve()
+
+def time(boggle: Boggle) -> float:
+    return boggle.time_solve()
+
+def words(boggle: Boggle) -> list[str]:
+    return boggle.get_words()
+
+def coords(boggle: Boggle) -> list[tuple[int, int]]:
+    return boggle.get_coords()
+
+def score(boggle: Boggle) -> list[int]:
+    return boggle.get_score()
+
+def rank(boggle: Boggle, top: int = 10) -> list[tuple[str, int]]:
+    return boggle.get_rank(top)
---
+++ packages/pyggle/pyggle-1.2.0.tar.gz/pyggle-1.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os import path
+
+# python -m build
+# twine upload dist/*
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='pyggle',
+    version='1.2.0',
+    author='Andre Arcaina',
+    author_email='dtandre331@gmail.com',
+    description='Python-based package that allows you to solve any N x M Boggle board.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/andrearcaina/pyggle',
+    py_modules=['boggle'],
+    python_requires='>=3.6',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'pyggle': ['data/words_alpha.txt']}
+)
