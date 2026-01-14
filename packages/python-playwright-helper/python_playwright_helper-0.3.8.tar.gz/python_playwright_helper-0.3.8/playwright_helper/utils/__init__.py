fdc3597953b0e1b0243cbec7b572137da9a493c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:07:08 +0000

    Add package python-playwright-helper/0.3.8/python_playwright_helper-0.3.8.tar.gz

---
+++ packages/python-playwright-helper/python_playwright_helper-0.3.8.tar.gz/python_playwright_helper-0.3.8/playwright_helper/utils/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+# ---------------------------------------------------------------------------------------------------------
+# ProjectName:  playwright-helper
+# FileName:     __init__.py
+# Description:  工具包
+# Author:       ASUS
+# CreateDate:   2025/12/08
+# Copyright ©2011-2025. Hunan xxxxxxx Company limited. All rights reserved.
+# ---------------------------------------------------------------------------------------------------------
+"""
