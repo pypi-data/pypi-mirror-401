4f5223bd96200d1b902ebd4e288802325145852d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 19:06:15 +0000

    Add package PyHardLinkBackup/1.0.0rc1/pyhardlinkbackup-1.0.0rc1.tar.gz

---
+++ packages/PyHardLinkBackup/pyhardlinkbackup-1.0.0rc1.tar.gz/pyhardlinkbackup-1.0.0rc1/PyHardLinkBackup/__init__.py
@@ -0,0 +1,7 @@
+"""DocWrite: README.md # PyHardLinkBackup
+    HardLink/Deduplication Backups with Python
+"""
+
+# See https://packaging.python.org/en/latest/specifications/version-specifiers/
+__version__ = '1.0.0rc1'
+__author__ = 'Jens Diemer <PyHardLinkBackup@jensdiemer.de>'
