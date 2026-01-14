22584386b2a2193eae02955f79318152e09035ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 16:08:59 +0000

    Add package shopyo-dashboard/1.4.0/shopyo_dashboard-1.4.0-py3-none-any.whl

---
+++ packages/shopyo-dashboard/shopyo_dashboard-1.4.0-py3-none-any.whl/shopyo_dashboard/__init__.py
@@ -0,0 +1,34 @@
+from typing import Any
+import os
+import json
+from flask import Flask
+from shopyo_dashboard.view import module_blueprint
+
+__version__ = "1.4.0"
+
+info = {}
+with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + "info.json") as f:
+    info = json.load(f)
+
+default_config = {"SHOPYO_DASHBOARD_URL": "/shopyo-dashboard"}
+
+
+class ShopyoDashboard:
+    def __init__(self, app: Any = None) -> None:
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app: Flask) -> None:
+        if not hasattr(app, "extensions"):
+            app.extensions = {}
+
+        for key, value in default_config.items():
+            app.config.setdefault(key, value)
+
+        app.extensions["shopyo_dashboard"] = self
+        bp = module_blueprint
+        app.register_blueprint(bp, url_prefix=app.config["SHOPYO_DASHBOARD_URL"])
+        app.jinja_env.globals["shopyo_dashboard"] = self
+
+    def get_info(self):
+        return info
---
+++ packages/shopyo-dashboard/shopyo_dashboard-1.4.0-py3-none-any.whl/shopyo_dashboard/view.py
@@ -0,0 +1,79 @@
+import json
+import os
+
+from flask import current_app
+from flask import flash
+from flask import render_template
+from flask_login import login_required
+from shopyo_appadmin.admin import admin_required
+from shopyo_auth.decorators import check_confirmed
+
+from shopyo.api.html import notify_success
+from shopyo.api.module import ModuleHelp
+
+mhelp = ModuleHelp(__file__, __name__)
+globals()[mhelp.blueprint_str] = mhelp.blueprint
+module_blueprint = globals()[mhelp.blueprint_str]
+
+
+all_info = {}
+
+
+import importlib
+import pkgutil
+
+
+@module_blueprint.route("/")
+@login_required
+@check_confirmed
+@admin_required
+def index():
+    context = {}
+
+    # for plugins
+
+    for plugin in current_app.extensions:
+        if plugin.startswith("shopyo_"):
+            all_info[plugin] = current_app.extensions[plugin].get_info()
+
+    # for local folders
+    for folder in os.listdir(os.path.join(current_app.config["BASE_DIR"], "modules")):
+        if folder.startswith("__"):
+            continue
+        elif folder.startswith("box__"):
+            for sub_folder in os.listdir(
+                os.path.join(current_app.config["BASE_DIR"], "modules", folder)
+            ):
+                if sub_folder in ["dashboard"]:
+                    continue
+                if sub_folder.startswith("__"):  # ignore __pycache__
+                    continue
+                elif sub_folder.endswith(".json"):  # box_info.json
+                    continue
+                with open(
+                    os.path.join(
+                        current_app.config["BASE_DIR"],
+                        "modules",
+                        folder,
+                        sub_folder,
+                        "info.json",
+                    )
+                ) as f:
+                    module_info = json.load(f)
+                    all_info[sub_folder] = module_info
+        else:
+            if folder not in ["dashboard"]:
+                with open(
+                    os.path.join(
+                        current_app.config["BASE_DIR"],
+                        "modules",
+                        folder,
+                        "info.json",
+                    )
+                ) as f:
+                    module_info = json.load(f)
+                    all_info[folder] = module_info
+
+    context["all_info"] = all_info
+    # flash(notify_success("Notif test"))
+    return render_template("shopyo_dashboard/index.html", **context)
