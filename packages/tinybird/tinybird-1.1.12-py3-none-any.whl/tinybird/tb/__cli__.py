33d937a36c8b93d904617ac0ef1853fabd29de13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:06:34 +0000

    Add package tinybird/1.1.12/tinybird-1.1.12-py3-none-any.whl

---
+++ packages/tinybird/tinybird-1.1.12-py3-none-any.whl/tinybird/tb/__cli__.py
@@ -0,0 +1,8 @@
+
+__name__ = 'tinybird'
+__description__ = 'Tinybird Command Line Tool'
+__url__ = 'https://www.tinybird.co/docs/forward/commands'
+__author__ = 'Tinybird'
+__author_email__ = 'support@tinybird.co'
+__version__ = '1.1.12'
+__revision__ = '8437ff4'
