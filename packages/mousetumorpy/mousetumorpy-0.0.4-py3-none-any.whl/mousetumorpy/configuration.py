2f5b1925f309a62748ad43a8c196efbebe8089ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 18:06:01 +0000

    Add package mousetumorpy/0.0.4/mousetumorpy-0.0.4-py3-none-any.whl

---
+++ packages/mousetumorpy/mousetumorpy-0.0.4-py3-none-any.whl/mousetumorpy/configuration.py
@@ -0,0 +1,23 @@
+MIN_SIZE_PX = 150
+
+NNUNET_MODELS = {
+    "v42": (
+        "https://zenodo.org/records/10074441/files/nnunetv2_export_fullres_nov23.zip",
+        "e343b4808a3741b0b5752961b1225e88ab96519c22510bfcc0726c597213ed1e",
+    ),
+    "oct24": (
+        "https://zenodo.org/records/13927724/files/model-oct-2024.zip",
+        "md5:99a600884b3b3e4dd0302af045d06a62",
+    ),
+    "may25": (
+        "https://sandbox.zenodo.org/records/213786/files/may25.zip",
+        "md5:c74a1b96010042bccbdc968d0fafc767",
+    ),
+}
+
+YOLO_MODELS = {
+    "v1": (
+        "https://zenodo.org/records/13268683/files/yolo_seg_mouselungs.pt",
+        "md5:139471da545565d033748dc0d54a2392",
+    )
+}
