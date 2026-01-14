f4cd1615a39b8ab648b541c074b09e0f71674306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:07:06 +0000

    Add package mysql-server-ddz/0.1.0/mysql_server_ddz-0.1.0.tar.gz

---
+++ packages/mysql-server-ddz/mysql_server_ddz-0.1.0.tar.gz/mysql_server_ddz-0.1.0/src/mysql_server_ddz/__init__.py
@@ -0,0 +1,16 @@
+
+from asyncio import transports
+from mcp.server.fastmcp import FastMCP
+
+
+mcp=FastMCP("ddzDemo")
+
+@mcp.tool()
+def add(a:int,b:int)->int:
+    """
+    Add tow numbers
+    """
+    return a+b
+
+def main() -> None:
+    mcp.run(transports="stdio")
