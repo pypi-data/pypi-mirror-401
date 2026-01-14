826ce1de89ec000ca578595c84a5fdfa5abf4f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 18:05:31 +0000

    Add package waldiez-jupyter/0.7.1/waldiez_jupyter-0.7.1-py3-none-any.whl

---
+++ packages/waldiez-jupyter/waldiez_jupyter-0.7.1-py3-none-any.whl/waldiez_jupyter/__init__.py
@@ -0,0 +1,47 @@
+# SPDX-License-Identifier: Apache-2.0.
+# Copyright (c) 2024 - 2026 Waldiez and contributors.
+
+# type: ignore
+# pylint: disable=import-error
+# pyright: reportMissingImports=false,reportUnusedFunction=false
+# pyright: reportUnknownVariableType=false,reportAttributeAccessIssue=false
+
+"""Jupyter server extension for Waldiez."""
+
+from jupyter_server.serverapp import ServerApp
+
+try:
+    from ._version import __version__  # noqa
+except ImportError:  # pragma: no cover
+    # Fallback when using the package in dev mode without installing
+    # in editable mode with pip. It is highly recommended to install
+    # the package from a stable release or in editable mode:
+    # https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
+    import warnings
+
+    warnings.warn("Importing 'waldiez_jupyter' outside a proper installation.")
+    __version__ = "dev"
+
+
+from .handlers import setup_handlers
+
+
+def _jupyter_labextension_paths() -> list[dict[str, str]]:
+    return [{"src": "labextension", "dest": "@waldiez/jupyter"}]
+
+
+def _jupyter_server_extension_points() -> list[dict[str, str]]:
+    return [{"module": "waldiez_jupyter"}]
+
+
+def _load_jupyter_server_extension(server_app: ServerApp) -> None:
+    """Register the API handler to receive HTTP requests from the frontend.
+
+    Parameters
+    ----------
+    server_app: ServerApp
+        JupyterLab application instance
+    """
+    setup_handlers(server_app.web_app)
+    name = "@waldiez/jupyter"
+    server_app.log.info(f"Registered {name} server extension")
---
+++ packages/waldiez-jupyter/waldiez_jupyter-0.7.1-py3-none-any.whl/waldiez_jupyter/handlers/interrupt_handler.py
@@ -0,0 +1,49 @@
+# SPDX-License-Identifier: Apache-2.0.
+# Copyright (c) 2024 - 2026 Waldiez and contributors.
+"""Handle gathering state after interrupt."""
+
+import json
+from collections.abc import Awaitable
+
+from jupyter_server.base.handlers import APIHandler
+from tornado.web import authenticated
+from waldiez import WaldiezRunner
+
+
+class InterruptHandler(APIHandler):
+    """Interrupt handler to gather any results after interrupt."""
+
+    @authenticated
+    def data_received(self, chunk: bytes) -> Awaitable[None] | None:
+        """Just to make the linter happy.
+
+        avoid: Pylint W0223:abstract-method
+
+        Parameters
+        ----------
+        chunk : bytes
+            The chunk of data received.
+
+        Returns
+        -------
+        Awaitable[None] | None
+            The data handling coroutine.
+        """
+        return super().data_received(chunk)  # pragma: no cover
+
+    @authenticated
+    async def get(self) -> None:
+        """Handle a GET request.
+
+        Raises
+        ------
+        HTTPError
+            If the request data is invalid.
+        """
+        # pylint: disable=broad-exception-caught
+        try:
+            _, msg = WaldiezRunner.gather()
+            self.log.debug(msg)
+        except BaseException as err:  # pragma: no cover
+            self.log.error(err)
+        await self.finish(json.dumps({"status": "ok"}))
