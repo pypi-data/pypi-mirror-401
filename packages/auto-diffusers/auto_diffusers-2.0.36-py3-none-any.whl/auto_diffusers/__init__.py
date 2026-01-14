1245fbaa6d9b70a3569cc1c5578962b3c4cb4fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:07:53 +0000

    Add package auto-diffusers/2.0.36/auto_diffusers-2.0.36-py3-none-any.whl

---
+++ packages/auto-diffusers/auto_diffusers-2.0.36-py3-none-any.whl/auto_diffusers/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "2.0.36"
+
+from .pipeline_easy import (
+    search_huggingface,
+    search_civitai,
+    load_pipeline_from_single_file,
+    EasyPipelineForText2Image,
+    EasyPipelineForImage2Image,
+    EasyPipelineForInpainting,
+)
