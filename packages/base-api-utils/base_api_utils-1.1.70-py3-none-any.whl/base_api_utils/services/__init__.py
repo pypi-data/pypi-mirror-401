ba96428f7998db60f0fad1227bb065d98634c29e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 20:05:43 +0000

    Add package base-api-utils/1.1.70/base_api_utils-1.1.70-py3-none-any.whl

---
+++ packages/base-api-utils/base_api_utils-1.1.70-py3-none-any.whl/base_api_utils/services/__init__.py
@@ -0,0 +1,16 @@
+from .abstract_domain_events_service import AbstractDomainEventsService
+from .abstract_event_dispatcher import AbstractEventDispatcher
+from .abstract_images_service import AbstractImagesService
+from .abstract_sponsor_metadata_service import AbstractSponsorMetadataService
+from .abstract_storage_service import AbstractStorageService
+from .abstract_summit_metadata_service import AbstractSummitMetadataService
+from .amqp_service import AMQPService
+from .celery_failed import (
+    list_failed_tasks,
+    get_task,
+    retry_task,
+    get_exception_summary,
+)
+from .event_dispatcher import EventDispatcher
+from .images_service import ImagesService
+from .s3_service import S3Service
