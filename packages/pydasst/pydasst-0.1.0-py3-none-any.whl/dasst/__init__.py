6476828025ccd7f4ee6952183bea05e34612b387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 18:05:31 +0000

    Add package pydasst/0.1.0/pydasst-0.1.0-py3-none-any.whl

---
+++ packages/pydasst/pydasst-0.1.0-py3-none-any.whl/dasst/__init__.py
@@ -0,0 +1,13 @@
+'''This id doc for entire package
+
+'''
+from .version import __version__
+
+from . import ejection_models
+from . import similarity
+from . import orbit_determination
+from . import plot
+from . import propagators
+from . import sampling
+
+from . import time_utils
---
+++ packages/pydasst/pydasst-0.1.0-py3-none-any.whl/dasst/distributions/kernel_density_estimation.py
@@ -0,0 +1,12 @@
+def MISE_kernel_optimization(kde_dist):
+    #https://link.springer.com/article/10.1007/s10827-009-0180-4
+    #
+    #implement the functions described in the paper and use 
+    #scipy to minimize them
+    #
+    # kde_dist.bandwidth = result
+    return NotImplementedError('TODO')
+
+
+def kernel_density_estimation():
+    pass
