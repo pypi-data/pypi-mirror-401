c4ba7b2623dacb06e51fbf969dedfed1c44a2a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:07:16 +0000

    Add package llm-ir/0.0.14/llm_ir-0.0.14-py3-none-any.whl

---
+++ packages/llm-ir/llm_ir-0.0.14-py3-none-any.whl/llm_ir/__init__.py
@@ -0,0 +1,11 @@
+from .chunks import AIChunk, AIChunkText, AIChunkFile
+from .roles import AIRoles
+from .messages import AIMessage
+
+__all__ = [
+    "AIChunk",
+    "AIChunkText",
+    "AIChunkFile",
+    "AIRoles",
+    "AIMessage",
+]
