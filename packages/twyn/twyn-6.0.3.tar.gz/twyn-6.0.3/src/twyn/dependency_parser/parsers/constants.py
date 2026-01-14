a505ae465a8ea8daf323586d3b7dcb572620e47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 16:09:02 +0000

    Add package twyn/6.0.3/twyn-6.0.3.tar.gz

---
+++ packages/twyn/twyn-6.0.3.tar.gz/twyn-6.0.3/src/twyn/dependency_parser/parsers/constants.py
@@ -0,0 +1,17 @@
+UV_LOCK = "uv.lock"
+"""Filename for uv package manager lock files."""
+
+PACKAGE_LOCK_JSON = "package-lock.json"
+"""Filename for npm package lock files."""
+
+PNPM_LOCK_YAML = "pnpm-lock.yaml"
+"""Filename for pnpm lock files."""
+
+POETRY_LOCK = "poetry.lock"
+"""Filename for Poetry dependency lock files."""
+
+REQUIREMENTS_TXT = "requirements.txt"
+"""Filename for pip requirements files."""
+
+YARN_LOCK = "yarn.lock"
+"""Filename for Yarn package lock files."""
