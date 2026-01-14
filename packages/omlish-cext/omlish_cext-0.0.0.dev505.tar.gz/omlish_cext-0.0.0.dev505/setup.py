69d4a51d6b10ee91ef4a889a945f1c0023221093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:07:53 +0000

    Add package omlish-cext/0.0.0.dev505/omlish_cext-0.0.0.dev505.tar.gz

---
+++ packages/omlish-cext/omlish_cext-0.0.0.dev505.tar.gz/omlish_cext-0.0.0.dev505/setup.py
@@ -0,0 +1,27 @@
+import setuptools as st
+
+
+st.setup(
+    ext_modules=[
+        st.Extension(
+            name='omlish._check',
+            sources=['omlish/_check.cc'],
+            extra_compile_args=['-std=c++20'],
+        ),
+        st.Extension(
+            name='omlish.lang._asyncs',
+            sources=['omlish/lang/_asyncs.cc'],
+            extra_compile_args=['-std=c++20'],
+        ),
+        st.Extension(
+            name='omlish.lang.imports._capture',
+            sources=['omlish/lang/imports/_capture.cc'],
+            extra_compile_args=['-std=c++20'],
+        ),
+        st.Extension(
+            name='omlish.typedvalues._collection',
+            sources=['omlish/typedvalues/_collection.cc'],
+            extra_compile_args=['-std=c++20'],
+        ),
+    ],
+)
