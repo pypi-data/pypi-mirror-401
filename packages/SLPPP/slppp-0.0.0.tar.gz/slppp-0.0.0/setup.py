df7bdee29b73bd5285bfa5b33084dcabdd7b0464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:06:53 +0000

    Add package SLPPP/0.0.0/slppp-0.0.0.tar.gz

---
+++ packages/SLPPP/slppp-0.0.0.tar.gz/slppp-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = "SLPPP",
+    version = "0.0.0",
+    description = "",
+    author = "zisia13",
+    github = "zisia13",
+    author_email = "nothing@nothing.com",
+    packages = ["core"],
+    install_requires = [],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+    ],
+)
