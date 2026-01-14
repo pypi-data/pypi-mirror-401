0b7162e3b052fa89e6756c70e8aad2a028f4c3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 14:07:33 +0000

    Add package sphere-can-cli/0.1.0/sphere_can_cli-0.1.0.tar.gz

---
+++ packages/sphere-can-cli/sphere_can_cli-0.1.0.tar.gz/sphere_can_cli-0.1.0/sphere_can/main.py
@@ -0,0 +1,18 @@
+import typer
+from sphere_can.readcan import readcan
+from sphere_can.sendcan import sendcan
+from sphere_can.status import status
+from sphere_can.setup import setup
+
+app = typer.Typer(help="SPHERE CAN command-line interface")
+
+app.command()(readcan)
+app.command()(sendcan)
+app.command()(status)
+app.add_typer(setup, name="setup")
+
+def main():
+    app()
+
+if __name__ == "__main__":
+    main()
