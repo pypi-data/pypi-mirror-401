2736ffe55a2a5e65f448a5528eb8bd73df1235ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 16:07:26 +0000

    Add package grepxl/0.1.10/grepxl-0.1.10-py3-none-any.whl

---
+++ packages/grepxl/grepxl-0.1.10-py3-none-any.whl/grepxl.py
@@ -0,0 +1,60 @@
+import argparse
+from importlib.metadata import version
+import functools
+
+import pandas as pd
+from rich import print
+from rich_tools import df_to_table
+
+def main():
+    """
+    Display rows of a dataframe from an Excel file that match a given pattern.
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pattern', help='Regular expression pattern to search for')
+    parser.add_argument('excel', help='Excel file')
+    parser.add_argument('--columns', nargs='*')
+    parser.add_argument('--sort', nargs='*')
+    parser.add_argument('--tab', type=int, default=0)
+    parser.add_argument('-i', '--ignore-case', action='store_true', help='Ignore case distinctions')
+    parser.add_argument('--version', action='version', version=version('grepxl'))
+    args = parser.parse_args()
+
+    pattern = args.pattern
+    excel = args.excel
+
+    data = pd.read_excel(excel, sheet_name=args.tab, engine='calamine').dropna(axis='columns', how='all')
+
+    if args.ignore_case:
+        pattern = '(?i)' + pattern
+
+    search = grep(pattern, data)
+
+    if args.sort:
+        search = search.sort_values(by=args.sort)
+
+    if args.columns:
+        try:
+            search = search.loc[:, args.columns]
+        except KeyError as e:
+            print(f"[red]Error:[/red] {e}")
+            print(df_to_table(data.head(0), show_index=False))
+            return
+    print(df_to_table(search, show_index=False))
+
+
+def grep(pattern:str, data:pd.DataFrame) -> pd.DataFrame:
+    """
+    Filter rows in a DataFrame that contain the given pattern in any column.
+    """
+
+    mask = functools.reduce(
+        lambda x, y: x | y,
+        (data[col].astype(str).str.contains(pattern) for col in data.columns)
+    )
+    return data[mask]
+
+
+if __name__ == "__main__":
+    main()
