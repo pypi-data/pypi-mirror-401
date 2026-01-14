520f8418fbbac89d8b3c00e9c320cb781fe63ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:07:07 +0000

    Add package eRCaGuy-PyColors/0.1.3/ercaguy_pycolors-0.1.3-py3-none-any.whl

---
+++ packages/eRCaGuy-PyColors/ercaguy_pycolors-0.1.3-py3-none-any.whl/eRCaGuy_PyColors/__init__.py
@@ -0,0 +1,73 @@
+"""
+eRCaGuy_PyColors - A Python module for ANSI color and format codes in terminal output.
+
+By Gabriel Staples
+https://github.com/ElectricRCAircraftGuy/eRCaGuy_PyColors
+
+Example usage:
+    import eRCaGuy_PyColors as colors
+
+    print(f"{colors.FGR}This text is green.{colors.END}")
+    print(f"{colors.FBB}This text is bright blue.{colors.END}")
+
+    colors.print_green("This text is green.")
+    colors.print_blue("This text is bright blue.")
+    colors.print_red("This text is bright red.")
+    colors.print_yellow("This text is bright yellow.")
+"""
+
+from eRCaGuy_PyColors.ansi_colors import (
+    # ANSI codes and color constants
+    ANSI_START,
+    ANSI_FG_GRE,
+    ANSI_FG_BLU,
+    ANSI_FG_BR_BLU,
+    ANSI_FG_RED,
+    ANSI_FG_BR_RED,
+    ANSI_FG_BR_YEL,
+    ANSI_END,
+    ANSI_OFF,
+    F,
+    END,
+    FGN,
+    FGR,
+    FBL,
+    FBB,
+    FRE,
+    FBR,
+    FBY,
+    # Print functions
+    print_red,
+    print_yellow,
+    print_green,
+    print_blue,
+)
+
+__version__ = "0.1.3"
+__author__ = "Gabriel Staples"
+__all__ = [
+    # ANSI codes and color constants
+    "ANSI_START",
+    "ANSI_FG_GRE",
+    "ANSI_FG_BLU",
+    "ANSI_FG_BR_BLU",
+    "ANSI_FG_RED",
+    "ANSI_FG_BR_RED",
+    "ANSI_FG_BR_YEL",
+    "ANSI_END",
+    "ANSI_OFF",
+    "F",
+    "END",
+    "FGN",
+    "FGR",
+    "FBL",
+    "FBB",
+    "FRE",
+    "FBR",
+    "FBY",
+    # Print functions
+    "print_red",
+    "print_yellow",
+    "print_green",
+    "print_blue",
+]
---
+++ packages/eRCaGuy-PyColors/ercaguy_pycolors-0.1.3-py3-none-any.whl/eRCaGuy_PyColors/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Entry point for running eRCaGuy_PyColors as a module.
+Usage: python3 -m eRCaGuy_PyColors
+"""
+
+from eRCaGuy_PyColors.ansi_colors import run_tests
+
+if __name__ == "__main__":
+    run_tests()
