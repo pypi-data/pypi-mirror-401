b37f829ef3eb4861b3aa2b3b0f8650e2f4b2fdfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 16:08:04 +0000

    Add package pysail/0.4.6/pysail-0.4.6-cp38-abi3-manylinux_2_24_aarch64.whl

---
+++ packages/pysail/pysail-0.4.6-cp38-abi3-manylinux_2_24_aarch64.whl/pysail/tests/spark/test_dataframe.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import pytest
+from pandas.testing import assert_frame_equal
+from pyspark.sql import Row
+from pyspark.sql.functions import col, lit
+
+
+def test_dataframe_drop(spark):
+    df = spark.createDataFrame([(14, "Tom"), (23, "Alice"), (16, "Bob")], ["age", "name"])
+    df2 = spark.createDataFrame([Row(height=80, name="Tom"), Row(height=85, name="Bob")])
+
+    assert_frame_equal(
+        df.drop("age").sort("name").toPandas(),
+        pd.DataFrame({"name": ["Alice", "Bob", "Tom"]}),
+    )
+    assert_frame_equal(
+        df.drop(df.age).sort("name").toPandas(),
+        pd.DataFrame({"name": ["Alice", "Bob", "Tom"]}),
+    )
+
+    assert_frame_equal(
+        df.join(df2, df.name == df2.name, "inner").drop("name").sort("age").toPandas(),
+        pd.DataFrame({"age": [14, 16], "height": [80, 85]}),
+    )
+
+    df3 = df.join(df2)
+    assert_frame_equal(
+        df3.select(
+            df["age"],
+            df["name"].alias("name_left"),
+            df2["height"],
+            df2["name"].alias("name_right"),
+        )
+        .sort("name_left", "name_right")
+        .toPandas(),
+        pd.DataFrame(
+            {
+                "age": [23, 23, 16, 16, 14, 14],
+                "name_left": ["Alice", "Alice", "Bob", "Bob", "Tom", "Tom"],
+                "height": [85, 80, 85, 80, 85, 80],
+                "name_right": ["Bob", "Tom", "Bob", "Tom", "Bob", "Tom"],
+            }
+        ),
+    )
+
+    assert_frame_equal(
+        df3.drop("name").sort("age", "height").toPandas(),
+        pd.DataFrame({"age": [14, 14, 16, 16, 23, 23], "height": [80, 85, 80, 85, 80, 85]}),
+    )
+
+    with pytest.raises(Exception, match="AMBIGUOUS_REFERENCE"):
+        df3.drop(col("name")).toPandas()
+
+    df4 = df.withColumn("a.b.c", lit(1))
+    assert_frame_equal(
+        df4.sort("age").toPandas(),
+        pd.DataFrame({"age": [14, 16, 23], "name": ["Tom", "Bob", "Alice"], "a.b.c": [1, 1, 1]}).astype(
+            {"a.b.c": "int32"}
+        ),
+    )
+
+    assert_frame_equal(
+        df4.drop("a.b.c").sort("age").toPandas(),
+        pd.DataFrame({"age": [14, 16, 23], "name": ["Tom", "Bob", "Alice"]}),
+    )
+
+    assert_frame_equal(
+        df4.drop(col("a.b.c")).sort("age").toPandas(),
+        pd.DataFrame({"age": [14, 16, 23], "name": ["Tom", "Bob", "Alice"], "a.b.c": [1, 1, 1]}).astype(
+            {"a.b.c": "int32"}
+        ),
+    )
