6406a8c66850d5d246b2ed87e1616ac008ed3dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 18:05:13 +0000

    Add package CopBET/1.0.1/copbet-1.0.1-py3-none-any.whl

---
+++ packages/CopBET/copbet-1.0.1-py3-none-any.whl/CopBET/__init__.py
@@ -0,0 +1,14 @@
+'''
+CopBET
+
+A Python wrapper for the Copenhagen Brain Entropy Toolbox (CopBET), originally developed by Anders S. Olsen.
+'''
+
+from .CopBET import *
+
+__version__ = '1.0.0'
+__author__ = 'Viswanath Missula'
+__credits__ = ['Anders S. Olsen',
+               'Johns Hopkins University',
+               'Janice Chen',
+               'Brian Winston']
