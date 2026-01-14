3d2ddcb2cb3fd3257f72e4b270969e80a4a614c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:06:40 +0000

    Add package ab-client-user/1.1.0/ab_client_user-1.1.0-py3-none-any.whl

---
+++ packages/ab-client-user/ab_client_user-1.1.0-py3-none-any.whl/ab_client/user/clients/async_client.py
@@ -0,0 +1,158 @@
+from typing import Any, Dict, Optional, Union
+
+import httpx
+from pydantic import BaseModel, HttpUrl
+
+from ..exceptions import HTTPException
+from ..models import *
+
+
+class AsyncClient(BaseModel):
+    model_config = {"validate_assignment": True}
+
+    base_url: str = "/"
+    verify: Union[bool, str] = True
+    access_token: Optional[str] = None
+
+    def get_access_token(self) -> Optional[str]:
+        return self.access_token
+
+    def set_access_token(self, value: str) -> None:
+        self.access_token = value
+
+    async def get_user_by_id_user__user_id__get(
+        self,
+        user_id: str,
+    ) -> Any:
+        base_url = self.base_url
+        path = f"/user/{user_id}"
+
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": f"Bearer { self.get_access_token() }",
+        }
+
+        query_params: Dict[str, Any] = {}
+        query_params = {k: v for (k, v) in query_params.items() if v is not None}
+
+        async with httpx.AsyncClient(base_url=base_url, verify=self.verify) as client:
+            response = await client.request(
+                "get",
+                httpx.URL(path),
+                headers=headers,
+                params=query_params,
+            )
+
+        if response.status_code != 200:
+            raise HTTPException(
+                response.status_code,
+                f"get_user_by_id_user__user_id__get failed with status code: {response.status_code}",
+            )
+
+        body = None if 200 == 204 else response.json()
+        return body
+
+    async def get_user_by_oidc_user_oidc_get(
+        self,
+        oidc_sub: str,
+        oidc_iss: str,
+    ) -> Any:
+        base_url = self.base_url
+        path = f"/user/oidc"
+
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": f"Bearer { self.get_access_token() }",
+        }
+
+        query_params: Dict[str, Any] = {
+            "oidc_sub": oidc_sub,
+            "oidc_iss": oidc_iss,
+        }
+        query_params = {k: v for (k, v) in query_params.items() if v is not None}
+
+        async with httpx.AsyncClient(base_url=base_url, verify=self.verify) as client:
+            response = await client.request(
+                "get",
+                httpx.URL(path),
+                headers=headers,
+                params=query_params,
+            )
+
+        if response.status_code != 200:
+            raise HTTPException(
+                response.status_code,
+                f"get_user_by_oidc_user_oidc_get failed with status code: {response.status_code}",
+            )
+
+        body = None if 200 == 204 else response.json()
+        return body
+
+    async def upsert_user_by_oidc_user_oidc_put(
+        self,
+        data: UpsertByOIDCRequest,
+    ) -> Any:
+        base_url = self.base_url
+        path = f"/user/oidc"
+
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": f"Bearer { self.get_access_token() }",
+        }
+
+        query_params: Dict[str, Any] = {}
+        query_params = {k: v for (k, v) in query_params.items() if v is not None}
+
+        async with httpx.AsyncClient(base_url=base_url, verify=self.verify) as client:
+            response = await client.request(
+                "put",
+                httpx.URL(path),
+                headers=headers,
+                params=query_params,
+                json=data.dict(),
+            )
+
+        if response.status_code != 200:
+            raise HTTPException(
+                response.status_code,
+                f"upsert_user_by_oidc_user_oidc_put failed with status code: {response.status_code}",
+            )
+
+        body = None if 200 == 204 else response.json()
+        return body
+
+    async def seen_user_user__user_id__seen_post(
+        self,
+        user_id: str,
+    ) -> Any:
+        base_url = self.base_url
+        path = f"/user/{user_id}/seen"
+
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": f"Bearer { self.get_access_token() }",
+        }
+
+        query_params: Dict[str, Any] = {}
+        query_params = {k: v for (k, v) in query_params.items() if v is not None}
+
+        async with httpx.AsyncClient(base_url=base_url, verify=self.verify) as client:
+            response = await client.request(
+                "post",
+                httpx.URL(path),
+                headers=headers,
+                params=query_params,
+            )
+
+        if response.status_code != 200:
+            raise HTTPException(
+                response.status_code,
+                f"seen_user_user__user_id__seen_post failed with status code: {response.status_code}",
+            )
+
+        body = None if 200 == 204 else response.json()
+        return body
