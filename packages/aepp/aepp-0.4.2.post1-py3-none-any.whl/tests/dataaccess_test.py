15f056d3239f2c36ad3e1d070ced7bd69bf7cba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 17:11:26 +0000

    Add package aepp/0.4.2.post1/aepp-0.4.2.post1-py3-none-any.whl

---
+++ packages/aepp/aepp-0.4.2.post1-py3-none-any.whl/tests/dataaccess_test.py
@@ -0,0 +1,36 @@
+#  Copyright 2023 Adobe. All rights reserved.
+#  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License. You may obtain a copy
+#  of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software distributed under
+#  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+#  OF ANY KIND, either express or implied. See the License for the specific language
+#  governing permissions and limitations under the License.
+
+from aepp.schema import Schema
+import unittest
+from unittest.mock import patch, MagicMock
+
+class DataAccessTest(unittest.TestCase):
+
+    def test_dataaccess_get_batch_files(self):
+        assert True
+
+    def test_dataaccess_get_batch_failed(self):
+        assert True
+
+    def test_dataaccess_get_batch_meta(self):
+        assert True
+
+    def test_dataaccess_getHeadFile(self):
+        assert True
+
+    def test_dataaccess_get_files(self):
+        assert True
+
+    def test_dataaccess_get_preview(self):
+        assert True
+
+    def test_dataaccess_get_resource(self):
+        assert True
