b4ab226960759c2bcea8145bd7f41b361c77feb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 15:06:55 +0000

    Add package ab-client-token-validator/1.0.0/ab_client_token_validator-1.0.0-py3-none-any.whl

---
+++ packages/ab-client-token-validator/ab_client_token_validator-1.0.0-py3-none-any.whl/ab_client/token_validator/services/Token_Validator_service.py
@@ -0,0 +1,43 @@
+from typing import *
+
+import httpx
+
+from ..api_config import APIConfig, HTTPException
+from ..models import *
+
+
+def validate_token_validate_post(
+    api_config_override: Optional[APIConfig] = None, *, data: ValidateTokenRequest
+) -> ValidatedOIDCClaims:
+    api_config = api_config_override if api_config_override else APIConfig()
+
+    base_path = api_config.base_path
+    path = f"/validate"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer { api_config.get_access_token() }",
+    }
+
+    query_params: Dict[str, Any] = {}
+    query_params = {key: value for (key, value) in query_params.items() if value is not None}
+
+    with httpx.Client(base_url=base_path, verify=api_config.verify) as client:
+        response = client.request(
+            "post",
+            httpx.URL(path),
+            headers=headers,
+            params=query_params,
+            json=data.dict(),
+        )
+
+    if response.status_code != 200:
+        raise HTTPException(
+            response.status_code,
+            f"validate_token_validate_post failed with status code: {response.status_code}",
+        )
+
+    body = None if 200 == 204 else response.json()
+
+    return ValidatedOIDCClaims(**body) if body is not None else ValidatedOIDCClaims()
