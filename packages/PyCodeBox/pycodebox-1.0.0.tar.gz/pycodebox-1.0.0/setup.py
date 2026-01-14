cddcdb96e5454e0a48dbda8b7be23eebf4e13fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 19:07:35 +0000

    Add package PyCodeBox/1.0.0/pycodebox-1.0.0.tar.gz

---
+++ packages/PyCodeBox/pycodebox-1.0.0.tar.gz/pycodebox-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyCodeBox',
+    version='1.0.0',
+    author='Zachary D Wallen',
+    description="""A personal collection of convenience functions for styling data 
+    displays and outputs, transforming and organizing data, performing statistical 
+    analyses, and other commonly performed actions in data science projects.""",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'adjustText',
+        'IPython',
+        'matplotlib',
+        'numpy',
+        'openpyxl',
+        'pandas',
+        'scipy',
+        'seaborn',
+        'scikit-learn',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
