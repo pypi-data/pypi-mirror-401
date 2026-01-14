33558f7f61759b7ee685f9d973170b6ab6abb8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 18:05:41 +0000

    Add package vease-modeling-back/1.0.0rc2/vease_modeling_back-1.0.0rc2.tar.gz

---
+++ packages/vease-modeling-back/vease_modeling_back-1.0.0rc2.tar.gz/vease_modeling_back-1.0.0rc2/src/vease_modeling_back/routes/schemas/voi.py
@@ -0,0 +1,24 @@
+from dataclasses_json import DataClassJsonMixin
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Voi(DataClassJsonMixin):
+    def __post_init__(self) -> None:
+        print(self, flush=True)
+
+    aoi_id: str
+    """ID of the corresponding AOI"""
+
+    name: str
+    """Name of the VOI"""
+
+    z_max: float
+    """Maximum Z coordinate"""
+
+    z_min: float
+    """Minimum Z coordinate"""
+
+    id: Optional[str] = None
+    """Optional ID for updating existing VOI"""
