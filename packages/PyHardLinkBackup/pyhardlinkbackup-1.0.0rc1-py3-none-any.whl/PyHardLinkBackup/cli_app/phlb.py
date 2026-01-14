b390135c1a743c39fdc59c04b3c61ab2bb859830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 19:06:15 +0000

    Add package PyHardLinkBackup/1.0.0rc1/pyhardlinkbackup-1.0.0rc1-py3-none-any.whl

---
+++ packages/PyHardLinkBackup/pyhardlinkbackup-1.0.0rc1-py3-none-any.whl/PyHardLinkBackup/cli_app/phlb.py
@@ -0,0 +1,50 @@
+import logging
+from pathlib import Path
+from typing import Annotated
+
+import tyro
+from cli_base.cli_tools.verbosity import setup_logging
+from cli_base.tyro_commands import TyroVerbosityArgType
+from rich import print  # noqa
+
+from PyHardLinkBackup.backup import backup_tree
+from PyHardLinkBackup.cli_app import app
+
+
+logger = logging.getLogger(__name__)
+
+
+@app.command
+def backup(
+    src: Annotated[
+        Path,
+        tyro.conf.arg(
+            metavar='source',
+            help='Source directory to back up.',
+        ),
+    ],
+    dst: Annotated[
+        Path,
+        tyro.conf.arg(
+            metavar='destination',
+            help='Destination directory for the backup.',
+        ),
+    ],
+    /,
+    excludes: Annotated[
+        tuple[str],
+        tyro.conf.arg(
+            help='List of directory or file names to exclude from backup.',
+        ),
+    ] = ('__pycache__', '.cache', '.temp', '.tmp', '.tox', '.nox'),
+    verbosity: TyroVerbosityArgType = 2,
+) -> None:
+    """
+    Backup the source directory to the destination directory using hard links for deduplication.
+    """
+    setup_logging(verbosity=verbosity)
+    backup_tree(
+        src_root=src,
+        backup_root=dst,
+        excludes=set(excludes),
+    )
