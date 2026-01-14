7b8fe6289b2cacd4bbb6093056eb1c946deedad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-14 18:06:04 +0000

    Add package anthropic-haystack/5.1.0/anthropic_haystack-5.1.0-py3-none-any.whl

---
+++ packages/anthropic-haystack/anthropic_haystack-5.1.0-py3-none-any.whl/haystack_integrations/components/generators/anthropic/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+from .chat.chat_generator import AnthropicChatGenerator
+from .chat.vertex_chat_generator import AnthropicVertexChatGenerator
+from .generator import AnthropicGenerator
+
+__all__ = ["AnthropicChatGenerator", "AnthropicGenerator", "AnthropicVertexChatGenerator"]
