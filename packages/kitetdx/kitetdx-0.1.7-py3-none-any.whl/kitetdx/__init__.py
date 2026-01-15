b571e5a8fdc70ba4f1dcc4ae57a28b6ebe9b84ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 15:07:40 +0000

    Add package kitetdx/0.1.7/kitetdx-0.1.7-py3-none-any.whl

---
+++ packages/kitetdx/kitetdx-0.1.7-py3-none-any.whl/kitetdx/__init__.py
@@ -0,0 +1,6 @@
+from .quotes import Quotes
+from .reader import Reader
+from .affair import Affair
+from .adjust import adjust_price, to_adjust, fetch_fq_factor
+
+__all__ = ['Quotes', 'Reader', 'Affair', 'adjust_price', 'to_adjust', 'fetch_fq_factor']
