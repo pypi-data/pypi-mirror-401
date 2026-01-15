5af128fdd96f225bf37cea1dd08795fc846560ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:15:02 +0000

    Add package auto-typing-final/1.0.3/auto_typing_final-1.0.3-py3-none-any.whl

---
+++ packages/auto-typing-final/auto_typing_final-1.0.3-py3-none-any.whl/auto_typing_final/main.py
@@ -0,0 +1,96 @@
+import argparse
+import sys
+from collections.abc import Iterable
+from difflib import unified_diff
+from pathlib import Path
+from typing import Final, cast, get_args
+
+from ast_grep_py import SgRoot
+
+from auto_typing_final.transform import IMPORT_STYLES_TO_IMPORT_CONFIGS, ImportConfig, ImportStyle, make_replacements
+
+
+def transform_file_content(source: str, import_config: ImportConfig, ignore_global_vars: bool) -> str:
+    root: Final = SgRoot(source, "python").root()
+    result: Final = make_replacements(root, import_config, ignore_global_vars)
+    new_text: Final = root.commit_edits(
+        [edit.node.replace(edit.new_text) for replacement in result.replacements for edit in replacement.edits]
+    )
+    return root.commit_edits([root.replace(f"{result.import_text}\n{new_text}")]) if result.import_text else new_text
+
+
+def take_python_source_files(paths: Iterable[Path]) -> Iterable[Path]:
+    for path in paths:
+        if path.suffix in {".py", ".pyi"}:
+            yield path
+
+
+def find_source_files_from_one_path(path: Path) -> Iterable[Path]:
+    if path.is_dir():
+        for inner_path in path.iterdir():
+            if inner_path.name.startswith("."):
+                continue
+            yield from take_python_source_files(find_source_files_from_one_path(inner_path))
+
+    else:
+        yield path
+
+
+def find_all_source_files(paths: list[Path]) -> Iterable[Path]:
+    for path in paths:
+        yield from find_source_files_from_one_path(path)
+
+
+def main() -> int:
+    parser: Final = argparse.ArgumentParser()
+    parser.add_argument("files", type=Path, nargs="*", default=[Path()])
+    parser.add_argument("--check", action="store_true")
+    parser.add_argument("--import-style", type=str, choices=get_args(ImportStyle), default="typing-final")
+    parser.add_argument(
+        "--ignore-global-vars", action="store_true", help="Ignore global variables when applying Final annotations"
+    )
+
+    args: Final = parser.parse_args()
+    import_config: Final = IMPORT_STYLES_TO_IMPORT_CONFIGS[args.import_style]
+    open_mode: Final = "r" if args.check else "r+"
+    changed_files_count = 0
+
+    for path in find_all_source_files(args.files):
+        with path.open(open_mode) as file:
+            source = file.read()
+            transformed_content = transform_file_content(
+                source=source, import_config=import_config, ignore_global_vars=args.ignore_global_vars
+            )
+            if source == transformed_content:
+                continue
+            changed_files_count += 1
+
+            if args.check:
+                sys.stdout.writelines(
+                    unified_diff(
+                        source.splitlines(keepends=True),
+                        transformed_content.splitlines(keepends=True),
+                        fromfile=str(path),
+                        tofile=str(path),
+                    )
+                )
+                sys.stdout.write("\n")
+            else:
+                file.seek(0)
+                file.write(transformed_content)
+                file.truncate()
+
+    match changed_files_count, cast("bool", args.check):
+        case 0, _:
+            result_message = "No errors found!"
+        case 1, True:
+            result_message = "Found errors in 1 file."
+        case 1, False:
+            result_message = "Fixed errors in 1 file."
+        case _, True:
+            result_message = f"Found errors in {changed_files_count} files."
+        case _, False:
+            result_message = f"Fixed errors in {changed_files_count} files."
+
+    sys.stdout.write(f"{result_message}\n")
+    return changed_files_count > 0 if args.check else 0
---
+++ packages/auto-typing-final/auto_typing_final-1.0.3-py3-none-any.whl/auto_typing_final/transform.py
@@ -0,0 +1,272 @@
+import typing
+from collections.abc import Iterable
+from dataclasses import dataclass
+from typing import Final, Literal
+
+from ast_grep_py import SgNode
+
+from auto_typing_final.finder import (
+    ImportsResult,
+    find_all_definitions_in_functions,
+    find_global_definitions,
+    find_imports_of_identifier_in_scope,
+    has_global_identifier_with_name,
+)
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class ImportConfig:
+    value: str
+    import_text: str
+    import_identifier: str
+
+
+ImportStyle = Literal["typing-final", "final"]
+IMPORT_STYLES_TO_IMPORT_CONFIGS: Final[dict[ImportStyle, ImportConfig]] = {
+    "typing-final": ImportConfig(value="typing.Final", import_text="import typing", import_identifier="typing"),
+    "final": ImportConfig(value="Final", import_text="from typing import Final", import_identifier="Final"),
+}
+IGNORED_DEFINITION_PATTERNS: typing.Final = {"TypeVar", "ParamSpec"}
+
+
+@dataclass(frozen=True, slots=True)
+class EditableAssignmentWithoutAnnotation:
+    node: SgNode
+    left: str
+    right: str
+
+
+@dataclass(frozen=True, slots=True)
+class EditableAssignmentWithAnnotation:
+    node: SgNode
+    left: str
+    annotation: SgNode
+    right: str
+
+
+@dataclass(frozen=True, slots=True)
+class OtherDefinition:
+    node: SgNode
+
+
+Definition = EditableAssignmentWithoutAnnotation | EditableAssignmentWithAnnotation | OtherDefinition
+
+
+@dataclass(frozen=True, slots=True)
+class AddFinal:
+    node: Definition
+
+
+@dataclass(frozen=True, slots=True)
+class RemoveFinal:
+    nodes: list[Definition]
+
+
+Operation = AddFinal | RemoveFinal
+
+
+def _make_definition_from_definition_node(node: SgNode) -> Definition:
+    if node.kind() != "assignment":
+        return OtherDefinition(node)
+
+    match tuple((child.kind(), child) for child in node.children()):
+        case (
+            ("identifier", left),
+            ("=", _),
+            (right_kind, right),
+        ) if right_kind != "assignment" and not ((parent := node.parent()) and parent.kind() == "assignment"):
+            return EditableAssignmentWithoutAnnotation(node=node, left=left.text(), right=right.text())
+        case (
+            ("identifier", left),
+            (":", _),
+            ("type", annotation),
+            ("=", _),
+            (right_kind, right),
+        ) if right_kind != "assignment" and not ((parent := node.parent()) and parent.kind() == "assignment"):
+            return EditableAssignmentWithAnnotation(
+                node=node, left=left.text(), annotation=annotation, right=right.text()
+            )
+        case _:
+            return OtherDefinition(node)
+
+
+def _should_skip_global_variable(definition: Definition) -> bool:
+    return isinstance(definition, EditableAssignmentWithoutAnnotation | EditableAssignmentWithAnnotation) and (
+        not (definition.left.isupper() and len(definition.left) > 1)
+        or any(one_pattern in definition.right for one_pattern in IGNORED_DEFINITION_PATTERNS)
+    )
+
+
+def _make_operation_from_definitions_of_one_name(nodes: list[SgNode], ignore_global_vars: bool) -> Operation | None:
+    value_definitions: Final[list[Definition]] = []
+    has_node_inside_loop = False
+    has_global_scope_definition = False
+
+    for node in nodes:
+        if any(ancestor.kind() in {"for_statement", "while_statement"} for ancestor in node.ancestors()):
+            has_node_inside_loop = True
+
+        if all(ancestor.kind() != "function_definition" for ancestor in node.ancestors()):
+            has_global_scope_definition = True
+
+        value_definitions.append(_make_definition_from_definition_node(node))
+
+    if has_node_inside_loop:
+        return RemoveFinal(value_definitions)
+
+    if (
+        not ignore_global_vars
+        and has_global_scope_definition
+        and value_definitions
+        and _should_skip_global_variable(value_definitions[0])
+    ):
+        return None
+
+    match value_definitions:
+        case [definition]:
+            return AddFinal(definition)
+        case definitions:
+            return RemoveFinal(definitions)
+
+
+def _match_exact_identifier(node: SgNode, imports_result: ImportsResult, identifier_name: str) -> str | None:
+    match tuple((inner_child.kind(), inner_child) for inner_child in node.children()):
+        case (("identifier", first_identifier), (".", _), ("identifier", second_identifier)):
+            for alias in imports_result.module_aliases:
+                if alias == first_identifier.text() and second_identifier.text() == identifier_name:
+                    return node.text()
+    return None
+
+
+def _strip_value_from_type_annotation_that_is_indeed_inside_given_identifier(
+    node: SgNode, imports_result: ImportsResult, identifier_name: str
+) -> str | None:
+    type_node_children: Final = node.children()
+    if len(type_node_children) != 1:
+        return None
+    inner_type_node: Final = type_node_children[0]
+    kind: Final = inner_type_node.kind()
+
+    if kind == "subscript":
+        match tuple((child.kind(), child) for child in inner_type_node.children()):
+            case (("attribute", attribute), ("[", _), *kinds_and_nodes, ("]", _)):
+                if _match_exact_identifier(attribute, imports_result, identifier_name):
+                    return "".join(node.text() for _, node in kinds_and_nodes)
+    elif kind == "generic_type" and imports_result.has_from_import:
+        match tuple((child.kind(), child) for child in inner_type_node.children()):
+            case (("identifier", identifier), ("type_parameter", type_parameter)):
+                if identifier.text() != identifier_name:
+                    return None
+                match tuple((inner_child.kind(), inner_child) for inner_child in type_parameter.children()):
+                    case (("[", _), *kinds_and_nodes, ("]", _)):
+                        return "".join(node.text() for _, node in kinds_and_nodes)
+    elif (kind == "identifier" and inner_type_node.text() == identifier_name) or (
+        kind == "attribute" and _match_exact_identifier(inner_type_node, imports_result, identifier_name)
+    ):
+        return ""
+    return None
+
+
+def _make_changed_text_from_operation(
+    operation: Operation, final_value: str, imports_result: ImportsResult, identifier_name: str
+) -> Iterable[tuple[SgNode, str]]:
+    match operation:
+        case AddFinal(assignment):
+            match assignment:
+                case EditableAssignmentWithoutAnnotation(node, left, right):
+                    yield node, f"{left}: {final_value} = {right}"
+                case EditableAssignmentWithAnnotation(node, left, annotation, right) if (
+                    _strip_value_from_type_annotation_that_is_indeed_inside_given_identifier(
+                        annotation, imports_result, identifier_name
+                    )
+                    is None
+                ):
+                    yield node, f"{left}: {final_value}[{annotation.text()}] = {right}"
+        case RemoveFinal(assignments):
+            for assignment in assignments:
+                match assignment:
+                    case EditableAssignmentWithoutAnnotation(node, left, right):
+                        yield node, node.text()
+                    case EditableAssignmentWithAnnotation(node, left, annotation, right):
+                        match _strip_value_from_type_annotation_that_is_indeed_inside_given_identifier(
+                            annotation, imports_result, identifier_name
+                        ):
+                            case "":
+                                yield node, f"{left} = {right}"
+                            case str(new_annotation):
+                                yield node, f"{left}: {new_annotation} = {right}"
+
+
+@dataclass(frozen=True, slots=True)
+class Edit:
+    node: SgNode
+    new_text: str
+
+
+@dataclass(frozen=True, slots=True)
+class Replacement:
+    operation_type: type[Operation]
+    edits: list[Edit]
+
+
+@dataclass(frozen=True, slots=True)
+class MakeReplacementsResult:
+    replacements: list[Replacement]
+    import_text: str | None
+
+
+def make_replacements(root: SgNode, import_config: ImportConfig, ignore_global_vars: bool) -> MakeReplacementsResult:
+    replacements: Final = []
+    has_added_final = False
+    imports_result: Final = find_imports_of_identifier_in_scope(root, module_name="typing", identifier_name="Final")
+
+    for current_definitions in find_all_definitions_in_functions(root):
+        if not (operation := _make_operation_from_definitions_of_one_name(current_definitions, ignore_global_vars)):
+            continue
+        edits = [
+            Edit(node=node, new_text=new_text)
+            for node, new_text in _make_changed_text_from_operation(
+                operation=operation,
+                final_value=import_config.value,
+                imports_result=imports_result,
+                identifier_name="Final",
+            )
+            if node.text() != new_text
+        ]
+
+        if (operation_type := type(operation)) == AddFinal and edits:
+            has_added_final = True
+
+        replacements.append(Replacement(operation_type=operation_type, edits=edits))
+
+    if not ignore_global_vars:
+        for current_definitions in find_global_definitions(root):
+            if (
+                not (operation := _make_operation_from_definitions_of_one_name(current_definitions, ignore_global_vars))
+                or (operation_type := type(operation)) == RemoveFinal
+            ):
+                continue
+
+            edits = [
+                Edit(node=node, new_text=new_text)
+                for node, new_text in _make_changed_text_from_operation(
+                    operation=operation,
+                    final_value=import_config.value,
+                    imports_result=imports_result,
+                    identifier_name="Final",
+                )
+                if node.text() != new_text
+            ]
+            if not edits:
+                continue
+            has_added_final = True
+            replacements.append(Replacement(operation_type=operation_type, edits=edits))
+
+    return MakeReplacementsResult(
+        replacements=replacements,
+        import_text=(
+            import_config.import_text
+            if has_added_final and not has_global_identifier_with_name(root=root, name=import_config.import_identifier)
+            else None
+        ),
+    )
