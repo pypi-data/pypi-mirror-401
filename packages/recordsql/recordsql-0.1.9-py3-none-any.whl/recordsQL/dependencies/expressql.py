2f8cbac339a7fa4815753fa7e4221bd59c3be1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:14:46 +0000

    Add package recordsql/0.1.9/recordsql-0.1.9-py3-none-any.whl

---
+++ packages/recordsql/recordsql-0.1.9-py3-none-any.whl/recordsQL/dependencies/expressql.py
@@ -0,0 +1,13 @@
+from expressql import (
+    SQLCondition,
+    no_condition,
+    SQLExpression,
+    ensure_sql_expression,
+    FalseCondition,
+    cols,
+    col,
+    num,
+    text,
+    set_expr,
+    Func,
+)
