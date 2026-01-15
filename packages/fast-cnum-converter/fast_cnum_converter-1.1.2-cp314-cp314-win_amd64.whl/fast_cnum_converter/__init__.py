fe252035711c8f2da86490dff8a3e00667421512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 15:07:40 +0000

    Add package fast-cnum-converter/1.1.2/fast_cnum_converter-1.1.2-cp314-cp314-win_amd64.whl

---
+++ packages/fast-cnum-converter/fast_cnum_converter-1.1.2-cp314-cp314-win_amd64.whl/fast_cnum_converter/__init__.py
@@ -0,0 +1,12 @@
+from . import exceptions
+from ._internal import (
+    convert_cnum_to_alpha,
+    convert_cnum_to_numeric,
+)
+
+
+__all__ = [
+    "convert_cnum_to_alpha",
+    "convert_cnum_to_numeric",
+    "exceptions",
+]
