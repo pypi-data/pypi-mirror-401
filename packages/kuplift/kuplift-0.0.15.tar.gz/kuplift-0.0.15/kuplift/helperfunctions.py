61ed04d005b6afea4ac45818ac9fb8363be5623d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 15:07:45 +0000

    Add package kuplift/0.0.15/kuplift-0.0.15.tar.gz

---
+++ packages/kuplift/kuplift-0.0.15.tar.gz/kuplift-0.0.15/kuplift/helperfunctions.py
@@ -0,0 +1,207 @@
+# Copyright (c) 2026 Orange. All rights reserved.
+# This software is distributed under the MIT License, the text of which is available
+# at https://spdx.org/licenses/MIT.html or see the "LICENSE" file for more details.
+
+from math import log
+
+_log_fact_table = []
+
+
+def log_fact(n):
+    """Compute log(fact(n)).
+
+    Parameters
+    ----------
+    n : int
+        Number on which to do the calculation.
+
+    Returns
+    -------
+    float
+        Value of log(fact(n)).
+    """
+    # Use approximation for large n
+    if n > 1e6:
+        raise ValueError("Value of n is too large")
+    # computation of values, tabulation in private array
+    else:
+        s = len(_log_fact_table)
+        if n >= s:
+            if s == 0:
+                _log_fact_table.append(0)
+            size = len(_log_fact_table)
+            while size <= n:
+                _log_fact_table.append(log(size) + _log_fact_table[size - 1])
+                size = size + 1
+        return _log_fact_table[n]
+
+
+def log_2_star(k):
+    """Computes the term log_2*(k)=log_2(k) + log_2(log_2(k)) + ...
+    of Rissanen's code for integers so long as the terms are positive.
+
+    Parameters
+    ----------
+    k : int
+        Number on which to do the calculation.
+
+    Returns
+    -------
+    float
+        Value of the result.
+    """
+    d_log2 = log(2.0)
+    d_cost = 0.0
+    d_logI = log(1.0 * k) / d_log2
+
+    if k < 1:
+        raise ValueError(
+            "Universal code is defined for natural numbers over 1"
+        )
+    else:
+        while d_logI > 0:
+            d_cost += d_logI
+            d_logI = log(d_logI) / d_log2
+        return d_cost
+
+
+def log_binomial_coefficient(n, k):
+    """Computes the log of the binomial coefficient (n
+                                                     k)
+    (log of the total number of combinations of k elements from n).
+
+    Parameters
+    ----------
+    n : int
+        Total number of elements.
+    k : int
+        Number of selected elements.
+
+    Returns
+    -------
+    float
+        Value of the result.
+    """
+    # start_counter(5)
+    nf = log_fact(n)
+    kf = log_fact(k)
+    nkf = log_fact(n - k)
+    # stop_counter(5)
+    return (nf - nkf) - kf
+
+
+def universal_code_natural_numbers(k):
+    """Compute the universal code for integers presented by Rissanen in
+    'A Universal Prior for Integers and Estimation by Minimum Description
+    Length', Rissanen 1983.
+
+    Parameters
+    ----------
+    k : int
+
+    Returns
+    -------
+    float
+        Value of the result.
+    """
+    dC0 = 2.86511  # First value computed following the given estimation
+    # formula, as e(3)=65536 + d_log2^5 / (1-d_log2)
+    d_log2 = log(2.0)
+
+    if k < 1:
+        raise ValueError(
+            "Universal code is defined for natural numbers over 1"
+        )
+    else:
+        d_cost = log(dC0) / d_log2  # Initialize code length cost to log_2(dC0)
+        d_cost += log_2_star(k)  # Add log_2*(k)
+        d_cost *= d_log2  # Go back to the natural log
+        return d_cost
+
+
+def preprocess_data(data, treatment_col="segment", y_col="visit"):
+    """preprocess data
+
+    Parameters
+    ----------
+    data : pd.Dataframe
+        Dataframe containing feature variables.
+    treatment_col : pd.Series, default "segment"
+        Treatment column.
+    y_col : pd.Series, default "visit"
+        Outcome column.
+
+    Returns
+    -------
+    pd.Dataframe
+        Pandas Dataframe that contains encoded data.
+    """
+    cols = data.columns
+    num_cols = list(data._get_numeric_data().columns)
+
+    if treatment_col in num_cols:
+        num_cols.remove(treatment_col)
+    if y_col in num_cols:
+        num_cols.remove(y_col)
+
+    num_col_index = 0
+    while num_col_index < len(num_cols):
+        num_col = num_cols[num_col_index]
+        if len(data[num_col].value_counts()) < 1000:
+            num_cols.remove(num_col)
+        else:
+            data[num_col] = data[num_col].fillna(data[num_col].min() - 1)
+            num_col_index = num_col_index + 1
+
+    categorical_cols = list(set(cols) - set(num_cols))
+    if treatment_col in categorical_cols:
+        categorical_cols.remove(treatment_col)
+    if y_col in categorical_cols:
+        categorical_cols.remove(y_col)
+    for cat_col in categorical_cols:
+        data[cat_col] = data[cat_col].fillna("NAN_VAL")
+        dict_val_vs_uplift = {}
+        for val in data[cat_col].value_counts().index:
+            if val == "NAN_VAL":
+                continue
+            dataset_slice = data[data[cat_col] == val]
+            t0j0 = dataset_slice[
+                (dataset_slice[treatment_col] == 0)
+                & (dataset_slice[y_col] == 0)
+            ].shape[0]
+            t0j1 = dataset_slice[
+                (dataset_slice[treatment_col] == 0)
+                & (dataset_slice[y_col] == 1)
+            ].shape[0]
+            t1j0 = dataset_slice[
+                (dataset_slice[treatment_col] == 1)
+                & (dataset_slice[y_col] == 0)
+            ].shape[0]
+            t1j1 = dataset_slice[
+                (dataset_slice[treatment_col] == 1)
+                & (dataset_slice[y_col] == 1)
+            ].shape[0]
+
+            if (t1j1 + t1j0) == 0:
+                uplift_in_this_slice = -1
+            elif (t0j1 + t0j1) == 0:
+                uplift_in_this_slice = 0
+            else:
+                uplift_in_this_slice = (t1j1 / (t1j1 + t1j0)) - (
+                    t0j1 / (t0j1 + t0j1)
+                )
+            dict_val_vs_uplift[val] = uplift_in_this_slice
+        ordered_dict = {
+            k: v
+            for k, v in sorted(
+                dict_val_vs_uplift.items(), key=lambda item: item[1]
+            )
+        }
+
+        data[cat_col] = data[cat_col].replace(["NAN_VAL"], -1)
+        encoded_i = 0
+        for k, v in ordered_dict.items():
+            data[cat_col] = data[cat_col].replace([k], encoded_i)
+            encoded_i += 1
+    data[treatment_col] = data[treatment_col].astype(str)
+    return data
---
+++ packages/kuplift/kuplift-0.0.15.tar.gz/kuplift-0.0.15/kuplift/univariate_encoding.py
@@ -0,0 +1,212 @@
+# Copyright (c) 2026 Orange. All rights reserved.
+# This software is distributed under the MIT License, the text of which is available
+# at https://spdx.org/licenses/MIT.html or see the "LICENSE" file for more details.
+
+import pandas as pd
+import multiprocessing as mp
+from .helperfunctions import preprocess_data
+from .umodl_search_algorithm import execute_greedy_search_and_post_opt
+
+
+class UnivariateEncoding:
+    """
+    The UnivariateEncoding class implements the UMODL algorithm for uplift data encoding described in:
+    Rafla, M., Voisine, N., Crémilleux, B., & Boullé, M.
+    (2023, March). A non-parametric bayesian approach for uplift
+    discretization and feature selection. ECML PKDD
+    """
+
+    def __init__(self,control_name=None):
+        self.var_vs_disc = {}
+        self.treatment_name = "treatment"
+        self.outcome_name = "outcome"
+        self.column_names=[]
+        self.control_name=control_name
+        self.disc_details={}
+    
+    def get_features_importance_details(self):
+        # Prepare lists to capture multi-index levels
+        outer_keys = []
+        inner_keys = []
+        values = []
+
+        # Loop through the dictionary to populate the lists
+        for key, sublist in self.disc_details.items():
+            for idx, val in enumerate(sublist):
+                outer_keys.append(key)
+                inner_keys.append(idx)
+                values.append(val)
+
+        # Create the multi-index
+        multi_index = pd.MultiIndex.from_tuples(list(zip(outer_keys, inner_keys)), names=["key", ""])
+
+        # Convert the lists to DataFrame
+        df = pd.DataFrame(values, columns=["interval", "P(Y|T=1)", "P(Y|T=0)", "uplift"], index=multi_index)
+        return df
+    
+    def fit_transform(
+        self, data, treatment_col, y_col, parallelized=False, num_processes=5
+    ):
+        """
+        fit_transform() learns a discretisation model using UMODL and
+        transforms the data.
+
+        Parameters
+        ----------
+        data : pd.Dataframe
+            Dataframe containing feature variables.
+        treatment_col : pd.Series
+            Treatment column.
+        y_col : pd.Series
+            Outcome column.
+        parallelized : bool, default False
+            Whether to run the code on several processes.
+        num_processes : int, default 5
+            Number of processes to use in parallel.
+
+        Returns
+        -------
+        pd.Dataframe
+            Pandas Dataframe that contains encoded data.
+        """
+        self.fit(data, treatment_col, y_col, parallelized, num_processes)
+        data = self.transform(data)
+        return data
+    
+    def fit(
+        self, data, treatment_col, y_col, parallelized=False, num_processes=5
+    ):
+        """
+        fit() learns a discretisation model using the UMODL approach.
+
+        Parameters
+        ----------
+        data : pd.Dataframe
+            Dataframe containing feature variables.
+        treatment_col : pd.Series
+            Treatment column.
+        y_col : pd.Series
+            Outcome column.
+        parallelized : bool, default False
+            Whether to run the code on several processes.
+        num_processes : int, default 5
+            Number of processes to use in parallel.
+        """
+        self.column_names=data.columns
+        if treatment_col.name in self.column_names:
+            raise Exception("The treatment column is in the data, it should be passed in 'treatment_col' argument")
+        if y_col.name in self.column_names:
+            raise Exception("The outcome column is in the data, it should be passed in 'y_col' argument")
+        
+        
+        data = data.assign(**{self.treatment_name: treatment_col.copy()})
+        data = data.assign(**{self.outcome_name: y_col.copy()})
+        
+        #dealing with control name
+        if self.control_name != None:
+            trt_vals=list(data[self.treatment_name].unique())
+            
+            #Verify that control name is in the treatment column, else raise an exception
+            if self.control_name not in trt_vals:
+                raise Exception("the control name is not in the treatment column")
+            data[self.treatment_name] = data[self.treatment_name].replace(self.control_name,0)
+
+            trt_vals.remove(self.control_name)
+            #the other value will be considered as the treatment
+            data[self.treatment_name] = data[self.treatment_name].replace(trt_vals[0],1)
+        
+
+        
+        cols = list(data.columns)
+        if self.treatment_name in cols:
+            cols.remove(self.treatment_name)
+        if self.outcome_name in cols:    
+            cols.remove(self.outcome_name)
+
+        data = data[cols + [self.treatment_name, self.outcome_name]]
+        data = preprocess_data(data, self.treatment_name, self.outcome_name)
+        var_vs_importance = {}
+        self.var_vs_disc = {}
+
+        if parallelized == True:
+            pool = mp.Pool(processes=num_processes)
+
+            arguments_to_pass_in_parallel = []
+            for col in cols:
+                arguments_to_pass_in_parallel.append(
+                    [data[[col, self.treatment_name, self.outcome_name]],True]
+                )
+            list_of_tuples_feature_vs_importance = pool.starmap(
+                execute_greedy_search_and_post_opt,
+                arguments_to_pass_in_parallel,
+            )
+            pool.close()
+            print(list_of_tuples_feature_vs_importance)
+            for el in list_of_tuples_feature_vs_importance:
+                col = el[2]
+                if len(el[1]) == 1:
+                    self.var_vs_disc[col] = None
+                else:
+                    self.var_vs_disc[col] = el[1][:-1]
+
+        else:
+            for col in cols:
+                (
+                    var_vs_importance[col],
+                    self.var_vs_disc[col],
+                    col_name,
+                    self.disc_details
+                ) = execute_greedy_search_and_post_opt(
+                    data[[col, self.treatment_name, self.outcome_name]]
+                )
+                if len(self.var_vs_disc[col]) == 1:
+                    self.var_vs_disc[col] = None
+                else:
+                    self.var_vs_disc[col] = self.var_vs_disc[col][:-1]
+    def transform(self, data):
+        """
+        transform() applies the discretisation model learned by the
+        fit() method.
+
+        Parameters
+        ----------
+        data : pd.Dataframe
+            Dataframe containing feature variables.
+
+        Returns
+        -------
+        pd.Dataframe
+            Pandas Dataframe that contains encoded data.
+        """
+        
+        cols = list(data.columns)
+        
+        not_in_self_column_names = any(x not in self.column_names for x in cols)
+        if not_in_self_column_names==True:
+            raise Exception("The data to be transformed contains unknown columns")
+
+        if self.treatment_name in cols:
+            cols.remove(self.treatment_name)
+        if self.outcome_name in cols:
+            cols.remove(self.outcome_name)        
+        for col in cols:
+            if self.var_vs_disc[col] is None:
+                data.drop(col, inplace=True, axis=1)
+            else:
+                minBoundary = min(
+                    data[col].min(), self.var_vs_disc[col][0] - 0.001
+                )
+                maxBoundary = max(
+                    data[col].max(), self.var_vs_disc[col][-1] + 0.001
+                )
+                
+                data_copy = data.copy()
+                data_copy[col] = pd.cut(
+                    data_copy[col],
+                    bins=[minBoundary] + self.var_vs_disc[col] + [maxBoundary],
+                )
+                data = data_copy.copy()
+
+                data[col] = data[col].astype("category")
+                data[col] = data[col].cat.codes
+        return data
