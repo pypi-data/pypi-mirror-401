81d3e36b1b27b2530acf07e20868378c1814bdea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:15:25 +0000

    Add package fastucp-python/0.1.1/fastucp_python-0.1.1.tar.gz

---
+++ packages/fastucp-python/fastucp_python-0.1.1.tar.gz/fastucp_python-0.1.1/fastucp/__init__.py
@@ -0,0 +1,6 @@
+from .core import FastUCP
+from .builders import CheckoutBuilder
+from .client import FastUCPClient
+
+__version__ = "0.2.0"
+__all__ = ["FastUCP", "CheckoutBuilder", "FastUCPClient"]
