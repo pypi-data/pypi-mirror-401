2918a550c6ff38aa9dd06908b6a8cc9e9ed010bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:14:19 +0000

    Add package zivid/2.17.2/zivid-2.17.2-cp38-cp38-win_amd64.whl

---
+++ packages/zivid/zivid-2.17.2-cp38-cp38-win_amd64.whl/zivid/_version.py
@@ -0,0 +1,31 @@
+"""Query module version."""
+
+import sys
+
+
+def get_version(module_name):
+    # pylint: disable=import-outside-toplevel
+    """Return the version of a module.
+
+    Args:
+        module_name: Name of a module
+
+    Returns:
+        The module version or None if not found.
+    """
+    # pkg_resources is deprecated and throws a warning in newer Python verisons.
+    # We use importlib.metadata instead, but this is only available since Python 3.8.
+    if sys.version_info < (3, 8):
+        from pkg_resources import DistributionNotFound, get_distribution
+
+        try:
+            return get_distribution(module_name).version
+        except DistributionNotFound:
+            return None
+
+    from importlib.metadata import PackageNotFoundError, version
+
+    try:
+        return version(module_name)
+    except PackageNotFoundError:
+        return None
