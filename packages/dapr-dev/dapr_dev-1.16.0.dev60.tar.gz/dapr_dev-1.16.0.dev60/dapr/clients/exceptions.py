d803f357e50d03677b9cd2833ff2a899009b2bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:14:46 +0000

    Add package dapr-dev/1.16.0.dev60/dapr_dev-1.16.0.dev60.tar.gz

---
+++ packages/dapr-dev/dapr_dev-1.16.0.dev60.tar.gz/dapr_dev-1.16.0.dev60/dapr/clients/exceptions.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright 2023 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import base64
+import json
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from dapr.serializers import Serializer
+
+from google.protobuf.json_format import MessageToDict
+from google.rpc import error_details_pb2  # type: ignore
+from grpc import RpcError  # type: ignore
+from grpc_status import rpc_status  # type: ignore
+
+ERROR_CODE_UNKNOWN = 'UNKNOWN'
+ERROR_CODE_DOES_NOT_EXIST = 'ERR_DOES_NOT_EXIST'
+
+
+class DaprInternalError(Exception):
+    """DaprInternalError encapsulates all Dapr exceptions"""
+
+    def __init__(
+        self,
+        message: Optional[str],
+        error_code: Optional[str] = ERROR_CODE_UNKNOWN,
+        raw_response_bytes: Optional[bytes] = None,
+    ):
+        self._message = message
+        self._error_code = error_code
+        self._raw_response_bytes = raw_response_bytes
+
+    def as_dict(self):
+        return {
+            'message': self._message,
+            'errorCode': self._error_code,
+            'raw_response_bytes': self._raw_response_bytes,
+        }
+
+    def as_json_safe_dict(self):
+        error_dict = self.as_dict()
+
+        if self._raw_response_bytes is not None:
+            # Encode bytes to base64 for JSON compatibility
+            error_dict['raw_response_bytes'] = base64.b64encode(self._raw_response_bytes).decode(
+                'utf-8'
+            )
+
+        return error_dict
+
+    @property
+    def message(self) -> Optional[str]:
+        """Get the error message"""
+        return self._message
+
+    @property
+    def error_code(self) -> Optional[str]:
+        """Get the error code"""
+        return self._error_code
+
+    @property
+    def raw_response_bytes(self) -> Optional[bytes]:
+        """Get the raw response bytes"""
+        return self._raw_response_bytes
+
+    def __str__(self):
+        if self._error_code != ERROR_CODE_UNKNOWN:
+            return f"('{self._message}', '{self._error_code}')"
+        return self._message or 'Unknown Dapr Error.'
+
+
+class StatusDetails:
+    def __init__(self):
+        self.error_info = None
+        self.retry_info = None
+        self.debug_info = None
+        self.quota_failure = None
+        self.precondition_failure = None
+        self.bad_request = None
+        self.request_info = None
+        self.resource_info = None
+        self.help = None
+        self.localized_message = None
+
+    def as_dict(self):
+        return {attr: getattr(self, attr) for attr in self.__dict__}
+
+
+class DaprHttpError(DaprInternalError):
+    """DaprHttpError encapsulates all Dapr HTTP exceptions
+
+    Attributes:
+        _status_code: HTTP status code
+        _reason: HTTP reason phrase
+    """
+
+    def __init__(
+        self,
+        serializer: 'Serializer',
+        raw_response_bytes: Optional[bytes] = None,
+        status_code: Optional[int] = None,
+        reason: Optional[str] = None,
+    ):
+        self._status_code = status_code
+        self._reason = reason
+        error_code: str = ERROR_CODE_UNKNOWN
+        message: Optional[str] = None
+        error_info: Optional[dict] = None
+
+        if (raw_response_bytes is None or len(raw_response_bytes) == 0) and status_code == 404:
+            error_code = ERROR_CODE_DOES_NOT_EXIST
+            raw_response_bytes = None
+        elif raw_response_bytes:
+            try:
+                error_info = serializer.deserialize(raw_response_bytes)
+            except Exception:
+                pass
+                # ignore any errors during deserialization
+
+            if error_info and isinstance(error_info, dict):
+                message = error_info.get('message')
+                error_code = error_info.get('errorCode') or ERROR_CODE_UNKNOWN
+
+        super().__init__(
+            message or f'HTTP status code: {status_code}', error_code, raw_response_bytes
+        )
+
+    @property
+    def status_code(self) -> Optional[int]:
+        return self._status_code
+
+    @property
+    def reason(self) -> Optional[str]:
+        return self._reason
+
+    def as_dict(self):
+        error_dict = super().as_dict()
+        error_dict['status_code'] = self._status_code
+        error_dict['reason'] = self._reason
+        return error_dict
+
+    def __str__(self):
+        if self._error_code != ERROR_CODE_UNKNOWN:
+            return f'{self._message} (Error Code: {self._error_code}, Status Code: {self._status_code})'
+        else:
+            return f'Unknown Dapr Error. HTTP status code: {self._status_code}.'
+
+
+class DaprGrpcError(RpcError):
+    def __init__(self, err: RpcError):
+        self._status_code = err.code()
+        self._err_message = err.details()
+        self._details = StatusDetails()
+
+        self._grpc_status = rpc_status.from_call(err)
+        self._parse_details()
+
+    def _parse_details(self):
+        if self._grpc_status is None:
+            return
+
+        for detail in self._grpc_status.details:
+            if detail.Is(error_details_pb2.ErrorInfo.DESCRIPTOR):
+                self._details.error_info = serialize_status_detail(detail)
+            elif detail.Is(error_details_pb2.RetryInfo.DESCRIPTOR):
+                self._details.retry_info = serialize_status_detail(detail)
+            elif detail.Is(error_details_pb2.DebugInfo.DESCRIPTOR):
+                self._details.debug_info = serialize_status_detail(detail)
+            elif detail.Is(error_details_pb2.QuotaFailure.DESCRIPTOR):
+                self._details.quota_failure = serialize_status_detail(detail)
+            elif detail.Is(error_details_pb2.PreconditionFailure.DESCRIPTOR):
+                self._details.precondition_failure = serialize_status_detail(detail)
+            elif detail.Is(error_details_pb2.BadRequest.DESCRIPTOR):
+                self._details.bad_request = serialize_status_detail(detail)
+            elif detail.Is(error_details_pb2.RequestInfo.DESCRIPTOR):
+                self._details.request_info = serialize_status_detail(detail)
+            elif detail.Is(error_details_pb2.ResourceInfo.DESCRIPTOR):
+                self._details.resource_info = serialize_status_detail(detail)
+            elif detail.Is(error_details_pb2.Help.DESCRIPTOR):
+                self._details.help = serialize_status_detail(detail)
+            elif detail.Is(error_details_pb2.LocalizedMessage.DESCRIPTOR):
+                self._details.localized_message = serialize_status_detail(detail)
+
+    def code(self):
+        return self._status_code
+
+    def details(self):
+        """
+        We're keeping the method name details() so it matches the grpc.RpcError interface.
+        @return:
+        """
+        return self._err_message
+
+    def error_code(self):
+        if not self.status_details() or not self.status_details().error_info:
+            return ERROR_CODE_UNKNOWN
+        return self.status_details().error_info.get('reason', ERROR_CODE_UNKNOWN)
+
+    def status_details(self):
+        return self._details
+
+    def get_grpc_status(self):
+        return self._grpc_status
+
+    def json(self):
+        error_details = {
+            'status_code': self.code().name,
+            'message': self.details(),
+            'error_code': self.error_code(),
+            'details': self._details.as_dict(),
+        }
+        return json.dumps(error_details)
+
+
+def serialize_status_detail(status_detail):
+    if not status_detail:
+        return None
+    return MessageToDict(status_detail, preserving_proto_field_name=True)
---
+++ packages/dapr-dev/dapr_dev-1.16.0.dev60.tar.gz/dapr_dev-1.16.0.dev60/dapr/clients/grpc/_request.py
@@ -0,0 +1,454 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright 2023 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import io
+from enum import Enum
+from typing import Dict, Optional, Union
+
+from google.protobuf.any_pb2 import Any as GrpcAny
+from google.protobuf.message import Message as GrpcMessage
+
+from dapr.clients._constants import DEFAULT_JSON_CONTENT_TYPE
+from dapr.clients.grpc._crypto import DecryptOptions, EncryptOptions
+from dapr.clients.grpc._helpers import (
+    MetadataDict,
+    MetadataTuple,
+    to_bytes,
+    to_str,
+    tuple_to_dict,
+    unpack,
+)
+from dapr.proto import api_v1, common_v1
+
+
+class DaprRequest:
+    """A base class for Dapr Request.
+
+    This is the base class for Dapr Request. User can get the metadata.
+
+    Attributes:
+        metadata(dict): A dict to include the headers from Dapr Request.
+    """
+
+    def __init__(self, metadata: MetadataTuple = ()):
+        self.metadata = metadata  # type: ignore
+
+    @property
+    def metadata(self) -> MetadataDict:
+        """Get metadata from :class:`DaprRequest`."""
+        return self.get_metadata(as_dict=True)  # type: ignore
+
+    @metadata.setter
+    def metadata(self, val) -> None:
+        """Sets metadata."""
+        if not isinstance(val, tuple):
+            raise ValueError('val is not tuple')
+        self._metadata = val
+
+    def get_metadata(self, as_dict: bool = False) -> Union[MetadataDict, MetadataTuple]:
+        """Gets metadata from the request.
+
+        Args:
+            as_dict (bool): dict type metadata if as_dict is True. Otherwise, return
+                tuple metadata.
+
+        Returns:
+            dict or tuple: request metadata.
+        """
+        if as_dict:
+            return tuple_to_dict(self._metadata)
+        return self._metadata
+
+
+class InvokeMethodRequest(DaprRequest):
+    """A request data representation for invoke_method API.
+
+    This stores the request data with the proper serialization. This seralizes
+    data to :obj:`google.protobuf.any_pb2.Any` if data is the type of protocol
+    buffer message.
+
+    Attributes:
+        metadata(dict): A dict to include the headers from Dapr Request.
+        data (str, bytes, GrpcAny, GrpcMessage, optional): the serialized data
+            for invoke_method request.
+        content_type (str, optional): the content type of data which is valid
+            only for bytes array data.
+    """
+
+    HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE']
+
+    def __init__(
+        self,
+        data: Union[str, bytes, GrpcAny, GrpcMessage, None] = None,
+        content_type: Optional[str] = None,
+    ):
+        """Inits InvokeMethodRequestData with data and content_type.
+
+        Args:
+            data (bytes, str, GrpcAny, GrpcMessage, optional): the data
+                which is used for invoke_method request.
+            content_type (str): the content_type of data when the data is bytes.
+                The default content type is application/json.
+
+        Raises:
+            ValueError: data is not supported.
+        """
+        super(InvokeMethodRequest, self).__init__(())
+
+        self._content_type = content_type
+        self._http_verb: Optional[str] = None
+        self._http_querystring: Dict[str, str] = {}
+
+        self.set_data(data)
+
+        # Set content_type to application/json type if content_type
+        # is not given and date is bytes or str type.
+        if not self.is_proto() and not content_type:
+            self.content_type = DEFAULT_JSON_CONTENT_TYPE
+
+    @property
+    def http_verb(self) -> Optional[str]:
+        """Gets HTTP method in Dapr invocation request."""
+        return self._http_verb
+
+    @http_verb.setter
+    def http_verb(self, val: Optional[str]) -> None:
+        """Sets HTTP method to Dapr invocation request."""
+        if val is not None and val not in self.HTTP_METHODS:
+            raise ValueError(f'{val} is the invalid HTTP verb.')
+        self._http_verb = val
+
+    @property
+    def http_querystring(self) -> Dict[str, str]:
+        """Gets HTTP querystring as dict."""
+        return self._http_querystring
+
+    def is_http(self) -> bool:
+        """Return true if this request is http compatible."""
+        return hasattr(self, '_http_verb') and not (not self._http_verb)
+
+    @property
+    def proto(self) -> GrpcAny:
+        """Gets raw data as proto any type."""
+        return self._data
+
+    def is_proto(self) -> bool:
+        """Returns true if data is protocol-buffer serialized."""
+        return hasattr(self, '_data') and self._data.type_url != ''
+
+    def pack(self, val: Union[GrpcAny, GrpcMessage]) -> None:
+        """Serializes protocol buffer message.
+
+        Args:
+            message (:class:`GrpcMessage`, :class:`GrpcAny`): the protocol buffer message object
+
+        Raises:
+            ValueError: message is neither GrpcAny nor GrpcMessage.
+        """
+        if isinstance(val, GrpcAny):
+            self._data = val
+        elif isinstance(val, GrpcMessage):
+            self._data = GrpcAny()
+            self._data.Pack(val)
+        else:
+            raise ValueError('invalid data type')
+
+    def unpack(self, message: GrpcMessage) -> None:
+        """Deserializes the serialized protocol buffer message.
+
+        Args:
+            message (:obj:`GrpcMessage`): the protocol buffer message object
+                to which the response data is deserialized.
+
+        Raises:
+            ValueError: message is not protocol buffer message object or message's type is not
+                matched with the response data type
+        """
+        unpack(self.proto, message)
+
+    @property
+    def data(self) -> bytes:
+        """Gets request data as bytes."""
+        if self.is_proto():
+            raise ValueError('data is protocol buffer message object.')
+        return self._data.value
+
+    @data.setter
+    def data(self, val: Union[str, bytes]) -> None:
+        """Sets str or bytes type data to request data."""
+        self.set_data(to_bytes(val))
+
+    def set_data(self, val: Union[str, bytes, GrpcAny, GrpcMessage, None]) -> None:
+        """Sets data to request data."""
+        if val is None:
+            self._data = GrpcAny()
+        elif isinstance(val, (bytes, str)):
+            self._data = GrpcAny(value=to_bytes(val))
+        elif isinstance(val, (GrpcAny, GrpcMessage)):
+            self.pack(val)
+        else:
+            raise ValueError(f'invalid data type {type(val)}')
+
+    def text(self) -> str:
+        """Gets the request data as str."""
+        return to_str(self.data)
+
+    @property
+    def content_type(self) -> Optional[str]:
+        """Gets content_type for bytes data."""
+        return self._content_type
+
+    @content_type.setter
+    def content_type(self, val: Optional[str]) -> None:
+        """Sets content type for bytes data."""
+        self._content_type = val
+
+
+class BindingRequest(DaprRequest):
+    """A request data representation for invoke_binding API.
+
+    This stores the request data and metadata with the proper serialization.
+    This seralizes data to bytes and metadata to a dictionary of key value pairs.
+
+    Attributes:
+        data (bytes): the data which is used for invoke_binding request.
+        metadata (Dict[str, str]): the metadata sent to the binding.
+    """
+
+    def __init__(self, data: Union[str, bytes], binding_metadata: Dict[str, str] = {}):
+        """Inits BindingRequest with data and metadata if given.
+
+        Args:
+            data (bytes, str): the data which is used for invoke_binding request.
+            binding_metadata (tuple, optional): the metadata to be sent to the binding.
+
+        Raises:
+            ValueError: data is not bytes or str.
+        """
+        super(BindingRequest, self).__init__(())
+        self.data = data  # type: ignore
+        self._binding_metadata = binding_metadata
+
+    @property
+    def data(self) -> bytes:
+        """Gets request data as bytes."""
+        return self._data
+
+    @data.setter
+    def data(self, val: Union[str, bytes]) -> None:
+        """Sets str or bytes type data to request data."""
+        self._data = to_bytes(val)
+
+    def text(self) -> str:
+        """Gets the request data as str."""
+        return to_str(self.data)
+
+    @property
+    def binding_metadata(self):
+        """Gets the metadata for output binding."""
+        return self._binding_metadata
+
+
+class TransactionOperationType(Enum):
+    """Represents the type of operation for a Dapr Transaction State Api Call"""
+
+    upsert = 'upsert'
+    delete = 'delete'
+
+
+class TransactionalStateOperation:
+    """An upsert or delete operation for a state transaction, 'upsert' by default.
+
+    Attributes:
+        key (str): state's key.
+        data (Union[bytes, str]): state's data.
+        etag (str): state's etag.
+        operation_type (TransactionOperationType): operation to be performed.
+    """
+
+    def __init__(
+        self,
+        key: str,
+        data: Optional[Union[bytes, str]] = None,
+        etag: Optional[str] = None,
+        operation_type: TransactionOperationType = TransactionOperationType.upsert,
+        metadata: Optional[Dict[str, str]] = None,
+    ):
+        """Initializes TransactionalStateOperation item from
+        :obj:`runtime_v1.TransactionalStateOperation`.
+
+        Args:
+            key (str): state's key.
+            data (Union[bytes, str]): state's data.
+            etag (str): state's etag.
+            operationType (Optional[TransactionOperationType]): operation to be performed.
+
+        Raises:
+            ValueError: data is not bytes or str.
+        """
+        if operation_type != TransactionOperationType.delete and not isinstance(data, (bytes, str)):
+            raise ValueError(f'invalid type for data {type(data)}')
+
+        self._key = key
+        self._data = data  # type: ignore
+        self._etag = etag
+        self._operation_type = operation_type
+        self._metadata = metadata
+
+    @property
+    def key(self) -> str:
+        """Gets key."""
+        return self._key
+
+    @property
+    def data(self) -> Union[bytes, str, None]:
+        """Gets raw data."""
+        return self._data
+
+    @property
+    def etag(self) -> Optional[str]:
+        """Gets etag."""
+        return self._etag
+
+    @property
+    def operation_type(self) -> TransactionOperationType:
+        """Gets etag."""
+        return self._operation_type
+
+    @property
+    def metadata(self) -> Dict[str, str]:
+        """Gets metadata."""
+        return {} if self._metadata is None else self._metadata
+
+
+class EncryptRequestIterator(DaprRequest):
+    """An iterator for cryptography encrypt API requests.
+
+    This reads data from a given stream by chunks and converts it to an iterator of
+    cryptography encrypt API requests.
+    This iterator will be used for encrypt gRPC bidirectional streaming requests.
+    """
+
+    def __init__(
+        self,
+        data: Union[str, bytes],
+        options: EncryptOptions,
+    ):
+        """Initialize EncryptRequestIterator with data and encryption options.
+
+        Args:
+            data (Union[str, bytes]): data to be encrypted
+            options (EncryptOptions): encryption options
+        """
+        self.data = io.BytesIO(to_bytes(data))
+        self.options = options.get_proto()
+        self.buffer_size = 2 << 10  # 2KiB
+        self.seq = 0
+
+    def __iter__(self):
+        """Returns the iterator object itself."""
+        return self
+
+    def __next__(self):
+        """Read the next chunk of data from the input stream and create a gRPC stream request."""
+        # Read data from the input stream, in chunks of up to 2KiB
+        # Send the data until we reach the end of the input stream
+        chunk = self.data.read(self.buffer_size)
+        if not chunk:
+            raise StopIteration
+
+        payload = common_v1.StreamPayload(data=chunk, seq=self.seq)
+        if self.seq == 0:
+            # If this is the first chunk, add the options
+            request_proto = api_v1.EncryptRequest(payload=payload, options=self.options)
+        else:
+            request_proto = api_v1.EncryptRequest(payload=payload)
+
+        self.seq += 1
+        return request_proto
+
+
+class DecryptRequestIterator(DaprRequest):
+    """An iterator for cryptography decrypt API requests.
+
+    This reads data from a given stream by chunks and converts it to an iterator of decrypt
+    cryptography API requests.
+    This iterator will be used for decrypt gRPC bidirectional streaming requests.
+    """
+
+    def __init__(
+        self,
+        data: Union[str, bytes],
+        options: DecryptOptions,
+    ):
+        """Initialize DecryptRequestIterator with data and decryption options.
+
+        Args:
+            data (Union[str, bytes]): data to be decrypted
+            options (DecryptOptions): decryption options
+        """
+        self.data = io.BytesIO(to_bytes(data))
+        self.options = options.get_proto()
+        self.buffer_size = 2 << 10  # 2KiB
+        self.seq = 0
+
+    def __iter__(self):
+        """Returns the iterator object itself."""
+        return self
+
+    def __next__(self):
+        """Read the next chunk of data from the input stream and create a gRPC stream request."""
+        # Read data from the input stream, in chunks of up to 2KiB
+        # Send the data until we reach the end of the input stream
+        chunk = self.data.read(self.buffer_size)
+        if not chunk:
+            raise StopIteration
+
+        payload = common_v1.StreamPayload(data=chunk, seq=self.seq)
+        if self.seq == 0:
+            # If this is the first chunk, add the options
+            request_proto = api_v1.DecryptRequest(payload=payload, options=self.options)
+        else:
+            request_proto = api_v1.DecryptRequest(payload=payload)
+
+        self.seq += 1
+        return request_proto
+
+
+class JobEvent:
+    """Represents a job event received from Dapr runtime.
+
+    This matches the Go SDK's common.JobEvent structure and represents
+    a job that is currently being executed, not a job definition.
+
+    Args:
+        name (str): The name/type of the job being executed.
+        data (bytes): The raw job data payload.
+    """
+
+    def __init__(self, name: str, data: bytes = b''):
+        self.name = name
+        self.data = data
+
+    def get_data_as_string(self, encoding: str = 'utf-8') -> str:
+        """Get the job data as a string.
+
+        Args:
+            encoding (str): The encoding to use for decoding bytes. Defaults to 'utf-8'.
+
+        Returns:
+            str: The job data as a string, or empty string if no data.
+        """
+        return self.data.decode(encoding) if self.data else ''
