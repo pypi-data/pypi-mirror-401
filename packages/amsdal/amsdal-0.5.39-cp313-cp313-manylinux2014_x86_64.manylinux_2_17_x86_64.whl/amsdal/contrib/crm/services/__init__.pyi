023e9f23ad19061dcf91fee06337bcf294a9a37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 15:07:40 +0000

    Add package amsdal/0.5.39/amsdal-0.5.39-cp313-cp313-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/amsdal/amsdal-0.5.39-cp313-cp313-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/amsdal/contrib/crm/services/__init__.pyi
@@ -0,0 +1,7 @@
+from amsdal.contrib.crm.services.activity_service import ActivityService as ActivityService
+from amsdal.contrib.crm.services.custom_field_service import CustomFieldService as CustomFieldService
+from amsdal.contrib.crm.services.deal_service import DealService as DealService
+from amsdal.contrib.crm.services.email_service import EmailService as EmailService
+from amsdal.contrib.crm.services.workflow_service import WorkflowService as WorkflowService
+
+__all__ = ['ActivityService', 'CustomFieldService', 'DealService', 'EmailService', 'WorkflowService']
