acc643d4e8df854f53226d8daf2dd6d6cfa76272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:14:20 +0000

    Add package microservice-chassis-grupo2/0.1.5rc10/microservice_chassis_grupo2-0.1.5rc10.tar.gz

---
+++ packages/microservice-chassis-grupo2/microservice_chassis_grupo2-0.1.5rc10.tar.gz/microservice_chassis_grupo2-0.1.5rc10/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="microservice_chassis_grupo2",
+    version="0.1.5rc10",
+    packages=find_packages(),
+    install_requires=[],
+    author="Grupo 2",
+    author_email="",
+    description="A reusable library for microservices",
+    url="https://github.com/Grupo-MACC/Chassis",
+)
