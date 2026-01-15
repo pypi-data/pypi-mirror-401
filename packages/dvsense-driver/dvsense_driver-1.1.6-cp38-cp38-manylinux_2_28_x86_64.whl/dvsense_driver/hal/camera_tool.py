0d7686a113fbdde3bd381a2c7a7a9cbba673735f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:15:34 +0000

    Add package dvsense-driver/1.1.6/dvsense_driver-1.1.6-cp38-cp38-manylinux_2_28_x86_64.whl

---
+++ packages/dvsense-driver/dvsense_driver-1.1.6-cp38-cp38-manylinux_2_28_x86_64.whl/dvsense_driver/hal/camera_tool.py
@@ -0,0 +1,6 @@
+from ..dvsense_hal_py import ToolType
+from ..dvsense_hal_py import ToolInfo
+from ..dvsense_hal_py import ToolParameterType, tool_parameter_type_to_string
+from ..dvsense_hal_py import BasicParameterInfo
+from ..dvsense_hal_py import IntParameterInfo, FloatParameterInfo, BoolParameterInfo, EnumParameterInfo
+from ..dvsense_hal_py import CameraTool
---
+++ packages/dvsense-driver/dvsense_driver-1.1.6-cp38-cp38-manylinux_2_28_x86_64.whl/dvsense_driver/mp4_file_reader.py
@@ -0,0 +1,2 @@
+from dvsense_driver.base import Event2D
+from .dvsense_driver_py import Mp4FileReader
