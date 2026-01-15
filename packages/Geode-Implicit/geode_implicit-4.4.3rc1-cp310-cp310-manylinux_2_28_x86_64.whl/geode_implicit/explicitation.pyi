44a63344e6bf53c85593ece5bc879adae811c2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 15:07:45 +0000

    Add package Geode-Implicit/4.4.3rc1/geode_implicit-4.4.3rc1-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Implicit/geode_implicit-4.4.3rc1-cp310-cp310-manylinux_2_28_x86_64.whl/geode_implicit/explicitation.pyi
@@ -0,0 +1,14 @@
+from __future__ import annotations
+import geode_background as geode_background
+import geode_common as geode_common
+import geode_conversion as geode_conversion
+import geode_explicit as geode_explicit
+from geode_implicit.lib64.geode_implicit_py_explicitation import GridScalarFunctionExpliciter2D
+from geode_implicit.lib64.geode_implicit_py_explicitation import GridScalarFunctionExpliciter3D
+from geode_implicit.lib64.geode_implicit_py_explicitation import ImplicitExplicitationLibrary
+from geode_implicit.lib64.geode_implicit_py_explicitation import ImplicitStructuralModelHorizonExpliciter
+from geode_implicit.lib64.geode_implicit_py_explicitation import SolidMeshScalarFunctionExpliciter
+import geode_numerics as geode_numerics
+import opengeode as opengeode
+import opengeode_geosciences as opengeode_geosciences
+__all__: list[str] = ['GridScalarFunctionExpliciter2D', 'GridScalarFunctionExpliciter3D', 'ImplicitExplicitationLibrary', 'ImplicitStructuralModelHorizonExpliciter', 'SolidMeshScalarFunctionExpliciter', 'geode_background', 'geode_common', 'geode_conversion', 'geode_explicit', 'geode_numerics', 'opengeode', 'opengeode_geosciences']
