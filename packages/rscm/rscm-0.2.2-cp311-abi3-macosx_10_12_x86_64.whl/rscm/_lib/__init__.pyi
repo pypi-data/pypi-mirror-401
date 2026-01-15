02e73b2618e379770b7fbde4a1b66dd4a74e29db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 15:07:39 +0000

    Add package rscm/0.2.2/rscm-0.2.2-cp311-abi3-macosx_10_12_x86_64.whl

---
+++ packages/rscm/rscm-0.2.2-cp311-abi3-macosx_10_12_x86_64.whl/rscm/_lib/__init__.pyi
@@ -0,0 +1,8 @@
+# This .pyi file and the other .pyi files within this module are handwritten.
+# See https://github.com/PyO3/pyo3/issues/2454 for more information about the ongoing
+# implementation of the automatic generation of .pyi files from rust
+from .core import ComponentBuilder
+
+__version__: str
+
+class TwoLayerComponentBuilder(ComponentBuilder): ...
