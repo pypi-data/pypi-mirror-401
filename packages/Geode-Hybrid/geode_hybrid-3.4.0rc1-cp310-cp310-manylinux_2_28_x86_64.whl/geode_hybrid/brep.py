ae0cea92c4b56b938f8fec0a7334aa27af689336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:15:04 +0000

    Add package Geode-Hybrid/3.4.0rc1/geode_hybrid-3.4.0rc1-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Hybrid/geode_hybrid-3.4.0rc1-cp310-cp310-manylinux_2_28_x86_64.whl/geode_hybrid/brep.py
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+import geode_numerics
+import geode_simplex
+
+from .lib64.geode_hybrid_py_brep import *
+HybridBRepLibrary.initialize()
