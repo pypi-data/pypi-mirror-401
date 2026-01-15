44f3aa07ae1e6051c61b746d3d7043d5455a1d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:15:11 +0000

    Add package Geode-Simplex/9.10.0rc1/geode_simplex-9.10.0rc1-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Simplex/geode_simplex-9.10.0rc1-cp312-cp312-manylinux_2_28_x86_64.whl/geode_simplex/metric_simplex.pyi
@@ -0,0 +1,7 @@
+from __future__ import annotations
+import geode_common as geode_common
+from geode_simplex.lib64.geode_simplex_py_metric import BRepMetricConstraints
+from geode_simplex.lib64.geode_simplex_py_metric import SectionMetricConstraints
+from geode_simplex.lib64.geode_simplex_py_metric import SimplexMetricLibrary
+import opengeode as opengeode
+__all__: list[str] = ['BRepMetricConstraints', 'SectionMetricConstraints', 'SimplexMetricLibrary', 'geode_common', 'opengeode']
