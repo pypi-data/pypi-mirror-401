5941d0d03a85931430c347e3bbd318062608fcd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:15:14 +0000

    Add package aspose-barcode-cloud/26.1.0/aspose_barcode_cloud-26.1.0-py2.py3-none-any.whl

---
+++ packages/aspose-barcode-cloud/aspose_barcode_cloud-26.1.0-py2.py3-none-any.whl/aspose_barcode_cloud/__init__.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+# flake8: noqa: F401
+
+from aspose_barcode_cloud.rest import ApiException
+
+# import apis into sdk package
+from aspose_barcode_cloud.api.generate_api import GenerateApi
+from aspose_barcode_cloud.api.recognize_api import RecognizeApi
+from aspose_barcode_cloud.api.scan_api import ScanApi
+
+# import ApiClient
+from aspose_barcode_cloud.api_client import ApiClient
+from aspose_barcode_cloud.configuration import Configuration
+
+# import models into sdk package
+from aspose_barcode_cloud.models.api_error import ApiError
+from aspose_barcode_cloud.models.api_error_response import ApiErrorResponse
+from aspose_barcode_cloud.models.barcode_image_format import BarcodeImageFormat
+from aspose_barcode_cloud.models.barcode_image_params import BarcodeImageParams
+from aspose_barcode_cloud.models.barcode_response import BarcodeResponse
+from aspose_barcode_cloud.models.barcode_response_list import BarcodeResponseList
+from aspose_barcode_cloud.models.code_location import CodeLocation
+from aspose_barcode_cloud.models.decode_barcode_type import DecodeBarcodeType
+from aspose_barcode_cloud.models.encode_barcode_type import EncodeBarcodeType
+from aspose_barcode_cloud.models.encode_data import EncodeData
+from aspose_barcode_cloud.models.encode_data_type import EncodeDataType
+from aspose_barcode_cloud.models.generate_params import GenerateParams
+from aspose_barcode_cloud.models.graphics_unit import GraphicsUnit
+from aspose_barcode_cloud.models.recognition_image_kind import RecognitionImageKind
+from aspose_barcode_cloud.models.recognition_mode import RecognitionMode
+from aspose_barcode_cloud.models.recognize_base64_request import RecognizeBase64Request
+from aspose_barcode_cloud.models.region_point import RegionPoint
+from aspose_barcode_cloud.models.scan_base64_request import ScanBase64Request
---
+++ packages/aspose-barcode-cloud/aspose_barcode_cloud-26.1.0-py2.py3-none-any.whl/aspose_barcode_cloud/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from aspose_barcode_cloud.api.generate_api import GenerateApi
+from aspose_barcode_cloud.api.recognize_api import RecognizeApi
+from aspose_barcode_cloud.api.scan_api import ScanApi
