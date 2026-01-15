7914205bf1b0fa3db327bba7ca45570d943bea60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 15:07:39 +0000

    Add package starrocks/1.3.3/starrocks-1.3.3-py3-none-any.whl

---
+++ packages/starrocks/starrocks-1.3.3-py3-none-any.whl/starrocks/__init__.py
@@ -0,0 +1,94 @@
+# Copyright 2021-present StarRocks, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__version__ = "1.3.3"
+
+# import it to import some internal alembic packages implicitly
+# but, it's not needed if users only want to use SQLAlchemy rather than Alembic
+# from . import alembic
+
+from .datatype import (
+    ARRAY,
+    BIGINT,
+    BINARY,
+    BITMAP,
+    BOOLEAN,
+    CHAR,
+    DATE,
+    DATETIME,
+    DECIMAL,
+    DOUBLE,
+    FLOAT,
+    HLL,
+    INTEGER,
+    JSON,
+    LARGEINT,
+    MAP,
+    PERCENTILE,
+    SMALLINT,
+    STRING,
+    STRUCT,
+    TINYINT,
+    VARBINARY,
+    VARCHAR,
+)
+from .sql.dml import (
+    AmazonS3,
+    AVROFormat,
+    AzureBlobStorage,
+    AzureDataLakeStorage1,
+    AzureDataLakeStorage2,
+    Compression,
+    CSVFormat,
+    FilesFormat,
+    FilesSource,
+    FilesSourceOptions,
+    FilesTarget,
+    FilesTargetOptions,
+    GoogleCloudStorage,
+    InsertFromFiles,
+    InsertIntoFiles,
+    ORCFormat,
+    ParquetFormat,
+)
+
+
+__all__ = (
+    "BOOLEAN", "TINYINT", "SMALLINT", "INTEGER", "BIGINT", "LARGEINT",
+    "DECIMAL", "DOUBLE", "FLOAT",
+    "DATETIME", "DATE",
+    "CHAR", "VARCHAR", "STRING", "BINARY", "VARBINARY",
+    "HLL", "BITMAP", "PERCENTILE",
+    "ARRAY", "MAP", "STRUCT", "JSON",
+
+    "InsertIntoFiles",
+    "FilesTarget",
+    "FilesTargetOptions",
+    "FilesFormat",
+    "InsertFromFiles",
+    "FilesSource",
+    "FilesSourceOptions",
+    "CSVFormat",
+    "ParquetFormat",
+    "ORCFormat",
+    "AVROFormat",
+    "AmazonS3",
+    "AzureBlobStorage",
+    "AzureDataLakeStorage1",
+    "AzureDataLakeStorage2",
+    "GoogleCloudStorage",
+    "Compression",
+)
+
+
