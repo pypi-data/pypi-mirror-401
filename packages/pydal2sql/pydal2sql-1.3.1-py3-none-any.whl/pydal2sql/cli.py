b6cb1008eced1820139ca73428601fd743b5e799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:15:09 +0000

    Add package pydal2sql/1.3.1/pydal2sql-1.3.1-py3-none-any.whl

---
+++ packages/pydal2sql/pydal2sql-1.3.1-py3-none-any.whl/pydal2sql/cli.py
@@ -0,0 +1,241 @@
+"""
+Create the Typer cli.
+"""
+
+import sys
+import typing
+from typing import Optional
+
+import typer
+from configuraptor import Singleton
+from pydal2sql_core.cli_support import core_alter, core_create, core_stub
+from rich import print
+from typing_extensions import Never
+
+from .__about__ import __version__
+from .typer_support import (
+    DEFAULT_VERBOSITY,
+    IS_DEBUG,
+    ApplicationState,
+    Verbosity,
+    state,
+    with_exit_code,
+)
+from .types import DBType_Option, OptionalArgument, OutputFormat_Option, Tables_Option
+
+app = typer.Typer(
+    no_args_is_help=True,
+)
+
+
+def info(*args: str) -> None:  # pragma: no cover
+    """
+    'print' but with blue text.
+    """
+    print(f"[blue]{' '.join(args)}[/blue]", file=sys.stderr)
+
+
+def warn(*args: str) -> None:  # pragma: no cover
+    """
+    'print' but with yellow text.
+    """
+    print(f"[yellow]{' '.join(args)}[/yellow]", file=sys.stderr)
+
+
+def danger(*args: str) -> None:  # pragma: no cover
+    """
+    'print' but with red text.
+    """
+    print(f"[red]{' '.join(args)}[/red]", file=sys.stderr)
+
+
+@app.command()
+@with_exit_code(hide_tb=not IS_DEBUG)
+def create(
+    filename: OptionalArgument[str] = None,
+    tables: Tables_Option = None,
+    db_type: DBType_Option = None,
+    dialect: DBType_Option = None,
+    magic: Optional[bool] = None,
+    noop: Optional[bool] = None,
+    function: Optional[str] = None,
+    output_format: OutputFormat_Option = None,
+    output_file: Optional[str] = None,
+) -> bool:
+    """
+    Build the CREATE statements for one or more pydal/typedal tables.
+
+    Todo:
+        more docs
+
+    Examples:
+        pydal2sql create models.py
+        cat models.py | pydal2sql
+        pydal2sql # output from stdin
+    """
+    dialect = db_type.value if db_type else dialect.value if dialect else None
+
+    config = state.update_config(
+        magic=magic,
+        noop=noop,
+        dialect=dialect,
+        tables=tables,
+        function=function,
+        format=output_format,
+        input=filename,
+        output=output_file,
+    )
+
+    if core_create(
+        filename=config.input,
+        db_type=config.db_type,
+        tables=config.tables,
+        verbose=state.verbosity > Verbosity.normal,
+        noop=config.noop,
+        magic=config.magic,
+        function=config.function,
+        output_format=config.format,
+        output_file=config.output,
+    ):
+        print("[green] success! [/green]", file=sys.stderr)
+        return True
+    else:
+        print("[red] create failed! [/red]", file=sys.stderr)
+        return False
+
+
+@app.command()
+@with_exit_code(hide_tb=not IS_DEBUG)
+def alter(
+    filename_before: OptionalArgument[str] = None,
+    filename_after: OptionalArgument[str] = None,
+    db_type: DBType_Option = None,
+    dialect: DBType_Option = None,
+    tables: Tables_Option = None,
+    magic: Optional[bool] = None,
+    noop: Optional[bool] = None,
+    function: Optional[str] = None,
+    output_format: OutputFormat_Option = None,
+    output_file: Optional[str] = None,
+) -> bool:
+    """
+    Create the migration statements from one state to the other, by writing CREATE, ALTER and DROP statements.
+
+    Todo:
+        docs
+
+    Examples:
+        > pydal2sql alter @b3f24091a9201d6 examples/magic.py
+        compare magic.py at commit b3f... to current (= as in workdir).
+
+        > pydal2sql alter examples/magic.py@@b3f24091a9201d6 examples/magic_after_rename.py@latest
+        compare magic.py (which was renamed to magic_after_rename.py),
+            at a specific commit to the latest version in git (ignore workdir version).
+    """
+    dialect = db_type.value if db_type else dialect.value if dialect else None
+
+    config = state.update_config(
+        magic=magic,
+        noop=noop,
+        tables=tables,
+        function=function,
+        format=output_format,
+        input=filename_before,
+        output=output_file,
+    ).update(dialect=dialect, _allow_none=True)
+
+    if core_alter(
+        config.input,
+        filename_after or config.input,
+        db_type=config.db_type,
+        tables=config.tables,
+        verbose=state.verbosity > Verbosity.normal,
+        noop=config.noop,
+        magic=config.magic,
+        function=config.function,
+        output_format=config.format,
+        output_file=config.output,
+    ):
+        print("[green] success! [/green]", file=sys.stderr)
+        return True
+    else:
+        print("[red] alter failed! [/red]", file=sys.stderr)
+        return False
+
+
+@app.command()
+@with_exit_code(hide_tb=not IS_DEBUG)
+def stub(
+    migration_name: typing.Annotated[str, typer.Argument()] = "stub_migration",
+    output_format: OutputFormat_Option = None,
+    output_file: Optional[str] = None,
+    dry_run: typing.Annotated[bool, typer.Option("--dry", "--dry-run")] = False,
+    is_typedal: typing.Annotated[bool, typer.Option("--typedal", "-t")] = False,
+) -> bool:
+    """
+    Command to generate a stub migration.
+
+    Returns:
+        bool: True if the stub migration is generated successfully, False otherwise.
+
+    This command updates the configuration with the provided options and calls the core_stub function to generate the
+    migration.
+    """
+    config = state.update_config(
+        format=output_format,
+        output=output_file,
+    )
+
+    return core_stub(
+        migration_name,  # raw, without date or number
+        output_format=config.format,
+        output_file=config.output,
+        dry_run=dry_run,
+        is_typedal=is_typedal,
+    )
+
+
+def show_config_callback() -> Never:
+    """
+    --show-config requested!
+    """
+    print(state)
+    raise typer.Exit(0)
+
+
+def version_callback() -> Never:
+    """
+    --version requested!
+    """
+    print(f"pydal2sql Version: {__version__}")
+
+    raise typer.Exit(0)
+
+
+@app.callback(invoke_without_command=True)
+def main(
+    _: typer.Context,
+    config: str = None,
+    verbosity: Verbosity = None,
+    # stops the program:
+    show_config: bool = False,
+    version: bool = False,
+) -> None:
+    """
+    This script can be used to generate the create or alter sql from pydal or typedal.
+    """
+    if state.config:
+        # if a config already exists, it's outdated, so we clear it.
+        # only really applicable in Pytest scenarios where multiple commands are executed after eachother
+        Singleton.clear(state.config)
+
+    if verbosity is None:
+        verbosity = DEFAULT_VERBOSITY
+
+    state.load_config(config_file=config, verbosity=verbosity)
+
+    if show_config:
+        show_config_callback()
+    elif version:
+        version_callback()
+    # else: just continue
