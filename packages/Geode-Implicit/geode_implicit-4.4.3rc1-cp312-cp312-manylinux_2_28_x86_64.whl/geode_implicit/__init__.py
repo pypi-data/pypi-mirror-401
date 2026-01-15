6050e20e94ba851a670c647920320eea7b4e2bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 15:07:46 +0000

    Add package Geode-Implicit/4.4.3rc1/geode_implicit-4.4.3rc1-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Implicit/geode_implicit-4.4.3rc1-cp312-cp312-manylinux_2_28_x86_64.whl/geode_implicit/__init__.py
@@ -0,0 +1,7 @@
+## Copyright (c) 2019 - 2025 Geode-solutions
+
+from .explicitation import *
+from .implicitation import *
+from .insertion import *
+from .model_io import *
+from .workflows import *
---
+++ packages/Geode-Implicit/geode_implicit-4.4.3rc1-cp312-cp312-manylinux_2_28_x86_64.whl/geode_implicit/workflows.py
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import opengeode_geosciences
+import geode_common
+import geode_conversion
+import geode_explicit
+import geode_simplex
+
+from .lib64.geode_implicit_py_workflows import *
+
+ImplicitWorkflowsLibrary.initialize()
