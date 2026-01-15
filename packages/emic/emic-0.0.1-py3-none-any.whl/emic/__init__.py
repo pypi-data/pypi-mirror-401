b5062b05c6380ce98a08c0221fae9cdc148fac91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 15:07:42 +0000

    Add package emic/0.0.1/emic-0.0.1-py3-none-any.whl

---
+++ packages/emic/emic-0.0.1-py3-none-any.whl/emic/__init__.py
@@ -0,0 +1,9 @@
+"""
+emic - Epsilon Machine Inference & Characterization
+
+A Python framework for constructing and analyzing epsilon-machines
+based on computational mechanics.
+"""
+
+__version__ = "0.0.1"
+__all__: list[str] = []
