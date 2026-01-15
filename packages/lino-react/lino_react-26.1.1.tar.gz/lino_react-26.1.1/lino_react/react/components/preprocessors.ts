90e3dc4cbd1024ae66a53558f0b2e7180ad41e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:15:18 +0000

    Add package lino-react/26.1.1/lino_react-26.1.1.tar.gz

---
+++ packages/lino-react/lino_react-26.1.1.tar.gz/lino_react-26.1.1/lino_react/react/components/preprocessors.ts
@@ -0,0 +1,21 @@
+
+import * as t from "./types";
+
+const Lino: t.Lino = window.Lino || {};
+
+Lino.get_current_grid_config = (context, preprocessedStack) => {
+    context.dataContext.root.get_current_grid_config(preprocessedStack);
+    return preprocessedStack;
+}
+
+Lino.captureImage = (context, preprocessedStack) => {
+    preprocessedStack.callback = {
+        callback: (windowId) => {
+            context.APP.dialogFactory.createWebcamDialog(context, preprocessedStack, windowId);
+        },
+        callbackType: "postWindowInit",
+    }
+    return preprocessedStack;
+}
+
+export { Lino };
