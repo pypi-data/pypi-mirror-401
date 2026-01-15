a265676b9466a67f18b1a9c91f3c60709ee615ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:14:56 +0000

    Add package talentro-commons/0.18.10/talentro_commons-0.18.10.tar.gz

---
+++ packages/talentro-commons/talentro_commons-0.18.10.tar.gz/talentro_commons-0.18.10/src/talentro/util/attributes.py
@@ -0,0 +1,16 @@
+def safe_get(data: dict, attribute: str, default: any = None, splitter:str = '.'):
+    if attribute is None:
+        return default
+
+    for key in attribute.split(splitter):
+        try:
+            data = data[key]
+
+            if data is None and default is not None:
+                return default
+        except KeyError:
+            return default
+        except TypeError:
+            return default
+
+    return data
