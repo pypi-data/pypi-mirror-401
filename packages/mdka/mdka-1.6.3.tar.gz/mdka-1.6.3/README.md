809072aea4eec64f997d739eb96840a1fb4812f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:14:24 +0000

    Add package mdka/1.6.3/mdka-1.6.3.tar.gz

---
+++ packages/mdka/mdka-1.6.3.tar.gz/mdka-1.6.3/README.md
@@ -0,0 +1,99 @@
+# mdka
+
+**HTML to Markdown (MD)** converter written in [Rust](https://www.rust-lang.org/).
+
+[![crates.io](https://img.shields.io/crates/v/mdka?label=latest)](https://crates.io/crates/mdka)
+[![Documentation](https://docs.rs/mdka/badge.svg?version=latest)](https://docs.rs/mdka)
+[![Dependency Status](https://deps.rs/crate/mdka/latest/status.svg)](https://deps.rs/crate/mdka)
+[![Executable](https://github.com/nabbisen/mdka-rs/actions/workflows/release-executable.yaml/badge.svg)](https://github.com/nabbisen/mdka-rs/actions/workflows/release-executable.yaml)
+[![PyPi](https://github.com/nabbisen/mdka-rs/actions/workflows/release-pypi.yaml/badge.svg)](https://github.com/nabbisen/mdka-rs/actions/workflows/release-pypi.yaml)
+[![License](https://img.shields.io/github/license/nabbisen/mdka-rs)](https://github.com/nabbisen/mdka-rs/blob/main/LICENSE)
+
+## Summary
+
+A kind of text manipulator named mdka. "ka" means "化 (か)" pointing to conversion.    
+Designed with in mind:
+
+- Fast speed
+- Low memory consumption
+- Easy usage
+
+## Usage
+
+### 🌠 Rust with cargo
+
+```toml
+# Cargo.toml
+[dependencies]
+mdka = "1"
+```
+
+```rust
+// awesome.rs
+use mdka::from_html
+
+fn awesome_fn() {
+    let input = r#"
+<h1>heading 1</h1>
+<p>Hello, world.</p>"#;
+    let ret = from_html(input);
+    println!("{}", ret);
+    // # heading 1
+    // 
+    // Hello, world.
+    // 
+}
+```
+
+For more details about functions, check out [the docs](docs/functions.md).
+
+### 🧩 Executable
+
+[**Assets**](https://github.com/nabbisen/mdka-rs/releases/latest) in Releases offer executables for multiple platforms → [For usage](docs/executable.md).
+
+### 🧩 Python integration
+
+Bindings for Python are supported → [For more examples](docs/BINDINGS_FOR_PYTHON.md#usage) | [PyPi](https://pypi.org/project/mdka/).
+
+```console
+$ pip install mdka
+```
+
+```python
+# awesome.py
+from mdka import md_from_html
+
+print(md_from_html("<p>Hello, world.</p>"))
+# Hello, world.
+# 
+```
+
+### 🧩 Node.js integration
+
+Bindings for Node.js are supported → [For more examples](napi/README.md#usage) | [npm](https://www.npmjs.com/package/mdka).
+
+```console
+$ npm install mdka
+```
+
+```js
+// awesome.js
+const { fromHtml } = require("mdka")
+
+console.log(fromHtml("<p>Hello, world.</p>"))
+// Hello, world.
+// 
+```
+
+---
+
+## Open-source, with care
+
+This project is lovingly built and maintained by volunteers.  
+We hope it helps streamline your work.  
+Please understand that the project has its own direction — while we welcome feedback, it might not fit every edge case 🌱
+
+## Acknowledgements
+
+Depends on Servo's [html5ever](https://github.com/servo/html5ever) / markup5ever.
+Also, on PyO3's [pyo3](https://github.com/PyO3/pyo3) / [maturin](https://github.com/PyO3/maturin) on bindings for Python. [napi-rs](https://github.com/napi-rs/napi-rs) for binding for Node.js.
---
+++ packages/mdka/mdka-1.6.3.tar.gz/mdka-1.6.3/src/lib.rs
@@ -0,0 +1,100 @@
+//! HTML to Markdown converter - Lightweight library written in Rust.
+
+mod elements;
+mod nodes;
+mod utils;
+
+use nodes::{node::root_node_md, utils::parse_html};
+use utils::file::{read_from_filepath, write_to_filepath};
+
+#[cfg(feature = "pyo3")]
+pub mod python_bindings;
+
+#[cfg(feature = "napi")]
+pub mod nodejs_bindings;
+
+/// Convert HTML to Markdown
+///
+/// ```
+/// use mdka::from_html;
+///
+/// let html_text = r#"
+/// <h1>heading 1</h1>
+/// <p>Hello, world.</p>"#;
+/// let expect = "# heading 1\n\nHello, world.\n\n";
+///
+/// let ret = from_html(html_text);
+/// assert_eq!(ret, expect);
+/// ```
+///
+pub fn from_html(html_text: &str) -> String {
+    let dom = parse_html(html_text);
+    root_node_md(&dom.document, None::<usize>)
+}
+
+/// Convert HTML to Markdown
+///
+/// ```
+/// use mdka::from_file;
+///
+/// let html_filepath = "tests/fixtures/simple-01.html";
+/// let expect = "# heading 1\n\nHello, world.\n\n";
+///
+/// let ret = from_file(html_filepath).expect("Failed to read");
+/// assert_eq!(ret.as_str(), expect);
+/// ```
+///
+pub fn from_file(html_filepath: &str) -> Result<String, String> {
+    let html_text = read_from_filepath(html_filepath)
+        .expect(format!("Failed to read: {}", html_filepath).as_str());
+    Ok(from_html(html_text.as_str()))
+}
+
+/// Convert HTML to Markdown
+///
+/// ```
+/// use mdka::from_html_to_file;
+///
+/// let html_text = r#"
+/// <h1>heading 1</h1>
+/// <p>Hello, world.</p>"#;
+/// let markdown_filepath = "tests/tmp/from_html_file_doc_test_result.html";
+/// let expect = "# heading 1\n\nHello, world.\n\n";
+///
+/// let ret = from_html_to_file(html_text, markdown_filepath, true).expect("Failed to write");
+/// let markdown_file_content = std::fs::read_to_string(markdown_filepath).expect("Failed to read from markdown filepath");
+/// assert_eq!(expect, markdown_file_content);
+/// ```
+///
+pub fn from_html_to_file(
+    html_text: &str,
+    markdown_filepath: &str,
+    overwrites: bool,
+) -> Result<(), String> {
+    let md = from_html(html_text);
+    write_to_filepath(md.as_str(), markdown_filepath, overwrites)
+}
+
+/// Convert HTML to Markdown
+///
+/// ```
+/// use mdka::from_file_to_file;
+///
+/// let html_filepath = "tests/fixtures/simple-01.html";
+/// let markdown_filepath = "tests/tmp/from_html_file_doc_test_result.html";
+/// let expect = "# heading 1\n\nHello, world.\n\n";
+///
+/// let ret = from_file_to_file(html_filepath, markdown_filepath, true).expect("Failed to write");
+/// let markdown_file_content = std::fs::read_to_string(markdown_filepath).expect("Failed to read from markdown filepath");
+/// assert_eq!(expect, markdown_file_content);
+/// ```
+///
+pub fn from_file_to_file(
+    html_filepath: &str,
+    markdown_filepath: &str,
+    overwrites: bool,
+) -> Result<(), String> {
+    let html = read_from_filepath(html_filepath)
+        .expect(format!("Failed to read: {}", html_filepath).as_str());
+    from_html_to_file(html.as_str(), markdown_filepath, overwrites)
+}
