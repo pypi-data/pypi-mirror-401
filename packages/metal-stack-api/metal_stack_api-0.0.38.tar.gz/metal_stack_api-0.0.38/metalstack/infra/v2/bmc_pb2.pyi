ecda491305686bbd770b9bfb0a36c6b71507959a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:15:16 +0000

    Add package metal-stack-api/0.0.38/metal_stack_api-0.0.38.tar.gz

---
+++ packages/metal-stack-api/metal_stack_api-0.0.38.tar.gz/metal_stack_api-0.0.38/metalstack/infra/v2/bmc_pb2.pyi
@@ -0,0 +1,14 @@
+from metalstack.api.v2 import common_pb2 as _common_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class UpdateBMCInfoRequest(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class UpdateBMCInfoResponse(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
