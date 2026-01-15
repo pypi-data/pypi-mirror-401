870172aa18724a4d3c376eec86dd26a167d9fcd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 14:14:20 +0000

    Add package neural-lam/0.5.0/neural_lam-0.5.0-py3-none-any.whl

---
+++ packages/neural-lam/neural_lam-0.5.0-py3-none-any.whl/neural_lam/plot_graph.py
@@ -0,0 +1,213 @@
+# Standard library
+import os
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+
+# Third-party
+import numpy as np
+import plotly.graph_objects as go
+import torch_geometric as pyg
+
+# Local
+from . import utils
+from .config import load_config_and_datastore
+
+MESH_HEIGHT = 0.1
+MESH_LEVEL_DIST = 0.2
+GRID_HEIGHT = 0
+
+
+def main():
+    """Plot graph structure in 3D using plotly."""
+    parser = ArgumentParser(
+        description="Plot graph",
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--datastore_config_path",
+        type=str,
+        default="tests/datastore_examples/mdp/config.yaml",
+        help="Path for the datastore config",
+    )
+    parser.add_argument(
+        "--graph",
+        type=str,
+        default="multiscale",
+        help="Graph to plot",
+    )
+    parser.add_argument(
+        "--save",
+        type=str,
+        help="Name of .html file to save interactive plot to",
+    )
+    parser.add_argument(
+        "--show_axis",
+        action="store_true",
+        help="If the axis should be displayed",
+    )
+
+    args = parser.parse_args()
+    _, datastore = load_config_and_datastore(
+        config_path=args.datastore_config_path
+    )
+
+    xy = datastore.get_xy("state", stacked=True)  # (N_grid, 2)
+    pos_max = np.max(np.abs(xy))
+    grid_pos = xy / pos_max  # Divide by maximum coordinate
+
+    # Load graph data
+    graph_dir_path = os.path.join(datastore.root_path, "graph", args.graph)
+    hierarchical, graph_ldict = utils.load_graph(graph_dir_path=graph_dir_path)
+    (g2m_edge_index, m2g_edge_index, m2m_edge_index,) = (
+        graph_ldict["g2m_edge_index"],
+        graph_ldict["m2g_edge_index"],
+        graph_ldict["m2m_edge_index"],
+    )
+    mesh_up_edge_index, mesh_down_edge_index = (
+        graph_ldict["mesh_up_edge_index"],
+        graph_ldict["mesh_down_edge_index"],
+    )
+    mesh_static_features = graph_ldict["mesh_static_features"]
+
+    # Add in z-dimension
+    z_grid = GRID_HEIGHT * np.ones((grid_pos.shape[0],))
+    grid_pos = np.concatenate(
+        (grid_pos, np.expand_dims(z_grid, axis=1)), axis=1
+    )
+
+    # List of edges to plot, (edge_index, color, line_width, label)
+    edge_plot_list = [
+        (m2g_edge_index.numpy(), "black", 0.4, "M2G"),
+        (g2m_edge_index.numpy(), "black", 0.4, "G2M"),
+    ]
+
+    # Mesh positioning and edges to plot differ if we have a hierarchical graph
+    if hierarchical:
+        mesh_level_pos = [
+            np.concatenate(
+                (
+                    level_static_features.numpy(),
+                    MESH_HEIGHT
+                    + MESH_LEVEL_DIST
+                    * height_level
+                    * np.ones((level_static_features.shape[0], 1)),
+                ),
+                axis=1,
+            )
+            for height_level, level_static_features in enumerate(
+                mesh_static_features, start=1
+            )
+        ]
+        mesh_pos = np.concatenate(mesh_level_pos, axis=0)
+
+        # Add inter-level mesh edges
+        edge_plot_list += [
+            (level_ei.numpy(), "blue", 1, f"M2M Level {level}")
+            for level, level_ei in enumerate(m2m_edge_index)
+        ]
+
+        # Add intra-level mesh edges
+        up_edges_ei = np.concatenate(
+            [level_up_ei.numpy() for level_up_ei in mesh_up_edge_index], axis=1
+        )
+        down_edges_ei = np.concatenate(
+            [level_down_ei.numpy() for level_down_ei in mesh_down_edge_index],
+            axis=1,
+        )
+        edge_plot_list.append((up_edges_ei, "green", 1, "Mesh up"))
+        edge_plot_list.append((down_edges_ei, "green", 1, "Mesh down"))
+
+        mesh_node_size = 2.5
+    else:
+        mesh_pos = mesh_static_features.numpy()
+
+        mesh_degrees = pyg.utils.degree(m2m_edge_index[1]).numpy()
+        z_mesh = MESH_HEIGHT + 0.01 * mesh_degrees
+        mesh_node_size = mesh_degrees / 2
+
+        mesh_pos = np.concatenate(
+            (mesh_pos, np.expand_dims(z_mesh, axis=1)), axis=1
+        )
+
+        edge_plot_list.append((m2m_edge_index.numpy(), "blue", 1, "M2M"))
+
+    # All node positions in one array
+    node_pos = np.concatenate((mesh_pos, grid_pos), axis=0)
+
+    # Add edges
+    data_objs = []
+    for (
+        ei,
+        col,
+        width,
+        label,
+    ) in edge_plot_list:
+        edge_start = node_pos[ei[0]]  # (M, 2)
+        edge_end = node_pos[ei[1]]  # (M, 2)
+        n_edges = edge_start.shape[0]
+
+        x_edges = np.stack(
+            (edge_start[:, 0], edge_end[:, 0], np.full(n_edges, None)), axis=1
+        ).flatten()
+        y_edges = np.stack(
+            (edge_start[:, 1], edge_end[:, 1], np.full(n_edges, None)), axis=1
+        ).flatten()
+        z_edges = np.stack(
+            (edge_start[:, 2], edge_end[:, 2], np.full(n_edges, None)), axis=1
+        ).flatten()
+
+        scatter_obj = go.Scatter3d(
+            x=x_edges,
+            y=y_edges,
+            z=z_edges,
+            mode="lines",
+            line={"color": col, "width": width},
+            name=label,
+        )
+        data_objs.append(scatter_obj)
+
+    # Add node objects
+
+    data_objs.append(
+        go.Scatter3d(
+            x=grid_pos[:, 0],
+            y=grid_pos[:, 1],
+            z=grid_pos[:, 2],
+            mode="markers",
+            marker={"color": "black", "size": 1},
+            name="Grid nodes",
+        )
+    )
+    data_objs.append(
+        go.Scatter3d(
+            x=mesh_pos[:, 0],
+            y=mesh_pos[:, 1],
+            z=mesh_pos[:, 2],
+            mode="markers",
+            marker={"color": "blue", "size": mesh_node_size},
+            name="Mesh nodes",
+        )
+    )
+
+    fig = go.Figure(data=data_objs)
+
+    fig.update_layout(scene_aspectmode="data")
+    fig.update_traces(connectgaps=False)
+
+    if not args.show_axis:
+        # Hide axis
+        fig.update_layout(
+            scene={
+                "xaxis": {"visible": False},
+                "yaxis": {"visible": False},
+                "zaxis": {"visible": False},
+            }
+        )
+
+    if args.save:
+        fig.write_html(args.save, include_plotlyjs="cdn")
+    else:
+        fig.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/neural-lam/neural_lam-0.5.0-py3-none-any.whl/neural_lam/vis.py
@@ -0,0 +1,182 @@
+# Third-party
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+
+# Local
+from . import utils
+from .datastore.base import BaseRegularGridDatastore
+
+
+@matplotlib.rc_context(utils.fractional_plot_bundle(1))
+def plot_error_map(errors, datastore: BaseRegularGridDatastore, title=None):
+    """
+    Plot a heatmap of errors of different variables at different
+    predictions horizons
+    errors: (pred_steps, d_f)
+    """
+    errors_np = errors.T.cpu().numpy()  # (d_f, pred_steps)
+    d_f, pred_steps = errors_np.shape
+    step_length = datastore.step_length
+
+    # Normalize all errors to [0,1] for color map
+    max_errors = errors_np.max(axis=1)  # d_f
+    errors_norm = errors_np / np.expand_dims(max_errors, axis=1)
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+
+    ax.imshow(
+        errors_norm,
+        cmap="OrRd",
+        vmin=0,
+        vmax=1.0,
+        interpolation="none",
+        aspect="auto",
+        alpha=0.8,
+    )
+
+    # ax and labels
+    for (j, i), error in np.ndenumerate(errors_np):
+        # Numbers > 9999 will be too large to fit
+        formatted_error = f"{error:.3f}" if error < 9999 else f"{error:.2E}"
+        ax.text(i, j, formatted_error, ha="center", va="center", usetex=False)
+
+    # Ticks and labels
+    label_size = 15
+    ax.set_xticks(np.arange(pred_steps))
+    pred_hor_i = np.arange(pred_steps) + 1  # Prediction horiz. in index
+    pred_hor_h = step_length * pred_hor_i  # Prediction horiz. in hours
+    ax.set_xticklabels(pred_hor_h, size=label_size)
+    ax.set_xlabel("Lead time (h)", size=label_size)
+
+    ax.set_yticks(np.arange(d_f))
+    var_names = datastore.get_vars_names(category="state")
+    var_units = datastore.get_vars_units(category="state")
+    y_ticklabels = [
+        f"{name} ({unit})" for name, unit in zip(var_names, var_units)
+    ]
+    ax.set_yticklabels(y_ticklabels, rotation=30, size=label_size)
+
+    if title:
+        ax.set_title(title, size=15)
+
+    return fig
+
+
+@matplotlib.rc_context(utils.fractional_plot_bundle(1))
+def plot_prediction(
+    datastore: BaseRegularGridDatastore,
+    da_prediction: xr.DataArray,
+    da_target: xr.DataArray,
+    title=None,
+    vrange=None,
+):
+    """
+    Plot example prediction and grond truth.
+
+    Each has shape (N_grid,)
+
+    """
+    # Get common scale for values
+    if vrange is None:
+        vmin = min(da_prediction.min(), da_target.min())
+        vmax = max(da_prediction.max(), da_target.max())
+    elif vrange is not None:
+        vmin, vmax = vrange
+
+    extent = datastore.get_xy_extent("state")
+
+    # Set up masking of border region
+    da_mask = datastore.unstack_grid_coords(datastore.boundary_mask)
+    mask_values = np.invert(da_mask.values.astype(bool)).astype(float)
+    pixel_alpha = mask_values.clip(0.7, 1)  # Faded border region
+
+    fig, axes = plt.subplots(
+        1,
+        2,
+        figsize=(13, 7),
+        subplot_kw={"projection": datastore.coords_projection},
+    )
+
+    # Plot pred and target
+    for ax, da in zip(axes, (da_target, da_prediction)):
+        ax.coastlines()  # Add coastline outlines
+        da.plot.imshow(
+            ax=ax,
+            origin="lower",
+            x="x",
+            extent=extent,
+            alpha=pixel_alpha.T,
+            vmin=vmin,
+            vmax=vmax,
+            cmap="plasma",
+            transform=datastore.coords_projection,
+        )
+
+    # Ticks and labels
+    axes[0].set_title("Ground Truth", size=15)
+    axes[1].set_title("Prediction", size=15)
+
+    if title:
+        fig.suptitle(title, size=20)
+
+    return fig
+
+
+@matplotlib.rc_context(utils.fractional_plot_bundle(1))
+def plot_spatial_error(
+    error, datastore: BaseRegularGridDatastore, title=None, vrange=None
+):
+    """
+    Plot errors over spatial map
+    Error and obs_mask has shape (N_grid,)
+    """
+    # Get common scale for values
+    if vrange is None:
+        vmin = error.min().cpu().item()
+        vmax = error.max().cpu().item()
+    else:
+        vmin, vmax = vrange
+
+    extent = datastore.get_xy_extent("state")
+
+    # Set up masking of border region
+    da_mask = datastore.unstack_grid_coords(datastore.boundary_mask)
+    mask_reshaped = da_mask.values
+    pixel_alpha = mask_reshaped.clip(0.7, 1)  # Faded border region
+
+    fig, ax = plt.subplots(
+        figsize=(5, 4.8),
+        subplot_kw={"projection": datastore.coords_projection},
+    )
+
+    ax.coastlines()  # Add coastline outlines
+    error_grid = (
+        error.reshape(
+            [datastore.grid_shape_state.x, datastore.grid_shape_state.y]
+        )
+        .T.cpu()
+        .numpy()
+    )
+
+    im = ax.imshow(
+        error_grid,
+        origin="lower",
+        extent=extent,
+        alpha=pixel_alpha,
+        vmin=vmin,
+        vmax=vmax,
+        cmap="OrRd",
+    )
+
+    # Ticks and labels
+    cbar = fig.colorbar(im, aspect=30)
+    cbar.ax.tick_params(labelsize=10)
+    cbar.ax.yaxis.get_offset_text().set_fontsize(10)
+    cbar.formatter.set_powerlimits((-3, 3))
+
+    if title:
+        fig.suptitle(title, size=10)
+
+    return fig
