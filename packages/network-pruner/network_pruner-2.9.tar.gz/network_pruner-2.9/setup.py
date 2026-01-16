a106d9ee5ebaecea4133cd49f7c141ab88d5428b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:36 +0000

    Add package network-pruner/2.9/network_pruner-2.9.tar.gz

---
+++ packages/network-pruner/network_pruner-2.9.tar.gz/network_pruner-2.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='network_pruner',
+    version='2.9',
+    author='Chenqing Lin',
+    author_email='24020040061@pop.zjgsu.edu.cn',
+    packages = ["Network_Pruner"],
+    install_requires=[
+
+    ]
+)
