00e5bc56dd0ccb614a0fefe53748838ff0e2873c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 11:06:45 +0000

    Add package dttb/0.2.1/dttb-0.2.1-py3-none-any.whl

---
+++ packages/dttb/dttb-0.2.1-py3-none-any.whl/dttb/__init__.py
@@ -0,0 +1,77 @@
+"""
+DateTime TraceBack
+
+PyPI: https://pypi.org/project/dttb/
+GitHub: https://github.com/rocky-d/dttb
+"""
+
+from __future__ import annotations
+
+import datetime as dt
+import functools
+import sys
+import threading
+from threading import ExceptHookArgs
+from types import TracebackType
+from typing import Any, Callable, Optional, Type
+
+__all__ = [
+    "apply",
+    "reset",
+]
+
+
+_sys_excepthook = sys.excepthook
+_threading_excepthook = threading.excepthook
+
+_SysExcepthook = Callable[
+    [Type[BaseException], BaseException, Optional[TracebackType]],
+    Any,
+]
+_ThreadingExcepthook = Callable[
+    [ExceptHookArgs],
+    object,
+]
+
+
+def _dt() -> None:
+    now = dt.datetime.now()
+    print(f"[{now}]", file=sys.stderr)
+
+
+def _dttb_sys_excepthook(
+    func: _SysExcepthook,
+) -> _SysExcepthook:
+    @functools.wraps(func)
+    def wrapper(
+        exc_type: Type[BaseException],
+        exc_value: BaseException,
+        exc_traceback: Optional[TracebackType],
+    ) -> Any:
+        _dt()
+        return func(exc_type, exc_value, exc_traceback)
+
+    return wrapper
+
+
+def _dttb_threading_excepthook(
+    func: _ThreadingExcepthook,
+) -> _ThreadingExcepthook:
+    @functools.wraps(func)
+    def wrapper(
+        args: ExceptHookArgs,
+    ) -> object:
+        _dt()
+        return func(args)
+
+    return wrapper
+
+
+def apply() -> None:
+    sys.excepthook = _dttb_sys_excepthook(_sys_excepthook)
+    threading.excepthook = _dttb_threading_excepthook(_threading_excepthook)
+
+
+def reset() -> None:
+    sys.excepthook = _sys_excepthook
+    threading.excepthook = _threading_excepthook
