60cece61916c60d03a0c2ed6977516d6069a97bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:58 +0000

    Add package passagemath-modules/10.6.47/passagemath_modules-10.6.47-cp312-cp312-win_amd64.whl

---
+++ packages/passagemath-modules/passagemath_modules-10.6.47-cp312-cp312-win_amd64.whl/sage/matroids/named_matroids.py
@@ -0,0 +1,5 @@
+# sage_setup: distribution = sagemath-modules
+from sage.misc.superseded import deprecation
+from sage.matroids.database_matroids import *
+deprecation(36842, "named_matroids is deprecated, use catalog instead")
+del deprecation
