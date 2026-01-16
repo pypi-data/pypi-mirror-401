6bf5783a33fc7c21e6172692a682879ab88f2f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:08:31 +0000

    Add package mmar-mcli/1.0.13/mmar_mcli-1.0.13.tar.gz

---
+++ packages/mmar-mcli/mmar_mcli-1.0.13.tar.gz/mmar_mcli-1.0.13/src/mmar_mcli/__init__.py
@@ -0,0 +1,5 @@
+from mmar_mcli.maestro_client import MaestroClient, MESSAGE_START
+from mmar_mcli.maestro_client_dummy import MaestroClientDummy
+from mmar_mcli.models import FileData, FileName, MessageData
+
+__all__ = [MaestroClient, MaestroClientDummy, FileName, FileData, MessageData, MESSAGE_START]
