89d04da4e71dcf84d4c153b7827d5a7e8133aa66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 04:18:43 +0000

    Add package etiq-copilot/2.2.3/etiq_copilot-2.2.3-cp312-cp312-manylinux1_x86_64.whl

---
+++ packages/etiq-copilot/etiq_copilot-2.2.3-cp312-cp312-manylinux1_x86_64.whl/etiq_copilot/core/db/migrations/20230615-hello-migration.sql
@@ -0,0 +1,5 @@
+-- This migration does nothing but serves as an example for future migrations
+-- All future migrations must start with the date YYYYMMDD followed by a
+-- "kebab-case" short description.
+
+SELECT 1;
