5c66a5646b85c915c79e2514c488b04860103819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:41 +0000

    Add package abram-text-library/1.0.0/abram_text_library-1.0.0.tar.gz

---
+++ packages/abram-text-library/abram_text_library-1.0.0.tar.gz/abram_text_library-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="abram_text_library",
+    version="1.0.0",
+    description="A library made by a 10-year-old to print text in cool ways",
+    author="Abram Jindal",
+    py_modules=["text"],
+    install_requires=["rich"],
+)
