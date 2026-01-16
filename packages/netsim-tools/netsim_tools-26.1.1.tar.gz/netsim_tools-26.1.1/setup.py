5988e8a4b188f2d0e0539a224eb1183e0dfa4fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 05:09:18 +0000

    Add package netsim-tools/26.1.1/netsim_tools-26.1.1.tar.gz

---
+++ packages/netsim-tools/netsim_tools-26.1.1.tar.gz/netsim_tools-26.1.1/setup.py
@@ -0,0 +1,35 @@
+""" redirect netsim-tools to networklab """
+import sys
+from pathlib import Path
+
+from setuptools import setup
+
+sys.path.append('..')
+
+version="26.01.01"
+
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+  name="netsim-tools",
+  version=version,
+  packages=[],
+  author="Ivan Pepelnjak",
+  author_email="ip@ipspace.net",
+  description="CLI-based Virtual Networking Lab Abstraction Layer",
+  long_description=long_description,
+  install_requires=[f"networklab>={version}"],
+  classifiers=[
+    "Topic :: Utilities",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: MacOS",
+  ],
+  url="https://github.com/ipspace/netlab",
+  python_requires='>=3.8',  # Due to e.g. 'capture_output' in subprocess.run
+)
