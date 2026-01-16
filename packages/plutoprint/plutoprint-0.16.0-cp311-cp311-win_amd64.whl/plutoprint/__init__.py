afefecda32911a5a077e0e73b741ae89f16c7fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 06:06:26 +0000

    Add package plutoprint/0.16.0/plutoprint-0.16.0-cp311-cp311-win_amd64.whl

---
+++ packages/plutoprint/plutoprint-0.16.0-cp311-cp311-win_amd64.whl/plutoprint/__init__.py
@@ -0,0 +1,104 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'plutoprint.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+from ._plutoprint import (
+    __version__,
+    __version_info__,
+    __build_info__,
+
+    plutobook_version,
+    plutobook_version_string,
+    plutobook_build_info,
+    plutobook_set_fontconfig_path,
+
+    Error,
+
+    PageSize,
+    PageMargins,
+    MediaType,
+    PDFMetadata,
+    ImageFormat,
+    Canvas,
+    ImageCanvas,
+    PDFCanvas,
+    ResourceData,
+    ResourceFetcher,
+    DefaultResourceFetcher,
+    Book,
+
+    default_resource_fetcher,
+
+    PAGE_SIZE_NONE,
+    PAGE_SIZE_LETTER,
+    PAGE_SIZE_LEGAL,
+    PAGE_SIZE_LEDGER,
+    PAGE_SIZE_A3,
+    PAGE_SIZE_A4,
+    PAGE_SIZE_A5,
+    PAGE_SIZE_B4,
+    PAGE_SIZE_B5,
+
+    PAGE_MARGINS_NONE,
+    PAGE_MARGINS_NORMAL,
+    PAGE_MARGINS_NARROW,
+    PAGE_MARGINS_MODERATE,
+    PAGE_MARGINS_WIDE,
+
+    MEDIA_TYPE_PRINT,
+    MEDIA_TYPE_SCREEN,
+
+    PDF_METADATA_TITLE,
+    PDF_METADATA_AUTHOR,
+    PDF_METADATA_SUBJECT,
+    PDF_METADATA_KEYWORDS,
+    PDF_METADATA_CREATOR,
+    PDF_METADATA_CREATION_DATE,
+    PDF_METADATA_MODIFICATION_DATE,
+
+    IMAGE_FORMAT_INVALID,
+    IMAGE_FORMAT_ARGB32,
+    IMAGE_FORMAT_RGB24,
+    IMAGE_FORMAT_A8,
+    IMAGE_FORMAT_A1,
+
+    MIN_PAGE_COUNT,
+    MAX_PAGE_COUNT,
+
+    UNITS_PT,
+    UNITS_PC,
+    UNITS_IN,
+    UNITS_CM,
+    UNITS_MM,
+    UNITS_PX,
+
+    PLUTOBOOK_VERSION,
+    PLUTOBOOK_VERSION_MAJOR,
+    PLUTOBOOK_VERSION_MINOR,
+    PLUTOBOOK_VERSION_MICRO,
+    PLUTOBOOK_VERSION_STRING
+)
+
+def _init_default_fontconfig_path():
+    import os
+    import sys
+
+    fontconfig_dirs = []
+    if fontconfig_path := os.environ.get('FONTCONFIG_PATH'):
+        fontconfig_dirs.append(fontconfig_path)
+    if sys.platform == 'linux' and os.path.exists(baseconfig_file := '/etc/fonts/fonts.conf'):
+        fontconfig_dirs.append(os.path.dirname(baseconfig_file))
+    if os.path.exists(fontconfig_file := os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fontconfig', 'fonts.conf')):
+        fontconfig_dirs.append(os.path.dirname(fontconfig_file))
+    if fontconfig_dirs:
+        plutobook_set_fontconfig_path(os.pathsep.join(fontconfig_dirs))
+
+_init_default_fontconfig_path()
+del _init_default_fontconfig_path
