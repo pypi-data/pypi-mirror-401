10a773baab86c907ffba61079abf3d538a1768bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:36 +0000

    Add package apk-website-converter/0.1.3/apk_website_converter-0.1.3.tar.gz

---
+++ packages/apk-website-converter/apk_website_converter-0.1.3.tar.gz/apk_website_converter-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="apk-website-converter",  # unique name for PyPI
+    version="0.1.3",
+    author="SHAIKJANU",
+    author_email="shaikjanu08012007@gmail.com",
+    description="A library to convert website URLs into Android APKs using Apktool.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shaiksadikjanu/apk-builder", # Optional
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "Pillow",  # We rely on PIL for image processing
+    ],
+    # This is critical: it tells setup to look at MANIFEST.in
+    include_package_data=True, 
+)
