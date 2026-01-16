7b2828e0021419725f36c4a1fa69fb71cdd950b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 07:10:28 +0000

    Add package reachy-mini/1.2.11/reachy_mini-1.2.11-py3-none-any.whl

---
+++ packages/reachy-mini/reachy_mini-1.2.11-py3-none-any.whl/reachy_mini/daemon/app/services/wireless/launcher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+"$SCRIPT_DIR/generate_asoundrc.sh"
+source /venvs/mini_daemon/bin/activate
+export GST_PLUGIN_PATH=$GST_PLUGIN_PATH:/opt/gst-plugins-rs/lib/aarch64-linux-gnu/
+export PATH=$PATH:/opt/uv
+
+# Run Python in unbuffered mode (-u) to ensure logs are immediately forwarded to systemd
+python -u -m reachy_mini.daemon.app.main --wireless-version --no-autostart
