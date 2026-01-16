e6fc8d341be08fa67172480b41721c3f8c084ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 08:07:12 +0000

    Add package osbot-utils/3.71.0/osbot_utils-3.71.0.tar.gz

---
+++ packages/osbot-utils/osbot_utils-3.71.0.tar.gz/osbot_utils-3.71.0/osbot_utils/helpers/flows/schemas/Schema__Flow__Task__Data.py
@@ -0,0 +1,16 @@
+from typing                                                                       import Optional, Any
+from osbot_utils.type_safe.Type_Safe                                              import Type_Safe
+from osbot_utils.type_safe.primitives.domains.identifiers.safe_int.Timestamp_Now  import Timestamp_Now
+
+
+
+class Schema__Flow__Task__Data(Type_Safe):                      # Represents the data associated with a task execution
+    task_id          : str
+    task_name        : str
+    start_time       : Timestamp_Now
+    end_time         : Optional[Timestamp_Now]
+    status           : str                                      # 'completed', 'failed', 'running'
+    error_message    : Optional[str]
+    return_value     : Any
+    input_args       : tuple
+    input_kwargs     : dict
