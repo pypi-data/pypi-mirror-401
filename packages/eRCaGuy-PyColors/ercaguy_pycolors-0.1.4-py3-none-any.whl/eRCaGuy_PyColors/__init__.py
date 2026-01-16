a7f39e4b1e4898c6ae01716cb4b26a3b5586aa8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 03:34:13 +0000

    Add package eRCaGuy-PyColors/0.1.4/ercaguy_pycolors-0.1.4-py3-none-any.whl

---
+++ packages/eRCaGuy-PyColors/ercaguy_pycolors-0.1.4-py3-none-any.whl/eRCaGuy_PyColors/__init__.py
@@ -0,0 +1,77 @@
+"""
+eRCaGuy_PyColors - A Python module for ANSI color and format codes in terminal output.
+
+By Gabriel Staples
+https://github.com/ElectricRCAircraftGuy/eRCaGuy_PyColors
+
+Example usage:
+    import eRCaGuy_PyColors as colors
+
+    print(f"{colors.FGR}This text is green.{colors.END}")
+    print(f"{colors.FBB}This text is bright blue.{colors.END}")
+
+    colors.print_green("This text is green.")
+    colors.print_blue("This text is bright blue.")
+    colors.print_red("This text is bright red.")
+    colors.print_yellow("This text is bright yellow.")
+"""
+
+# NB: use relative imports here to avoid import issues when importing this package directly
+# as a submodule from a higher-level repo.
+from .ansi_colors import (
+    # ANSI codes and color constants
+    ANSI_START,
+    ANSI_FG_GRE,
+    ANSI_FG_BLU,
+    ANSI_FG_BR_BLU,
+    ANSI_FG_RED,
+    ANSI_FG_BR_RED,
+    ANSI_FG_BR_YEL,
+    ANSI_END,
+    ANSI_OFF,
+    F,
+    END,
+    FGN,
+    FGR,
+    FBL,
+    FBB,
+    FRE,
+    FBR,
+    FBY,
+    # Print functions
+    print_red,
+    print_yellow,
+    print_green,
+    print_blue,
+)
+
+# NB: This version number **must** be incremented, and the changes fully committed in git, to
+# trigger a new release on PyPI when you run `./deploy.sh`.
+__version__ = "0.1.4"
+__author__ = "Gabriel Staples"
+__all__ = [
+    # ANSI codes and color constants
+    "ANSI_START",
+    "ANSI_FG_GRE",
+    "ANSI_FG_BLU",
+    "ANSI_FG_BR_BLU",
+    "ANSI_FG_RED",
+    "ANSI_FG_BR_RED",
+    "ANSI_FG_BR_YEL",
+    "ANSI_END",
+    "ANSI_OFF",
+    "F",
+    "END",
+    "FGN",
+    "FGR",
+    "FBL",
+    "FBB",
+    "FRE",
+    "FBR",
+    "FBY",
+    # Print functions
+    "print_red",
+    "print_yellow",
+    "print_green",
+    "print_blue",
+]
