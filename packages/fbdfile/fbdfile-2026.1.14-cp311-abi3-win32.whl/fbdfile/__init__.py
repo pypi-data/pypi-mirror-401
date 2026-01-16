d7008170ac6b41980b074c03618e6e0ecb2b03d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 08:07:15 +0000

    Add package fbdfile/2026.1.14/fbdfile-2026.1.14-cp311-abi3-win32.whl

---
+++ packages/fbdfile/fbdfile-2026.1.14-cp311-abi3-win32.whl/fbdfile/__init__.py
@@ -0,0 +1,9 @@
+# fbdfile/__init__.py
+
+from .fbdfile import *
+from .fbdfile import __all__, __doc__, __version__
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Fbdfile version string."""
