77c84a54ac4a056ffff5d9a47bb57255fc765af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 04:19:26 +0000

    Add package sigexec/2026.35/sigexec-2026.35-py3-none-any.whl

---
+++ packages/sigexec/sigexec-2026.35-py3-none-any.whl/sigexec/core/__init__.py
@@ -0,0 +1,8 @@
+"""Core components for the signal processing execution graph."""
+
+from .data import GraphData
+from .graph import Graph, create_graph
+
+__all__ = ['GraphData', 'Graph', 'create_graph']
+
+
