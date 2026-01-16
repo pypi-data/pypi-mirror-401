330419399267fdc99fe545d5f9c714eb6884f55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:29 +0000

    Add package comfyui-frontend-package/1.38.2/comfyui_frontend_package-1.38.2.tar.gz

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.38.2.tar.gz/comfyui_frontend_package-1.38.2/comfyui_frontend_package/static/assets/WidgetLegacy-DgBI3-l2.js
@@ -0,0 +1,2 @@
+import{b3 as o}from"./index-Bc79VbnU.js";import"./vendor-primevue-Cif--Rbw.js";import"./vendor-other-Dsj-QuOx.js";import"./vendor-vue-DNQSPQQ2.js";import"./vendor-reka-ui-WjwjFuZo.js";import"./vendor-xterm-CWYFmgbN.js";import"./vendor-three-ByuY8CdW.js";import"./vendor-tiptap-YMjM2h-Z.js";export{o as default};
+//# sourceMappingURL=WidgetLegacy-DgBI3-l2.js.map
