6896480b499cc29f856d11aa55caf647a2724158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 08:07:13 +0000

    Add package airbyte-agent-linear/0.19.39/airbyte_agent_linear-0.19.39.tar.gz

---
+++ packages/airbyte-agent-linear/airbyte_agent_linear-0.19.39.tar.gz/airbyte_agent_linear-0.19.39/airbyte_agent_linear/__init__.py
@@ -0,0 +1,43 @@
+"""
+Blessed Linear connector for Airbyte SDK.
+
+Auto-generated from OpenAPI specification.
+"""
+
+from .connector import LinearConnector
+from .models import (
+    LinearAuthConfig,
+    Issue,
+    IssuesListResponseDataIssuesPageinfo,
+    IssuesListResponseDataIssues,
+    IssuesListResponseData,
+    IssuesListResponse,
+    IssueResponseData,
+    IssueResponse,
+    Project,
+    ProjectsListResponseDataProjectsPageinfo,
+    ProjectsListResponseDataProjects,
+    ProjectsListResponseData,
+    ProjectsListResponse,
+    ProjectResponseData,
+    ProjectResponse,
+    Team,
+    TeamsListResponseDataTeamsPageinfo,
+    TeamsListResponseDataTeams,
+    TeamsListResponseData,
+    TeamsListResponse,
+    TeamResponseData,
+    TeamResponse,
+    LinearExecuteResult,
+    LinearExecuteResultWithMeta
+)
+from .types import (
+    IssuesListParams,
+    IssuesGetParams,
+    ProjectsListParams,
+    ProjectsGetParams,
+    TeamsListParams,
+    TeamsGetParams
+)
+
+__all__ = ["LinearConnector", "LinearAuthConfig", "Issue", "IssuesListResponseDataIssuesPageinfo", "IssuesListResponseDataIssues", "IssuesListResponseData", "IssuesListResponse", "IssueResponseData", "IssueResponse", "Project", "ProjectsListResponseDataProjectsPageinfo", "ProjectsListResponseDataProjects", "ProjectsListResponseData", "ProjectsListResponse", "ProjectResponseData", "ProjectResponse", "Team", "TeamsListResponseDataTeamsPageinfo", "TeamsListResponseDataTeams", "TeamsListResponseData", "TeamsListResponse", "TeamResponseData", "TeamResponse", "LinearExecuteResult", "LinearExecuteResultWithMeta", "IssuesListParams", "IssuesGetParams", "ProjectsListParams", "ProjectsGetParams", "TeamsListParams", "TeamsGetParams"]
