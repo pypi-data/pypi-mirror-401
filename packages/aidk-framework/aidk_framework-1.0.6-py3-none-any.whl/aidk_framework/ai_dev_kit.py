7ef8a4b3439e03707053440dba7fd0a4829c0a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 03:34:26 +0000

    Add package aidk-framework/1.0.6/aidk_framework-1.0.6-py3-none-any.whl

---
+++ packages/aidk-framework/aidk_framework-1.0.6-py3-none-any.whl/aidk_framework/ai_dev_kit.py
@@ -0,0 +1,6 @@
+import aidk_framework.tranformers_module as transformers_kit
+import aidk_framework.neuralnet_module as neuralnet_kit
+import aidk_framework.prediction_module as prediction_kit
+import aidk_framework.neuromorphic_nn as neuromorphic_nn
+
+
