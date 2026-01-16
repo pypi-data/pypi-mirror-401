4e31be633b1d9fa70699839677dccd75a8325dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 13:25:44 +0000

    Add package capellambse/0.6.21/capellambse-0.6.21-cp310-abi3-macosx_10_13_x86_64.whl

---
+++ packages/capellambse/capellambse-0.6.21-cp310-abi3-macosx_10_13_x86_64.whl/capellambse/filehandler/git_askpass.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+# SPDX-FileCopyrightText: Copyright DB InfraGO AG
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import sys
+
+if __name__ == "__main__":
+    if "username" in sys.argv[1].lower():
+        print(os.environ["GIT_USERNAME"])
+    elif "password" in sys.argv[1].lower():
+        print(os.environ["GIT_PASSWORD"])
+    else:
+        raise SystemExit(1)
