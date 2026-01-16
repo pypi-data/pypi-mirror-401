da92351a4693e3d405693f2d470fcbdff0c51dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 11:06:41 +0000

    Add package yklibpy/0.1.11/yklibpy-0.1.11-py3-none-any.whl

---
+++ packages/yklibpy/yklibpy-0.1.11-py3-none-any.whl/yklibpy/common/__init__.py
@@ -0,0 +1,19 @@
+from .env import Env
+from .info import Info
+from .util import Util
+
+__all__ = ["Env", "Info", "Util"]
+
+
+def xmain() -> str:
+    print("Hello from yklibpy!")
+    return "Hello from yklibpy!"
+
+
+def ymain() -> str:
+    print("Y Hello from yklibpy!")
+    return "Y Hello from yklibpy!"
+
+
+if __name__ == "__main__":
+    xmain()
