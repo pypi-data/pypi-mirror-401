a07f903420278a93db456177de0f9c5ac23231e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 03:33:59 +0000

    Add package smartmeter-datacollector-configurator/1.3.0/smartmeter_datacollector_configurator-1.3.0-py3-none-any.whl

---
+++ packages/smartmeter-datacollector-configurator/smartmeter_datacollector_configurator-1.3.0-py3-none-any.whl/smartmeter_datacollector_configurator/dto.py
@@ -0,0 +1,112 @@
+from enum import Enum
+from typing import List, Optional
+
+from pydantic import BaseModel as PydanticBaseModel
+from pydantic import Field
+from pydantic.class_validators import validator
+
+LOGGER_LEVEL = ["DEBUG", "INFO", "WARNING", "ERROR", "FATAL", "CRITICAL"]
+
+
+class MeterType(str, Enum):
+    LGE450 = "lge450"
+    LGE360 = "lge360"
+    LGE570 = "lge570"
+    ISKRAAM550 = "iskraam550"
+    KAMSTRUP_HAN = "kamstrup_han"
+
+
+class SinkType(str, Enum):
+    MQTT = "mqtt"
+    LOGGER = "logger"
+
+
+class BaseModel(PydanticBaseModel):
+    # pylint: disable=too-few-public-methods
+    class Config:
+        anystr_strip_whitespace = True
+        use_enum_values = True
+
+
+class MeterDto(BaseModel):
+    type: MeterType
+    port: str
+    key: Optional[str] = None
+
+    @validator("port")
+    @classmethod
+    def port_not_empty(cls, val: str):
+        if not val.strip():
+            raise ValueError("Smart meter port must not be empty.")
+        return val.strip()
+
+
+class MqttSinkDto(BaseModel):
+    type: SinkType = Field(default=SinkType.MQTT, const=True)
+    host: str
+    port: int = 1883
+    tls: bool = False
+    ca_cert: Optional[str] = None
+    check_hostname: bool = True
+    password: Optional[str] = None
+    username: Optional[str] = None
+
+    @validator("host")
+    @classmethod
+    def host_not_empty(cls, val: str):
+        if not val:
+            raise ValueError("MQTT host must not be empty.")
+        return val
+
+    @validator("port")
+    @classmethod
+    def port_valid_range(cls, val: int):
+        if val <= 0 or val > 65535:
+            raise ValueError(f"Invalid MQTT sink port {val}.")
+        return val
+
+    @validator("username")
+    @classmethod
+    def username_password_exists(cls, val: str, values):
+        if val and not values["password"]:
+            raise ValueError("No password set for username.")
+        return val
+
+
+class LoggerSinkDto(BaseModel):
+    type: SinkType = Field(default=SinkType.LOGGER, const=True)
+    name: str = "DataLogger"
+
+    @validator("name")
+    @classmethod
+    def name_not_empty(cls, val: str):
+        if not val.strip():
+            raise ValueError("Name must not be empty.")
+        return val.strip()
+
+
+class ConfigDto(BaseModel):
+    log_level: str = "WARNING"
+    meters: List[MeterDto] = []
+    mqtt_sink: Optional[MqttSinkDto] = None
+    logger_sink: Optional[LoggerSinkDto] = None
+
+    @validator("log_level")
+    @classmethod
+    def log_level_valid(cls, val: str):
+        lvl = val.strip().upper()
+        if lvl not in LOGGER_LEVEL:
+            raise ValueError(f"Invalid logging level '{lvl}'. Must be one of {LOGGER_LEVEL}")
+        return lvl
+
+
+class CredentialsDto(BaseModel):
+    password: str
+
+    @validator("password")
+    @classmethod
+    def password_valid(cls, val: str):
+        pwd = val.strip()
+        if len(pwd) < 8 or len(pwd) > 30:
+            raise ValueError("Invalid password length.")
+        return pwd
---
+++ packages/smartmeter-datacollector-configurator/smartmeter_datacollector_configurator-1.3.0-py3-none-any.whl/smartmeter_datacollector_configurator/system.py
@@ -0,0 +1,109 @@
+import asyncio
+import logging
+import subprocess
+from typing import List
+
+LOGGER = logging.getLogger("uvicorn.error")
+
+SYSCTL_BIN = "/bin/systemctl"
+LS_BIN = "/bin/ls"
+
+DATACOL_SERVICE = "smartmeter-datacollector"
+
+# Demo services
+BROKER_SERVICE = "mosquitto"
+TELEGRAF_SERVICE = "telegraf"
+INFLUX_SERVICE = "influxdb"
+GRAFANA_SERVICE = "grafana-server"
+
+
+class NoPermissionError(Exception):
+    pass
+
+
+class NotInstalledError(Exception):
+    pass
+
+
+class GeneralSystemError(Exception):
+    pass
+
+
+async def restart_datacollector() -> None:
+    proc = await asyncio.create_subprocess_exec(
+        SYSCTL_BIN,
+        "restart",
+        DATACOL_SERVICE)
+
+    return_code = await proc.wait()
+    _check_for_error(return_code, DATACOL_SERVICE)
+    LOGGER.info("%s successfully restarted.", DATACOL_SERVICE)
+
+
+async def restart_demo() -> List[str]:
+    demo_services = [
+        GRAFANA_SERVICE,
+        TELEGRAF_SERVICE,
+        INFLUX_SERVICE,
+        BROKER_SERVICE
+    ]
+    not_installed = []
+
+    # Stop all demo services
+    for service in demo_services:
+        proc = await asyncio.create_subprocess_exec(
+            SYSCTL_BIN,
+            "stop",
+            service)
+
+        return_code = await proc.wait()
+        try:
+            _check_for_error(return_code, service)
+        except NotInstalledError:
+            # continue if one of the services is not installed
+            not_installed.append(service)
+            continue
+
+    demo_services.reverse()
+    # Start all demo services in other directions
+    for service in demo_services:
+        proc = await asyncio.create_subprocess_exec(
+            SYSCTL_BIN,
+            "start",
+            service)
+
+        return_code = await proc.wait()
+        try:
+            _check_for_error(return_code, service)
+        except NotInstalledError:
+            # continue if one of the services is not installed
+            continue
+
+    if len(not_installed) > 0:
+        LOGGER.warning("Services %s not found.", not_installed)
+    else:
+        LOGGER.info("Demo services successfully restarted.")
+
+    return not_installed
+
+
+def retrieve_tty_devices() -> List[str]:
+    cmd = f"{LS_BIN} /dev/serial/by-id/*"
+    try:
+        output = subprocess.run(cmd, shell=True, text=True, capture_output=True, check=True)
+    except subprocess.CalledProcessError as ex:
+        LOGGER.error("Unable to get tty devices. (%s)", ex)
+        return []
+    return output.stdout.rsplit()
+
+
+def _check_for_error(return_code: int, service: str) -> None:
+    if return_code == 4:
+        LOGGER.error("Insufficient system privileges.")
+        raise NoPermissionError("Insufficient system privileges to restart service.")
+    if return_code == 5:
+        LOGGER.error("%s is not installed.", service)
+        raise NotInstalledError(f"{service} is not installed.")
+    if return_code > 0:
+        LOGGER.error("General error %s", service)
+        raise GeneralSystemError(f"Return code: {return_code}")
