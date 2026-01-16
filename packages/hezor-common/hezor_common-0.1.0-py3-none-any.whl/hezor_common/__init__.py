cd6a9c1475c4a770305ff0c89830a75a38787456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:43 +0000

    Add package hezor-common/0.1.0/hezor_common-0.1.0-py3-none-any.whl

---
+++ packages/hezor-common/hezor_common-0.1.0-py3-none-any.whl/hezor_common/__init__.py
@@ -0,0 +1,11 @@
+"""Hezor Common - Common utilities for Hezor projects."""
+
+__version__ = "0.1.0"
+
+def hello() -> str:
+    """Return a greeting message."""
+    return "Hello from hezor-common!"
+
+
+__all__ = ["hello", "__version__"]
+
