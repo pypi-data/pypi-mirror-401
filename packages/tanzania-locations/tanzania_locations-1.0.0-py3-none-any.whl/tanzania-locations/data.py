2751379b48cd5c16103104cbb7ddeb9fa151633f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:08:25 +0000

    Add package tanzania-locations/1.0.0/tanzania_locations-1.0.0-py3-none-any.whl

---
+++ packages/tanzania-locations/tanzania_locations-1.0.0-py3-none-any.whl/tanzania-locations/data.py
@@ -0,0 +1,30 @@
+REGIONS = {
+    "Dar es Salaam": [
+        "Ilala",
+        "Kinondoni",
+        "Temeke",
+        "Ubungo",
+        "Kigamboni"
+    ],
+    "Arusha": [
+        "Arusha City",
+        "Arumeru",
+        "Monduli",
+        "Karatu",
+        "Longido"
+    ],
+    "Mwanza": [
+        "Ilemela",
+        "Nyamagana",
+        "Sengerema",
+        "Magu",
+        "Kwimba"
+    ],
+    "Dodoma": [
+        "Dodoma City",
+        "Bahi",
+        "Chamwino",
+        "Kondoa",
+        "Mpwapwa"
+    ]
+}
