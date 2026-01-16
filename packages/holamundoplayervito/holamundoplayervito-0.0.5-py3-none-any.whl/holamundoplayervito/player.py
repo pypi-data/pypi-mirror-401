713ec1a38dd69d404945fd679c7b07fa9c407ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 03:34:45 +0000

    Add package holamundoplayervito/0.0.5/holamundoplayervito-0.0.5-py3-none-any.whl

---
+++ packages/holamundoplayervito/holamundoplayervito-0.0.5-py3-none-any.whl/holamundoplayervito/player.py
@@ -0,0 +1,23 @@
+"""Esta es el modulo que incluye la clase de reproductor de musica"""
+
+class Player:
+    """
+    Esta clase crea un reproductor de musica
+    """
+
+    def play(self, song):
+        """
+        Reproduce la cancion que se paso como parametro
+        
+        Parameters:
+        song (str): este es un string con el path de la cancion
+
+        Returns:
+        int: Devuele 1 si reproduce con exito, devuelve 0 en casod e fracaso
+        """
+
+
+        print("Reproduciendo musica")
+
+    def stop(self):    
+        print("stopping")
