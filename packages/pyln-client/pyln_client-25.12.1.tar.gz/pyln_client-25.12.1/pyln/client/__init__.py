1d267dbb9fb65eddcf43f265c57c53e7ec706ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 08:07:18 +0000

    Add package pyln-client/25.12.1/pyln_client-25.12.1.tar.gz

---
+++ packages/pyln-client/pyln_client-25.12.1.tar.gz/pyln_client-25.12.1/pyln/client/__init__.py
@@ -0,0 +1,25 @@
+from .lightning import LightningRpc, RpcError, Millisatoshi
+from .plugin import Plugin, monkey_patch, RpcException
+from .gossmap import Gossmap, GossmapNode, GossmapChannel, GossmapHalfchannel, GossmapNodeId, LnFeatureBits
+from .gossmapstats import GossmapStats
+from .version import NodeVersion
+
+__version__ = "25.12.1"
+
+__all__ = [
+    "LightningRpc",
+    "Plugin",
+    "RpcError",
+    "RpcException",
+    "Millisatoshi",
+    "__version__",
+    "monkey_patch",
+    "Gossmap",
+    "GossmapNode",
+    "GossmapChannel",
+    "GossmapHalfchannel",
+    "GossmapNodeId",
+    "LnFeatureBits",
+    "GossmapStats",
+    "NodeVersion",
+]
