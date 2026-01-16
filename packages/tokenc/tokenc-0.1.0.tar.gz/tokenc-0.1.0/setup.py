5793b684c6e79c68401b9e1e62700aa30f648ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:10:18 +0000

    Add package tokenc/0.1.0/tokenc-0.1.0.tar.gz

---
+++ packages/tokenc/tokenc-0.1.0.tar.gz/tokenc-0.1.0/setup.py
@@ -0,0 +1,44 @@
+"""
+Setup configuration for tokenc package
+"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tokenc",
+    version="0.1.0",
+    author="The Token Company",
+    description="Python SDK for The Token Company API - Compress LLM inputs to reduce costs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/tokenc",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "requests>=2.25.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=7.0.0",
+            "pytest-cov>=3.0.0",
+            "black>=22.0.0",
+            "mypy>=0.950",
+            "flake8>=4.0.0",
+        ],
+    },
+)
