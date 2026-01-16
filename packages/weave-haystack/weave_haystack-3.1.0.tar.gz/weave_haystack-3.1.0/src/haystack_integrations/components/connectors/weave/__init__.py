8b1191bf7b01d4df326b38ff33c2f06e0976dc21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 04:19:28 +0000

    Add package weave-haystack/3.1.0/weave_haystack-3.1.0.tar.gz

---
+++ packages/weave-haystack/weave_haystack-3.1.0.tar.gz/weave_haystack-3.1.0/src/haystack_integrations/components/connectors/weave/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2025-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .weave_connector import WeaveConnector
+
+__all__ = ["WeaveConnector"]
