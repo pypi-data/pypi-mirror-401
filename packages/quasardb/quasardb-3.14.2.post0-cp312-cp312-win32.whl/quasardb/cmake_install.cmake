65cc99709a219669236714133af351f018593c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 13:26:02 +0000

    Add package quasardb/3.14.2.post0/quasardb-3.14.2.post0-cp312-cp312-win32.whl

---
+++ packages/quasardb/quasardb-3.14.2.post0-cp312-cp312-win32.whl/quasardb/cmake_install.cmake
@@ -0,0 +1,48 @@
+# Install script for directory: C:/TeamCity/work/938b0bdf6727d1ad/thirdparty
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/quasardb")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/TeamCity/work/938b0bdf6727d1ad/build/lib.win32-cpython-312/quasardb/date/cmake_install.cmake")
+  include("C:/TeamCity/work/938b0bdf6727d1ad/build/lib.win32-cpython-312/quasardb/pybind11/cmake_install.cmake")
+  include("C:/TeamCity/work/938b0bdf6727d1ad/build/lib.win32-cpython-312/quasardb/range-v3/cmake_install.cmake")
+
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "C:/TeamCity/work/938b0bdf6727d1ad/build/lib.win32-cpython-312/quasardb/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
