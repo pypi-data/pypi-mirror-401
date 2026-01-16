b03e0357a69cb5971806bd920c6ccb0b5d98101e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:08:51 +0000

    Add package prospr/1.2.10/prospr-1.2.10-cp312-cp312-win32.whl

---
+++ packages/prospr/prospr-1.2.10-cp312-cp312-win32.whl/prospr/core/src/depth_first_bnb.hpp
@@ -0,0 +1,19 @@
+/* File:            depth_first_bnb.h
+ * Description:     Header file for a depth-first branch-and-bound search
+ *                  function.
+ * License:         This file is licensed under the GNU LGPL V3 license by
+ *                  Okke van Eck (2020 - 2023). See the LICENSE file for the
+ *                  specifics.
+ */
+
+#ifndef DEPTH_FIRST_BNB_H
+#define DEPTH_FIRST_BNB_H
+
+#include "protein.hpp"
+
+/* A depth-first branch-and-bound search function for finding a minimum energy
+ * conformation.
+ */
+void depth_first_bnb(Protein *protein, std::string prune_func = "");
+
+#endif
