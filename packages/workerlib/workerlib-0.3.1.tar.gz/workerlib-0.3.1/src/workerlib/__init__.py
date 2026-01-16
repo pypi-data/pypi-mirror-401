769c31bdb8ddc772f803d78000151ae0d1d16d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:09:17 +0000

    Add package workerlib/0.3.1/workerlib-0.3.1.tar.gz

---
+++ packages/workerlib/workerlib-0.3.1.tar.gz/workerlib-0.3.1/src/workerlib/__init__.py
@@ -0,0 +1,22 @@
+from src.workerlib.connection import RabbitMQConnection, ConnectionParams, ConnectionError
+from src.workerlib.mq_queue import RabbitMQQueue, QueueConfig
+from src.workerlib.consumer import RabbitMQConsumer, ErrorHandlingStrategy
+from src.workerlib.pool import WorkerPool
+from src.workerlib.producer import RabbitMQProducer
+from src.workerlib.retry import RetryConfig, retry_on_failure
+
+__version__ = "0.3.1"
+
+__all__ = [
+    'WorkerPool',
+    'RabbitMQConnection',
+    'ConnectionParams',
+    'ConnectionError',
+    'RabbitMQQueue',
+    'QueueConfig',
+    'RabbitMQConsumer',
+    'ErrorHandlingStrategy',
+    'RabbitMQProducer',
+    'RetryConfig',
+    'retry_on_failure'
+]
