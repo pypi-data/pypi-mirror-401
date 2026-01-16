71112ee3ce2ef8a6c6b135b422b6a90bb184edb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:57 +0000

    Add package passagemath-cmr/10.6.47/passagemath_cmr-10.6.47-cp310-cp310-win_amd64.whl

---
+++ packages/passagemath-cmr/passagemath_cmr-10.6.47-cp310-cp310-win_amd64.whl/sage/all__sagemath_cmr.py
@@ -0,0 +1,14 @@
+# sage_setup: distribution = sagemath-cmr
+# start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'passagemath_cmr.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+from sage.all__sagemath_graphs import *
+from sage.all__sagemath_modules import *
