2cd429ec951f448ad1fe80460c40658d34a9de2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 07:09:43 +0000

    Add package codepacker/12.4.0/codepacker-12.4.0.tar.gz

---
+++ packages/codepacker/codepacker-12.4.0.tar.gz/codepacker-12.4.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="codepacker",
+    version="12.4.0",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "codepacker=codepacker.GUI:start",
+        ],
+    },
+)
