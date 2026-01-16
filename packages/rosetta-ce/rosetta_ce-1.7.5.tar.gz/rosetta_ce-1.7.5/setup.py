008022e272a0273cb1d973261b841a19872874a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 13:26:08 +0000

    Add package rosetta-ce/1.7.5/rosetta_ce-1.7.5.tar.gz

---
+++ packages/rosetta-ce/rosetta_ce-1.7.5.tar.gz/rosetta_ce-1.7.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rosetta-ce",
+    version="1.7.5",
+    author="Ayman Mahmoud",
+    author_email="content@ayman.online",
+    description="Rosetta is a Python package that can be used to fake security logs and alerts for testing different "
+                "detection and response use cases.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ayman-m/rosetta",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "requests",
+        "faker",
+        "urllib3"
+    ]
+)
