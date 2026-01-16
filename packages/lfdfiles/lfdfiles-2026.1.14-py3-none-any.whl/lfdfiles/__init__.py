a0a45f1c2902ea7808c62351d36a1451df2f7b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 08:06:45 +0000

    Add package lfdfiles/2026.1.14/lfdfiles-2026.1.14-py3-none-any.whl

---
+++ packages/lfdfiles/lfdfiles-2026.1.14-py3-none-any.whl/lfdfiles/__init__.py
@@ -0,0 +1,9 @@
+# lfdfiles/__init__.py
+
+from .lfdfiles import *
+from .lfdfiles import __all__, __doc__, __version__
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Lfdfiles version string."""
