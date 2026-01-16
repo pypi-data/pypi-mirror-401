c431fce945c054050413c681d45e78e673714fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:08:16 +0000

    Add package kitconcept.voltolighttheme/7.6.7/kitconcept_voltolighttheme-7.6.7-py3-none-any.whl

---
+++ packages/kitconcept.voltolighttheme/kitconcept_voltolighttheme-7.6.7-py3-none-any.whl/kitconcept/voltolighttheme/__init__.py
@@ -0,0 +1,14 @@
+"""Init and utils."""
+
+from zope.i18nmessageid import MessageFactory
+
+import logging
+
+
+__version__ = "7.6.7"
+
+PACKAGE_NAME = "kitconcept.voltolighttheme"
+
+_ = MessageFactory(PACKAGE_NAME)
+
+logger = logging.getLogger(PACKAGE_NAME)
