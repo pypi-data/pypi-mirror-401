9b5947a86109afe55cdf1c4057266d4ed95f11d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 03:34:26 +0000

    Add package django-integrations/0.4.0/django_integrations-0.4.0-py3-none-any.whl

---
+++ packages/django-integrations/django_integrations-0.4.0-py3-none-any.whl/integrations/api/seriealizers.py
@@ -0,0 +1,9 @@
+# --------------------------------------------
+# Copyright 2021, Grant Viklund
+# @Author: Grant Viklund
+# @Date:   2021-06-01 17:00:10
+# --------------------------------------------
+
+# from rest_framework import fields, serializers
+
+# from integrations.models import *
