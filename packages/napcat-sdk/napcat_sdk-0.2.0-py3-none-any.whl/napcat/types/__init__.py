373000ce033483eb5e6c9bf7c25094ac7c7e7cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 03:33:50 +0000

    Add package napcat-sdk/0.2.0/napcat_sdk-0.2.0-py3-none-any.whl

---
+++ packages/napcat-sdk/napcat_sdk-0.2.0-py3-none-any.whl/napcat/types/__init__.py
@@ -0,0 +1,52 @@
+# src/napcat/types/__init__.py
+from .events import (
+    GroupMessageEvent,
+    HeartbeatEvent,
+    LifecycleMetaEvent,
+    MessageEvent,
+    MessageSender,
+    MetaEvent,
+    NapCatEvent,
+    PrivateMessageEvent,
+    UnknownEvent,
+)
+from .messages import (
+    AtMessageSegment,
+    FileMessageSegment,
+    ForwardMessageSegment,
+    ImageMessageSegment,
+    ImageSubType,
+    MessageSegment,
+    ReplyMessageSegment,
+    TextMessageSegment,
+    UnknownMessageSegment,
+    VideoMessageSegment,
+)
+from .utils import IgnoreExtraArgsMixin, TypeValidatorMixin
+
+__all__ = [
+    # events (most used)
+    "NapCatEvent",
+    "MetaEvent",
+    "MessageEvent",
+    "PrivateMessageEvent",
+    "GroupMessageEvent",
+    "HeartbeatEvent",
+    "LifecycleMetaEvent",
+    "UnknownEvent",
+    "MessageSender",
+    # message segments (most used)
+    "MessageSegment",
+    "TextMessageSegment",
+    "ReplyMessageSegment",
+    "ImageMessageSegment",
+    "VideoMessageSegment",
+    "FileMessageSegment",
+    "AtMessageSegment",
+    "ForwardMessageSegment",
+    "UnknownMessageSegment",
+    # misc
+    "ImageSubType",
+    "IgnoreExtraArgsMixin",
+    "TypeValidatorMixin",
+]
