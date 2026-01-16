a4fa913322b72b324878402c0009d78476c5aa86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:08:43 +0000

    Add package substrait/0.26.0/substrait-0.26.0.tar.gz

---
+++ packages/substrait/substrait-0.26.0.tar.gz/substrait-0.26.0/tests/test_proto.py
@@ -0,0 +1,32 @@
+# ruff: noqa: F401
+
+
+def test_imports():
+    """Temporary sanity test"""
+    from substrait.gen.proto.algebra_pb2 import Expression
+    from substrait.gen.proto.capabilities_pb2 import Capabilities
+    from substrait.gen.proto.extended_expression_pb2 import ExtendedExpression
+    from substrait.gen.proto.extensions.extensions_pb2 import SimpleExtensionURN
+    from substrait.gen.proto.function_pb2 import FunctionSignature
+    from substrait.gen.proto.parameterized_types_pb2 import ParameterizedType
+    from substrait.gen.proto.plan_pb2 import Plan
+    from substrait.gen.proto.type_expressions_pb2 import DerivationExpression
+    from substrait.gen.proto.type_pb2 import Type
+
+
+def test_proto_proxy_module():
+    """Test that protocol classes are made available in substrait.proto"""
+    import substrait.proto
+
+    assert {"Plan", "Type", "NamedStruct", "RelRoot"} <= set(dir(substrait.proto))
+    assert {
+        "algebra",
+        "capabilities",
+        "extensions",
+        "extended_expression",
+        "function",
+        "parameterized_types",
+        "plan",
+        "type_expressions",
+        "type",
+    } <= set(dir(substrait.proto))
---
+++ packages/substrait/substrait-0.26.0.tar.gz/substrait-0.26.0/update_submodule.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if ! command -v curl > /dev/null 2>&1; then
+    echo "curl is required to grab the latest version tag"
+fi
+
+VERSION=$(curl -s https://api.github.com/repos/substrait-io/substrait/releases/latest | grep 'tag_name' | cut -d '"' -f 4)
+
+echo "Updating substrait submodule..."
+git submodule update --remote third_party/substrait
+
+DIR=$(cd "$(dirname "$0")" && pwd)
+pushd "${DIR}"/third_party/substrait/ || exit
+git checkout "$VERSION"
+SUBSTRAIT_HASH=$(git rev-parse --short HEAD)
+popd || exit
+
+VERSION=${VERSION//v/}
+
+sed -i "s#__substrait_hash__.*#__substrait_hash__ = \"$SUBSTRAIT_HASH\"#g" src/substrait/__init__.py
+sed -i "s#__substrait_version__.*#__substrait_version__ = \"$VERSION\"#g" src/substrait/__init__.py
