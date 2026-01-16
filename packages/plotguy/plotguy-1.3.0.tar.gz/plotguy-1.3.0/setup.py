078008960ad9073b48e0e2f72bf7281e9656fbd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 03:34:13 +0000

    Add package plotguy/1.3.0/plotguy-1.3.0.tar.gz

---
+++ packages/plotguy/plotguy-1.3.0.tar.gz/plotguy-1.3.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plotguy",
+    version="1.3.0",
+    author="Plotguy Team",
+    author_email="plotguy.info@gmail.com",
+    description="Plotguy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[         
+        'pandas',  
+        'numpy',  
+        'hkfdb', 
+        'pyarrow',
+        'polars==0.18.15',
+        'plotly==5.18.0',
+        'lxml',        
+        'dash==2.9.3',
+        'dash_bootstrap_components',
+        'dash_daq',
+        'dash_dangerously_set_inner_html',
+    ],
+    url="https://pypi.org/project/plotguy/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
