0a20e4fde80c43db47958851e9b8c55810fbeeff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 08:07:06 +0000

    Add package ai4pa-opencode-sdk/0.10.0/ai4pa_opencode_sdk-0.10.0-py3-none-any.whl

---
+++ packages/ai4pa-opencode-sdk/ai4pa_opencode_sdk-0.10.0-py3-none-any.whl/opencode_sdk/resources/provider/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .oauth import (
+    OAuthResource,
+    AsyncOAuthResource,
+    OAuthResourceWithRawResponse,
+    AsyncOAuthResourceWithRawResponse,
+    OAuthResourceWithStreamingResponse,
+    AsyncOAuthResourceWithStreamingResponse,
+)
+from .provider import (
+    ProviderResource,
+    AsyncProviderResource,
+    ProviderResourceWithRawResponse,
+    AsyncProviderResourceWithRawResponse,
+    ProviderResourceWithStreamingResponse,
+    AsyncProviderResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "OAuthResource",
+    "AsyncOAuthResource",
+    "OAuthResourceWithRawResponse",
+    "AsyncOAuthResourceWithRawResponse",
+    "OAuthResourceWithStreamingResponse",
+    "AsyncOAuthResourceWithStreamingResponse",
+    "ProviderResource",
+    "AsyncProviderResource",
+    "ProviderResourceWithRawResponse",
+    "AsyncProviderResourceWithRawResponse",
+    "ProviderResourceWithStreamingResponse",
+    "AsyncProviderResourceWithStreamingResponse",
+]
