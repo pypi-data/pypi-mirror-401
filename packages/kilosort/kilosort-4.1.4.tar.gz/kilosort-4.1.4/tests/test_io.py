e4921f18dd29438ed6b025102d0cb0f4abfb9542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 05:09:52 +0000

    Add package kilosort/4.1.4/kilosort-4.1.4.tar.gz

---
+++ packages/kilosort/kilosort-4.1.4.tar.gz/kilosort-4.1.4/tests/test_io.py
@@ -0,0 +1,292 @@
+import pytest
+import tempfile
+from pathlib import Path
+
+import numpy as np
+import torch
+
+from kilosort import io
+
+
+def test_probe_io():
+    # Create one-column probe with 5 contacts, spaced 1um apart.
+    json_probe = {
+        'chanMap': np.arange(5),
+        'xc': np.ones(5),
+        'yc': np.arange(5),
+        'kcoords': np.zeros(5),
+        'n_chan': 5
+    }
+    # Repeat in .prb format
+    prb_probe = """
+channel_groups = {
+    0: {
+            'channels' : [0,1,2,3,4],
+            'geometry': {
+                0: [1, 0],
+                1: [1, 1],
+                2: [1, 2],
+                3: [1, 3],
+                4: [1, 4]
+            }
+    }
+}
+"""
+    
+    # Save both to file
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.json', delete=False) as f:
+        json_file = Path(f.name)
+        print(json_file)
+        io.save_probe(json_probe, json_file)
+
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.prb', delete=False) as f:
+        f.write(prb_probe)
+        prb_file = Path(f.name)
+
+    # Load both with kilosort.io
+    probe1 = io.load_probe(json_file)
+    probe2 = io.load_probe(prb_file)
+
+    print('probe1:')
+    print(probe1)
+    print('probe2:')
+    print(probe2)
+
+    try:
+        # Verify that loaded probes contain the same information
+        for k in ['chanMap', 'xc', 'yc', 'kcoords', 'n_chan']:
+            print(f'testing key {k}')
+            assert (k in probe1) and (k in probe2)
+            assert np.all(probe1[k] == probe2[k])
+    finally:
+        # Remove temporary files
+        json_file.unlink()
+        prb_file.unlink()
+
+
+def test_bad_channels():
+    probe = {
+        'xc': np.zeros(5), 'yc': np.arange(5)*10, 'kcoords': np.zeros(5),
+        'chanMap': np.arange(5), 'n_chan': 5
+        }
+    bad_channels = [2, 4]
+    probe2 = io.remove_bad_channels(probe, bad_channels)
+    assert probe2['n_chan'] == 3
+    for k in ['xc', 'yc', 'kcoords']:
+        assert probe2[k].size == 3
+    assert np.all(probe2['chanMap'] == [0, 1, 3])
+    assert np.all(probe['chanMap'] == [0, 1, 2, 3, 4])  # original unchanged
+
+    with pytest.raises(IndexError):
+        # These are not in the channel map.
+        _ = io.remove_bad_channels(probe, [5, 6])
+
+
+def test_bat_extension(torch_device, data_directory):
+    # Create memmap, write to file, close the file again.
+    path = data_directory / 'binary_test' / 'temp_memmap.bat'
+    path.parent.mkdir(parents=True, exist_ok=True)
+    N, C = (1000, 10)
+    r = np.random.rand(N,C)*2 - 1    # scale to (-1,1)
+    r = (r*(2**14)).astype(np.int16)     # scale up
+
+    try:
+        a = np.memmap(path, mode='w+', shape=(N,C), dtype=np.int16)
+        a[:] = r[:]
+        a.flush()
+        del(a)
+
+        directory = path.parent
+        filename = io.find_binary(directory)
+        assert filename == path
+        bfile = io.BinaryFiltered(filename, C, device=torch_device)
+        x = bfile[0:100]  # Test data retrieval
+
+    finally:
+        # Delete memmap file and re-raise exception
+        path.unlink()
+
+
+def test_dat_extension(torch_device, data_directory):
+    # Create memmap, write to file, close the file again.
+    path = data_directory / 'binary_test' / 'temp_memmap.dat'
+    path.parent.mkdir(parents=True, exist_ok=True)
+    N, C = (1000, 10)
+    r = np.random.rand(N,C)*2 - 1    # scale to (-1,1)
+    r = (r*(2**14)).astype(np.int16)     # scale up
+
+    try:
+        a = np.memmap(path, mode='w+', shape=(N,C), dtype=np.int16)
+        a[:] = r[:]
+        a.flush()
+        del(a)
+
+        directory = path.parent
+        filename = io.find_binary(directory)
+        assert filename == path
+        bfile = io.BinaryFiltered(filename, C, device=torch_device)
+        x = bfile[0:100]  # Test data retrieval
+
+    finally:
+        # Delete memmap file and re-raise exception
+        path.unlink()
+
+
+def test_tmin_tmax(torch_device, data_directory):
+    N, C = (1000, 10)
+    NT = 300
+    nt = 61
+    data = np.repeat(np.arange(N)[...,np.newaxis], repeats=C, axis=1)
+    fs = 10
+    path = data_directory / 'time_interval_test' / 'temp_memmap.dat'
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    try:
+        a = np.memmap(path, mode='w+', shape=(N,C), dtype=np.int16)
+        a[:] = data[:]
+        a.flush()
+        del(a)
+
+        bfile = io.BinaryRWFile(path, n_chan_bin=C, fs=fs, device=torch_device,
+                                tmin=10, tmax=85, NT=NT, nt=nt)
+
+        assert bfile.imin == 100
+        assert bfile.imax == 850
+        assert bfile.n_samples == 750
+        assert bfile[0:50].min() == 100
+        assert bfile[700:750].max() == 849
+        assert bfile.n_batches == 3
+
+        X0 = bfile.padded_batch_to_torch(ibatch=0)
+        assert X0.min() == 100
+        assert X0.max() == 100 + NT + nt - 1
+
+        X1 = bfile.padded_batch_to_torch(ibatch=1)
+        assert X1.min() == 100 + NT - nt
+        assert X1.max() == 100 + 2*NT + nt - 1
+
+        X2 = bfile.padded_batch_to_torch(ibatch=2)
+        assert X2.min() == 100 + 2*NT - nt
+        assert X2.max() == 849
+
+    finally:
+        # Delete memmap file and re-raise exception
+        path.unlink()
+
+
+def test_tmin_only(torch_device, data_directory):
+    N, C = (1000, 10)
+    NT = 300
+    nt = 61
+    data = np.repeat(np.arange(N)[...,np.newaxis], repeats=C, axis=1)
+    fs = 10
+    path = data_directory / 'time_interval_test' / 'temp_memmap2.dat'
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    try:
+        a = np.memmap(path, mode='w+', shape=(N,C), dtype=np.int16)
+        a[:] = data[:]
+        a.flush()
+        del(a)
+
+        bfile = io.BinaryRWFile(path, n_chan_bin=C, fs=fs, device=torch_device,
+                                tmin=43, NT=NT, nt=nt)
+
+        assert bfile.imin == 430
+        assert bfile.imax == 1000
+        assert bfile.n_samples == 570
+        assert bfile[0:10].min() == 430
+        assert bfile[400:].max() == 999
+        assert bfile.n_batches == 2
+
+    finally:
+        # Delete memmap file and re-raise exception
+        path.unlink()
+
+
+def test_tmax_only(torch_device, data_directory):
+    N, C = (1000, 10)
+    NT = 300
+    nt = 61
+    data = np.repeat(np.arange(N)[...,np.newaxis], repeats=C, axis=1)
+    fs = 10
+    path = data_directory / 'time_interval_test' / 'temp_memmap3.dat'
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    try:
+        a = np.memmap(path, mode='w+', shape=(N,C), dtype=np.int16)
+        a[:] = data[:]
+        a.flush()
+        del(a)
+
+        bfile = io.BinaryRWFile(path, n_chan_bin=C, fs=fs, device=torch_device,
+                                tmax=78, NT=NT, nt=nt)
+
+        assert bfile.imin == 0
+        assert bfile.imax == 780
+        assert bfile.n_samples == 780
+        assert bfile[0:100].min() == 0
+        assert bfile[700:].max() == 779
+        assert bfile.n_batches == 3
+
+    finally:
+        # Delete memmap file and re-raise exception
+        path.unlink()
+
+
+def test_file_group(torch_device, data_directory, bfile):
+    file = data_directory / 'ZFM-02370_mini.imec0.ap.short.bin'
+    fs = bfile.fs
+    n_chans = bfile.n_chan_bin
+
+    # Test with file_objects option
+    # Load as three 15-second files instead of one 45-second file.
+    objs = [np.memmap(file, dtype='int16', shape=bfile.shape, mode='r')
+            for _ in range(3)]
+    objs[0] = objs[0][:int(15*fs),:]
+    objs[1] = objs[1][int(15*fs):int(30*fs),:]
+    objs[2] = objs[2][int(30*fs):,:]
+    bfg = io.BinaryFileGroup(file_objects=objs)
+    bfile2 = io.BinaryFiltered(
+        filename='test', n_chan_bin=n_chans, fs=fs, chan_map=bfile.chan_map,
+        device=torch_device, file_object=bfg, dtype='int16'
+        )
+
+    # First batch, overlapping batch, and last batch
+    # (assumes 45s test dataset with 2s batch size)
+    for i in [0, 7, 22]:
+        b1 = bfile.padded_batch_to_torch(i, skip_preproc=True)
+        b2 = bfile2.padded_batch_to_torch(i)
+        assert torch.allclose(b1, b2)
+
+    # Test with filenames option
+    files = [file]*3  # Load the same data three times
+    bfile3 = io.BinaryFiltered(
+        filename=files, n_chan_bin=n_chans, fs=fs, chan_map=bfile.chan_map,
+        device=torch_device, dtype='int16'
+    )
+
+    # First and first, last and last, last of original and last of concat
+    for i,j in [(0,0), (21,21), (22,67)]:
+        b1 = bfile.padded_batch_to_torch(i, skip_preproc=True)
+        b2 = bfile3.padded_batch_to_torch(j)
+        assert torch.allclose(b1, b2)
+
+
+def test_downsampling(bfile):
+    b0a = bfile.padded_batch_to_torch(0)
+    b5a = bfile.padded_batch_to_torch(5)
+    b15a = bfile.padded_batch_to_torch(15)
+    nba = bfile.n_batches
+    bfile.set_downsampling(3)
+    b0b = bfile.padded_batch_to_torch(0)
+    b5b = bfile.padded_batch_to_torch(5)
+    nbb = bfile.n_batches
+
+    # First batch should be the same for both.
+    assert torch.allclose(b0a, b0b)
+    # Same batch index should be different since the latter skips batches.
+    assert not torch.allclose(b5a, b5b)
+    # But batch(i) should be the same as batch(j*3)
+    assert torch.allclose(b15a, b5b)
+    assert nbb <= 3*nba
---
+++ packages/kilosort/kilosort-4.1.4.tar.gz/kilosort-4.1.4/tests/test_parameters.py
@@ -0,0 +1,48 @@
+'''Check that configurable parameters are propagated correctly.'''
+
+import numpy as np
+
+from kilosort.spikedetect import template_centers
+from kilosort.io import load_probe
+from kilosort.utils import PROBE_DIR
+
+
+def test_dmin():
+    settings = {'dmin': None, 'dminx': 32}
+    ops = {'xc': np.array([10, 20, 30]), 'yc': np.array([40, 40, 60]),
+           'settings': settings, 'kcoords': np.array([0, 0, 0])}
+    ops = template_centers(ops)
+    assert ops['dmin'] is not None  # set based on xc, yc
+    assert ops['dminx'] is not None
+    assert ops['settings']['dmin'] is None  # shouldn't change
+    
+    # Neuropixels 1 3B1 (4 columns with stagger)
+    np1_probe = load_probe(PROBE_DIR / 'NeuroPix1_default.mat')
+    ops = {'xc': np1_probe['xc'], 'yc': np1_probe['yc'], 
+           'kcoords': np1_probe['kcoords'], 'settings': settings}
+    ops = template_centers(ops)
+    assert ops['dmin'] == 20     # Median vertical spacing of contacts
+    assert ops['xup'].size == 4  # Number of lateral pos for universal templates
+
+    # Just one shank of NP2
+    np2_probe = load_probe(PROBE_DIR / 'NeuroPix2_default.mat')
+    ops = {'xc': np2_probe['xc'], 'yc': np2_probe['yc'],
+           'kcoords': np2_probe['kcoords'], 'settings': settings}
+    ops = template_centers(ops)
+    assert ops['dmin'] == 15
+    assert ops['xup'].size == 3
+
+    # Linear probe
+    lin_probe = load_probe(PROBE_DIR / 'Linear16x1_test.mat')
+    ops = {'xc': lin_probe['xc'], 'yc': lin_probe['yc']*20,
+           'kcoords': lin_probe['kcoords'], 'settings': settings}
+    ops = template_centers(ops)
+    assert ops['dmin'] == 20
+    assert ops['xup'].size == 1
+
+    settings = {'dmin': 5, 'dminx': 7}
+    ops = {'xc': np.array([10, 20, 30]), 'yc': np.array([40, 40, 60]),
+           'settings': settings, 'kcoords': np.array([0, 0, 0])}
+    ops = template_centers(ops)
+    assert ops['dmin'] == 5
+    assert ops['dminx'] == 7
