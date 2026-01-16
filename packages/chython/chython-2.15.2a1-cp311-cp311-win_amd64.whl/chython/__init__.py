9f856f84730e84683917580849da7640767436c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 11:06:47 +0000

    Add package chython/2.15.2a1/chython-2.15.2a1-cp311-cp311-win_amd64.whl

---
+++ packages/chython/chython-2.15.2a1-cp311-cp311-win_amd64.whl/chython/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2014-2025 Ramil Nugmanov <nougmanoff@protonmail.com>
+#  Copyright 2014-2019 Timur Madzhidov tmadzhidov@gmail.com features and API discussion
+#  Copyright 2014-2019 Alexandre Varnek <varnek@unistra.fr> base idea of CGR approach
+#  This file is part of chython.
+#
+#  chython is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, see <https://www.gnu.org/licenses/>.
+#
+from os import getenv
+from typing import Literal
+from .algorithms.depict import depict_settings
+from .containers import *
+from .files import *
+from .reactor import *
+from .utils import *
+
+
+torch_device = 'cpu'  # AAM model device. Change before first `reset_mapping` call!
+clean2d_engine: Literal['rdkit', 'smilesdrawer', 'cdk', 'obabel', 'indigo'] = 'smilesdrawer'
+conformer_engine: Literal['rdkit', 'cdpkit'] = 'rdkit'
+class_paths = [getenv('CDK_PATH', 'cdk.jar'), getenv('OPSIN_PATH', 'opsin.jar')]
+
+
+__all__ = []
