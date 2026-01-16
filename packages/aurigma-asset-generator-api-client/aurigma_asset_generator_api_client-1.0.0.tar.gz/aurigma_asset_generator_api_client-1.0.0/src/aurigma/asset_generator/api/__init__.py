c7d34f029c126436b6ffb54c00299ab76c44409f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:09:16 +0000

    Add package aurigma-asset-generator-api-client/1.0.0/aurigma_asset_generator_api_client-1.0.0.tar.gz

---
+++ packages/aurigma-asset-generator-api-client/aurigma_asset_generator_api_client-1.0.0.tar.gz/aurigma_asset_generator_api_client-1.0.0/src/aurigma/asset_generator/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from aurigma.asset_generator.api.build_info_api import BuildInfoApi
+from aurigma.asset_generator.api.mockup_generator_api import MockupGeneratorApi
+from aurigma.asset_generator.api.tenants_api import TenantsApi
+
