66c4802ad87110baf5529a474d991fc63a8bbd77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 08:06:59 +0000

    Add package pyafv/0.3.9/pyafv-0.3.9-cp313-cp313-macosx_10_13_x86_64.whl

---
+++ packages/pyafv/pyafv-0.3.9-cp313-cp313-macosx_10_13_x86_64.whl/pyafv/__init__.py
@@ -0,0 +1,18 @@
+"""
+PyAFV - A Python implementation of the **active-finite-Voronoi (AFV) model** in 2D.
+"""
+
+from .physical_params import PhysicalParams, target_delta
+from .finite_voronoi import FiniteVoronoiSimulator
+
+try:
+    from ._version import __version__
+except ImportError:                          # pragma: no cover
+    __version__ = "unknown"
+
+
+__all__ = [
+    "PhysicalParams",
+    "FiniteVoronoiSimulator",
+    "target_delta",
+]
