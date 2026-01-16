d835725207abbfa8588b58ddd245279aa841c5f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:08:49 +0000

    Add package prospr/1.2.10/prospr-1.2.10-cp310-cp310-win_amd64.whl

---
+++ packages/prospr/prospr-1.2.10-cp310-cp310-win_amd64.whl/prospr/__init__.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from prospr_core import (
+    AminoAcid,
+    Protein,
+    depth_first,
+    depth_first_bnb,
+    beam_search,
+)
+from .datasets import load_vanEck250, load_vanEck1000, load_vanEck_hratio
+from .helpers import export_protein
+from .visualize import plot_protein
+
+# Import __version__ from _version.py during compile time.
+exec(open(Path(__file__).parent.absolute() / "_version.py").read())
+
+__all__ = [
+    "__version__",
+    "AminoAcid",
+    "Protein",
+    "depth_first",
+    "depth_first_bnb",
+    "beam_search",
+    "load_vanEck250",
+    "load_vanEck1000",
+    "load_vanEck_hratio",
+    "export_protein",
+    "plot_protein",
+]
