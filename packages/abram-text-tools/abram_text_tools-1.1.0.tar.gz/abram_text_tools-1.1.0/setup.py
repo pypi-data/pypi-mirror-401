00a10d3dbe2f75e5488c7696fc568a4558807439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:38 +0000

    Add package abram-text-tools/1.1.0/abram_text_tools-1.1.0.tar.gz

---
+++ packages/abram-text-tools/abram_text_tools-1.1.0.tar.gz/abram_text_tools-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="abram_text_tools",
+    version="1.1.0",
+    description="A library made by a 10-year-old to print text in cool ways",
+    author="Abram Jindal",
+    py_modules=["text"],
+    install_requires=["rich"],
+)
