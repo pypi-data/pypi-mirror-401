6e5892b517f5c35d01aadd7801f74a7e95fee271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 05:08:52 +0000

    Add package turkishgraphy/2.0.0/turkishgraphy-2.0.0-py3-none-any.whl

---
+++ packages/turkishgraphy/turkishgraphy-2.0.0-py3-none-any.whl/turkishgraphy/__init__.py
@@ -0,0 +1,11 @@
+from .cipher import (
+    encrypt_4fe,
+    decrypt_4fe,
+    decrypt_4fe_offline,
+    key_olustur,
+    symbol_to_search_term,
+    kelimeler_dosyadan_yukle,
+    kelime_sec,
+    tpc_kaydet,
+    tpc_yukle
+)
