881ff53a9a1044f07adf84c0177319ee8f8e9dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 11:06:45 +0000

    Add package Transformer-Hierarchical-Layers/0.1.5/transformer_hierarchical_layers-0.1.5-py3-none-any.whl

---
+++ packages/Transformer-Hierarchical-Layers/transformer_hierarchical_layers-0.1.5-py3-none-any.whl/thl/__init__.py
@@ -0,0 +1,5 @@
+from .config import THLConfig
+from .model import THLModel, THLForSequenceClassification, THLForMultipleChoice, THLForTokenClassification
+from .tokenizer import Tokenizer
+
+__version__ = "0.1.0"
