03d7bb71d4944cab5d8ef281f5493ce1e729cc1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 11:06:59 +0000

    Add package yaml-configuration/0.3.2/yaml_configuration-0.3.2-py3-none-any.whl

---
+++ packages/yaml-configuration/yaml_configuration-0.3.2-py3-none-any.whl/yaml_configuration/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2016-2017 DLR
+#
+# All rights reserved. This program and the accompanying materials are made
+# available under the terms of the 2-Clause BSD License ("Simplified BSD
+# License") which accompanies this distribution, and is available at
+# https://opensource.org/licenses/BSD-2-Clause
+#
+# Contributors:
+# Franz Steinmetz <franz.steinmetz@dlr.de>
+# Sebastian Brunner <sebastian.brunner@dlr.de>
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("yaml_configuration")
+except PackageNotFoundError:
+    __version__ = "unknown"
