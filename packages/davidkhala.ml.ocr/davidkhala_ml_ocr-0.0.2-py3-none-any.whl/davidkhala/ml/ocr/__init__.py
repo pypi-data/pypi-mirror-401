f05f4f702c893943a9434117c999d4d8f866ead9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 08:07:17 +0000

    Add package davidkhala.ml.ocr/0.0.2/davidkhala_ml_ocr-0.0.2-py3-none-any.whl

---
+++ packages/davidkhala.ml.ocr/davidkhala_ml_ocr-0.0.2-py3-none-any.whl/davidkhala/ml/ocr/__init__.py
@@ -0,0 +1,12 @@
+import base64
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+
+class Client(ABC):
+	def _read(self,file:Path)->str:
+		with open(file, "rb") as f:
+			content = base64.b64encode(f.read()).decode('utf-8')
+		return content
+	@abstractmethod
+	def process(self, file: Path, **kwargs) -> list[dict] | dict:...
