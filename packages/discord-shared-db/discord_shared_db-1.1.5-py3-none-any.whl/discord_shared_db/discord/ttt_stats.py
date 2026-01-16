1039c24676adde990b3e45cd43436783fb469426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:39 +0000

    Add package discord-shared-db/1.1.5/discord_shared_db-1.1.5-py3-none-any.whl

---
+++ packages/discord-shared-db/discord_shared_db-1.1.5-py3-none-any.whl/discord_shared_db/discord/ttt_stats.py
@@ -0,0 +1,37 @@
+# This file is part of discord-shared-db
+#
+# Copyright (C) 2026 CouchComfy
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, BigInteger, ForeignKey, Integer
+
+from discord_shared_db.base import Base
+
+class TTTStats(Base):
+    __tablename__ = "ttt_stats"
+
+    discord_user_id = Column(
+        BigInteger,
+        ForeignKey("discord_users.discord_user_id"),
+        primary_key=True,
+    )
+
+    wins = Column(Integer, default=0, nullable=False)
+    losses = Column(Integer, default=0, nullable=False)
+    games_played = Column(Integer, default=0, nullable=False)
+
+    user = relationship("DiscordUser", back_populates="ttt_stats")
+
