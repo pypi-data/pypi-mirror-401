9e3d9826131fd8042cb71ec8ffd6531bcf4e7915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 03:34:37 +0000

    Add package tifftools/1.7.0/tifftools-1.7.0-py3-none-any.whl

---
+++ packages/tifftools/tifftools-1.7.0-py3-none-any.whl/tifftools/__init__.py
@@ -0,0 +1,41 @@
+import importlib.metadata
+import logging
+
+from .commands import main, tiff_concat, tiff_dump, tiff_info, tiff_merge, tiff_set, tiff_split
+from .constants import Datatype, Tag, TiffDatatype, TiffTag
+from .exceptions import (MustBeBigTiffError, MustBeBigTiffException, TifftoolsError,
+                         TifftoolsException, UnknownTagError, UnknownTagException)
+from .tifftools import read_tiff, write_tiff
+
+__version__ = importlib.metadata.version(__name__)
+
+
+logger = logging.getLogger(__name__)
+
+# See http://docs.python.org/3.3/howto/logging.html#configuring-logging-for-a-library
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__all__ = (
+    'Datatype', 'TiffDatatype',
+    'Tag', 'TiffTag',
+
+    'TifftoolsError',
+    'UnknownTagError',
+    'MustBeBigTiffError',
+    'TifftoolsException',
+    'UnknownTagException',
+    'MustBeBigTiffException',
+
+    'read_tiff',
+    'write_tiff',
+
+    'tiff_concat',
+    'tiff_dump',
+    'tiff_info',
+    'tiff_merge',
+    'tiff_set',
+    'tiff_split',
+
+    '__version__',
+    'main',
+)
