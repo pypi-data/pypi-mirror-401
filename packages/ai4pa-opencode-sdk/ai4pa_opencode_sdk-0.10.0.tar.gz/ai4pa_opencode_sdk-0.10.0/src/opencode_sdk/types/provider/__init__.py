931d9c4a808a89cc704e511e43c2113733dfbeef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 08:07:06 +0000

    Add package ai4pa-opencode-sdk/0.10.0/ai4pa_opencode_sdk-0.10.0.tar.gz

---
+++ packages/ai4pa-opencode-sdk/ai4pa_opencode_sdk-0.10.0.tar.gz/ai4pa_opencode_sdk-0.10.0/src/opencode_sdk/types/provider/__init__.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .oauth_authorize_params import OAuthAuthorizeParams as OAuthAuthorizeParams
+from .oauth_authorize_response import OAuthAuthorizeResponse as OAuthAuthorizeResponse
+from .oauth_handle_callback_params import OAuthHandleCallbackParams as OAuthHandleCallbackParams
+from .oauth_handle_callback_response import OAuthHandleCallbackResponse as OAuthHandleCallbackResponse
