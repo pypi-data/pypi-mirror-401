891b1db589594e9debaba82081d9ec7474203db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 11:07:00 +0000

    Add package huaweicloudsdkelb/3.1.183/huaweicloudsdkelb-3.1.183-py3-none-any.whl

---
+++ packages/huaweicloudsdkelb/huaweicloudsdkelb-3.1.183-py3-none-any.whl/huaweicloudsdkelb/v3/model/quota_info.py
@@ -0,0 +1,191 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class QuotaInfo:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'quota_key': 'str',
+        'quota_limit': 'int',
+        'used': 'int',
+        'unit': 'str'
+    }
+
+    attribute_map = {
+        'quota_key': 'quota_key',
+        'quota_limit': 'quota_limit',
+        'used': 'used',
+        'unit': 'unit'
+    }
+
+    def __init__(self, quota_key=None, quota_limit=None, used=None, unit=None):
+        r"""QuotaInfo
+
+        The model defined in huaweicloud sdk
+
+        :param quota_key: **参数解释**：配额类型。  **取值范围**：loadbalancer、listener、ipgroup、pool、member、healthmonitor、l7policy、certificate、security_policy、listeners_per_loadbalancer、listeners_per_pool、members_per_pool、condition_per_policy、ipgroup_bindings、ipgroup_max_length、ipgroups_per_listener、pools_per_l7policy、l7policies_per_listener、free_instance_members_per_pool、free_instance_listeners_per_loadbalancer。
+        :type quota_key: str
+        :param quota_limit: **参数解释**：总配额。  **取值范围**： - 大于等于0：表示当前配额数量。 - -1：表示无配额限制。
+        :type quota_limit: int
+        :param used: **参数解释**：已使用配额。  **取值范围**：大于等于0。
+        :type used: int
+        :param unit: **参数解释**：配额单位。  **取值范围**：count，表示个数。
+        :type unit: str
+        """
+        
+        
+
+        self._quota_key = None
+        self._quota_limit = None
+        self._used = None
+        self._unit = None
+        self.discriminator = None
+
+        self.quota_key = quota_key
+        self.quota_limit = quota_limit
+        self.used = used
+        self.unit = unit
+
+    @property
+    def quota_key(self):
+        r"""Gets the quota_key of this QuotaInfo.
+
+        **参数解释**：配额类型。  **取值范围**：loadbalancer、listener、ipgroup、pool、member、healthmonitor、l7policy、certificate、security_policy、listeners_per_loadbalancer、listeners_per_pool、members_per_pool、condition_per_policy、ipgroup_bindings、ipgroup_max_length、ipgroups_per_listener、pools_per_l7policy、l7policies_per_listener、free_instance_members_per_pool、free_instance_listeners_per_loadbalancer。
+
+        :return: The quota_key of this QuotaInfo.
+        :rtype: str
+        """
+        return self._quota_key
+
+    @quota_key.setter
+    def quota_key(self, quota_key):
+        r"""Sets the quota_key of this QuotaInfo.
+
+        **参数解释**：配额类型。  **取值范围**：loadbalancer、listener、ipgroup、pool、member、healthmonitor、l7policy、certificate、security_policy、listeners_per_loadbalancer、listeners_per_pool、members_per_pool、condition_per_policy、ipgroup_bindings、ipgroup_max_length、ipgroups_per_listener、pools_per_l7policy、l7policies_per_listener、free_instance_members_per_pool、free_instance_listeners_per_loadbalancer。
+
+        :param quota_key: The quota_key of this QuotaInfo.
+        :type quota_key: str
+        """
+        self._quota_key = quota_key
+
+    @property
+    def quota_limit(self):
+        r"""Gets the quota_limit of this QuotaInfo.
+
+        **参数解释**：总配额。  **取值范围**： - 大于等于0：表示当前配额数量。 - -1：表示无配额限制。
+
+        :return: The quota_limit of this QuotaInfo.
+        :rtype: int
+        """
+        return self._quota_limit
+
+    @quota_limit.setter
+    def quota_limit(self, quota_limit):
+        r"""Sets the quota_limit of this QuotaInfo.
+
+        **参数解释**：总配额。  **取值范围**： - 大于等于0：表示当前配额数量。 - -1：表示无配额限制。
+
+        :param quota_limit: The quota_limit of this QuotaInfo.
+        :type quota_limit: int
+        """
+        self._quota_limit = quota_limit
+
+    @property
+    def used(self):
+        r"""Gets the used of this QuotaInfo.
+
+        **参数解释**：已使用配额。  **取值范围**：大于等于0。
+
+        :return: The used of this QuotaInfo.
+        :rtype: int
+        """
+        return self._used
+
+    @used.setter
+    def used(self, used):
+        r"""Sets the used of this QuotaInfo.
+
+        **参数解释**：已使用配额。  **取值范围**：大于等于0。
+
+        :param used: The used of this QuotaInfo.
+        :type used: int
+        """
+        self._used = used
+
+    @property
+    def unit(self):
+        r"""Gets the unit of this QuotaInfo.
+
+        **参数解释**：配额单位。  **取值范围**：count，表示个数。
+
+        :return: The unit of this QuotaInfo.
+        :rtype: str
+        """
+        return self._unit
+
+    @unit.setter
+    def unit(self, unit):
+        r"""Sets the unit of this QuotaInfo.
+
+        **参数解释**：配额单位。  **取值范围**：count，表示个数。
+
+        :param unit: The unit of this QuotaInfo.
+        :type unit: str
+        """
+        self._unit = unit
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, QuotaInfo):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
---
+++ packages/huaweicloudsdkelb/huaweicloudsdkelb-3.1.183-py3-none-any.whl/huaweicloudsdkelb/v3/model/session_persistence.py
@@ -0,0 +1,165 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class SessionPersistence:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'cookie_name': 'str',
+        'type': 'str',
+        'persistence_timeout': 'int'
+    }
+
+    attribute_map = {
+        'cookie_name': 'cookie_name',
+        'type': 'type',
+        'persistence_timeout': 'persistence_timeout'
+    }
+
+    def __init__(self, cookie_name=None, type=None, persistence_timeout=None):
+        r"""SessionPersistence
+
+        The model defined in huaweicloud sdk
+
+        :param cookie_name: **参数解释**：cookie名称。  **取值范围**：最大长度1024个字符。  [不支持该字段，请勿使用。](tag:hws_eu,hcso_dt)
+        :type cookie_name: str
+        :param type: **参数解释**：会话保持类型。  **取值范围**：SOURCE_IP、HTTP_COOKIE、APP_COOKIE。  [荷兰region不支持QUIC。](tag:dt) [不支持QUIC。](tag:tm)
+        :type type: str
+        :param persistence_timeout: **参数解释**：会话保持的时间。当type为APP_COOKIE时不生效。  **取值范围**： - 若pool的protocol为TCP、UDP和QUIC则范围为[1,60]（分钟），默认值1。 - 若pool的protocol为HTTP和HTTPS则范围为[1,1440]（分钟），默认值1440。  [荷兰region不支持QUIC。](tag:dt)
+        :type persistence_timeout: int
+        """
+        
+        
+
+        self._cookie_name = None
+        self._type = None
+        self._persistence_timeout = None
+        self.discriminator = None
+
+        if cookie_name is not None:
+            self.cookie_name = cookie_name
+        self.type = type
+        if persistence_timeout is not None:
+            self.persistence_timeout = persistence_timeout
+
+    @property
+    def cookie_name(self):
+        r"""Gets the cookie_name of this SessionPersistence.
+
+        **参数解释**：cookie名称。  **取值范围**：最大长度1024个字符。  [不支持该字段，请勿使用。](tag:hws_eu,hcso_dt)
+
+        :return: The cookie_name of this SessionPersistence.
+        :rtype: str
+        """
+        return self._cookie_name
+
+    @cookie_name.setter
+    def cookie_name(self, cookie_name):
+        r"""Sets the cookie_name of this SessionPersistence.
+
+        **参数解释**：cookie名称。  **取值范围**：最大长度1024个字符。  [不支持该字段，请勿使用。](tag:hws_eu,hcso_dt)
+
+        :param cookie_name: The cookie_name of this SessionPersistence.
+        :type cookie_name: str
+        """
+        self._cookie_name = cookie_name
+
+    @property
+    def type(self):
+        r"""Gets the type of this SessionPersistence.
+
+        **参数解释**：会话保持类型。  **取值范围**：SOURCE_IP、HTTP_COOKIE、APP_COOKIE。  [荷兰region不支持QUIC。](tag:dt) [不支持QUIC。](tag:tm)
+
+        :return: The type of this SessionPersistence.
+        :rtype: str
+        """
+        return self._type
+
+    @type.setter
+    def type(self, type):
+        r"""Sets the type of this SessionPersistence.
+
+        **参数解释**：会话保持类型。  **取值范围**：SOURCE_IP、HTTP_COOKIE、APP_COOKIE。  [荷兰region不支持QUIC。](tag:dt) [不支持QUIC。](tag:tm)
+
+        :param type: The type of this SessionPersistence.
+        :type type: str
+        """
+        self._type = type
+
+    @property
+    def persistence_timeout(self):
+        r"""Gets the persistence_timeout of this SessionPersistence.
+
+        **参数解释**：会话保持的时间。当type为APP_COOKIE时不生效。  **取值范围**： - 若pool的protocol为TCP、UDP和QUIC则范围为[1,60]（分钟），默认值1。 - 若pool的protocol为HTTP和HTTPS则范围为[1,1440]（分钟），默认值1440。  [荷兰region不支持QUIC。](tag:dt)
+
+        :return: The persistence_timeout of this SessionPersistence.
+        :rtype: int
+        """
+        return self._persistence_timeout
+
+    @persistence_timeout.setter
+    def persistence_timeout(self, persistence_timeout):
+        r"""Sets the persistence_timeout of this SessionPersistence.
+
+        **参数解释**：会话保持的时间。当type为APP_COOKIE时不生效。  **取值范围**： - 若pool的protocol为TCP、UDP和QUIC则范围为[1,60]（分钟），默认值1。 - 若pool的protocol为HTTP和HTTPS则范围为[1,1440]（分钟），默认值1440。  [荷兰region不支持QUIC。](tag:dt)
+
+        :param persistence_timeout: The persistence_timeout of this SessionPersistence.
+        :type persistence_timeout: int
+        """
+        self._persistence_timeout = persistence_timeout
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, SessionPersistence):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
