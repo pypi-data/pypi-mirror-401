a8e86c298a03ca71bc9992550b1f931db486a0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:08:49 +0000

    Add package prospr/1.2.10/prospr-1.2.10-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/prospr/prospr-1.2.10-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/prospr/datasets.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+File:           datasets.py
+Description:    This file contains functions for loading datasets.
+License:        This file is licensed under the GNU LGPL V3 license by
+                Okke van Eck (2020 - 2023). See the LICENSE file for the
+                specifics.
+"""
+
+from pathlib import Path
+import pandas as pd
+
+
+def _load_dataset(folder, filename):
+    """Returns a specified dataset as a dataframe."""
+    return pd.read_csv(f"{Path(__file__).parent}/data/{folder}/{filename}")
+
+
+def load_vanEck250(length=10):
+    """Returns a vanEck250 dataset as a pandas dataframe."""
+    return _load_dataset("vanEck250", f"HP{length}.csv")
+
+
+def load_vanEck1000(length=10):
+    """Returns a vanEck1000 dataset as a pandas dataframe."""
+    return _load_dataset("vanEck1000", f"HP{length}.csv")
+
+
+def load_vanEck_hratio(length=25, hratio=0.1):
+    """Returns a vanEck_hratio dataset as a pandas dataframe."""
+    return _load_dataset("vanEck_hratio", f"HP{length}_r{hratio}.csv")
