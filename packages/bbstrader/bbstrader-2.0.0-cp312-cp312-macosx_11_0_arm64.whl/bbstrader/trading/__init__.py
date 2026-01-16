cdbc7c428ce1b690e5be63a97de062aca8cecc65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 06:06:31 +0000

    Add package bbstrader/2.0.0/bbstrader-2.0.0-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/bbstrader/bbstrader-2.0.0-cp312-cp312-macosx_11_0_arm64.whl/bbstrader/trading/__init__.py
@@ -0,0 +1,11 @@
+"""
+Trading strategies execution module
+
+This module is responsible for executing trading strategies. 
+It provides a framework for executing trading strategies and managing the trading process. 
+The module is designed to be flexible and extensible, allowing users to define their own trading 
+strategies and customize the trading process.
+
+"""
+from bbstrader.trading.execution import *  # noqa: F403
+from bbstrader.trading.strategy import *  # noqa: F403
