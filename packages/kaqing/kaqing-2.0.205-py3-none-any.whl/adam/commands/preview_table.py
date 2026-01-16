67e991270082400cf631c014b617b0697a6227fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 07:08:53 +0000

    Add package kaqing/2.0.205/kaqing-2.0.205-py3-none-any.whl

---
+++ packages/kaqing/kaqing-2.0.205-py3-none-any.whl/adam/commands/preview_table.py
@@ -0,0 +1,39 @@
+from adam.commands import validate_args
+from adam.commands.command import Command
+from adam.commands.devices.devices import Devices
+from adam.repl_state import ReplState, RequiredState
+
+class PreviewTable(Command):
+    COMMAND = 'preview'
+
+    # the singleton pattern
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'): cls.instance = super(PreviewTable, cls).__new__(cls)
+
+        return cls.instance
+
+    def __init__(self, successor: Command=None):
+        super().__init__(successor)
+
+    def command(self):
+        return PreviewTable.COMMAND
+
+    def required(self):
+        return [RequiredState.CLUSTER_OR_POD, RequiredState.PG_DATABASE, ReplState.L, RequiredState.EXPORT_DB]
+
+    def run(self, cmd: str, state: ReplState):
+        if not(args := self.args(cmd)):
+            return super().run(cmd, state)
+
+        with self.validate(args, state) as (args, state):
+            with validate_args(args, state, at_least=1) as table:
+                Devices.of(state).preview(table, state)
+
+            return state
+
+    def completion(self, _: ReplState):
+        # taken care of by the sql completer
+        return {}
+
+    def help(self, _: ReplState):
+        return f'{PreviewTable.COMMAND} TABLE\t preview table'
---
+++ packages/kaqing/kaqing-2.0.205-py3-none-any.whl/adam/commands/show/show_storage.py
@@ -0,0 +1,43 @@
+from adam.commands import extract_options, extract_trailing_options
+from adam.commands.command import Command
+from adam.commands.cql.utils_cql import cassandra
+from adam.config import Config
+from adam.repl_state import ReplState, RequiredState
+
+class ShowStorage(Command):
+    COMMAND = 'show storage'
+
+    # the singleton pattern
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'): cls.instance = super(ShowStorage, cls).__new__(cls)
+
+        return cls.instance
+
+    def __init__(self, successor: Command=None):
+        super().__init__(successor)
+
+    def command(self):
+        return ShowStorage.COMMAND
+
+    def required(self):
+        return RequiredState.CLUSTER_OR_POD
+
+    def run(self, cmd: str, state: ReplState):
+        if not(args := self.args(cmd)):
+            return super().run(cmd, state)
+
+        with self.validate(args, state) as (args, state):
+            with extract_trailing_options(args, '&') as (args, backgrounded):
+                with extract_options(args, ['-s', '--show']) as (args, show_out):
+                    cols = Config().get('storage.columns', 'pod,volume_root,volume_cassandra,snapshots,data,compactions')
+                    header = Config().get('storage.header', 'POD_NAME,VOLUME /,VOLUME CASS,SNAPSHOTS,DATA,COMPACTIONS')
+                    with cassandra(state) as pods:
+                        pods.display_table(cols, header, show_out=show_out, backgrounded=backgrounded)
+
+                    return state
+
+    def completion(self, state: ReplState):
+        return super().completion(state, {'-s': {'&': None}, '&': None})
+
+    def help(self, _: ReplState):
+        return f'{ShowStorage.COMMAND} [-s]\t show storage overview  -s show commands on nodes'
