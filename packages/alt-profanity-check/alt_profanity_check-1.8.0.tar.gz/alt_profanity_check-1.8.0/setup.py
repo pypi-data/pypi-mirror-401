e65d1b5ec22b3e35978b435a3b8dd3efa6e7e93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 03:34:46 +0000

    Add package alt-profanity-check/1.8.0/alt_profanity_check-1.8.0.tar.gz

---
+++ packages/alt-profanity-check/alt_profanity_check-1.8.0.tar.gz/alt_profanity_check-1.8.0/setup.py
@@ -0,0 +1,39 @@
+"""Setup"""
+
+from pathlib import Path
+import setuptools
+
+long_description = Path("README.md").read_text(encoding="utf8")
+
+setuptools.setup(
+    name="alt-profanity-check",
+    version="1.8.0",
+    author="Victor Zhou (original author), Menelaos Kotoglou, Dimitrios Mistriotis",
+    author_email="dimitrios@mistriotis.com",
+    description=(
+        "A fast, robust library to check for offensive language in strings. "
+        'Dropdown replacement of "profanity-check".'
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dimitrismistriotis/alt-profanity-check",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "scikit-learn==1.8.0",
+        "joblib==1.5.3",
+    ],
+    python_requires=">=3.11",
+    package_data={"profanity_check": ["data/model.joblib", "data/vectorizer.joblib"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "profanity_check=profanity_check.command_line:main",
+        ],
+    },
+)
