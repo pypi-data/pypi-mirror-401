ebbdce8d1c08b31d2396c9258ab77dfbd7176ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:26 +0000

    Add package garf-google-ads/1.0.0/garf_google_ads-1.0.0-py3-none-any.whl

---
+++ packages/garf-google-ads/garf_google_ads-1.0.0-py3-none-any.whl/garf/community/google/ads/__init__.py
@@ -0,0 +1,25 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Library for getting reports from Google Ads API."""
+
+from garf.community.google.ads.api_clients import GoogleAdsApiClient
+from garf.community.google.ads.report_fetcher import GoogleAdsApiReportFetcher
+
+__all__ = [
+  'GoogleAdsApiClient',
+  'GoogleAdsApiReportFetcher',
+]
+
+__version__ = '1.0.0'
