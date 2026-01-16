cc3e19f6d9e4a65771ea6b7d093c40ce139b72a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 13:26:02 +0000

    Add package commitizen/4.11.4/commitizen-4.11.4.tar.gz

---
+++ packages/commitizen/commitizen-4.11.4.tar.gz/commitizen-4.11.4/commitizen/config/__init__.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from commitizen import defaults, git, out
+from commitizen.config.factory import create_config
+from commitizen.exceptions import ConfigFileIsEmpty, ConfigFileNotFound
+
+from .base_config import BaseConfig
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+
+def _resolve_config_paths(filepath: str | None = None) -> Generator[Path, None, None]:
+    if filepath is not None:
+        out_path = Path(filepath)
+        if not out_path.exists():
+            raise ConfigFileNotFound()
+
+        yield out_path
+        return
+
+    git_project_root = git.find_git_project_root()
+    cfg_search_paths = [Path(".")]
+    if git_project_root:
+        cfg_search_paths.append(git_project_root)
+
+    for path in cfg_search_paths:
+        for filename in defaults.CONFIG_FILES:
+            out_path = path / Path(filename)
+            if out_path.exists():
+                yield out_path
+
+
+def read_cfg(filepath: str | None = None) -> BaseConfig:
+    config_candidates = list(_resolve_config_paths(filepath))
+
+    # Check for multiple config files and warn the user
+    config_candidates_exclude_pyproject = [
+        path for path in config_candidates if path.name != "pyproject.toml"
+    ]
+    if len(config_candidates_exclude_pyproject) > 1:
+        filenames = [path.name for path in config_candidates_exclude_pyproject]
+        out.warn(
+            f"Multiple config files detected: {', '.join(filenames)}. "
+            f"Using config file: '{filenames[0]}'."
+        )
+
+    for filename in config_candidates:
+        with open(filename, "rb") as f:
+            data: bytes = f.read()
+
+        conf = create_config(data=data, path=filename)
+        if not conf.is_empty_config:
+            return conf
+
+        if filepath is not None:
+            raise ConfigFileIsEmpty()
+
+    return BaseConfig()
