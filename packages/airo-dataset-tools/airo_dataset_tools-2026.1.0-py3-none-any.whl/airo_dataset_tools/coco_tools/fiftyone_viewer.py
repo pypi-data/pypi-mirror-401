6d7107b5c4e313016596636137d215784550df83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 03:34:12 +0000

    Add package airo-dataset-tools/2026.1.0/airo_dataset_tools-2026.1.0-py3-none-any.whl

---
+++ packages/airo-dataset-tools/airo_dataset_tools-2026.1.0-py3-none-any.whl/airo_dataset_tools/coco_tools/fiftyone_viewer.py
@@ -0,0 +1,30 @@
+import os
+from typing import List, Optional
+
+
+def view_coco_dataset(
+    labels_json_path: str, dataset_dir: Optional[str] = None, label_types: Optional[List[str]] = None
+) -> None:
+    """visualize a coco dataset in fiftyone"""
+    # lazy import because fiftyone is slow to import and makes the CLI slow.
+    import fiftyone as fo
+
+    if dataset_dir is None:
+        dataset_dir = os.path.dirname(labels_json_path)
+    if label_types is None or not label_types:
+        label_types = ["detections", "segmentations", "keypoints"]
+    else:
+        if not all([label_type in ["detections", "segmentations", "keypoints"] for label_type in label_types]):
+            raise ValueError("label_types must be a subset of ['detections', 'segmentations', 'keypoints']")
+
+    labels_json_path = os.path.realpath(labels_json_path)
+
+    dataset = fo.Dataset.from_dir(
+        dataset_type=fo.types.COCODetectionDataset,
+        label_types=label_types,
+        data_path=dataset_dir,
+        labels_path=labels_json_path,
+    )
+
+    session = fo.launch_app(dataset)
+    session.wait()
---
+++ packages/airo-dataset-tools/airo_dataset_tools-2026.1.0-py3-none-any.whl/airo_dataset_tools/data_parsers/__init__.py
@@ -0,0 +1,5 @@
+# import pydantic
+
+# remove this check once pydantic 2.0.0 has been around for a while
+# I've removed it, it's now over 2 years later - MDC (28/08/2025)
+# assert pydantic.VERSION >= "2.0.0", "Please upgrade pydantic to version 2.0.0 or higher."
