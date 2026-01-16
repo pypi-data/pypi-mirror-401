3bb3b57da574647614b9aa395e2f4117306b1ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 04:18:51 +0000

    Add package kolena-agents/0.8.0/kolena_agents-0.8.0-py3-none-any.whl

---
+++ packages/kolena-agents/kolena_agents-0.8.0-py3-none-any.whl/kolena_agents/_generated/openapi_client/api/__init__.py
@@ -0,0 +1,6 @@
+# type: ignore
+# flake8: noqa
+
+# import apis into api package
+from kolena_agents._generated.openapi_client.api.client_api import ClientApi
+
---
+++ packages/kolena-agents/kolena_agents-0.8.0-py3-none-any.whl/kolena_agents/types/agent.py
@@ -0,0 +1,11 @@
+from kolena_agents._generated.openapi_client import Agent  # type: ignore
+from kolena_agents._generated.openapi_client import CopyAgentRequest  # type: ignore
+from kolena_agents._generated.openapi_client import ListAgentsResponse  # type: ignore
+from kolena_agents._generated.openapi_client import UpdateAgentRequest  # type: ignore
+
+__all__ = [
+    "Agent",
+    "ListAgentsResponse",
+    "CopyAgentRequest",
+    "UpdateAgentRequest",
+]
