237f88b72fbe3ef1852984f1ea89284bc0c1e739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 11:07:12 +0000

    Add package toolsaf/0.4.0/toolsaf-0.4.0-py3-none-any.whl

---
+++ packages/toolsaf/toolsaf-0.4.0-py3-none-any.whl/toolsaf/core/matcher.py
@@ -0,0 +1,264 @@
+"""Match events into system model"""
+
+from typing import Optional, Self, Tuple, Dict
+
+from dataclasses import dataclass
+
+from toolsaf.common.address import AnyAddress, EndpointAddress, IPAddress
+from toolsaf.common.basics import ExternalActivity, Status
+from toolsaf.core.matcher_engine import FlowMatcher, MatcherEngine
+from toolsaf.core.model import IoTSystem, Connection, Host, Addressable, Service, EvidenceNetworkSource, ModelListener
+from toolsaf.common.traffic import NO_EVIDENCE, Flow, EvidenceSource
+from toolsaf.common.verdict import Verdict
+
+
+class SystemMatcher(ModelListener):
+    """Match system model"""
+    def __init__(self, system: IoTSystem) -> None:
+        self.system = system
+        self.contexts: Dict[EvidenceSource, MatchingContext] = {}
+        system.model_listeners.append(self)
+
+    def reset(self) -> Self:
+        """Reset the model"""
+        self.contexts.clear()
+        self.system.reset()
+        return self
+
+    def address_change(self, host: Host) -> None:
+        for ctx in self.contexts.values():
+            ctx.engine.update_host(host)
+
+    def connection(self, flow: Flow) -> Connection:
+        """Find the connection matching the given flow"""
+        return self.connection_w_ends(flow)[0]
+
+    def connection_w_ends(self, flow: Flow) -> Tuple[Connection, AnyAddress, AnyAddress, bool]:
+        """Find the connection matching the given flow, return also endpoint addresses"""
+        source = flow.evidence.source
+        ctx = self.get_context(source)
+        m = ctx.get_connection(flow)
+        return m.connection, m.source, m.target, m.reply
+
+    def endpoint(self, address: AnyAddress, source: EvidenceSource) -> Addressable:
+        """Find endpoint by address"""
+        ctx = self.get_context(source)
+        e = ctx.get_endpoint(address)
+        return e
+
+    def get_context(self, source: Optional[EvidenceSource] = None) -> 'MatchingContext':
+        """Get matching context for source"""
+        source = NO_EVIDENCE.source if source is None else source
+        ctx = self.contexts.get(source)
+        if ctx is None:
+            ctx = MatchingContext(self, source)
+            self.contexts[source] = ctx
+        return ctx
+
+
+@dataclass(frozen=True)
+class ConnectionMatch:
+    """Matched connection"""
+    connection: Connection
+    source: AnyAddress
+    target: AnyAddress
+    reply: bool = False
+
+    def __repr__(self) -> str:
+        return f"{self.connection}"
+
+
+class MatchingContext:
+    """Matching context"""
+    def __init__(self, system: SystemMatcher, source: EvidenceSource) -> None:
+        self.system = system
+        self.observed: Dict[Flow, ConnectionMatch] = {}
+        self.engine = MatcherEngine(system.system)
+
+        # load system model into matching engine
+        for c in system.system.get_connections(relevant_only=False):
+            self.engine.add_connection(c)
+        for h in system.system.get_hosts():
+            self.engine.add_host(h)
+
+        # load evidence source -specific address mappings
+        self.source = source if isinstance(source, EvidenceNetworkSource) else None
+        if self.source:
+            for ad, ent in self.source.address_map.items():
+                self.engine.add_address_mapping(ad, ent)
+
+            # TODO: Activity maps are now ignored. So far, matching work without them
+            # assert not self.source.activity_map, "Activity map not supported in matcher engine"
+            # check if exteranal activity changes for some entities
+            # for me in itertools.chain(*self.endpoints.values()):
+            #     fs = self.source.activity_map.get(me.entity)
+            #     if fs is not None:
+            #         me.external_activity = fs
+
+    def get_connection(self, flow: Flow) -> ConnectionMatch:
+        """Get connection matching the given flow"""
+        match = self.observed.get(flow)
+        if match:
+            # old connection
+            return match
+
+        flow_matcher = FlowMatcher(self.engine, flow)
+        conn = flow_matcher.get_connection()
+        source_add, target_add = flow_matcher.get_host_addresses()
+        if isinstance(conn, Connection):
+            assert source_add is not None and target_add is not None
+            match = ConnectionMatch(conn, source_add, target_add, flow_matcher.reverse)
+            if not conn.target.is_service() and flow_matcher.reverse:
+                # Reply to unexpected connection
+                self.create_unknown_service(match)
+            if match.connection.status == Status.PLACEHOLDER:
+                # this is an UNEXPECTED connection found after reset
+                self.set_connection_status(match.connection, (conn.source, source_add), (conn.target, target_add))
+            self.observed[flow] = match
+        else:
+            # no connection, must create new
+            source, target = conn
+            if source is None:
+                # no suitable source, make new
+                source, source_add = self.new_endpoint(flow, target=False)
+            if target is None:
+                # no suitable target, make new
+                target, target_add = self.new_endpoint(flow, target=True)
+            assert source_add is not None and target_add is not None
+
+            match = self.new_connection((source, source_add), (target, target_add))
+            self.observed[flow] = match
+        connection = match.connection
+        if not match.reply:
+            self.system.system.connections[match.source, match.target] = connection
+        else:
+            self.system.system.connections[match.target, match.source] = connection
+        connection.source.new_connection(connection, flow, target=match.reply)
+        connection.target.new_connection(connection, flow, target=not match.reply)
+        return match
+
+    def get_endpoint(self, address: AnyAddress) -> Addressable:
+        """Get endpoint by address, create new if not found"""
+        host = self.engine.find_host(address)
+        net = self.system.system.get_networks_for(address)
+        if host:
+            if not address.get_protocol_port():
+                return host
+            e = host.get_endpoint(address, at_network=net[0] if net else None)
+            self.engine.add_host(e)  # if created
+            return e
+        # no such host
+        e = self.system.system.get_endpoint(address)
+        self.engine.add_host(e)
+        return e
+
+
+    def new_endpoint(self, flow: Flow, target: bool) -> Tuple[Addressable, AnyAddress]:
+        """Create a new endpoint host"""
+        system = self.system.system
+        stack = flow.stack(target)
+        use_ad = stack[0]
+        for ad in stack[1:]:
+            if isinstance(ad, IPAddress):
+                if system.is_external(ad) or ad.is_multicast():
+                    use_ad = ad  # must use the IP address
+                    break
+            if use_ad.is_null() and not ad.is_null():
+                use_ad = ad  # prefer non-null address
+        host = system.get_endpoint(use_ad, at_network=flow.network)
+        self.engine.add_host(host)
+        # return the matching address
+        match_address = EndpointAddress(use_ad, flow.protocol, flow.port(target))
+        return host, match_address
+
+    def new_connection(self, source: Tuple[Addressable, AnyAddress], target: Tuple[Addressable, AnyAddress]) \
+        -> ConnectionMatch:
+        """Create new unexpected connection"""
+        system = self.system.system
+        c = system.new_connection(source, target)
+        self.set_connection_status(c, source, target)
+        self.engine.add_connection(c)
+        return ConnectionMatch(c, source[1], target[1])
+
+    def set_connection_status(self, connection: Connection, source: Tuple[Addressable, AnyAddress],
+                              target: Tuple[Addressable, AnyAddress]) -> Connection:
+        """Set status for unexpected connection"""
+        c = connection
+        c.status = Status.UNEXPECTED
+
+        # TODO: More strict matching logic experiemented - works fine!
+        # - Could also change external activity PASSIVE -> BANNED for local hosts, e.g. devices
+        # system = self.engine.system
+        # if not system.is_external(source[1]) and not system.is_external(target[1]):
+        #     if not (c.target.is_service() and c.target.is_admin()):
+        #         return c
+        # all local connections should be defined explicitly, except admin services
+
+        def set_external(e: Addressable) -> None:
+            if e.status == Status.UNEXPECTED and e.get_expected_verdict() == Verdict.INCON:
+                # entity is fresh and unexpected, make it external
+                e.status = Status.EXTERNAL
+                if isinstance(e.parent, Addressable):
+                    set_external(e.parent)
+
+        # new connection status by external activity policies and reply status
+        source_act = source[0].external_activity
+        target_act = target[0].external_activity
+        if source_act > ExternalActivity.BANNED and target_act > ExternalActivity.BANNED:
+            # unexpected connections may be allowed
+            reply = c.source == target[0]
+            if source_act >= ExternalActivity.UNLIMITED:
+                # source is free to make connections
+                c.status = Status.EXTERNAL
+                set_external(c.source)
+            elif reply and source_act >= ExternalActivity.OPEN:
+                # source can make replies
+                c.status = Status.EXTERNAL
+                set_external(c.source)
+            if c.status == Status.EXTERNAL and target_act >= ExternalActivity.PASSIVE:
+                # target is free receive connections
+                set_external(c.target)
+        return c
+
+    def create_unknown_service(self, match: ConnectionMatch) -> None:
+        """Create an unknown service due observing reply from it"""
+        system = self.system.system
+        conn = match.connection
+        target_host = conn.target
+        assert isinstance(target_host, Host), "Expected target to be a host of unreplied unknown connection"
+        assert conn not in target_host.connections, "Connection already added to target host"
+        service_address = match.target
+        assert isinstance(service_address, EndpointAddress), "Expected endpoint address for service"
+
+        # change connection to point the new service
+        new_service = target_host.get_endpoint(service_address)  # NOTE: Network not specified - how can there be many?
+        assert new_service is not None
+        assert isinstance(new_service, Service)
+        self.engine.add_addressable(new_service)
+
+        if target_host.external_activity >= ExternalActivity.UNLIMITED and conn.status == Status.EXTERNAL \
+            and new_service.status == Status.UNEXPECTED:
+            # host is free to provide unlisted services
+            new_service.status = Status.EXTERNAL
+
+        target_host.connections.append(conn)
+        self.engine.remove_connection(conn)  # remove old connection, we put it back with new target
+        conn.target = new_service
+        self.engine.add_connection(conn)
+        # create new connection for connections from same the source to the same target host, but different port
+        new_c = None
+        new_obs: Dict[Flow, ConnectionMatch] = {}
+        for o_flow, o_m in self.observed.items():
+            if o_m.connection == conn and o_m.target != service_address:
+                # same connection, but different target service
+                if new_c is None:
+                    new_source = conn.source, o_m.source
+                    new_target = target_host, o_m.target
+                    new_c = system.new_connection(new_source, new_target)
+                    self.engine.add_connection(new_c)
+                    self.set_connection_status(new_c, new_source, new_target)
+                else:
+                    system.connections[o_m.source, o_m.target] = new_c
+                new_m = ConnectionMatch(new_c, o_m.source, o_m.target, o_m.reply)
+                new_obs[o_flow] = new_m
+        self.observed.update(new_obs)
---
+++ packages/toolsaf/toolsaf-0.4.0-py3-none-any.whl/toolsaf/diagram_visualizer.py
@@ -0,0 +1,189 @@
+"""Security Statement Visualizer"""
+# pylint: disable=pointless-statement
+# pylint: disable=expression-not-assigned
+# pylint: disable=cyclic-import
+
+from typing import Dict, List, Tuple, Any, Optional
+from pathlib import Path
+from diagrams import Diagram, Edge, Node
+from diagrams.custom import Custom
+
+from toolsaf.common.verdict import Verdict
+from toolsaf.common.basics import HostType
+from toolsaf.core.model import Host
+import toolsaf.builder_backend as BB
+from toolsaf.main import DiagramVisualizer as DV
+
+
+class DiagramVisualizer(DV):
+    """Security statement visualizer"""
+
+    __font_size_node = "18"
+    __font_size_edge = "16"
+    __graph_attr = {"splines": "polyline", "center": "true"}
+
+    def __init__(self, system: 'BB.SystemBackend'):
+        self.system = system
+        self.show: bool = False
+        self.delete: bool = False
+        self.outformat: str = "png"
+        self.filename = system.system.long_name()
+        self.should_create_diagram: bool = False
+        self._path = Path(__file__).parent.as_posix()
+        self.nodes: Dict[str, Node] = {}
+        self.connections: set[Tuple[str, Any, str, str, str]] = set()
+
+    def set_outformat(self, create_diagram: Optional[str], show_diagram: Optional[str]) -> None:
+        """Set outputformat for created diagram"""
+        if create_diagram is not None and show_diagram is not None:
+            self.outformat = next((format for format in [create_diagram, show_diagram] if format != "png"), "png")
+        else:
+            self.outformat = str(show_diagram or create_diagram)
+
+    def set_file_name(self, file_name: str = "") -> None:
+        """Set filename for created diagram. Default is the system's name"""
+        if file_name:
+            self.filename = file_name
+
+    def _get_hosts(self) -> List[Host]:
+        return [
+            host for host in self.system.system.get_hosts() if host.is_relevant()
+        ]
+
+    def _sanitize_label(self, label: str) -> str:
+        """Turn certain symbols to HTML character"""
+        return label.replace("&", "&amp;")
+
+    def _custom_image(self, label: str, path: str) -> Custom:
+        return Custom(
+            label=label,
+            icon_path=path,
+            fontsize=self.__font_size_node
+        )
+
+    def iot_device(self, label: str) -> Custom:
+        """Custom representation for IoT devices"""
+        return self._custom_image(
+            label, f"{self._path}/diagram_visualizer/device.png"
+        )
+
+    def iot_device_with_multicast(self, label: str) -> Custom:
+        """Custom representation for IoT devices that send multicasts"""
+        return self._custom_image(
+            label, f"{self._path}/diagram_visualizer/device_with_multicast.png"
+        )
+
+    def mobile(self, label: str) -> Custom:
+        """Custom representation for mobile applications"""
+        return self._custom_image(
+            label, f"{self._path}/diagram_visualizer/mobile.png"
+        )
+
+    def mobile_with_multicast(self, label: str) -> Custom:
+        """Custom representation of mobile applications that send multicasts"""
+        return self._custom_image(
+            label, f"{self._path}/diagram_visualizer/mobile_with_multicast.png"
+        )
+
+    def browser(self, label: str) -> Custom:
+        """Custom representation for browsers"""
+        return self._custom_image(
+            label, f"{self._path}/diagram_visualizer/browser.png"
+        )
+
+    def backend(self, label: str) -> Custom:
+        """Custom representation for backend services"""
+        return self._custom_image(
+            label, f"{self._path}/diagram_visualizer/backend.png"
+        )
+
+    def _should_return_multicast(self, host: Host) -> bool:
+        """Determines if a multicast symbol variant should be used"""
+        for connection in host.connections:
+            if connection.target.is_multicast():
+                if connection.target.host_type != HostType.ADMINISTRATIVE and connection.source == host:
+                    return True
+        return False
+
+    def _get_node_by_type(self, host: Host, label: str) -> Optional[Node]:
+        """Returns a suitable visual representation based on host's type."""
+        match host.host_type:
+            case HostType.DEVICE:
+                if self._should_return_multicast(host):
+                    return self.iot_device_with_multicast(label)
+                return self.iot_device(label)
+            case HostType.MOBILE:
+                if self._should_return_multicast(host):
+                    return self.mobile_with_multicast(label)
+                return self.mobile(label)
+            case HostType.BROWSER:
+                return self.browser(label)
+            case HostType.REMOTE:
+                return self.backend(label)
+            case _:
+                return None
+
+    def _get_verdict_text(self, verdict: Verdict) -> str:
+        if verdict in [Verdict.PASS, Verdict.FAIL]:
+            return f"[{verdict.value}] "
+        return " "
+
+    def _get_label_color(self, verdict: Verdict) -> str:
+        if verdict == Verdict.PASS:
+            return "darkgreen"
+        if verdict == Verdict.FAIL:
+            return "darkred"
+        return "black"
+
+    def _get_node_label(self, host: Host, verdict: Verdict) -> str:
+        if (verdict_text := self._get_verdict_text(verdict)) != " ":
+            return f"<<font color='{self._get_label_color(verdict)}'><b>" + \
+                   f"\n\n<br/>{verdict_text}<br/>"                          + \
+                   f"{self._sanitize_label(host.name)}</b></font>>"
+
+        return f"<<font color='{self._get_label_color(verdict)}'><b>" + \
+               f"\n{self._sanitize_label(host.name)}</b></font>>"
+
+    def _get_node(self, host: Host) -> Optional[Node]:
+        """Returns a suitable visual representation for a given Host"""
+        verdict = host.get_verdict({})
+        label = self._get_node_label(host, verdict)
+        return self._get_node_by_type(host, label)
+
+    def _add_connections(self, host: Host) -> None:
+        """Adds connections between nodes"""
+        for connection in host.connections:
+            verdict = connection.get_verdict({})
+            self.connections.add((
+                connection.source.name, connection.target.parent.name,
+                f"{self._get_verdict_text(verdict)}{connection.target.name}", "black", self._get_label_color(verdict)
+            ))
+
+    def create_diagram(self) -> None:
+        """Execute diagram visualization and handle any exceptions"""
+        try:
+            self.visualize()
+        except Exception as e:
+            if 'ExecutableNotFound' in str(type(e)):
+                raise OSError("Missing Graphviz? You can install it here: https://graphviz.org/download/") from e
+            raise e
+
+    def visualize(self) -> None:
+        """Create the actual visualization and show it"""
+        with Diagram(
+            name="", filename=self.filename, graph_attr=self.__graph_attr,
+            show=self.show, outformat=self.outformat
+        ):
+            for host in self._get_hosts():
+                self._add_connections(host)
+                if (node := self._get_node(host)) is not None:
+                    self.nodes[host.name] = node
+
+            for connection in self.connections:
+                source, target, connection_name, edge_color, label_color = connection
+                if source in self.nodes and target in self.nodes:
+                    edge = Edge(
+                        label=f"<<font color='{label_color}'><b>{connection_name}</b></font>>",
+                        minlen="4", style="dashed", penwidth="3", fontsize=self.__font_size_edge, color=edge_color
+                    )
+                    self.nodes[source] >> edge >> self.nodes[target]
