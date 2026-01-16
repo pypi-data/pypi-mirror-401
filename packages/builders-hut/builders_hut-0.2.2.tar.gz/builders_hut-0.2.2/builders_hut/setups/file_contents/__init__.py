6647d062ae932ad08efc1223f0d7cd897459f611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 13:25:34 +0000

    Add package builders-hut/0.2.2/builders_hut-0.2.2.tar.gz

---
+++ packages/builders-hut/builders_hut-0.2.2.tar.gz/builders_hut-0.2.2/builders_hut/setups/file_contents/__init__.py
@@ -0,0 +1,16 @@
+from .dev_file import DEV_FILE_CONTENT
+from .main_file import MAIN_FILE_CONTENT
+from .common_api_file import COMMON_API_CONTENT, COMMON_API_INIT
+from .core_file import CORE_FILE_CONTENT
+from .template_file import TEMPLATE_FILE_CONTENT
+from .env_file import ENV_FILE_CONTENT
+
+__all__ = [
+    DEV_FILE_CONTENT,
+    MAIN_FILE_CONTENT,
+    COMMON_API_CONTENT,
+    COMMON_API_INIT,
+    CORE_FILE_CONTENT,
+    TEMPLATE_FILE_CONTENT,
+    ENV_FILE_CONTENT,
+]
---
+++ packages/builders-hut/builders_hut-0.2.2.tar.gz/builders_hut-0.2.2/builders_hut/setups/structure_setup.py
@@ -0,0 +1,52 @@
+from builders_hut.setups.base_setup import BaseSetup
+from rich import print
+
+# Define all necessary directories for the project structure
+ALL_DIRS = [
+    "api",
+    "api/v1",
+    "database",
+    "schemas",
+    "services",
+    "repositories",
+    "core",
+    "models",
+    "workers",
+    "utils",
+    "scripts",
+    "templates",
+]
+
+
+class SetupStructure(BaseSetup):
+    """
+    Setup all the required directory structure for the project.
+
+    Structure:
+    .
+    ├── app
+    │   ├── api           # API route definitions
+    |   |   ├── v1        # Version 1 endpoints
+    │   ├── core          # Core configuration and application settings
+    │   ├── database      # Database setup, connections, and sessions
+    │   ├── models        # ORM / data models
+    │   ├── repositories  # Data access layer
+    │   ├── schemas       # Request and response schemas
+    │   ├── services      # Business logic
+    │   ├── utils         # Utility and helper functions
+    │   ├── workers       # Background jobs and async workers
+    |   ├── scripts       # Utility and automation scripts to run server
+    |   ├── templates     # Templates
+    │
+    ├── tests             # Unit and integration tests
+    """
+
+    def create(self):
+        print(
+            f"Creating directory structure at: [bold green]{self.location}[/bold green]"
+        )
+        (self.location / "tests").mkdir(exist_ok=True, parents=True)
+        (self.location / "app").mkdir(exist_ok=True, parents=True)
+        for dir_name in ALL_DIRS:
+            (self.location / "app" / dir_name).mkdir(exist_ok=True, parents=True)
+        print("All directories created [bold green]successfully![/bold green]")
