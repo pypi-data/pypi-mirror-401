5d6c73c97b4f3aca103fe2179e7a90b28814bc43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:08:50 +0000

    Add package prospr/1.2.10/prospr-1.2.10-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/prospr/prospr-1.2.10-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/prospr/core/src/depth_first.hpp
@@ -0,0 +1,16 @@
+/* File:            depth_first.h
+ * Description:     Header file for a depth-first search function.
+ * License:         This file is licensed under the GNU LGPL V3 license by
+ *                  Okke van Eck (2020 - 2023). See the LICENSE file for the
+ *                  specifics.
+ */
+
+#ifndef DEPTH_FIRST_H
+#define DEPTH_FIRST_H
+
+#include "protein.hpp"
+
+/* A depth-first search function for finding a minimum energy conformation. */
+void depth_first(Protein *protein);
+
+#endif
