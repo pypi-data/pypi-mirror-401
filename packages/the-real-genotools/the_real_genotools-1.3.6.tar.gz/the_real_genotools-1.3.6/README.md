1575a9fdc9947d2ce24772073be9e65b76305a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 06:06:33 +0000

    Add package the-real-genotools/1.3.6/the_real_genotools-1.3.6.tar.gz

---
+++ packages/the-real-genotools/the_real_genotools-1.3.6.tar.gz/the_real_genotools-1.3.6/README.md
@@ -0,0 +1,98 @@
+# GenoTools
+## Published in G3: [https://www.biorxiv.org/content/10.1101/2024.03.26.586362v1.full.pdf](https://doi.org/10.1093/g3journal/jkae268)
+[![DOI](https://zenodo.org/badge/337965715.svg)](https://zenodo.org/doi/10.5281/zenodo.10443257)
+[![PyPI version](https://badge.fury.io/py/the-real-genotools.svg)](https://badge.fury.io/py/the-real-genotools)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+![GitHub License](https://img.shields.io/github/license/dvitale199/GenoTools)
+![Python](https://img.shields.io/badge/python-3.8-blue.svg)
+![Python](https://img.shields.io/badge/python-3.9-blue.svg)
+![Python](https://img.shields.io/badge/python-3.10-blue.svg)
+
+
+## Documentation
+You can find the full documentation with the following links:
+- [GenoTools Command Line Arguments](https://github.com/dvitale199/GenoTools/blob/main/docs/cli_args.md)
+- [Default Pipeline Overview](https://github.com/dvitale199/GenoTools/blob/main/docs/default_pipeline_overview.md)
+- [Package Function Guide (for developers)](https://github.com/dvitale199/GenoTools/blob/main/docs/genotools_function_guide.md)
+- [JSON output guide](https://github.com/dvitale199/GenoTools/blob/main/docs/json_output_overview.md)
+
+## Getting Started
+
+GenoTools is a suite of automated genotype data processing steps written in Python. The core pipeline was built for Quality Control and Ancestry estimation of data in the Global Parkinson's Genetics Program (GP2)
+
+To download the most current version from pip:
+```
+pip install the-real-genotools
+```
+Alternatively, if you'd like to download from github:
+```
+git clone https://github.com/dvitale199/GenoTools.git
+cd GenoTools
+pip install .
+```
+you can pull the most current references by running:
+```
+genotools-download
+```
+By default, the reference panel will be downloaded to ~/.genotools/ref. but can be download to a location of choice with `--destination`.
+
+To download specific references/models, you can run the download with the following options:
+```
+genotools-download --ref 1kg_30x_hgdp_ashk_ref_panel --model nba_v1 --destination /path/to/download_directory/
+```
+
+Currently, `1kg_30x_hgdp_ashk_ref_panel` is the only available reference panel. Available models are `nba_v1` for the NeuroBooster array and `neurochip_v1` for the NeuroChip Array and both are in GRCh38. If using a different array, we would suggest training a new model by running the standard command below. Please ensure the reference panel and your genotypes are in the same build. If you're using our reference panel, your genotypes must be in GRCh38.
+
+Modify the paths in the following command to run the standard GP2 pipeline:
+```
+genotools \
+  --pfile /path/to/genotypes/for/qc \
+  --out /path/to/qc/output \
+  --ancestry \
+  --ref_panel /path/to/reference/panel \
+  --ref_labels /path/to/reference/ancestry/labels \
+  --all_sample \
+  --all_variant
+```
+This will find common snps between your genotype data and the reference panel, run PCA, UMAP-transform PCs, and train a new XGBoost classifier specific to your data/ref panel.
+
+if you'd like to run the pipeline using an existing model, you can do that like so (take note of the `--model` option):
+```
+genotools \
+  --pfile /path/to/genotypes/for/qc \
+  --out /path/to/qc/output \
+  --ancestry \
+  --ref_panel /path/to/reference/panel \
+  --ref_labels /path/to/reference/ancestry/labels \
+  --all_sample \
+  --all_variant
+  --model /path/to/nba_v1/model
+```
+
+if you'd like to run the pipeline using the default nba_v1 model in a Docker container, you can do that like so:
+```
+genotools \
+  --pfile /path/to/genotypes/for/qc \
+  --out /path/to/qc/output \
+  --ancestry \
+  --ref_panel /path/to/reference/panel \
+  --ref_labels /path/to/reference/ancestry/labels \
+  --container \
+  --all_sample \
+  --all_variant
+```
+Note: add the ```--singularity``` flag to run containerized ancestry predictions on HPC
+
+genotools accepts `--pfile`, `--bfile`, or `--vcf`. Any bfile or vcf will be converted to a pfile before running any steps. 
+
+Note: multiallelic pfiles will be converted to biallelic format by excluding multiallelic variants before running '--ancestry' steps. If you would prefer to not remove multiallelic snps, please pre-split the SNPs using bcftools prior to running genotools.  
+
+Please consult the docs links listed at the top of the README for the full argument guide, function guide, Default pipeline overview, and guide for navigating the output JSON.
+
+## Acknowledgements
+GenoTools was developed as the core genotype and wgs processing pipeline for the Global Parkinson's Genetics Program (GP2) at the Center for Alzheimer's and Related Dementias (CARD) at the National Institutes of Health.
+
+This tool relies on PLINK, a whole genome association analysis toolset, for various genetic data processing functionalities. We gratefully acknowledge the developers of PLINK for their foundational contributions to the field of genetics. More about PLINK can be found at [their website](https://www.cog-genomics.org/plink/2.0/).
+
+
+
---
+++ packages/the-real-genotools/the_real_genotools-1.3.6.tar.gz/the_real_genotools-1.3.6/genotools/dependencies.py
@@ -0,0 +1,221 @@
+# Copyright 2023 The GenoTools Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+
+import io
+import logging
+import os
+import pathlib
+import platform
+import requests
+import stat
+import subprocess
+import zipfile
+import tarfile
+import sys
+
+# can decide where to throw the executable files later
+## for now /$HOME/.genotools/misc/executables
+def __get_executable_folder():
+    key = "GENOTOOLS_DEP_DIR"
+    if key in os.environ:
+        return os.path.abspath(os.environ.get(key))
+    else:
+        return os.path.join(str(pathlib.Path.home()), ".genotools", "misc",
+                            "executables")
+
+
+__executable_folder = __get_executable_folder()
+
+def shell_do(command, log=False, return_log=False):
+    print(f'Executing: {(" ").join(command.split())}', file=sys.stderr)
+
+    res=subprocess.run(command.split(), stdout=subprocess.PIPE)
+
+    if log:
+        print(res.stdout.decode('utf-8'))
+    if return_log:
+        return(res.stdout.decode('utf-8'))
+
+def __check_exec(exec_path, *args, absolute_path=False):
+    if not absolute_path:
+        binary_path = os.path.join(__executable_folder, exec_path)
+    else:
+        binary_path = exec_path
+    if not os.path.exists(binary_path):
+        return False
+
+    _ = subprocess.run([binary_path, *args], stdout=subprocess.DEVNULL,
+                       stderr=subprocess.DEVNULL)
+    return True
+
+
+def __install_exec(url, exec_path):
+    r = requests.get(url, verify=False, stream=True, headers=_HEADERS)
+
+    if '.zip' in url:
+        try:
+            r.raw.decode_content = True
+            buffer = io.BytesIO()
+            buffer.write(r.content)
+            with zipfile.ZipFile(buffer, "r") as fp:
+                fp.extractall(__executable_folder)
+        except:
+            zip_file = url.split('/')[-1]
+            zip_file_path = os.path.join(__executable_folder, zip_file)
+
+            r = requests.get(url)
+            r.raise_for_status()
+            with open(zip_file_path, 'wb') as fp:
+                fp.write(r.content)
+
+            with zipfile.ZipFile(zip_file_path, 'r') as fp:
+                fp.extractall(__executable_folder)
+
+            permissions = 0o100
+            os.chmod(os.path.join(__executable_folder, 'plink2'), permissions)
+
+    elif '.tar.gz' in url:
+        file = tarfile.open(fileobj=r.raw, mode="r|gz")
+        file.extractall(__executable_folder)
+
+    binary_path = os.path.join(__executable_folder, exec_path)
+
+    os.chmod(binary_path, stat.S_IEXEC)
+
+
+def __check_package(name):
+    platform_system = platform.system()
+    platform_processor = platform.processor()
+
+    if name not in __DEPENDENCIES:
+        raise EnvironmentError("Unknown package: {}".format(name))
+
+    if platform_system not in __DEPENDENCIES[name]:
+        if name == 'KING':
+            logging.warning("Can not install {} on non-Linux OS".format(name))
+            return
+        else:
+            raise EnvironmentError(
+                "Unknown supported OK: {}".format(platform_system))
+
+    if (platform_system == "Darwin") and (platform_processor == "arm") and (name == "Plink2"):
+        platform_system = "Darwin_arm64"
+
+    entry = __DEPENDENCIES[name][platform_system]
+
+    binary_name = entry["binary"]
+    args = entry["version_args"]
+    url = entry["url"]
+
+    if __check_exec(binary_name, *args):
+        # print(f'{name} is found')
+        logging.debug("{} is found".format(name))
+        return os.path.join(__executable_folder, binary_name)
+
+    logging.warning("Installing {}".format(name))
+    __install_exec(url, binary_name)
+    if not __check_exec(binary_name, *args):
+        logging.warning("Failed to run {} after installation".format(name))
+        raise EnvironmentError("Can not install {}".format(name))
+    else:
+        return os.path.join(__executable_folder, binary_name)
+
+
+def check_dependencies():
+    global __DEPENDENCIES
+    ret = {}
+    for package, data in __DEPENDENCIES.items():
+        if "checker" in data:
+            # instead of GenoMLs description loader
+            print('Loaded')
+
+    return ret
+
+def check_plink():
+    return __check_package('Plink')
+
+def check_plink2():
+    return __check_package('Plink2')
+
+def check_king():
+    return __check_package('KING')
+
+__DEPENDENCIES = {
+    'Plink': {
+        'checker': check_plink,
+        'Darwin': {
+            'binary': 'plink',
+            'version_args': ['--version'],
+            'url': 'https://s3.amazonaws.com/plink1-assets/dev/plink_mac.zip'
+        },
+        'Linux': {
+            'binary': 'plink',
+            'version_args': ['--version'],
+            'url': 'https://s3.amazonaws.com/plink1-assets/dev/plink_linux_x86_64.zip'
+        },
+        'Windows': {
+            'binary': 'plink',
+            'version_args': ['--version'],
+            'url': 'https://s3.amazonaws.com/plink1-assets/plink_win64_20220402.zip'
+        }
+    },
+
+    'Plink2': {
+        'checker': check_plink2,
+        'Darwin_arm64': {
+            'binary': 'plink2',
+            'version_args': ['--version'],
+            'url': 'https://s3.amazonaws.com/plink2-assets/alpha5/plink2_mac_arm64_20240105.zip'
+        },
+        'Darwin': {
+            'binary': 'plink2',
+            'version_args': ['--version'],
+            'url': 'https://s3.amazonaws.com/plink2-assets/alpha5/plink2_mac_avx2_20240105.zip'
+        },
+        'Linux': {
+            'binary': 'plink2',
+            'version_args': ['--version'],
+            'url': 'https://s3.amazonaws.com/plink2-assets/alpha5/plink2_linux_x86_64_20240105.zip'
+        },
+    },
+
+    'KING': {
+        'checker': check_king,
+        # 'Darwin': {
+        #     'binary': 'king',
+        #     'version_args': ['--version'],
+        #     'url': 'https://www.kingrelatedness.com/executables/Linux-king232.tar.gz'
+        # },
+        'Linux': {
+            'binary': 'king',
+            'version_args': ['--version'],
+            'url': 'https://www.kingrelatedness.com/executables/Linux-king232.tar.gz'
+        },
+        'Windows': {
+            'binary': 'king',
+            'version_args': ['--version'],
+            'url': 'https://www.kingrelatedness.com/executables/Windows-king232.zip'
+        }
+    }
+}
+
+_HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/120.0.0.0 Safari/537.36"
+    )
+}
