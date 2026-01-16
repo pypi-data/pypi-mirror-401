ac3eecb2dc7425f9ebbfa5a06b49a12936811f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 05:10:16 +0000

    Add package bbstrader/1.0.0/bbstrader-1.0.0-cp312-cp312-win_amd64.whl

---
+++ packages/bbstrader/bbstrader-1.0.0-cp312-cp312-win_amd64.whl/bbstrader/core/__init__.py
@@ -0,0 +1,1 @@
+from bbstrader.core.data import *  # noqa: F403
---
+++ packages/bbstrader/bbstrader-1.0.0-cp312-cp312-win_amd64.whl/bbstrader/models/__init__.py
@@ -0,0 +1,7 @@
+"""
+The `models` module provides a foundational framework for implementing various quantitative finance models. 
+
+It is designed to be a versatile base module for different types of models used in financial analysis and trading. 
+"""
+from bbstrader.models.optimization import *  # noqa: F403
+from bbstrader.models.nlp import *  # noqa: F403
