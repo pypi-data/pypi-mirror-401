327a299732619ca9e9f2e4bd679edaa008bfed16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 06:06:25 +0000

    Add package qlever/0.5.42/qlever-0.5.42-py3-none-any.whl

---
+++ packages/qlever/qlever-0.5.42-py3-none-any.whl/qlever/commands/index_stats.py
@@ -0,0 +1,306 @@
+from __future__ import annotations
+
+import re
+from datetime import datetime
+from pathlib import Path
+
+from qlever.command import QleverCommand
+from qlever.log import log
+from qlever.util import get_total_file_size
+
+
+class IndexStatsCommand(QleverCommand):
+    """
+    Class for executing the `index-stats` command.
+    """
+
+    def __init__(self):
+        pass
+
+    def description(self) -> str:
+        return "Breakdown of the time and space used for the index build"
+
+    def should_have_qleverfile(self) -> bool:
+        return False
+
+    def relevant_qleverfile_arguments(self) -> dict[str, list[str]]:
+        return {"data": ["name"]}
+
+    def additional_arguments(self, subparser) -> None:
+        subparser.add_argument(
+            "--only-time",
+            action="store_true",
+            default=False,
+            help="Show only the time used",
+        )
+        subparser.add_argument(
+            "--only-space",
+            action="store_true",
+            default=False,
+            help="Show only the space used",
+        )
+        subparser.add_argument(
+            "--ignore-text-index",
+            action="store_true",
+            default=False,
+            help="Ignore the text index",
+        )
+        subparser.add_argument(
+            "--time-unit",
+            choices=["s", "min", "h", "auto"],
+            default="auto",
+            help="The time unit",
+        )
+        subparser.add_argument(
+            "--size-unit",
+            choices=["B", "MB", "GB", "TB", "auto"],
+            default="auto",
+            help="The size unit",
+        )
+
+    def execute_time(self, args, log_file_name) -> bool:
+        """
+        Part of `execute` that shows the time used.
+        """
+
+        # Read the content of `log_file_name` into a list of lines.
+        try:
+            with open(log_file_name, "r") as log_file:
+                lines = log_file.readlines()
+        except Exception as e:
+            log.error(f"Problem reading index log file {log_file_name}: {e}")
+            return False
+        # If there is a separate `add-text-index-log.txt` file, append those
+        # lines.
+        try:
+            text_log_file_name = f"{args.name}.text-index-log.txt"
+            if Path(text_log_file_name).exists():
+                with open(text_log_file_name, "r") as text_log_file:
+                    lines.extend(text_log_file.readlines())
+        except Exception as e:
+            log.error(
+                f"Problem reading text index log file " f"{text_log_file_name}: {e}"
+            )
+            return False
+
+        # Helper function that finds the next line matching the given `regex`,
+        # starting from `current_line`, and extracts the time. Returns a tuple
+        # of the time and the regex match object.
+        #
+        # If `update_current_line` is `False`, then `current_line` will not be
+        # updated by this call.
+        #
+        # Otherwise, and this is the default behavior, `current_line` will be
+        # updated to the line after the first match, or one beyond the last
+        # line if no match is found.
+        current_line = 0
+
+        def find_next_line(regex, update_current_line=True):
+            nonlocal lines
+            nonlocal current_line
+            current_line_backup = current_line
+            # Find starting from `current_line`.
+            while current_line < len(lines):
+                line = lines[current_line]
+                current_line += 1
+                timestamp_regex = r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}"
+                timestamp_format = "%Y-%m-%d %H:%M:%S"
+                regex_match = re.search(regex, line)
+                if regex_match:
+                    try:
+                        return datetime.strptime(
+                            re.match(timestamp_regex, line).group(), timestamp_format
+                        ), regex_match
+                    except Exception as e:
+                        log.error(
+                            f"Could not parse timestamp of form "
+                            f'"{timestamp_regex}" from line '
+                            f' "{line.rstrip()}" ({e})'
+                        )
+            # If we get here, we did not find a matching line.
+            if not update_current_line:
+                current_line = current_line_backup
+            return None, None
+
+        # Find the lines matching the key_lines_regex and extract the time
+        # information from them.
+        overall_begin, _ = find_next_line(r"INFO:\s*Processing")
+        merge_begin, _ = find_next_line(r"INFO:\s*Merging partial vocab")
+        convert_begin, _ = find_next_line(r"INFO:\s*Converting triples")
+        perm_begin_and_info = []
+        while True:
+            # Find the next line that starts a permutation.
+            #
+            # NOTE: Should work for the old and new format of the index log
+            # file (old format: "Creating a pair" + names of permutations in
+            # line "Writing meta data for ..."; new format: name of
+            # permutations already in line "Creating permutations ...").
+            perm_begin, _ = find_next_line(
+                r"INFO:\s*Creating a pair", update_current_line=False
+            )
+            if perm_begin is None:
+                perm_begin, perm_info = find_next_line(
+                    r"INFO:\s*Creating permutations ([A-Z]+ and [A-Z]+)",
+                    update_current_line=False,
+                )
+            else:
+                _, perm_info = find_next_line(
+                    r"INFO:\s*Writing meta data for ([A-Z]+ and [A-Z]+)",
+                    update_current_line=False,
+                )
+            if perm_info is None:
+                break
+            perm_begin_and_info.append((perm_begin, perm_info))
+        convert_end = (
+            perm_begin_and_info[0][0] if len(perm_begin_and_info) > 0 else None
+        )
+        normal_end, _ = find_next_line(r"INFO:\s*Index build completed")
+        text_begin, _ = find_next_line(
+            r"INFO:\s*Adding text index", update_current_line=False
+        )
+        text_end, _ = find_next_line(
+            r"INFO:\s*Text index build comp", update_current_line=False
+        )
+        if args.ignore_text_index:
+            text_begin = text_end = None
+
+        # Check whether at least the first phase is done.
+        if overall_begin is None:
+            log.error("Missing line that index build has started")
+            return False
+        if overall_begin and not merge_begin:
+            log.error(
+                "According to the log file, the index build "
+                "has started, but is still in its first "
+                "phase (parsing the input)"
+            )
+            return False
+
+        # Helper function that shows the duration for a phase (if the start and
+        # end timestamps are available).
+        def show_duration(heading, start_end_pairs):
+            nonlocal time_unit
+            num_start_end_pairs = 0
+            diff_seconds = 0
+            for start, end in start_end_pairs:
+                if start and end:
+                    diff_seconds += (end - start).total_seconds()
+                    num_start_end_pairs += 1
+            if num_start_end_pairs > 0:
+                if time_unit == "h":
+                    diff = diff_seconds / 3600
+                elif time_unit == "min":
+                    diff = diff_seconds / 60
+                else:
+                    diff = diff_seconds
+                log.info(f"{heading:<21} : {diff:>6.1f} {time_unit}")
+
+        # Get the times of the various phases (hours or minutes, depending on
+        # how long the first phase took).
+        time_unit = args.time_unit
+        if time_unit == "auto":
+            time_unit = "h"
+            if merge_begin and overall_begin:
+                parse_duration = (merge_begin - overall_begin).total_seconds()
+                if parse_duration < 200:
+                    time_unit = "s"
+                elif parse_duration < 3600:
+                    time_unit = "min"
+        show_duration("Parse input", [(overall_begin, merge_begin)])
+        show_duration("Build vocabularies", [(merge_begin, convert_begin)])
+        show_duration("Convert to global IDs", [(convert_begin, convert_end)])
+        for i in range(len(perm_begin_and_info)):
+            perm_begin, perm_info = perm_begin_and_info[i]
+            perm_end = (
+                perm_begin_and_info[i + 1][0]
+                if i + 1 < len(perm_begin_and_info)
+                else normal_end
+            )
+            perm_info_text = (
+                perm_info.group(1).replace(" and ", " & ") if perm_info else f"#{i + 1}"
+            )
+            show_duration(f"Permutation {perm_info_text}", [(perm_begin, perm_end)])
+        show_duration("Text index", [(text_begin, text_end)])
+        if text_begin and text_end:
+            log.info("")
+            show_duration(
+                "TOTAL time", [(overall_begin, normal_end), (text_begin, text_end)]
+            )
+        elif normal_end:
+            log.info("")
+            show_duration("TOTAL time", [(overall_begin, normal_end)])
+        return True
+
+    def execute_space(self, args) -> bool:
+        """
+        Part of `execute` that shows the space used.
+        """
+
+        # Get the sizes for the various groups of index files.
+        index_size = get_total_file_size([f"{args.name}.index.*"])
+        vocab_size = get_total_file_size([f"{args.name}.vocabulary.*"])
+        text_size = get_total_file_size([f"{args.name}.text.*"])
+        if args.ignore_text_index:
+            text_size = 0
+        total_size = index_size + vocab_size + text_size
+
+        # Determing the proper unit for the size.
+        size_unit = args.size_unit
+        if size_unit == "auto":
+            size_unit = "TB"
+            if total_size < 1e6:
+                size_unit = "B"
+            elif total_size < 1e9:
+                size_unit = "MB"
+            elif total_size < 1e12:
+                size_unit = "GB"
+
+        # Helper function for showing the size in a uniform way.
+        def show_size(heading, size):
+            nonlocal size_unit
+            if size_unit == "GB":
+                size /= 1e9
+            elif size_unit == "MB":
+                size /= 1e6
+            elif size_unit == "TB":
+                size /= 1e12
+            if size_unit == "B":
+                log.info(f"{heading:<21} :  {size:,} {size_unit}")
+            else:
+                log.info(f"{heading:<21} : {size:>6.1f} {size_unit}")
+
+        show_size("Files index.*", index_size)
+        show_size("Files vocabulary.*", vocab_size)
+        if text_size > 0:
+            show_size("Files text.*", text_size)
+        log.info("")
+        show_size("TOTAL size", total_size)
+        return True
+
+    def execute(self, args) -> bool:
+        return_value = True
+
+        # The "time" part of the command.
+        if not args.only_space:
+            log_file_name = f"{args.name}.index-log.txt"
+            self.show(
+                f"Breakdown of the time used for "
+                f"building the index, based on the timestamps for key "
+                f'lines in "{log_file_name}"',
+                only_show=args.show,
+            )
+            if not args.show:
+                return_value &= self.execute_time(args, log_file_name)
+            if not args.only_time:
+                log.info("")
+
+        # The "space" part of the command.
+        if not args.only_time:
+            self.show(
+                "Breakdown of the space used for building the index",
+                only_show=args.show,
+            )
+            if not args.show:
+                return_value &= self.execute_space(args)
+
+        return return_value
---
+++ packages/qlever/qlever-0.5.42-py3-none-any.whl/qlever/util.py
@@ -0,0 +1,341 @@
+from __future__ import annotations
+
+import errno
+import re
+import secrets
+import shlex
+import shutil
+import socket
+import string
+import subprocess
+from datetime import date, datetime
+from pathlib import Path
+from typing import Any, Optional
+
+import psutil
+
+from qlever.log import log
+
+
+def get_total_file_size(patterns: list[str]) -> int:
+    """
+    Helper function that gets the total size of all files mathing the given
+    patterns in bytes.
+    """
+
+    total_size = 0
+    search_dir = Path.cwd()
+    for pattern in patterns:
+        for file in search_dir.glob(pattern):
+            total_size += file.stat().st_size
+    return total_size
+
+
+def run_command(
+    cmd: str,
+    return_output: bool = False,
+    show_output: bool = False,
+    show_stderr: bool = False,
+    use_popen: bool = False,
+) -> Optional[str | subprocess.Popen]:
+    """
+    Run the given command and throw an exception if the exit code is non-zero.
+    If `return_output` is `True`, return what the command wrote to `stdout`.
+
+    NOTE: The `set -o pipefail` ensures that the exit code of the command is
+    non-zero if any part of the pipeline fails (not just the last part).
+
+    TODO: Find the executable for `bash` in `__init__.py`.
+    """
+
+    subprocess_args = {
+        "executable": shutil.which("bash"),
+        "shell": True,
+        "text": True,
+        "stdout": None if show_output else subprocess.PIPE,
+        "stderr": None if show_stderr else subprocess.PIPE,
+    }
+
+    # With `Popen`, the command runs in the current shell and a process object
+    # is returned (which can be used, e.g., to kill the process).
+    if use_popen:
+        if return_output:
+            raise Exception("Cannot return output if `use_popen` is `True`")
+        return subprocess.Popen(f"set -o pipefail; {cmd}", **subprocess_args)
+
+    # With `run`, the command runs in a subshell and the output is captured.
+    result = subprocess.run(f"set -o pipefail; {cmd}", **subprocess_args)
+
+    # If the exit code is non-zero, throw an exception. If something was
+    # written to `stderr`, use that as the exception message. Otherwise, use a
+    # generic message (which is also what `subprocess.run` does with
+    # `check=True`).
+    if result.returncode != 0:
+        if len(result.stderr) > 0:
+            raise Exception(result.stderr.replace("\n", " ").strip())
+        else:
+            raise Exception(
+                f"Command failed with exit code {result.returncode}, "
+                f" nothing written to stderr (stdout: {result.stdout})"
+            )
+    # Optionally, return what was written to `stdout`.
+    if return_output:
+        return result.stdout
+
+
+def run_curl_command(
+    url: str,
+    headers: dict[str, str] = {},
+    params: dict[str, str] = {},
+    result_file: Optional[str] = None,
+) -> str:
+    """
+    Run `curl` with the given `url`, `headers`, and `params`. If `result_file`
+    is `None`, return the output, otherwise, write the output to the given file
+    and return the HTTP code. If the `curl` command fails, throw an exception.
+
+    """
+    # Construct and run the `curl` command.
+    default_result_file = "/tmp/qlever.curl.result"
+    actual_result_file = result_file if result_file else default_result_file
+    curl_cmd = (
+        f'curl -Ls -o "{actual_result_file}"'
+        f' -w "%{{http_code}}\n" {url}'
+        + "".join([f' -H "{key}: {value}"' for key, value in headers.items()])
+        + "".join(
+            [
+                f" --data-urlencode {key}={shlex.quote(value)}"
+                for key, value in params.items()
+            ]
+        )
+    )
+    result = subprocess.run(
+        curl_cmd,
+        shell=True,
+        text=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    # Case 1: An error occurred, raise an exception.
+    if result.returncode != 0:
+        if len(result.stderr) > 0:
+            raise Exception(result.stderr)
+        else:
+            raise Exception(
+                f"curl command failed with exit code "
+                f"{result.returncode}, stderr is empty"
+            )
+    # Case 2: Return output (read from `default_result_file`).
+    if result_file is None:
+        result_file_path = Path(default_result_file)
+        result = result_file_path.read_text()
+        result_file_path.unlink()
+        return result
+    # Case 3: Return HTTP code.
+    return result.stdout
+
+
+def is_qlever_server_alive(endpoint_url: str) -> bool:
+    """
+    Helper function that checks if a QLever server is running on the given
+    endpoint. Return `True` if the server is alive, `False` otherwise.
+    """
+
+    message = "from the `qlever` CLI"
+    curl_cmd = (
+        f"curl -s {endpoint_url}/ping"
+        f" --data-urlencode msg={shlex.quote(message)}"
+    )
+    log.debug(curl_cmd)
+    try:
+        run_command(curl_cmd)
+        return True
+    except Exception:
+        return False
+
+
+def get_existing_index_files(basename: str, add_non_essential: bool = False) -> list[str]:
+    """
+    Helper function that returns a list of all index files for `basename` in
+    the current working directory.
+    """
+
+    # Essential index files.
+    existing_index_files = []
+    existing_index_files.extend(Path.cwd().glob(f"{basename}.index.*"))
+    existing_index_files.extend(Path.cwd().glob(f"{basename}.internal.index.*"))
+    existing_index_files.extend(Path.cwd().glob(f"{basename}.text.*"))
+    existing_index_files.extend(Path.cwd().glob(f"{basename}.vocabulary.*"))
+    existing_index_files.extend(Path.cwd().glob(f"{basename}.meta-data.json"))
+    existing_index_files.extend(Path.cwd().glob(f"{basename}.prefixes"))
+
+    # Non-essential index files.
+    if add_non_essential:
+        existing_index_files.extend(Path.cwd().glob(f"{basename}.view.*"))
+        existing_index_files.extend(Path.cwd().glob(f"{basename}.settings.json"))
+        existing_index_files.extend(Path.cwd().glob(f"{basename}.index-log.txt"))
+        existing_index_files.extend(Path.cwd().glob(f"{basename}.server-log.txt"))
+
+    # Return only the file names, not the full paths.
+    return [path.name for path in existing_index_files]
+
+
+def show_process_info(psutil_process, cmdline_regex, show_heading=True):
+    """
+    Helper function that shows information about a process if information
+    about the process can be retrieved and the command line matches the
+    given regex (in which case the function returns `True`). The heading is
+    only shown if `show_heading` is `True` and the function returns `True`.
+    """
+
+    # Helper function that shows a line of the process table.
+    def show_table_line(pid, user, start_time, rss, cmdline):
+        log.info(f"{pid:<8} {user:<8} {start_time:>5}  {rss:>5} {cmdline}")
+
+    try:
+        pinfo = psutil_process.as_dict(
+            attrs=["pid", "username", "create_time", "memory_info", "cmdline"]
+        )
+        # Note: pinfo[`cmdline`] is `None` if the process is a zombie.
+        cmdline = " ".join(pinfo["cmdline"] or [])
+        if len(cmdline) == 0 or not re.search(cmdline_regex, cmdline):
+            return False
+        pid = pinfo["pid"]
+        user = pinfo["username"] if pinfo["username"] else ""
+        start_time = datetime.fromtimestamp(pinfo["create_time"])
+        if start_time.date() == date.today():
+            start_time = start_time.strftime("%H:%M")
+        else:
+            start_time = start_time.strftime("%b%d")
+        rss = f"{pinfo['memory_info'].rss / 1e9:.0f}G"
+        if show_heading:
+            show_table_line("PID", "USER", "START", "RSS", "COMMAND")
+        show_table_line(pid, user, start_time, rss, cmdline)
+        return True
+    except Exception as e:
+        log.error(f"Could not get process info: {e}")
+        return False
+
+
+def get_random_string(length: int) -> str:
+    """
+    Helper function that returns a randomly chosen string of the given
+    length. Take the current time as seed.
+    """
+    characters = string.ascii_letters + string.digits
+    return "".join(secrets.choice(characters) for _ in range(length))
+
+
+def is_port_used(port: int) -> bool:
+    """
+    Try to bind to the port on all interfaces to check if the port is already in use.
+    If the port is already in use, `socket.bind` will raise an `OSError` with errno EADDRINUSE.
+    """
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Ensure that the port is not blocked after the check.
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("", port))
+        sock.close()
+        return False
+    except OSError as err:
+        if err.errno != errno.EADDRINUSE:
+            log.warning(f"Failed to determine if port is used: {err}")
+        return True
+
+
+def format_size(bytes, suffix="B"):
+    """
+    Scale bytes to its proper format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f} {unit}{suffix}"
+        bytes /= factor
+
+
+def stop_process(proc: psutil.Process, pinfo: dict[str, Any]) -> bool:
+    """
+    Try to kill the given process, return True iff it was killed
+    successfully. The process_info is used for logging.
+    """
+    try:
+        proc.kill()
+        log.info(f"Killed process {pinfo['pid']}")
+        return True
+    except Exception as e:
+        log.error(
+            f"Could not kill process with PID "
+            f"{pinfo['pid']} ({e}) ... try to kill it "
+            f"manually"
+        )
+        log.info("")
+        show_process_info(proc, "", show_heading=True)
+        return False
+
+
+def stop_process_with_regex(cmdline_regex: str) -> list[bool] | None:
+    """
+    Given a cmdline_regex for a native process, try to kill the processes that
+    match the regex and return a list of their stopped status (bool).
+    Show the matched processes as log info.
+    """
+    stop_process_results = []
+    for proc in psutil.process_iter():
+        try:
+            pinfo = proc.as_dict(
+                attrs=[
+                    "pid",
+                    "username",
+                    "create_time",
+                    "memory_info",
+                    "cmdline",
+                ]
+            )
+            cmdline = " ".join(pinfo["cmdline"])
+        except Exception as e:
+            # For some processes (e.g., zombies), getting info may fail.
+            log.debug(f"Error getting process info: {e}")
+            continue
+        if re.search(cmdline_regex, cmdline):
+            log.info(
+                f"Found process {pinfo['pid']} from user "
+                f"{pinfo['username']} with command line: {cmdline}"
+            )
+            log.info("")
+            stop_process_results.append(stop_process(proc, pinfo))
+    return stop_process_results
+
+
+def binary_exists(binary: str, cmd_arg: str) -> bool:
+    """
+    When a command is run natively, check if the binary exists on the system
+    """
+    try:
+        run_command(f"{binary} --help")
+        return True
+    except Exception as e:
+        log.error(
+            f'Running "{binary}" failed, '
+            f"set `--{cmd_arg}` to a different binary or "
+            f"set `--system to a container system`"
+        )
+        log.info("")
+        log.info(f"The error message was: {e}")
+        return False
+
+
+def is_server_alive(url: str) -> bool:
+    """
+    Check if the server is already alive at the given endpoint url
+    """
+    check_server_cmd = f"curl -s {url}"
+    try:
+        run_command(check_server_cmd)
+        return True
+    except Exception:
+        return False
