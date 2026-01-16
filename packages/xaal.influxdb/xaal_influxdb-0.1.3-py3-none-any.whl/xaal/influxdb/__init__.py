2fb642cb00cce99a0ca615463ea53479d8aca1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 08:07:01 +0000

    Add package xaal.influxdb/0.1.3/xaal_influxdb-0.1.3-py3-none-any.whl

---
+++ packages/xaal.influxdb/xaal_influxdb-0.1.3-py3-none-any.whl/xaal/influxdb/__init__.py
@@ -0,0 +1,1 @@
+from .influxdblogger import setup
---
+++ packages/xaal.influxdb/xaal_influxdb-0.1.3-py3-none-any.whl/xaal/influxdb/__main__.py
@@ -0,0 +1,5 @@
+from . import influxdblogger
+from xaal.lib import helpers
+
+helpers.run_package(influxdblogger.PACKAGE_NAME,influxdblogger.setup)
+
