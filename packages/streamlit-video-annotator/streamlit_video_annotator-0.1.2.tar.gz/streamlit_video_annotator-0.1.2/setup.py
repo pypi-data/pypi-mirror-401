c8c11e35d8bb2b8b98324f8e5275822dff9d047b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 11:07:20 +0000

    Add package streamlit-video-annotator/0.1.2/streamlit_video_annotator-0.1.2.tar.gz

---
+++ packages/streamlit-video-annotator/streamlit_video_annotator-0.1.2.tar.gz/streamlit_video_annotator-0.1.2/setup.py
@@ -0,0 +1,9 @@
+"""
+Setup configuration for streamlit-video-annotator package
+Note: Configuration is primarily in pyproject.toml
+This file exists for backwards compatibility.
+"""
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
