fa4e1c72981ab55c5885e9864e6964abbf1f9079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:10:03 +0000

    Add package passagemath-combinat/10.6.47/passagemath_combinat-10.6.47-cp314-cp314-win_amd64.whl

---
+++ packages/passagemath-combinat/passagemath_combinat-10.6.47-cp314-cp314-win_amd64.whl/sage/combinat/species/all.py
@@ -0,0 +1,48 @@
+# sage_setup: distribution = sagemath-combinat
+r"""
+Combinatorial species
+
+Introductory material
+---------------------
+
+- :ref:`section-examples-catalan`
+- :ref:`section-generic-species`
+
+Basic Species
+-------------
+
+- :ref:`sage.combinat.species.species`
+- :ref:`sage.combinat.species.empty_species`
+- :ref:`sage.combinat.species.recursive_species`
+- :ref:`sage.combinat.species.characteristic_species`
+- :ref:`sage.combinat.species.cycle_species`
+- :ref:`sage.combinat.species.partition_species`
+- :ref:`sage.combinat.species.permutation_species`
+- :ref:`sage.combinat.species.linear_order_species`
+- :ref:`sage.combinat.species.set_species`
+- :ref:`sage.combinat.species.subset_species`
+- :ref:`sage.combinat.species.library`
+
+Operations on Species
+---------------------
+
+- :ref:`sage.combinat.species.sum_species`
+- :ref:`sage.combinat.species.product_species`
+- :ref:`sage.combinat.species.composition_species`
+- :ref:`sage.combinat.species.functorial_composition_species`
+
+Miscellaneous
+-------------
+
+- :ref:`sage.combinat.species.structure`
+- :ref:`sage.combinat.species.misc`
+"""
+# install the docstring of this module to the containing package
+from sage.misc.namespace_package import install_doc
+install_doc(__package__, __doc__)
+
+from sage.misc.lazy_import import lazy_import
+lazy_import("sage.combinat.species.recursive_species", "CombinatorialSpecies")
+lazy_import("sage.combinat.species", "library", as_='species')
+del lazy_import
+del install_doc
---
+++ packages/passagemath-combinat/passagemath_combinat-10.6.47-cp314-cp314-win_amd64.whl/sage/sat/solvers/__init__.py
@@ -0,0 +1,5 @@
+# sage_setup: distribution = sagemath-combinat
+from .satsolver import SatSolver
+from .cryptominisat import CryptoMiniSat
+
+from .dimacs import Glucose, GlucoseSyrup, RSat, Kissat
