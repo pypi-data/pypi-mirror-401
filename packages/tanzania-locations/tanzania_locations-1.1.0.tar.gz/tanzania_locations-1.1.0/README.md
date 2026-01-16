d610330086204b694c793010ccc6ef9eb16a58ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:08:39 +0000

    Add package tanzania-locations/1.1.0/tanzania_locations-1.1.0.tar.gz

---
+++ packages/tanzania-locations/tanzania_locations-1.1.0.tar.gz/tanzania_locations-1.1.0/README.md
@@ -0,0 +1,8 @@
+# tz-regions 🇹🇿
+
+A lightweight Python package providing Tanzania regions and districts.
+Perfect for Django forms and APIs.
+
+## Installation
+```bash
+pip install tz-regions
---
+++ packages/tanzania-locations/tanzania_locations-1.1.0.tar.gz/tanzania_locations-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tanzania_locations',
+    version='1.1.0',
+    author='Cornel Mtavangu',
+    packages = find_packages(),
+    install_requires=[],
+    description='A package for managing Tanzania locations data',
+)
