9b7b883863baca2df5973485afd94888fd4ffe08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:55 +0000

    Add package em-toolhub/0.1.0/em_toolhub-0.1.0.tar.gz

---
+++ packages/em-toolhub/em_toolhub-0.1.0.tar.gz/em_toolhub-0.1.0/em_toolhub/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from em_toolhub.api.files_api import FilesApi
+from em_toolhub.api.surveys_api import SurveysApi
+from em_toolhub.api.wells_api import WellsApi
+
