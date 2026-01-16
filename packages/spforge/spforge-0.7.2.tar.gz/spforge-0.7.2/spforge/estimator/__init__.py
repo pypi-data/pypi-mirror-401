5760fa49c4cf4a0c2f4e2b4fc9bcaaf07d74ae42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:08:55 +0000

    Add package spforge/0.7.2/spforge-0.7.2.tar.gz

---
+++ packages/spforge/spforge-0.7.2.tar.gz/spforge-0.7.2/spforge/estimator/__init__.py
@@ -0,0 +1,11 @@
+from ._distribution import (
+    NegativeBinomialEstimator as NegativeBinomialEstimator,
+    NormalDistributionPredictor as NormalDistributionPredictor,
+)
+from .sklearn_estimator import (
+    ConditionalEstimator as ConditionalEstimator,
+    FrequencyBucketingClassifier as FrequencyBucketingClassifier,
+    GranularityEstimator as GranularityEstimator,
+    OrdinalClassifier as OrdinalClassifier,
+    SkLearnEnhancerEstimator as SkLearnEnhancerEstimator,
+)
