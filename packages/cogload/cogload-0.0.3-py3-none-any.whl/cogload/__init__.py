43c40ec1cac3e159107930e4c8437c165a6f0383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:55 +0000

    Add package cogload/0.0.3/cogload-0.0.3-py3-none-any.whl

---
+++ packages/cogload/cogload-0.0.3-py3-none-any.whl/cogload/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+cogload
+~~~~~~~~~~~~~~~~~~~
+
+Pre-loading for discord.py cog files.
+
+Copyright 2026-present Tycho
+License: MIT, see LICENSE
+
+"""
+
+# pyright: reportUnusedImport=false
+
+from cogload.loader import load
