27ae3c76237c4cdae2bd49deba170d18c2472b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:43 +0000

    Add package pixie-sdk/0.1.1.dev17/pixie_sdk-0.1.1.dev17-py3-none-any.whl

---
+++ packages/pixie-sdk/pixie_sdk-0.1.1.dev17-py3-none-any.whl/pixie/__init__.py
@@ -0,0 +1,11 @@
+"""Pixie SDK for running AI applications and agents."""
+
+from pixie.registry import app
+from pixie.types import PixieGenerator, InputRequired
+
+
+__all__ = [
+    "app",
+    "PixieGenerator",
+    "InputRequired",
+]
