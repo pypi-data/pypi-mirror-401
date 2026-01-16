1e597ef2b077b2ab225069306bb8bb4f135509ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:33 +0000

    Add package hdx-python-api/6.6.1/hdx_python_api-6.6.1-py3-none-any.whl

---
+++ packages/hdx-python-api/hdx_python_api-6.6.1-py3-none-any.whl/hdx/facades/keyword_arguments.py
@@ -0,0 +1,39 @@
+"""Facade to simplify project setup that calls project main function with kwargs"""
+
+import logging
+from collections.abc import Callable
+from typing import Any
+
+from hdx.utilities.easy_logging import setup_logging
+from hdx.utilities.useragent import UserAgent
+
+from hdx.api import __version__
+from hdx.api.configuration import Configuration
+
+logger = logging.getLogger(__name__)
+setup_logging(log_file="errors.log")
+
+
+def facade(projectmainfn: Callable[[Any], None], **kwargs: Any):
+    """Facade to simplify project setup that calls project main function
+
+    Args:
+        projectmainfn: main function of project
+        **kwargs: configuration parameters to pass to HDX Configuration & other parameters to pass to main function
+
+    Returns:
+        None
+    """
+
+    #
+    # Setting up configuration
+    #
+    site_url = Configuration._create(**kwargs)
+
+    logger.info("--------------------------------------------------")
+    logger.info(f"> Using HDX Python API Library {__version__}")
+    logger.info(f"> HDX Site: {site_url}")
+
+    UserAgent.user_agent = Configuration.read().user_agent
+
+    projectmainfn(**kwargs)
