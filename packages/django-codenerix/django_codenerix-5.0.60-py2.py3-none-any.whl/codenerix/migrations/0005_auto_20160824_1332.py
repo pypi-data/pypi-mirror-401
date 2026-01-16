7e0aac76c105c7d79b46e84b306a5b653ae73ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 13:25:18 +0000

    Add package django-codenerix/5.0.60/django_codenerix-5.0.60-py2.py3-none-any.whl

---
+++ packages/django-codenerix/django_codenerix-5.0.60-py2.py3-none-any.whl/codenerix/migrations/0005_auto_20160824_1332.py
@@ -0,0 +1,15 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("codenerix", "0004_auto_20160824_0757"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="log",
+            name="action_flag",
+            field=models.PositiveSmallIntegerField(verbose_name="Acci\xf3n"),
+        ),
+    ]
