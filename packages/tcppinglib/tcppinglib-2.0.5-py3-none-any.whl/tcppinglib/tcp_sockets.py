7c1bb8a6f621732beadb88e40e1e7f3f46a05f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 04:18:50 +0000

    Add package tcppinglib/2.0.5/tcppinglib-2.0.5-py3-none-any.whl

---
+++ packages/tcppinglib/tcppinglib-2.0.5-py3-none-any.whl/tcppinglib/tcp_sockets.py
@@ -0,0 +1,254 @@
+"""
+tcppinglib
+~~~~~~~
+
+Monitor your endpoints with TCP Ping.
+
+    https://github.com/EnginEken/tcppinglib
+
+:copyright: Copyright 2021-2026 Engin EKEN.
+:license: GNU LGPLv3, see the LICENSE for details.
+
+~~~~~~~
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public
+License along with this program.  If not, see
+<https://www.gnu.org/licenses/>.
+"""
+
+import asyncio
+import socket
+import time
+
+from .exceptions import *
+
+
+class TCPSocket:
+    """
+    Base class for TCP sockets
+    """
+
+    def __init__(self, source: str = "") -> None:
+        self._sock = None
+        try:
+            self._sock = self._create_socket(
+                type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
+            )
+            if source:
+                self._sock.bind((source, 0))
+        except OSError as err:
+            raise err
+
+    def __enter__(self):
+        """
+        Return this object.
+        """
+        return self
+
+    def __exit__(self, type, value, traceback):
+        """
+        Call the `close` method.
+        """
+        self.close()
+
+    def __del__(self):
+        """
+        Call the `close` method.
+        """
+        self.close()
+
+    def _create_socket(self):
+        """
+        Create new socket. Must be overridden by proper socket class.
+        """
+        raise NotImplementedError
+
+    def _set_timeout(self, timeout):
+        """
+        Set the timeout for this socket.
+        """
+        self._sock.settimeout(timeout)
+
+    def connect(self, request):
+        """
+        With this function the port scanner tries to complete a full
+        TCP three-way handshake ([SYN], [SYN ACK], [ACK]) with the
+        targeted port on the system.
+
+        Be informed that this function is not calculate the packet round-trip time.
+        It is calculating the 3-way handshake time which is enough for TCP session
+        with the destination system.
+
+        This operation is non-blocking.
+
+        :type request: TCPRequest
+        :param request: The TCP request object that have been created.
+        """
+
+        if not self._sock:
+            raise NotImplementedError
+
+        try:
+            sock_port = int(request.port)
+
+            self._set_timeout(request.timeout)
+            start_time = time.perf_counter()
+
+            self._sock.connect((request.destination, sock_port))
+            request._time = time.perf_counter() - start_time
+
+        except socket.timeout:
+            raise PortNotOpenError(sock_port, request.destination)
+
+        except OSError as err:
+            if err.errno == 8:
+                raise NameLookupError(request.destination)
+            raise TCPSocketError(str(err))
+
+    def close(self):
+        """
+        Close the socket. It cannot be used after this call.
+        """
+        if self._sock:
+            self._sock.close()
+            self._sock = None
+
+    @property
+    def sock(self):
+        """
+        Return the underlying socket (`socket.socket` object) or `None`
+        if the socket is closed.
+        """
+        return self._sock
+
+    @property
+    def blocking(self):
+        """
+        Indicate whether the socket is in blocking mode.
+        Return a `boolean`.
+        """
+        return self._sock.getblocking()
+
+    @blocking.setter
+    def blocking(self, enable):
+        return self._sock.setblocking(enable)
+
+    @property
+    def is_closed(self):
+        """
+        Indicate whether the socket is closed.
+        Return a `boolean`.
+        """
+        return self._sock is None
+
+
+class TCPv4Socket(TCPSocket):
+    """
+    Class for creating TCPv4 socket.
+    """
+
+    def _create_socket(self, type, proto):
+        """
+        Create and return new socket.
+        """
+        return socket.socket(family=socket.AF_INET, type=type, proto=proto)
+
+
+class TCPv6Socket(TCPSocket):
+    """
+    Class for creating TCPv6 socket.
+    """
+
+    def _create_socket(self, type, proto):
+        """
+        Create and return new socket.
+        """
+        return socket.socket(family=socket.AF_INET6, type=type, proto=proto)
+
+
+class AsyncTCPSocket:
+    """
+    Class for creating sockets asynchronously.
+
+    :type tcp_sock: TCPSocket
+    :param tcp_socket: TCP Socket. Once the wrapper is instantiated,
+    this socket should no longer be used directly.
+    """
+
+    def __init__(self, tcp_sock) -> None:
+        self._tcp_sock = tcp_sock
+        self._tcp_sock.blocking = False
+
+    def __enter__(self):
+        """
+        Return this object.
+        """
+        return self
+
+    def __exit__(self, type, value, traceback):
+        """
+        Call the `close` method.
+        """
+        self.close()
+
+    def __del__(self):
+        """
+        Call the `close` method.
+        """
+        self.close()
+
+    async def connect(self, request):
+        """
+        With this function the port scanner tries to complete a full
+        TCP three-way handshake ([SYN], [SYN ACK], [ACK]) with the
+        targeted port on the system asyncronously.
+
+        This operation is non-blocking.
+
+        Be informed that this function is not calculate the packet round-trip time.
+        It is calculating the 3-way handshake time which is enough for TCP session
+        with the destination system.
+
+        :type request: TCPRequest
+        :param request: The TCP request object that have been created.
+        """
+
+        if not self._tcp_sock:
+            raise NotImplementedError
+
+        try:
+            loop = asyncio.get_running_loop()
+            start_time = time.perf_counter()
+
+            await asyncio.wait_for(
+                loop.sock_connect(
+                    self._tcp_sock.sock, (request.destination, request.port)
+                ),
+                timeout=request.timeout,
+            )
+            request._time = time.perf_counter() - start_time
+
+        except asyncio.TimeoutError:
+            raise PortNotOpenError(request.port, request.destination)
+
+        except socket.gaierror:
+            raise NameLookupError(request.destination)
+
+        except OSError as err:
+            raise TCPSocketError(str(err))
+
+    def close(self):
+        """
+        Close the socket. It cannot be used after this call.
+        """
+        if self._tcp_sock:
+            self._tcp_sock.close()
+            self._tcp_sock = None
---
+++ packages/tcppinglib/tcppinglib-2.0.5-py3-none-any.whl/tcppinglib/utils.py
@@ -0,0 +1,126 @@
+"""
+tcppinglib
+~~~~~~~
+
+Monitor your endpoints with TCP Ping.
+
+    https://github.com/EnginEken/tcppinglib
+
+:copyright: Copyright 2021-2026 Engin EKEN.
+:license: GNU LGPLv3, see the LICENSE for details.
+
+~~~~~~~
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public
+License along with this program.  If not, see
+<https://www.gnu.org/licenses/>.
+"""
+
+import asyncio
+import socket
+from re import match, sub
+
+from .exceptions import *
+
+
+def hostname_lookup(hostname, port, family):
+    """
+    Resolve a hostname or FQDN to an IP address. This function is resolving hostnames based on
+    your configured DNS in your system. Several IP addresses may return. In that case, first resolved
+    IP address will be used.
+
+    :type hostname: str
+    :param hostname: Hostname or FQDN
+
+    :type port: int
+    :param port: Port number of the destination system
+
+    :type family: socket family
+    :param family: socket.AF_INET for v4, socket.AF_INET6 for v6
+    """
+    try:
+        host_ip = socket.getaddrinfo(
+            host=hostname, port=port, family=family, proto=socket.IPPROTO_TCP
+        )
+        return [resolved_ip[4][0] for resolved_ip in host_ip]
+
+    except socket.gaierror:
+        raise NameLookupError(hostname)
+
+
+async def async_hostname_lookup(hostname, port, family):
+    """
+    Resolve a hostname or FQDN to an IP address asyncronously. This function is resolving hostnames based on
+    your configured DNS in your system. Several IP addresses may return. In that case, first resolved
+    IP address will be used.
+
+    This function is non-blocking.
+
+    :type hostname: str
+    :param hostname: Hostname or FQDN
+
+    :type port: int
+    :param port: Port number of the destination system
+
+    :type family: socket family
+    :param family: socket.AF_INET for v4, socket.AF_INET6 for v6
+    """
+    loop = asyncio.get_running_loop()
+
+    try:
+        host_ip = await loop.getaddrinfo(
+            host=hostname, port=port, family=family, proto=socket.IPPROTO_TCP
+        )
+        return [resolved_ip[4][0] for resolved_ip in host_ip]
+
+    except socket.gaierror:
+        raise NameLookupError(hostname)
+
+
+def is_hostname(name):
+    """
+    Indicate whether the specified name is a hostname or an FQDN.
+    Return a `boolean`.
+    """
+    pattern = r"(?i)^([a-z0-9-]+|([a-z0-9_-]+[.])+[a-z]+)$"
+    return match(pattern, name) is not None
+
+
+def is_ipv6(ip):
+    """
+    Indicate whether the ip address is IPv6 address.
+    Return a `boolean`.
+    """
+    pattern = r"^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$"
+    return match(pattern, ip) is not None
+
+
+def strip_http_https(name):
+    """
+    Stripping `http://` and `https://` from hostname if there is.
+    Return a `string`
+    """
+    return sub(r"^(http|https)?:\/\/", "", name)
+
+
+def extract_cli_address_and_port(address):
+    """
+    Extract the IP address and port from the address argument if it contains a port.
+    This function also strips any URL scheme (http:// or https://) before processing.
+
+    :param address: The IP address or FQDN, optionally with a port (e.g., 192.168.1.1:8080 or http://cnn.com:80)
+    :return: A tuple (ip_address, port)
+    """
+    address = strip_http_https(address)
+
+    ip_address, port_str = address.rsplit(":", 1)
+    port = int(port_str)
+    return ip_address, port
