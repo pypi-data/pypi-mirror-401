74758e5a896b7f00c8cbac93588e833d0b6f91ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 11:07:27 +0000

    Add package iris-sdk/0.1.17/iris_sdk-0.1.17.tar.gz

---
+++ packages/iris-sdk/iris_sdk-0.1.17.tar.gz/iris_sdk-0.1.17/src/iris_sdk/context.py
@@ -0,0 +1,7 @@
+from contextvars import ContextVar
+from typing import List, Dict, Set
+from collections import defaultdict
+
+current_run: ContextVar[str] = ContextVar("current_run", default=None)
+run_stack: ContextVar[List[str]] = ContextVar("run_stack", default=[])
+parent_children_mappings: ContextVar[Dict[str, Set[str]]] = ContextVar("parent_children_mappings", default=defaultdict(set))
