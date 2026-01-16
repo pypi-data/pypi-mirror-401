4b1929a9cb9a159db9cba466e6aa4a351efc947f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 08:07:23 +0000

    Add package iris-sdk/0.1.16/iris_sdk-0.1.16-py3-none-any.whl

---
+++ packages/iris-sdk/iris_sdk-0.1.16-py3-none-any.whl/iris_sdk/enums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+from . import constants
+
+class TaskTypes(Enum):
+    GENERIC = constants.GENERIC_NAME
+    LLM = constants.LLM_NAME
+    TOOL = constants.TOOL_NAME
+    AGENT = constants.AGENT_NAME
