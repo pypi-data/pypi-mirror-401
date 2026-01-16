f7306d86909a11fef6883d569b6d1538926ad2b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 03:35:08 +0000

    Add package mango/1.0.2/mango-1.0.2-py3-none-any.whl

---
+++ packages/mango/mango-1.0.2-py3-none-any.whl/mango/__init__.py
@@ -0,0 +1,6 @@
+"""
+Main file
+"""
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+__version__ = "1.0.1"
---
+++ packages/mango/mango-1.0.2-py3-none-any.whl/mango/shared/exceptions.py
@@ -0,0 +1,18 @@
+class InvalidCredentials(Exception):
+    pass
+
+
+class ValidationError(Exception):
+    pass
+
+
+class JobError(Exception):
+    pass
+
+
+class BadResponse(Exception):
+    pass
+
+
+class ApiKeyError(Exception):
+    pass
