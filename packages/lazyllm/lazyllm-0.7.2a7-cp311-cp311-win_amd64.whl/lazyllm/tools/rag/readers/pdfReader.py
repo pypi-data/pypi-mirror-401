f4ab7879e7f42e2d830db270b98e578f31ee2501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 09:09:32 +0000

    Add package lazyllm/0.7.2a7/lazyllm-0.7.2a7-cp311-cp311-win_amd64.whl

---
+++ packages/lazyllm/lazyllm-0.7.2a7-cp311-cp311-win_amd64.whl/lazyllm/tools/rag/readers/pdfReader.py
@@ -0,0 +1,42 @@
+import io
+from pathlib import Path
+from typing import List, Optional
+from lazyllm.common import retry
+from lazyllm.thirdparty import fsspec, pypdf
+
+from .readerBase import LazyLLMReaderBase, get_default_fs, is_default_fs
+from ..doc_node import DocNode
+
+RETRY_TIMES = 3
+
+class PDFReader(LazyLLMReaderBase):
+    """Reader for extracting text content from PDF files.
+
+Args:
+    return_full_document (bool): Whether to merge the entire PDF into a single document node. If False, each page becomes a separate node.
+    return_trace (bool): Whether to return the processing trace. Default is True.
+"""
+    def __init__(self, return_full_document: bool = False, return_trace: bool = True) -> None:
+        super().__init__(return_trace=return_trace)
+        self._return_full_document = return_full_document
+
+    @retry(stop_after_attempt=3)
+    def _load_data(self, file: Path, fs: Optional['fsspec.AbstractFileSystem'] = None) -> List[DocNode]:
+        if not isinstance(file, Path): file = Path(file)
+
+        fs = fs or get_default_fs()
+        with fs.open(file, 'rb') as fp:
+            stream = fp if is_default_fs(fs) else io.BytesIO(fp.read())
+            pdf = pypdf.PdfReader(stream)
+            num_pages = len(pdf.pages)
+            docs = []
+            if self._return_full_document:
+                text = '\n'.join(pdf.pages[page].extract_text() for page in range(num_pages))
+                docs.append(DocNode(text=text))
+            else:
+                for page in range(num_pages):
+                    page_text = pdf.pages[page].extract_text()
+                    page_label = pdf.page_labels[page]
+                    metadata = {'page_label': page_label}
+                    docs.append(DocNode(text=page_text, metadata=metadata))
+            return docs
