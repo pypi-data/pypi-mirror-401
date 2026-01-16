5fc594d80ffe055914584b2d61f8cff4a74deb27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 13:25:43 +0000

    Add package calfi/1.0.0/calfi-1.0.0-py3-none-any.whl

---
+++ packages/calfi/calfi-1.0.0-py3-none-any.whl/calfi/__init__.py
@@ -0,0 +1,11 @@
+from calfi.fin_cal import BusinessCalendar, Target, USGovt
+from calfi.conventions import DayCountConvention, BusinessDayConvention, Frequency
+
+__all__ = [
+    "BusinessCalendar",
+    "Target",
+    "USGovt",
+    "DayCountConvention",
+    "BusinessDayConvention",
+    "Frequency",
+]
