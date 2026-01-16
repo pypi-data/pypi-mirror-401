57706e0ca6267d2a13d6d1eeafd8d27b219925e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-16 12:08:40 +0000

    Add package django-codenerix/5.0.59/django_codenerix-5.0.59.tar.gz

---
+++ packages/django-codenerix/django_codenerix-5.0.59.tar.gz/django_codenerix-5.0.59/codenerix/mixins.py
@@ -0,0 +1,79 @@
+#
+# django-codenerix
+#
+# Codenerix GNU
+#
+# Project URL : http://www.codenerix.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+
+from django.core.serializers.json import DjangoJSONEncoder
+from django.http import HttpResponse
+
+
+class JSONResponseMixin:
+    """
+    A mixin that can be used to render a JSON response.
+    """
+
+    def render_to_json_response(self, context, **response_kwargs):
+        """
+        Returns a JSON response, transforming 'context' to make the payload.
+        """
+        return HttpResponse(
+            self.convert_context_to_json(context),
+            content_type="application/json",
+            **response_kwargs,
+        )
+
+    def convert_context_to_json(self, context):
+        "Convert the context dictionary into a JSON object"
+        # Note: This is *EXTREMELY* naive; in reality, you'll need
+        # to do much more complex handling to ensure that arbitrary
+        # objects -- such as Django model instances or querysets
+        # -- can be serialized as JSON.
+        return json.dumps(context, cls=DjangoJSONEncoder)
+
+
+class AjaxableResponseMixin:
+    """
+    Mixin to add AJAX support to a form.
+    Must be used with an object-based FormView (e.g. CreateView)
+    """
+
+    def render_to_json_response(self, context, **response_kwargs):
+        data = json.dumps(context, cls=DjangoJSONEncoder)
+        response_kwargs["content_type"] = "application/json"
+        return HttpResponse(data, **response_kwargs)
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        if self.request.is_ajax():
+            return self.render_to_json_response(form.errors, status=400)
+        else:
+            return response
+
+    def form_valid(self, form):
+        # We make sure to call the parent's form_valid() method because
+        # it might do some processing (in the case of CreateView, it will
+        # call form.save() for example).
+        response = super().form_valid(form)
+        if self.request.is_ajax():
+            data = {
+                "pk": self.object.pk,
+            }
+            return self.render_to_json_response(data)
+        else:
+            return response
