b6a6818747f806169dfb0d58b8834b581a377114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:50:42 +0000

    Add package gamspy-mpsge/52.4.0/gamspy_mpsge-52.4.0-py3-none-macosx_13_0_arm64.whl

---
+++ packages/gamspy-mpsge/gamspy_mpsge-52.4.0-py3-none-macosx_13_0_arm64.whl/gamspy_mpsge/__init__.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+from .version import __version__
+directory = str(Path(__file__).resolve().parent)
+
+files = ['mpsgeset', 'gmsge_us.run', 'gmsge_ux.out', 'libmpsgeval.dylib', 'gmsgewus.run', 'gmsgewux.out']
+
+file_paths = [directory + os.sep + file for file in files]
+verbatim = 'MPSGE 100001 5 GE 1 0 1\ngmsgewus.run\ngmsgewux.out'
