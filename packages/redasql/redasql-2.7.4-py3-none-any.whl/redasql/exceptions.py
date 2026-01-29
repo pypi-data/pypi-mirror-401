02b4290a21025452932f01e11c3ef1fc5733a2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:14:50 +0000

    Add package redasql/2.7.4/redasql-2.7.4-py3-none-any.whl

---
+++ packages/redasql/redasql-2.7.4-py3-none-any.whl/redasql/exceptions.py
@@ -0,0 +1,34 @@
+class RedasqlException(Exception):
+    """ base exception """
+
+
+class QueryRuntimeError(RedasqlException):
+    """ redash said, sql error """
+
+
+class QueryTimeoutError(RedasqlException):
+    """ too long """
+
+
+class DataSourceNotFoundError(RedasqlException):
+    """ not found datasource name """
+
+
+class InvalidMetaCommand(RedasqlException):
+    """ invalid meta command"""
+
+
+class InvalidSettingError(RedasqlException):
+    """ invalid setting requested. """
+
+
+class InsufficientParametersError(RedasqlException):
+    """ api key and endpoint must be set """
+
+
+class NoDataSourceError(RedasqlException):
+    """ not set datasource """
+
+
+class SqlFileNotFoundError(RedasqlException):
+    """ specified file is not exists. """
