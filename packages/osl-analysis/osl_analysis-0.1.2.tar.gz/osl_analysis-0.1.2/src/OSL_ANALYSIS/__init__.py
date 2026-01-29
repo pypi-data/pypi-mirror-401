7dec84fe0f6b7d600e098fbaaa65e6fc35d317de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:14:41 +0000

    Add package osl-analysis/0.1.2/osl_analysis-0.1.2.tar.gz

---
+++ packages/osl-analysis/osl_analysis-0.1.2.tar.gz/osl_analysis-0.1.2/src/OSL_ANALYSIS/__init__.py
@@ -0,0 +1,16 @@
+# OSL_ANALYSIS package initialization
+
+__version__ = "0.1.2"
+
+E1=float(input("enter E1: "))
+E2=float(input("enter E2: "))
+E3=float(input("enter E3: "))
+
+# You can import the main module here if desired
+from . import modulo1
+from . import modulo2
+from . import modulo3
+from . import modulo4
+
+
+
