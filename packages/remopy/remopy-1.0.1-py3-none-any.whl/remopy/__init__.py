e9fdc5849bbf77b0686d1f9db84b8e79b62e554f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:16:47 +0000

    Add package remopy/1.0.1/remopy-1.0.1-py3-none-any.whl

---
+++ packages/remopy/remopy-1.0.1-py3-none-any.whl/remopy/__init__.py
@@ -0,0 +1,32 @@
+'''
+REMO v1 regulatory element modules for GRCh38.
+
+A data package providing module coordinates, metadata,
+and cell ontology annotations for the human GRCh38 genome.
+'''
+
+from typing import TYPE_CHECKING, Optional
+
+from remopy._data import modules, metadata, terms, ontology, tissues
+
+if TYPE_CHECKING:
+    from anndata import AnnData
+    import polars as pl
+
+__version__ = '1.0.1'
+__all__ = ['modules', 'metadata', 'terms', 'ontology', 'tissues', 'quantify']
+
+
+def quantify(
+    fragments: str,
+    min_fragments: int = 1000,
+    modules: Optional['pl.DataFrame'] = None,
+) -> 'AnnData':
+    '''
+    Quantify fragments into modules.
+    
+    See remopy._quantify.quantify for full documentation.
+    Requires: pip install remopy[quantify]
+    '''
+    from remopy._quantify import quantify as _quantify
+    return _quantify(fragments, min_fragments, modules)
