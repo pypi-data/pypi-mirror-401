b51dd9be565cb6cc700c3e4a99148b7b05c3ea21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:21:38 +0000

    Add package invenio-requests/11.2.3/invenio_requests-11.2.3-py2.py3-none-any.whl

---
+++ packages/invenio-requests/invenio_requests-11.2.3-py2.py3-none-any.whl/invenio_requests/services/requests/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+# Copyright (C) 2021 TU Wien.
+#
+# Invenio-Requests is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Services module."""
+
+from .components import RequestNumberComponent
+from .config import RequestsServiceConfig
+from .links import RequestLink
+from .results import RequestItem, RequestList
+from .service import RequestsService
+
+__all__ = (
+    "RequestNumberComponent",
+    "RequestLink",
+    "RequestItem",
+    "RequestList",
+    "RequestsService",
+    "RequestsServiceConfig",
+)
