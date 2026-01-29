08134c7cb2d5216b75eb255f7a67e816c4ceb3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:16:38 +0000

    Add package refi-calculator/0.8.0/refi_calculator-0.8.0-py3-none-any.whl

---
+++ packages/refi-calculator/refi_calculator-0.8.0-py3-none-any.whl/refi_calculator/__init__.py
@@ -0,0 +1,9 @@
+"""Refinance calculator package exports."""
+
+from __future__ import annotations
+
+__all__ = []
+
+__description__ = """
+Root package for the refinance calculator application.
+"""
