7e25d972acefe3d1e5e63b1e46a6732fa48e325a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:35:25 +0000

    Add package vibe-widget/0.2.4/vibe_widget-0.2.4-py3-none-any.whl

---
+++ packages/vibe-widget/vibe_widget-0.2.4-py3-none-any.whl/vibe_widget/__init__.py
@@ -0,0 +1,26 @@
+from vibe_widget.core import VibeWidget, WidgetHandle, create, edit, load, clear
+from vibe_widget.api import outputs, inputs, output, actions, action, ExportHandle
+from vibe_widget.config import config, Config, models
+from vibe_widget.themes import Theme, theme, themes
+
+__version__ = "0.1.0"
+__all__ = [
+    "VibeWidget",
+    "WidgetHandle",
+    "create",
+    "edit",
+    "load",
+    "clear",
+    "config",
+    "Config",
+    "models",
+    "Theme",
+    "theme",
+    "themes",
+    "output",
+    "outputs",
+    "inputs",
+    "action",
+    "actions",
+    "ExportHandle",
+]
