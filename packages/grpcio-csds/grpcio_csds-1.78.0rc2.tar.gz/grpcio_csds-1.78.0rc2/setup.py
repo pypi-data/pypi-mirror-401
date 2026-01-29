cdec7ecf76409403d9cc76889a5ff0cd0820b811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:21:53 +0000

    Add package grpcio-csds/1.78.0rc2/grpcio_csds-1.78.0rc2.tar.gz

---
+++ packages/grpcio-csds/grpcio_csds-1.78.0rc2.tar.gz/grpcio_csds-1.78.0rc2/setup.py
@@ -0,0 +1,42 @@
+# Copyright 2021 The gRPC Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Setup module for CSDS in gRPC Python."""
+
+import os
+import sys
+
+import setuptools
+
+# Manually insert the source directory into the Python path for local module
+# imports to succeed
+sys.path.insert(0, os.path.abspath("."))
+
+import grpc_version
+import python_version
+
+INSTALL_REQUIRES = (
+    "protobuf>=6.31.1,<7.0.0",
+    f"xds-protos=={grpc_version.VERSION}",
+    f"grpcio>={grpc_version.VERSION}",
+)
+SETUP_REQUIRES = INSTALL_REQUIRES
+
+PYTHON_REQUIRES = f">={python_version.MIN_PYTHON_VERSION}"
+
+if __name__ == "__main__":
+    setuptools.setup(
+        python_requires=f">={python_version.MIN_PYTHON_VERSION}",
+        install_requires=INSTALL_REQUIRES,
+        setup_requires=SETUP_REQUIRES,
+    )
