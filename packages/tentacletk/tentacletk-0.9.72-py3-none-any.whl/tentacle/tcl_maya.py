acdb7b24a9fef4c794209c4f9c7899d181d1e20a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 05:28:03 +0000

    Add package tentacletk/0.9.72/tentacletk-0.9.72-py3-none-any.whl

---
+++ packages/tentacletk/tentacletk-0.9.72-py3-none-any.whl/tentacle/tcl_maya.py
@@ -0,0 +1,31 @@
+# !/usr/bin/python
+# coding=utf-8
+import mayatk as mtk
+from tentacle.tcl import Tcl
+
+
+class TclMaya(Tcl):
+    """Tcl class overridden for use with Autodesk Maya."""
+
+    def __init__(self, parent=None, slot_source="slots/maya", *args, **kwargs):
+        if not parent:
+            try:
+                parent = mtk.get_main_window()
+            except Exception as error:
+                print(f"Error getting main window: {error}")
+
+        super().__init__(parent, slot_source=slot_source, *args, **kwargs)
+
+
+# --------------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    main = TclMaya()
+    main.show()
+
+
+# module name
+# print(__name__)
+# --------------------------------------------------------------------------------------------
+# Notes
+# --------------------------------------------------------------------------------------------
