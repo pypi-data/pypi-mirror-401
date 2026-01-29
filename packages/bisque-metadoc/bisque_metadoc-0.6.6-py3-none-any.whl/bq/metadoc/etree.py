c61293acd8335c24250c4db05a9eca2d239abdb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:17:23 +0000

    Add package bisque-metadoc/0.6.6/bisque_metadoc-0.6.6-py3-none-any.whl

---
+++ packages/bisque-metadoc/bisque_metadoc-0.6.6-py3-none-any.whl/bq/metadoc/etree.py
@@ -0,0 +1,89 @@
+###################################################################################
+# Indirection layer to convert all etree occurances quickly to Metadoc.
+# Simply replace
+#     from lxml import etree
+# with
+#     from bq.metadoc import etree
+###################################################################################
+
+
+from bq.metadoc.formats import (
+    _fromstring_fix_empty,
+    _tostring_fix_empty,
+    metadoc_everywhere,
+)
+
+if not metadoc_everywhere:
+    from lxml.builder import E
+    from lxml.etree import (
+        XML,
+        Element,
+        ElementTree,
+        SubElement,
+        _Element,
+        _ElementTree,
+    )
+    from lxml.etree import parse as _xml_parse
+
+    def fromstring(s):
+        return _fromstring_fix_empty(s)
+
+    def tostring(xml, **kw):
+        # escape any empty strings (XML is not round-tripable for empty strings in text nodes!)
+        return _tostring_fix_empty(xml, **kw)
+
+    def parse(f):
+        res = _xml_parse(f).getroot()
+        # fix any escaped empty strings
+        for kid in res.iter(tag=Element):
+            if kid.attrib.get("value") == "":
+                del kid.attrib["value"]
+                kid.text = ""
+        return ElementTree(res)
+
+else:
+    from functools import partial
+
+    from lxml.etree import parse as _xml_parse
+
+    from bq.metadoc.formats import Metadoc
+
+    _Element = Metadoc
+
+    class ElementTree:
+        def __init__(self, root):
+            self.root = root
+
+        def getroot(self):
+            return self.root
+
+    _ElementTree = ElementTree
+
+    def Element(tag, **kw):
+        return Metadoc(tag=tag, **kw)
+
+    def SubElement(node, tag, **kw):
+        return node.add_tag(tag, **kw)
+
+    def fromstring(s):
+        return Metadoc.from_etree_like_string(s)
+
+    def parse(f):
+        return ElementTree(Metadoc.from_etree_like_string(f))
+
+    XML = fromstring
+
+    def tostring(xml, **kw):
+        return xml.to_etree_like_string()
+
+    class ElementMaker:
+        def __call__(self, tag, *children, **attr):
+            res = Metadoc(tag=tag, **attr)
+            for kid in children:
+                res.add_child(kid)
+            return res
+
+        def __getattr__(self, tag):
+            return partial(self, tag)
+
+    E = ElementMaker()
