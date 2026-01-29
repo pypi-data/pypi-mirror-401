90ef9d840472891276a07387f71ffd4238324a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 05:25:25 +0000

    Add package wyrmx-cli/1.0.111/wyrmx_cli-1.0.111.tar.gz

---
+++ packages/wyrmx-cli/wyrmx_cli-1.0.111.tar.gz/wyrmx_cli-1.0.111/wyrmx_cli/commands/__init__.py
@@ -0,0 +1,6 @@
+from wyrmx_cli.commands.serve import serve
+from wyrmx_cli.commands.new import new
+from wyrmx_cli.commands.test import test
+from wyrmx_cli.commands.run import run
+from wyrmx_cli.commands.version import version
+from wyrmx_cli.commands.vibe import vibe
