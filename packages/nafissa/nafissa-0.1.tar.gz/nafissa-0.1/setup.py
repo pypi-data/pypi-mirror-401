bf559e8a9f5b626e7c4d5b484008191d487d659a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:16:53 +0000

    Add package nafissa/0.1/nafissa-0.1.tar.gz

---
+++ packages/nafissa/nafissa-0.1.tar.gz/nafissa-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nafissa",            # nom du package
+    version="0.1",              # version initiale
+    packages=find_packages(),   # trouve automatiquement le package rocket
+    description="Mon package rocket",
+    author="sirine",
+    license="MIT",
+)
