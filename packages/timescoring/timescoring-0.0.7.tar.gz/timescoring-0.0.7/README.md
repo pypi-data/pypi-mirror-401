b63e033c8b03a3dd303c1ae49a08ec2c06a9a743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:17:18 +0000

    Add package timescoring/0.0.7/timescoring-0.0.7.tar.gz

---
+++ packages/timescoring/timescoring-0.0.7.tar.gz/timescoring-0.0.7/README.md
@@ -0,0 +1,143 @@
+# Library for measuring performance of time series classification
+
+![PyPI](https://img.shields.io/pypi/v/timescoring?style=flat-square)
+![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/esl-epfl/epilepsy_performance_metrics/python-app.yml?label=unittest&style=flat-square)
+
+## Motivation
+
+For temporal and sequential data (e.g. in biomedical applications), standard performance evaluation metrics, such as sensitivity and specificity, may not always be the most appropriate and can even be misleading. Evaluation metrics must ultimately reflect the needs of users and also be sufficiently sensitive to guide algorithm development.
+
+For example, for epilepsy monitoring, neurologists ask for assesments on the level of seizure episodes (events), rather than duration or sample-by-sample based metrics. Similarly,  another performance measure with a strong practical impact in epilepsy monitoring, is the false alarm rate (FAR), or the number of false positives per hour/day. Clinicians and patients see this measure as more meaningful than some established metrics in the ML community, and are very demanding in terms of performance, requiring it to be as low as possible for potential wearable applications (e.g., less than 1 FP/day). This also necessitates exceptionally high constraints on the required precision (usually much higher than 99\%).
+
+For this reason, here we provide code that measures performance on the level of events and on a sample-by-sample basis.
+
+## Metrics
+
+In more details, we measures performance on the level of:
+
+- Sample : Performance metric that threats every label sample independently.
+- Events (e.g. epileptic seizure) : Classifies each event in both reference and hypothesis based on overlap of both.
+
+Both methods are illustrated in the following figures :
+
+![Illustration of sample based scoring.](https://user-images.githubusercontent.com/747240/248309097-b7f76fde-c87a-41df-812d-9821375b640e.png)
+![Illustration of event based scoring.](https://user-images.githubusercontent.com/747240/248308898-64b4ae39-d02f-4f06-9b10-f07aaf6110d1.png)
+
+## Installation
+
+The timescoring package is released for macOS, Windows and Linux on [PyPi](https://pypi.org/project/timescoring/). It can be installed using `pip` :
+
+```bash
+python -m pip install -U pip
+python -m pip install -U timescoring
+```
+
+To include visualization capabilities, install with the plotting extras:
+```bash
+python -m pip install -U "timescoring[plotting]"
+```
+
+The package is also available on [conda-forge](https://anaconda.org/conda-forge/timescoring). It can be installed using `conda` :
+
+```bash
+conda install -c conda-forge timescoring
+```
+
+It can also be installed from source with a modern build of `pip` :
+
+```bash
+python -m pip install -U pip
+git clone https://github.com/esl-epfl/epilepsy_performance_metrics.git
+cd epilepsy_performance_metrics
+python -m pip install -e .
+```
+
+## Code
+
+The `timescoring` package provides three classes :
+
+- `annotation.Annotation` : store annotations
+- `scoring.SampleScoring(ref, hyp)` : Compute sample based scoring
+- `scoring.EventScoring(ref, hyp)` : Compute event based scoring
+
+In addition it also provides functions to visualize the output of the scoring algorithm (see `visualization.py`). Note that visualization functions require the optional matplotlib dependency. Install with `pip install timescoring[plotting]` to enable these features.
+
+### Parameters
+
+Sample based scoring allows to set the sampling frequency of the labels. It defaults to 1 Hz.
+
+Event based scoring allows to define certain parameters which are provided as an instance of `scoring.EventScoring.Parameters` :
+
+- `toleranceStart` (float): Allow some tolerance on the start of an event without counting a false detection. Defaults to 30  # [seconds].
+- `toleranceEnd` (float): Allow some tolerance on the end of an event without counting a false detection. Defaults to 60  # [seconds].
+- `minOverlap` (float): Minimum relative overlap between ref and hyp for a detection. Defaults to 0 which corresponds to any overlap  # [relative].
+- `maxEventDuration` (float): Automatically split events longer than a given duration. Defaults to 5*60  # [seconds].
+- `minDurationBetweenEvents` (float): Automatically merge events that are separated by less than the given duration. Defaults to 90 # [seconds].
+
+### Scores
+
+Scores are provided as attributes of the scoring class. The following metrics can be accesses :
+
+- `sensitivity`
+- `precision`
+- `f1` : F1-score
+- `fpRate` : False alarm rate per 24h
+
+## Example of usage
+
+```python
+# Loading Annotations #
+
+
+from timescoring.annotations import Annotation
+
+# Annotation objects can be instantiated from a binary mask
+
+fs = 1
+mask = [0, 1, 1, 0, 0, 0, 1, 1, 1, 0]
+
+labels = Annotation(mask, fs)
+
+print('Annotation objects contain a representation as a mask and as a list of events:')
+print(labels.mask)
+print(labels.events)
+
+
+# Annotation object can also be instantiated from a list of events
+fs = 1
+numSamples = 10  # In this case the duration of the recording in samples should be provided
+events = [(1, 3), (6, 9)]
+
+labels = Annotation(events, fs, numSamples)
+
+
+# Computing performance score #
+
+from timescoring import scoring
+from timescoring import visualization
+
+fs = 1
+duration = 66 * 60
+ref = Annotation([(8 * 60, 12 * 60), (30 * 60, 35 * 60), (48 * 60, 50 * 60)], fs, duration)
+hyp = Annotation([(8 * 60, 12 * 60), (28 * 60, 32 * 60), (50.5 * 60, 51 * 60), (60 * 60, 62 * 60)], fs, duration)
+scores = scoring.SampleScoring(ref, hyp)
+figSamples = visualization.plotSampleScoring(ref, hyp)
+
+# Scores can also be computed per event
+param = scoring.EventScoring.Parameters(
+    toleranceStart=30,
+    toleranceEnd=60,
+    minOverlap=0,
+    maxEventDuration=5 * 60,
+    minDurationBetweenEvents=90)
+scores = scoring.EventScoring(ref, hyp, param)
+figEvents = visualization.plotEventScoring(ref, hyp, param)
+
+print("# Event scoring\n" +
+      "- Sensitivity : {:.2f} \n".format(scores.sensitivity) +
+      "- Precision   : {:.2f} \n".format(scores.precision) +
+      "- F1-score    : {:.2f} \n".format(scores.f1) +
+      "- FP/24h      : {:.2f} \n".format(scores.fpRate))
+```
+
+A presentation explaining these metrics is available [here](https://drive.google.com/file/d/1-k6i2jVpU7bzqnV6zQPUKlfPkO7qaXau/view?usp=sharing).
---
+++ packages/timescoring/timescoring-0.0.7.tar.gz/timescoring-0.0.7/tests/test.py
@@ -0,0 +1,288 @@
+''' Unit testing
+'''
+
+__author__ = "Jonathan Dan"
+__email__ = "jonathan.dan at epfl.ch"
+
+import unittest
+
+import numpy as np
+
+from src.timescoring.annotations import Annotation
+from src.timescoring import scoring
+
+
+class TestAnnotation(unittest.TestCase):
+    def assertListOfTupleEqual(expected, actual, message):
+        difference = set(expected) ^ set(actual)
+        assert not difference, 'List of events check failed : {}'.format(message)
+
+    def assertMask(expected, actual, message):
+        difference = np.sum(expected ^ actual)
+        assert not difference, 'Mask check failed : {}'.format(message)
+
+    def checkMaskEvents(mask, events, fs, numSamples, message):
+        labels = Annotation(events, fs, numSamples)
+        TestAnnotation.assertMask(mask, labels.mask, message)
+
+        labels = Annotation(mask, fs)
+        TestAnnotation.assertListOfTupleEqual(events, labels.events, message)
+
+    def test_mask_to_eventList(self):
+        fs = 10
+        numSamples = 10
+
+        # Simple events
+        mask = [0, 1, 1, 0, 0, 0, 1, 1, 1, 0]
+        events = [(0.1, 0.3), (0.6, 0.9)]
+        TestAnnotation.checkMaskEvents(mask, events, fs, numSamples, 'Simple events')
+
+        # Event == File duration
+        mask = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        events = [(0.0, 1.0)]
+        TestAnnotation.checkMaskEvents(mask, events, fs, numSamples, 'Event = file duration')
+
+        # Event at start - single events
+        mask = [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+        events = [(0.0, 0.4)]
+        TestAnnotation.checkMaskEvents(mask, events, fs, numSamples, 'event at start - single events')
+
+        # Event at end - single events
+        mask = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1]
+        events = [(0.7, 1.)]
+        TestAnnotation.checkMaskEvents(mask, events, fs, numSamples, 'event at end - single events')
+
+        # Event at start - multiple events
+        mask = [1, 1, 1, 1, 0, 0, 1, 0, 0, 0]
+        events = [(0.0, 0.4), (0.6, 0.7)]
+        TestAnnotation.checkMaskEvents(mask, events, fs, numSamples, 'event at start - multiple events')
+
+        # Event at end - multiple events
+        mask = [0, 1, 1, 0, 0, 0, 0, 1, 1, 1]
+        events = [(0.1, 0.3), (0.7, 1.)]
+        TestAnnotation.checkMaskEvents(mask, events, fs, numSamples, 'event at end - multiple events')
+
+        # Event at start and end
+        mask = [1, 1, 1, 0, 0, 0, 0, 1, 0, 1]
+        events = [(0.0, 0.3), (0.7, 0.8), (0.9, 1.0)]
+        TestAnnotation.checkMaskEvents(mask, events, fs, numSamples, 'event at start and end')
+
+
+class TestSampleScoring(unittest.TestCase):
+    def test_sample_scoring(self):
+        fs = 1
+
+        # Simple events
+        ref = Annotation([1, 1, 1, 0, 0, 0, 1, 1, 1, 0], fs)
+        hyp = Annotation([0, 1, 1, 0, 1, 1, 0, 0, 1, 0], fs)
+        scores = scoring.SampleScoring(ref, hyp)
+        np.testing.assert_equal(scores.sensitivity, 3 / 6, 'sensitivity simple test')
+        np.testing.assert_equal(scores.precision, 3 / 5, 'precision simple test')
+        np.testing.assert_equal(scores.fpRate, 2 * 3600 * 24 / 10, 'FP / day simple test')
+
+        # No detections
+        ref = Annotation([1, 1, 1, 0, 0, 0, 1, 1, 1, 0], fs)
+        hyp = Annotation([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], fs)
+        scores = scoring.SampleScoring(ref, hyp)
+        np.testing.assert_equal(scores.sensitivity, 0, 'sensitivity no detections')
+        np.testing.assert_equal(scores.precision, np.nan, 'precision no detections')
+        np.testing.assert_equal(scores.fpRate, 0, 'FP / day no detections')
+
+        # No events
+        ref = Annotation([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], fs)
+        hyp = Annotation([0, 1, 1, 0, 1, 1, 0, 0, 1, 0], fs)
+        scores = scoring.SampleScoring(ref, hyp)
+        np.testing.assert_equal(scores.sensitivity, np.nan, 'sensitivity no events')
+        np.testing.assert_equal(scores.precision, 0, 'precision no events')
+        np.testing.assert_equal(scores.fpRate, 5 * 3600 * 24 / 10, 'FP / day no events')
+
+        # Resampling
+        fs = 256
+        ref = Annotation([(1.2, 5.4), (7.1, 8.7)], fs, fs * 10)
+        hyp = Annotation([(3, 5), (6.8, 9.2)], fs, fs * 10)
+        scores = scoring.SampleScoring(ref, hyp)
+        np.testing.assert_equal(scores.sensitivity, 4 / 6, 'sensitivity resampling')
+        np.testing.assert_equal(scores.precision, 1, 'precision resampling')
+        np.testing.assert_equal(scores.fpRate, 0, 'FP / day resampling')
+
+
+class TestEventScoring(unittest.TestCase):
+    def test_long_event_splitting(self):
+        fs = 10
+        numSamples = 10
+        events = [(0.0, 0.5), (0.6, 0.9)]
+        maxEventDuration = 0.2
+
+        labels = Annotation(events, fs, numSamples)
+        shortLabels = scoring.EventScoring._splitLongEvents(labels, maxEventDuration)
+
+        # Check maximum duration
+        for event in shortLabels.events:
+            self.assertLessEqual(event[1] - event[0], maxEventDuration + 1e-10,  # Computre precision tolerance
+                                 "Long event splitting resulted in long event")
+
+        # Check mask remains unchanged
+        TestAnnotation.assertMask(labels.mask, shortLabels.mask,
+                                  "Event mask changed when splitting events")
+
+    def test_merge_neighbouring_events(self):
+        fs = 10
+        labels = Annotation([(1, 2), (2, 3), (3.5, 4), (4, 5), (18, 20)], fs, 600)
+        minDurationBetweenEvents = 0.2
+
+        mergedEvents = scoring.EventScoring._mergeNeighbouringEvents(labels, minDurationBetweenEvents)
+        expectedMergedEvents = Annotation([(1, 3), (3.5, 5), (18, 20)], fs, 600)
+        TestAnnotation.assertListOfTupleEqual(expectedMergedEvents.events, mergedEvents.events, 'Test merge events')
+
+    def test_extending_events(self):
+        fs = 1
+        numSamples = 10
+        events = [(0.0, 2.0), (6.0, 8.0)]
+        before = 0.5
+        after = 1.0
+
+        target = [(0.0, 3.0), (5.5, 9.0)]
+
+        labels = Annotation(events, fs, numSamples)
+        extendedLabels = scoring.EventScoring._extendEvents(labels, before, after)
+
+        TestAnnotation.assertListOfTupleEqual(target, extendedLabels.events,
+                                              "Extended events mismatch")
+
+    def test_event_scoring(self):
+        fs = 10
+        numSamples = 60 * 60 * fs  # 1 hour
+
+        # Simple events
+        message = 'Simple events'
+        ref = Annotation([(40, 60)], fs, numSamples)
+        hyp = Annotation([(10, 20), (42, 65)], fs, numSamples)
+        param = scoring.EventScoring.Parameters(toleranceStart=0,
+                                                toleranceEnd=10,
+                                                minDurationBetweenEvents=0)
+        scores = scoring.EventScoring(ref, hyp, param)
+        np.testing.assert_equal(scores.sensitivity, 1, 'sensitivity : ' + message)
+        np.testing.assert_equal(scores.precision, 0.5, 'precision : ' + message)
+        np.testing.assert_equal(scores.fpRate, 1 * 24, 'FP / day : ' + message)
+
+        # Tolerance before events
+        # REF      <----->
+        # HYP    <------->
+        message = 'Tolerance before events'
+        ref = Annotation([(40, 60)], fs, numSamples)
+        hyp = Annotation([(39, 60)], fs, numSamples)
+        param = scoring.EventScoring.Parameters(toleranceStart=1)
+        scores = scoring.EventScoring(ref, hyp, param)
+        np.testing.assert_equal(scores.sensitivity, 1, 'sensitivity : ' + message)
+        np.testing.assert_equal(scores.precision, 1, 'precision : ' + message)
+        np.testing.assert_equal(scores.fpRate, 0, 'FP / day : ' + message)
+
+        # Split long events
+        # REF <----->
+        # HYP   <-------------------------->
+        # SPLIT  <----------------><-------->
+        message = 'Split long events'
+        ref = Annotation([(40, 60)], fs, numSamples)
+        hyp = Annotation([(42, 65 + 6 * 60)], fs, numSamples)
+        param = scoring.EventScoring.Parameters(maxEventDuration=5 * 60)
+        scores = scoring.EventScoring(ref, hyp, param)
+        np.testing.assert_equal(scores.sensitivity, 1, 'sensitivity : ' + message)
+        np.testing.assert_equal(scores.precision, 1 / 2, 'precision : ' + message)
+        np.testing.assert_equal(scores.fpRate, 1 * 24, 'FP / day : ' + message)
+
+        # No detections
+        message = 'No detections'
+        ref = Annotation([(40, 60)], fs, numSamples)
+        hyp = Annotation([], fs, numSamples)
+        scores = scoring.EventScoring(ref, hyp)
+        np.testing.assert_equal(scores.sensitivity, 0, 'sensitivity : ' + message)
+        np.testing.assert_equal(scores.precision, np.nan, 'precision : ' + message)
+        np.testing.assert_equal(scores.fpRate, 0, 'FP / day : ' + message)
+
+        # No events
+        message = 'No events'
+        ref = Annotation([], fs, numSamples)
+        hyp = Annotation([(40, 60)], fs, numSamples)
+        scores = scoring.EventScoring(ref, hyp)
+        np.testing.assert_equal(scores.sensitivity, np.nan, 'sensitivity : ' + message)
+        np.testing.assert_equal(scores.precision, 0, 'precision : ' + message)
+        np.testing.assert_equal(scores.fpRate, 1 * 24, 'FP / day : ' + message)
+
+        #
+        # Seizure with typical distribution, some overlapping some not
+        ref = Annotation([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                          0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,
+                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         fs)
+        hyp = Annotation([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+                          1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0,
+                          0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+                         fs)
+        param = scoring.EventScoring.Parameters(
+            toleranceStart=0,
+            toleranceEnd=0,
+            minOverlap=0,
+            maxEventDuration=5 * 60,
+            minDurationBetweenEvents=0)
+        scores = scoring.EventScoring(ref, hyp, param)
+        np.testing.assert_equal(scores.sensitivity, 2 / 3, 'sensitivity typical distribution')
+        np.testing.assert_equal(scores.precision, 0.5, 'precision typical distribution')
+
+        param.minOverlap = 0.5
+        scores = scoring.EventScoring(ref, hyp, param)
+        np.testing.assert_equal(scores.sensitivity, 1 / 3, 'sensitivity typical distribution')
+        np.testing.assert_equal(scores.precision, 1 / 4, 'precision typical distribution')
+
+        #
+        # Long hypothesis seizure spaning several true seizures
+        message = 'Long predicted seizure spaning several true seizures'
+        ref = Annotation([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+                          0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,
+                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         fs)
+        hyp = Annotation([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+                          0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         fs)
+
+        param.minOverlap = 0.0
+        scores = scoring.EventScoring(ref, hyp, param)
+        np.testing.assert_equal(scores.sensitivity, 1, 'sensitivity : ' + message)
+        np.testing.assert_equal(scores.precision, 1, 'precision : ' + message)
+
+        #
+        # Long true seizure and many short predicted seizures​
+        message = 'Long true seizure and many short predicted seizures​'
+        ref = Annotation([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                          1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,
+                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         fs)
+        hyp = Annotation([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+                          0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                          0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
+                         fs)
+
+        param.minOverlap = 0.0
+        scores = scoring.EventScoring(ref, hyp, param)
+        np.testing.assert_equal(scores.sensitivity, 0.5, 'sensitivity : ' + message)
+        np.testing.assert_equal(scores.precision, 0.5, 'precision : ' + message)
+
+        # Typial distribution (one missed extended REF would hide FP)
+        fs = 1
+        ref = Annotation([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                          0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,
+                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         fs)
+        hyp = Annotation([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+                          1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0,
+                          0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+                         fs)
+        message = 'typical - extended REF would hide FP'
+        param.minOverlap = 0.6
+        scores = scoring.EventScoring(ref, hyp, param)
+        np.testing.assert_equal(scores.sensitivity, 1 / 3, 'sensitivity : ' + message)
+        np.testing.assert_equal(scores.precision, 0.25, 'precision : ' + message)
+
+
+if __name__ == '__main__':
+    unittest.main()
