1477aa102165b251c9611313831733f9fcb937b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:14:10 +0000

    Add package wetlab/2.0.1/wetlab-2.0.1-py2.py3-none-any.whl

---
+++ packages/wetlab/wetlab-2.0.1-py2.py3-none-any.whl/wetlab/__init__.py
@@ -0,0 +1,58 @@
+"""Basic wetlab entities [`source <https://github.com/laminlabs/wetlab/blob/main/wetlab/models.py>`__].
+
+Install and mount `wetlab` in a new instance:
+
+>>> pip install wetlab
+>>> lamin init --storage ./test-wetlab --modules bionty,wetlab
+
+Import the package:
+
+>>> import wetlab as wl
+
+Create records:
+
+>>> biologic = wl.Biologic(name="IFNG").save()
+
+Perturbation registries:
+
+.. autosummary::
+   :toctree: .
+
+   GeneticPerturbation
+   Biologic
+   Compound
+   CompoundPerturbation
+   EnvironmentalPerturbation
+   CombinationPerturbation
+
+Auxiliary registries:
+
+.. autosummary::
+   :toctree: .
+
+   PerturbationTarget
+
+Types:
+
+.. autosummary::
+   :toctree: .
+
+   GeneticPerturbationSystem
+   BiologicType
+
+"""
+
+__version__ = "2.0.1"
+
+from lamin_utils import logger
+
+try:
+    from pertdb import *  # noqa:F403
+except ImportError:
+    logger.warning(
+        "pertdb is not installed. please install it with `pip install pertdb`"
+    )
+
+logger.warning(
+    "please `import pertdb` instead of `import wetlab`. The wetlab module was renamed. Everything else stays the same."
+)
