1cf347e74383b9ac406507a83449754d5d10129d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:15:53 +0000

    Add package cf-units/3.3.1/cf_units-3.3.1.tar.gz

---
+++ packages/cf-units/cf_units-3.3.1.tar.gz/cf_units-3.3.1/cf_units/tests/integration/test__Unit_num2date.py
@@ -0,0 +1,339 @@
+# Copyright cf-units contributors
+#
+# This file is part of cf-units and is released under the BSD license.
+# See LICENSE in the root of the repository for full licensing details.
+"""Test method :meth:`cf_units.Unit.num2date`."""
+
+import datetime
+
+import cftime
+import numpy as np
+import pytest
+
+import cf_units
+
+
+class Test:
+    def setup_units(self, calendar):
+        self.useconds = cf_units.Unit("seconds since 1970-01-01", calendar)
+        self.uminutes = cf_units.Unit("minutes since 1970-01-01", calendar)
+        self.uhours = cf_units.Unit("hours since 1970-01-01", calendar)
+        self.udays = cf_units.Unit("days since 1970-01-01", calendar)
+
+    def check_dates(self, nums, units, expected, only_cftime=True):
+        for num, unit, exp in zip(nums, units, expected, strict=False):
+            res = unit.num2date(num, only_use_cftime_datetimes=only_cftime)
+            assert exp == res
+            assert isinstance(res, type(exp))
+
+    def check_timedelta(self, nums, units, expected):
+        for num, unit, exp in zip(nums, units, expected, strict=False):
+            epoch = cftime.num2date(0, unit.cftime_unit, unit.calendar)
+            res = unit.num2date(num)
+            delta = res - epoch
+            assert (delta.days, delta.seconds, delta.microseconds) == exp
+
+    def test_scalar(self):
+        unit = cf_units.Unit("seconds since 1970-01-01", "gregorian")
+        num = 5.0
+        exp = datetime.datetime(1970, 1, 1, 0, 0, 5)
+        res = unit.num2date(num)
+        assert exp == res
+        assert isinstance(res, cftime.datetime)
+
+    def test_sequence(self):
+        unit = cf_units.Unit("seconds since 1970-01-01", "gregorian")
+        nums = [20.0, 40.0, 60.0, 80, 100.0]
+        exp = [
+            datetime.datetime(1970, 1, 1, 0, 0, 20),
+            datetime.datetime(1970, 1, 1, 0, 0, 40),
+            datetime.datetime(1970, 1, 1, 0, 1),
+            datetime.datetime(1970, 1, 1, 0, 1, 20),
+            datetime.datetime(1970, 1, 1, 0, 1, 40),
+        ]
+        res = unit.num2date(nums)
+        np.testing.assert_array_equal(exp, res)
+
+    def test_multidim_sequence(self):
+        unit = cf_units.Unit("seconds since 1970-01-01", "gregorian")
+        nums = [[20.0, 40.0, 60.0], [80, 100.0, 120.0]]
+        exp_shape = (2, 3)
+        res = unit.num2date(nums)
+        assert exp_shape == res.shape
+
+    def test_masked_ndarray(self):
+        unit = cf_units.Unit("seconds since 1970-01-01", "gregorian")
+        nums = np.ma.masked_array([20.0, 40.0, 60.0], [False, True, False])
+        exp = [
+            datetime.datetime(1970, 1, 1, 0, 0, 20),
+            None,
+            datetime.datetime(1970, 1, 1, 0, 1),
+        ]
+        res = unit.num2date(nums)
+        np.testing.assert_array_equal(exp, res)
+
+    # Gregorian Calendar tests
+
+    def test_simple_gregorian_py_datetime_type(self):
+        self.setup_units("gregorian")
+        nums = [20.0, 40.0, 75.0, 150.0, 8.0, 16.0, 300.0, 600.0]
+        units = [
+            self.useconds,
+            self.useconds,
+            self.uminutes,
+            self.uminutes,
+            self.uhours,
+            self.uhours,
+            self.udays,
+            self.udays,
+        ]
+        expected = [
+            datetime.datetime(1970, 1, 1, 0, 0, 20),
+            datetime.datetime(1970, 1, 1, 0, 0, 40),
+            datetime.datetime(1970, 1, 1, 1, 15),
+            datetime.datetime(1970, 1, 1, 2, 30),
+            datetime.datetime(1970, 1, 1, 8),
+            datetime.datetime(1970, 1, 1, 16),
+            datetime.datetime(1970, 10, 28),
+            datetime.datetime(1971, 8, 24),
+        ]
+
+        self.check_dates(nums, units, expected, only_cftime=False)
+
+    def test_simple_gregorian(self):
+        self.setup_units("gregorian")
+        nums = [20.0, 40.0, 75.0, 150.0, 8.0, 16.0, 300.0, 600.0]
+        units = [
+            self.useconds,
+            self.useconds,
+            self.uminutes,
+            self.uminutes,
+            self.uhours,
+            self.uhours,
+            self.udays,
+            self.udays,
+        ]
+        expected = [
+            cftime.datetime(1970, 1, 1, 0, 0, 20, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 0, 0, 40, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 1, 15, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 2, 30, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 8, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 16, calendar="gregorian"),
+            cftime.datetime(1970, 10, 28, calendar="gregorian"),
+            cftime.datetime(1971, 8, 24, calendar="gregorian"),
+        ]
+
+        self.check_dates(nums, units, expected)
+
+    def test_fractional_gregorian(self):
+        self.setup_units("gregorian")
+        nums = [
+            5.0 / 60.0,
+            10.0 / 60.0,
+            15.0 / 60.0,
+            30.0 / 60.0,
+            8.0 / 24.0,
+            16.0 / 24.0,
+        ]
+        units = [
+            self.uminutes,
+            self.uminutes,
+            self.uhours,
+            self.uhours,
+            self.udays,
+            self.udays,
+        ]
+        expected = [
+            cftime.datetime(1970, 1, 1, 0, 0, 5, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 0, 0, 10, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 0, 15, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 0, 30, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 8, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 16, calendar="gregorian"),
+        ]
+
+        self.check_dates(nums, units, expected)
+
+    def test_fractional_second_gregorian(self):
+        self.setup_units("gregorian")
+        nums = [0.25, 0.5, 0.75, 1.5, 2.5, 3.5, 4.5]
+        units = [self.useconds] * 7
+        expected = [
+            cftime.datetime(1970, 1, 1, 0, 0, 0, 250000, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 0, 0, 0, 500000, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 0, 0, 0, 750000, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 0, 0, 1, 500000, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 0, 0, 2, 500000, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 0, 0, 3, 500000, calendar="gregorian"),
+            cftime.datetime(1970, 1, 1, 0, 0, 4, 500000, calendar="gregorian"),
+        ]
+
+        self.check_dates(nums, units, expected)
+
+    # 360 day Calendar tests
+
+    def test_simple_360_day(self):
+        self.setup_units("360_day")
+        nums = [20.0, 40.0, 75.0, 150.0, 8.0, 16.0, 300.0, 600.0]
+        units = [
+            self.useconds,
+            self.useconds,
+            self.uminutes,
+            self.uminutes,
+            self.uhours,
+            self.uhours,
+            self.udays,
+            self.udays,
+        ]
+        # Expected results in (days, seconds, microseconds) delta from unit
+        # epoch.
+        expected = [
+            (0, nums[0], 0),
+            (0, nums[1], 0),
+            (0, nums[2] * 60, 0),
+            (0, nums[3] * 60, 0),
+            (0, nums[4] * 60 * 60, 0),
+            (0, nums[5] * 60 * 60, 0),
+            (nums[6], 0, 0),
+            (nums[7], 0, 0),
+        ]
+
+        self.check_timedelta(nums, units, expected)
+
+    def test_fractional_360_day(self):
+        self.setup_units("360_day")
+        nums = [
+            5.0 / 60.0,
+            10.0 / 60.0,
+            15.0 / 60.0,
+            30.0 / 60.0,
+            8.0 / 24.0,
+            16.0 / 24.0,
+        ]
+        units = [
+            self.uminutes,
+            self.uminutes,
+            self.uhours,
+            self.uhours,
+            self.udays,
+            self.udays,
+        ]
+        # Expected results in (days, seconds, microseconds) delta from unit
+        # epoch.
+        expected = [
+            (0, nums[0] * 60, 0),
+            (0, nums[1] * 60, 0),
+            (0, nums[2] * 60 * 60, 0),
+            (0, nums[3] * 60 * 60, 0),
+            (0, nums[4] * 24 * 60 * 60, 0),
+            (0, nums[5] * 24 * 60 * 60, 0),
+        ]
+
+        self.check_timedelta(nums, units, expected)
+
+    def test_fractional_second_360_day(self):
+        self.setup_units("360_day")
+        nums = [0.25, 0.5, 0.75, 1.5, 2.5, 3.5, 4.5]
+        units = [self.useconds] * 7
+        # Expected results in (days, seconds, microseconds) delta from unit
+        # epoch.
+        expected = [
+            (0, 0, 250000),
+            (0, 0, 500000),
+            (0, 0, 750000),
+            (0, 1, 500000),
+            (0, 2, 500000),
+            (0, 3, 500000),
+            (0, 4, 500000),
+        ]
+
+        self.check_timedelta(nums, units, expected)
+
+    def test_pydatetime_wrong_calendar(self):
+        unit = cf_units.Unit("days since 1970-01-01", calendar="360_day")
+        with pytest.raises(ValueError, match="illegal calendar or reference date"):
+            unit.num2date(
+                1,
+                only_use_cftime_datetimes=False,
+                only_use_python_datetimes=True,
+            )
+
+    # 365 day Calendar tests
+
+    def test_simple_365_day(self):
+        self.setup_units("365_day")
+        nums = [20.0, 40.0, 75.0, 150.0, 8.0, 16.0, 300.0, 600.0]
+        units = [
+            self.useconds,
+            self.useconds,
+            self.uminutes,
+            self.uminutes,
+            self.uhours,
+            self.uhours,
+            self.udays,
+            self.udays,
+        ]
+        # Expected results in (days, seconds, microseconds) delta from unit
+        # epoch.
+        expected = [
+            (0, nums[0], 0),
+            (0, nums[1], 0),
+            (0, nums[2] * 60, 0),
+            (0, nums[3] * 60, 0),
+            (0, nums[4] * 60 * 60, 0),
+            (0, nums[5] * 60 * 60, 0),
+            (nums[6], 0, 0),
+            (nums[7], 0, 0),
+        ]
+
+        self.check_timedelta(nums, units, expected)
+
+    def test_fractional_365_day(self):
+        self.setup_units("365_day")
+        nums = [
+            5.0 / 60.0,
+            10.0 / 60.0,
+            15.0 / 60.0,
+            30.0 / 60.0,
+            8.0 / 24.0,
+            16.0 / 24.0,
+        ]
+        units = [
+            self.uminutes,
+            self.uminutes,
+            self.uhours,
+            self.uhours,
+            self.udays,
+            self.udays,
+        ]
+        # Expected results in (days, seconds, microseconds) delta from unit
+        # epoch.
+        expected = [
+            (0, nums[0] * 60, 0),
+            (0, nums[1] * 60, 0),
+            (0, nums[2] * 60 * 60, 0),
+            (0, nums[3] * 60 * 60, 0),
+            (0, nums[4] * 24 * 60 * 60, 0),
+            (0, nums[5] * 24 * 60 * 60, 0),
+        ]
+
+        self.check_timedelta(nums, units, expected)
+
+    def test_fractional_second_365_day(self):
+        self.setup_units("365_day")
+        nums = [0.25, 0.5, 0.75, 1.5, 2.5, 3.5, 4.5]
+        units = [self.useconds] * 7
+        # Expected results in (days, seconds, microseconds) delta from unit
+        # epoch.
+        expected = [
+            (0, 0, 250000),
+            (0, 0, 500000),
+            (0, 0, 750000),
+            (0, 1, 500000),
+            (0, 2, 500000),
+            (0, 3, 500000),
+            (0, 4, 500000),
+        ]
+
+        self.check_timedelta(nums, units, expected)
---
+++ packages/cf-units/cf_units-3.3.1.tar.gz/cf_units-3.3.1/setup.py
@@ -0,0 +1,172 @@
+"""Setup routines to enable cf-units' Cython elements.
+
+All other setup configuration is in `pyproject.toml`.
+"""
+
+from distutils.sysconfig import get_config_var
+from os import environ, getenv
+from pathlib import Path
+from shutil import copy
+import sys
+import sysconfig
+
+from setuptools import Command, Extension, setup
+from setuptools.command import build_ext
+
+# Default to using cython, but use the .c files if it doesn't exist.
+#  Supports the widest possible range of developer setups.
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = False
+
+USE_PY_LIMITED_API = (
+    # Builds are not ABI3 default
+    getenv("CF_UNITS_LIMITED_API", "0") == "1"
+    and sys.version_info >= (3, 11)
+    # Free-threaded builds do not support ABI3
+    and not sysconfig.get_config_var("Py_GIL_DISABLED")
+)
+
+COMPILER_DIRECTIVES = {}
+# This Cython macro disables a build warning, obsolete with Cython>=3
+#  see : https://cython.readthedocs.io/en/latest/src/userguide/migrating_to_cy30.html#numpy-c-api
+DEFINE_MACROS = [("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
+if USE_PY_LIMITED_API:
+    # 0x030B0000 -> 3.11
+    DEFINE_MACROS.append(("Py_LIMITED_API", "0x030B0000"))
+FLAG_COVERAGE = "--cython-coverage"  # custom flag enabling Cython line tracing
+BASEDIR = Path(__file__).resolve().parent
+PACKAGE = "cf_units"
+CFUNITS_DIR = BASEDIR / PACKAGE
+
+
+class CleanCython(Command):
+    description = "Purge artifacts built by Cython"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for path in CFUNITS_DIR.rglob("*"):
+            if path.suffix in (".pyc", ".pyo", ".c", ".so"):
+                msg = f"clean: removing file {path}"
+                print(msg)
+                path.unlink()
+
+
+def get_dirs(env_var: str, config_var: str):
+    """Get a directory from an env variable or a distutils config variable."""
+    result = environ.get(env_var) or get_config_var(config_var)
+    return [result] if result else []
+
+
+def get_package_data():
+    """Find and correctly package the UDUNITS2 XML files for a wheel build."""
+    package_data = {}
+    # Determine whether we're building a wheel.
+    if "bdist_wheel" in sys.argv:
+        # The protocol is that the UDUNITS2_XML_PATH environment variable
+        # identifies the root UDUNITS2 XML file and parent directory containing
+        # all the XML resource files that require to be bundled within this
+        # wheel. Note that, this should match the UDUNITS2 distribution for the
+        # UDUNITS2 library cf-units is linking against.
+        xml_env = "UDUNITS2_XML_PATH"
+        xml_database = environ.get(xml_env)
+        if xml_database is None:
+            emsg = f"Require to set {xml_env} for a cf-units wheel build."
+            raise ValueError(emsg)
+        xml_database = Path(xml_database)
+        if not xml_database.is_file():
+            emsg = (
+                f"Can't open {xml_env} file {xml_database} during cf-units wheel build."
+            )
+            raise ValueError(emsg)
+        # We have a valid XML file, so copy the distro bundle into the
+        # cf_units/etc/share directory.
+        xml_dir = xml_database.expanduser().resolve().parent
+        share_base = Path("etc") / "share"
+        share_dir = CFUNITS_DIR / share_base
+        if not share_dir.is_dir():
+            share_dir.mkdir(parents=True)
+        else:
+            # Purge any existing XML share files.
+            [fname.unlink() for fname in share_dir.glob("*.xml")]
+        # Bundle the UDUNITS2 XML file/s for the wheel.
+        [copy(fname, share_dir) for fname in xml_dir.glob("*.xml")]
+        # Register our additional wheel content.
+        package_data = {PACKAGE: [str(share_base / "*.xml")]}
+    return package_data
+
+
+class NumpyBuildExt(build_ext.build_ext):
+    """Subclass of build_ext to make NumPy headers available for the Cython layer."""
+
+    def finalize_options(self):
+        # See https://github.com/SciTools/cf-units/issues/151
+        def _set_builtin(name, value):
+            if isinstance(__builtins__, dict):
+                __builtins__[name] = value
+            else:
+                setattr(__builtins__, name, value)
+
+        super().finalize_options()
+        _set_builtin("__NUMPY_SETUP__", False)
+        import numpy  # noqa: PLC0415
+
+        self.include_dirs.append(numpy.get_include())
+
+
+if FLAG_COVERAGE in sys.argv or environ.get("CYTHON_COVERAGE", None):
+    COMPILER_DIRECTIVES = {"linetrace": True}
+    DEFINE_MACROS += [("CYTHON_TRACE", "1"), ("CYTHON_TRACE_NOGIL", "1")]
+    if FLAG_COVERAGE in sys.argv:
+        sys.argv.remove(FLAG_COVERAGE)
+    print('enable: "linetrace" Cython compiler directive')
+
+include_dirs = get_dirs("UDUNITS2_INCDIR", "INCLUDEDIR")
+library_dirs = get_dirs("UDUNITS2_LIBDIR", "LIBDIR")
+
+# Some of the complexity MUST remain in setup.py due to its dynamic nature. To
+#  reduce confusion, the Extension is 100% defined here, rather than splitting
+#  between setup.py and pyproject.toml `ext-modules`.
+udunits_ext = Extension(
+    f"{PACKAGE}._udunits2",
+    [str(Path(f"{PACKAGE}") / f"_udunits2.{'pyx' if cythonize else 'c'}")],
+    include_dirs=include_dirs,
+    library_dirs=library_dirs,
+    libraries=["udunits2"],
+    runtime_library_dirs=(None if sys.platform.startswith("win") else library_dirs),
+    define_macros=DEFINE_MACROS,
+    py_limited_api=True,
+)
+
+if cythonize:
+    # https://docs.cython.org/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
+    [udunits_ext] = cythonize(
+        udunits_ext,
+        compiler_directives=COMPILER_DIRECTIVES,
+        # Assert python 3 source syntax: Currently required to suppress a
+        #  warning, even though this is now the default (as-of Cython v3).
+        language_level="3str",
+    )
+
+cmdclass = {"clean_cython": CleanCython, "build_ext": NumpyBuildExt}
+
+if USE_PY_LIMITED_API:
+    SETUP_OPTIONS = {"bdist_wheel": {"py_limited_api": "cp311"}}
+else:
+    SETUP_OPTIONS = {}
+
+kwargs = {
+    "cmdclass": cmdclass,
+    "ext_modules": [udunits_ext],
+    "package_data": get_package_data(),
+    "options": SETUP_OPTIONS,
+}
+
+setup(**kwargs)
