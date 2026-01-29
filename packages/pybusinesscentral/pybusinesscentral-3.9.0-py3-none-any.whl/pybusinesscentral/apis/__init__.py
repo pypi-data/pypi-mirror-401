68839d046c4ef5cb168659a279288e565cc646c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:14:56 +0000

    Add package pybusinesscentral/3.9.0/pybusinesscentral-3.9.0-py3-none-any.whl

---
+++ packages/pybusinesscentral/pybusinesscentral-3.9.0-py3-none-any.whl/pybusinesscentral/apis/__init__.py
@@ -0,0 +1,45 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from .api.account_api import AccountApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from pybusinesscentral.api.account_api import AccountApi
+from pybusinesscentral.api.company_api import CompanyApi
+from pybusinesscentral.api.customer_api import CustomerApi
+from pybusinesscentral.api.default_api import DefaultApi
+from pybusinesscentral.api.dimension_api import DimensionApi
+from pybusinesscentral.api.dimension_set_line_api import DimensionSetLineApi
+from pybusinesscentral.api.payment_term_api import PaymentTermApi
+from pybusinesscentral.api.purchase_invoice_api import PurchaseInvoiceApi
+from pybusinesscentral.api.sales_invoice_api import SalesInvoiceApi
+from pybusinesscentral.api.sales_invoice_line_api import SalesInvoiceLineApi
+from pybusinesscentral.api.sales_credit_memo_api import SalesCreditMemoApi
+from pybusinesscentral.api.sales_credit_memo_line_api import SalesCreditMemoLineApi
+from pybusinesscentral.api.vendor_api import VendorApi
+
+__all__ = [
+    "AccountApi",
+    "CompanyApi",
+    "CustomerApi",
+    "DefaultApi",
+    "DimensionApi",
+    "DimensionSetLineApi",
+    "PaymentTermApi",
+    "PurchaseInvoiceApi",
+    "SalesInvoiceApi",
+    "SalesInvoiceLineApi",
+    "SalesCreditMemoApi",
+    "SalesCreditMemoLineApi",
+    "VendorApi"
+]
---
+++ packages/pybusinesscentral/pybusinesscentral-3.9.0-py3-none-any.whl/pybusinesscentral/models/__init__.py
@@ -0,0 +1,156 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from pybusinesscentral.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from pybusinesscentral.model.account import Account
+from pybusinesscentral.model.aged_accounts_payable import AgedAccountsPayable
+from pybusinesscentral.model.aged_accounts_receivable import AgedAccountsReceivable
+from pybusinesscentral.model.attachments import Attachments
+from pybusinesscentral.model.balance_sheet import BalanceSheet
+from pybusinesscentral.model.bank_account import BankAccount
+from pybusinesscentral.model.cash_flow_statement import CashFlowStatement
+from pybusinesscentral.model.company import Company
+from pybusinesscentral.model.company_information import CompanyInformation
+from pybusinesscentral.model.country_region import CountryRegion
+from pybusinesscentral.model.currency import Currency
+from pybusinesscentral.model.customer import Customer
+from pybusinesscentral.model.customer_financial_detail import CustomerFinancialDetail
+from pybusinesscentral.model.customer_payment import CustomerPayment
+from pybusinesscentral.model.customer_payment_journal import CustomerPaymentJournal
+from pybusinesscentral.model.customer_sale import CustomerSale
+from pybusinesscentral.model.default_dimensions import DefaultDimensions
+from pybusinesscentral.model.dimension import Dimension
+from pybusinesscentral.model.dimension_line import DimensionLine
+from pybusinesscentral.model.dimension_set_line import DimensionSetLine
+from pybusinesscentral.model.dimension_value import DimensionValue
+from pybusinesscentral.model.dimensiontype import Dimensiontype
+from pybusinesscentral.model.documentlineobjectdetailstype import Documentlineobjectdetailstype
+from pybusinesscentral.model.employee import Employee
+from pybusinesscentral.model.general_ledger_entry import GeneralLedgerEntry
+from pybusinesscentral.model.general_ledger_entry_attachments import GeneralLedgerEntryAttachments
+from pybusinesscentral.model.general_ledger_setup import GeneralLedgerSetup
+from pybusinesscentral.model.get_general_ledger_setup200_response import GetGeneralLedgerSetup200Response
+from pybusinesscentral.model.income_statement import IncomeStatement
+from pybusinesscentral.model.item import Item
+from pybusinesscentral.model.item_category import ItemCategory
+from pybusinesscentral.model.itemunitofmeasureconversiontype import Itemunitofmeasureconversiontype
+from pybusinesscentral.model.journal import Journal
+from pybusinesscentral.model.journal_line import JournalLine
+from pybusinesscentral.model.payment_method import PaymentMethod
+from pybusinesscentral.model.payment_term import PaymentTerm
+from pybusinesscentral.model.pdf_document import PdfDocument
+from pybusinesscentral.model.picture import Picture
+from pybusinesscentral.model.post_customer_request import PostCustomerRequest
+from pybusinesscentral.model.post_purchase_invoice_request import PostPurchaseInvoiceRequest
+from pybusinesscentral.model.post_purchase_invoice_line_for_purchase_invoice_request import PostPurchaseInvoiceLineForPurchaseInvoiceRequest
+from pybusinesscentral.model.post_sales_credit_memo_request import PostSalesCreditMemoRequest
+from pybusinesscentral.model.post_sales_credit_memo_line_for_sales_credit_memo_request import PostSalesCreditMemoLineForSalesCreditMemoRequest
+from pybusinesscentral.model.post_sales_invoice_request import PostSalesInvoiceRequest
+from pybusinesscentral.model.post_sales_invoice_dimension_set_line_request import PostSalesInvoiceDimensionSetLineRequest
+from pybusinesscentral.model.post_sales_credit_memo_dimension_set_line_request import PostSalesCreditMemoDimensionSetLineRequest
+from pybusinesscentral.model.post_sales_invoice_line_for_sales_invoice_request import PostSalesInvoiceLineForSalesInvoiceRequest
+from pybusinesscentral.model.post_vendor_request import PostVendorRequest
+from pybusinesscentral.model.postaladdresstype import Postaladdresstype
+from pybusinesscentral.model.project import Project
+from pybusinesscentral.model.purchase_invoice import PurchaseInvoice
+from pybusinesscentral.model.purchase_invoice_line import PurchaseInvoiceLine
+from pybusinesscentral.model.retained_earnings_statement import RetainedEarningsStatement
+from pybusinesscentral.model.sales_credit_memo import SalesCreditMemo
+from pybusinesscentral.model.sales_credit_memo_line import SalesCreditMemoLine
+from pybusinesscentral.model.sales_invoice import SalesInvoice
+from pybusinesscentral.model.sales_invoice_line import SalesInvoiceLine
+from pybusinesscentral.model.sales_credit_memo import SalesCreditMemo
+from pybusinesscentral.model.sales_credit_memo_line import SalesCreditMemoLine
+from pybusinesscentral.model.sales_order import SalesOrder
+from pybusinesscentral.model.sales_order_line import SalesOrderLine
+from pybusinesscentral.model.sales_quote import SalesQuote
+from pybusinesscentral.model.sales_quote_line import SalesQuoteLine
+from pybusinesscentral.model.shipment_method import ShipmentMethod
+from pybusinesscentral.model.tax_area import TaxArea
+from pybusinesscentral.model.tax_group import TaxGroup
+from pybusinesscentral.model.time_registration_entry import TimeRegistrationEntry
+from pybusinesscentral.model.trial_balance import TrialBalance
+from pybusinesscentral.model.unit_of_measure import UnitOfMeasure
+from pybusinesscentral.model.unitofmeasuretype import Unitofmeasuretype
+from pybusinesscentral.model.vendor import Vendor
+from pybusinesscentral.model.vendor_purchase import VendorPurchase
+
+__all__ = [
+    "Account",
+    "AgedAccountsPayable",
+    "AgedAccountsReceivable",
+    "Attachments",
+    "BalanceSheet",
+    "BankAccount",
+    "CashFlowStatement",
+    "Company",
+    "CompanyInformation",
+    "CountryRegion",
+    "Currency",
+    "Customer",
+    "CustomerFinancialDetail",
+    "CustomerPayment",
+    "CustomerPaymentJournal",
+    "CustomerSale",
+    "DefaultDimensions",
+    "Dimension",
+    "DimensionLine",
+    "DimensionSetLine",
+    "DimensionValue",
+    "Dimensiontype",
+    "Documentlineobjectdetailstype",
+    "Employee",
+    "GeneralLedgerEntry",
+    "GeneralLedgerEntryAttachments",
+    "GeneralLedgerSetup",
+    "GetGeneralLedgerSetup200Response",
+    "IncomeStatement",
+    "Item",
+    "ItemCategory",
+    "Itemunitofmeasureconversiontype",
+    "Journal",
+    "JournalLine",
+    "PaymentMethod",
+    "PaymentTerm",
+    "PdfDocument",
+    "Picture",
+    "PostCustomerRequest",
+    "PostPurchaseInvoiceRequest",
+    "PostPurchaseInvoiceLineForPurchaseInvoiceRequest",
+    "PostSalesInvoiceDimensionSetLineRequest",
+    "PostSalesCreditMemoDimensionSetLineRequest",
+    "PostSalesCreditMemoRequest",
+    "PostSalesCreditMemoLineForSalesCreditMemoRequest",
+    "PostSalesInvoiceRequest",
+    "PostSalesInvoiceLineForSalesInvoiceRequest",
+    "PostVendorRequest",
+    "Postaladdresstype",
+    "Project",
+    "PurchaseInvoice",
+    "PurchaseInvoiceLine",
+    "RetainedEarningsStatement",
+    "SalesCreditMemo",
+    "SalesCreditMemoLine",
+    "SalesInvoice",
+    "SalesInvoiceLine",
+    "SalesOrder",
+    "SalesOrderLine",
+    "SalesQuote",
+    "SalesQuoteLine",
+    "ShipmentMethod",
+    "TaxArea",
+    "TaxGroup",
+    "TimeRegistrationEntry",
+    "TrialBalance",
+    "UnitOfMeasure",
+    "Unitofmeasuretype",
+    "Vendor",
+    "VendorPurchase"
+]
