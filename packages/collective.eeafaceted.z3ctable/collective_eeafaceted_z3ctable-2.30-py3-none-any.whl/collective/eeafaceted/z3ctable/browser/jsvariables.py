708bb994cb3da579cd61b6cdce0791966bb8f56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:49:57 +0000

    Add package collective.eeafaceted.z3ctable/2.30/collective_eeafaceted_z3ctable-2.30-py3-none-any.whl

---
+++ packages/collective.eeafaceted.z3ctable/collective_eeafaceted_z3ctable-2.30-py3-none-any.whl/collective/eeafaceted/z3ctable/browser/jsvariables.py
@@ -0,0 +1,22 @@
+from zope.i18n import translate as _
+from zope.publisher.browser import BrowserView
+
+
+TEMPLATE = """\
+var no_selected_items = "%(no_selected_items)s";
+"""
+
+
+class JSVariables(BrowserView):
+
+    def __call__(self, *args, **kwargs):
+        response = self.request.response
+        response.setHeader('content-type', 'text/javascript;;charset=utf-8')
+
+        no_selected_items = _('no_selected_items',
+                              default="Please select at least one element.",
+                              domain='collective.eeafaceted.z3ctable',
+                              context=self.request)
+        return TEMPLATE % dict(
+            no_selected_items=no_selected_items,
+        )
---
+++ packages/collective.eeafaceted.z3ctable/collective_eeafaceted_z3ctable-2.30-py3-none-any.whl/collective/eeafaceted/z3ctable/interfaces.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""Module where all interfaces, events and exceptions live."""
+
+from plone.theme.interfaces import IDefaultPloneLayer
+from z3c.table.interfaces import IColumn
+from z3c.table.interfaces import ITable
+from zope.viewlet.interfaces import IViewletManager
+
+
+class ICollectiveEeafacetedZ3ctableLayer(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer."""
+
+
+class IFacetedTable(ITable):
+    """ """
+
+
+class IFacetedColumn(IColumn):
+    """ """
+
+
+class ITopAboveNavManager(IViewletManager):
+    """A viewlet manager that sits at the top of the rendered table,
+       above the batching navigation."""
+
+
+class ITopBelowNavManager(IViewletManager):
+    """A viewlet manager that sits at the top of the rendered table,
+       below the batching navigation."""
+
+
+class IBottomAboveNavManager(IViewletManager):
+    """A viewlet manager that sits at the bottom of the rendered table,
+       above the batching navigation."""
+
+
+class IBottomBelowNavManager(IViewletManager):
+    """A viewlet manager that sits at the bottom of the rendered table,
+       below the batching navigation."""
