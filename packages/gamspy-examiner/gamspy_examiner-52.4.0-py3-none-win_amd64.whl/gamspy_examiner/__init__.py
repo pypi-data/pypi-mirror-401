32098c09d15b8208bd9e9f6fd34f5ef9082dbae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:50:56 +0000

    Add package gamspy-examiner/52.4.0/gamspy_examiner-52.4.0-py3-none-win_amd64.whl

---
+++ packages/gamspy-examiner/gamspy_examiner-52.4.0-py3-none-win_amd64.whl/gamspy_examiner/__init__.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+from .version import __version__
+directory = str(Path(__file__).resolve().parent)
+
+files = ['optexaminer.def', 'ex_cclib64.dll']
+
+file_paths = [directory + os.sep + file for file in files]
+verbatim = 'EXAMINER 103011 5 00010203040506070809 0 0 2 LP MIP RMIP NLP MCP MPEC RMPEC DNLP RMINLP MINLP QCP MIQCP RMIQCP\ngmsgennt.cmd\ngmsgennx.exe\nex_cclib64.dll ex_ 2 0'
