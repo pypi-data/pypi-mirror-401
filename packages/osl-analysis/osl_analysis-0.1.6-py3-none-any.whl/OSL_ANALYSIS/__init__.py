9f097ada4b9f01c6955a7c290fd1353d611bb322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:17:22 +0000

    Add package osl-analysis/0.1.6/osl_analysis-0.1.6-py3-none-any.whl

---
+++ packages/osl-analysis/osl_analysis-0.1.6-py3-none-any.whl/OSL_ANALYSIS/__init__.py
@@ -0,0 +1,15 @@
+# OSL_ANALYSIS package initialization
+
+__version__ = "0.1.6"
+
+
+# You can import the main module here if desired
+from . import E_values
+from . import download
+from . import modulo1
+from . import modulo2
+from . import modulo3
+from . import modulo4
+
+
+
