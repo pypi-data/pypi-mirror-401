b10302fcad51d6709a989d6ac74ea6a93782efe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:50:35 +0000

    Add package gamspy-cbc/52.4.0/gamspy_cbc-52.4.0-py3-none-macosx_13_0_arm64.whl

---
+++ packages/gamspy-cbc/gamspy_cbc-52.4.0-py3-none-macosx_13_0_arm64.whl/gamspy_cbc/__init__.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+from .version import __version__
+directory = str(Path(__file__).resolve().parent)
+
+files = ['optcbc.def', 'libcbccclib64.dylib']
+
+file_paths = [directory + os.sep + file for file in files]
+verbatim = 'CBC 2011 5 00010203040506070809 1 0 2 LP MIP RMIP\ngmsgenus.run\ngmsgenux.out\nlibcbccclib64.dylib cbc 1 0'
