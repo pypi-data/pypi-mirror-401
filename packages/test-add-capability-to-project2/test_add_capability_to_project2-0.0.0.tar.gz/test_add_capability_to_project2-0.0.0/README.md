effbddcd82480077c193be8940594a0e79ba5e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:21:44 +0000

    Add package test-add-capability-to-project2/0.0.0/test_add_capability_to_project2-0.0.0.tar.gz

---
+++ packages/test-add-capability-to-project2/test_add_capability_to_project2-0.0.0.tar.gz/test_add_capability_to_project2-0.0.0/README.md
@@ -0,0 +1,8 @@
+# test_add_capability_to_project2
+
+This is a placeholder package. The actual package is coming soon.
+
+## Status
+
+This version (0.0.0) is a placeholder to reserve the package name.
+Please check back later for the actual release.
---
+++ packages/test-add-capability-to-project2/test_add_capability_to_project2-0.0.0.tar.gz/test_add_capability_to_project2-0.0.0/src/test_add_capability_to_project2/__init__.py
@@ -0,0 +1,12 @@
+"""Placeholder - will be replaced by first release."""
+
+import warnings
+
+warnings.warn(
+    "test_add_capability_to_project2 version 0.0.0 is a placeholder. "
+    "Install the latest version for actual functionality.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+__version__ = "0.0.0"
