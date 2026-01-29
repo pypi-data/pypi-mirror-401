af620439d2db8f5a460961ea95c2614349656862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:14:25 +0000

    Add package smart-utils-eman/0.1.0/smart_utils_eman-0.1.0.tar.gz

---
+++ packages/smart-utils-eman/smart_utils_eman-0.1.0.tar.gz/smart_utils_eman-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smart_utils_eman",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies here
+    author="Your Name",
+    description="A library for math and string utilities",
+)
