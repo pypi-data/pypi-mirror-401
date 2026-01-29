8f4bc1dcfc04e393fa7601d8b61a56a0fdbd962e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:15:03 +0000

    Add package odoo-addon-account-statement-import-file/16.0.1.1.3.1/odoo_addon_account_statement_import_file-16.0.1.1.3.1-py3-none-any.whl

---
+++ packages/odoo-addon-account-statement-import-file/odoo_addon_account_statement_import_file-16.0.1.1.3.1-py3-none-any.whl/odoo/addons/account_statement_import_file/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright 2004-2020 Odoo S.A.
+# Copyright 2020 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# Licence LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    "name": "Import Statement Files",
+    "category": "Accounting",
+    "version": "16.0.1.1.3",
+    "license": "LGPL-3",
+    "depends": ["account_statement_import_base"],
+    "author": "Odoo SA, Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via", "etobella"],
+    "development_status": "Mature",
+    "website": "https://github.com/OCA/bank-statement-import",
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/account_statement_import_view.xml",
+        "views/account_journal.xml",
+    ],
+    "demo": [
+        "demo/partner_bank.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "account_statement_import_file/static/src/**/*.xml",
+            "account_statement_import_file/static/src/**/*.esm.js",
+        ],
+    },
+    "installable": True,
+}
