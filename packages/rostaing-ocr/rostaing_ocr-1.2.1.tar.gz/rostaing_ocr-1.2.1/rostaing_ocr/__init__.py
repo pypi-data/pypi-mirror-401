06ff9484da3924088a9d1ddd5a8394b51f2b4c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:22:30 +0000

    Add package rostaing-ocr/1.2.1/rostaing_ocr-1.2.1.tar.gz

---
+++ packages/rostaing-ocr/rostaing_ocr-1.2.1.tar.gz/rostaing_ocr-1.2.1/rostaing_ocr/__init__.py
@@ -0,0 +1,10 @@
+"""
+rostaing-ocr: Powerful OCR Package for RAG/LLM Text Extraction
+Version: 1.2.1
+"""
+
+from .rostaing_ocr import ocr_extractor
+
+__version__ = "1.2.1"
+__author__ = "Rostaing"
+__all__ = ["ocr_extractor"]
