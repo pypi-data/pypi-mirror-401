11b5d5cca30b25b78c235d626b5a89bec1acf2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:14:34 +0000

    Add package mlflow-modal-deploy/0.4.0/mlflow_modal_deploy-0.4.0-py3-none-any.whl

---
+++ packages/mlflow-modal-deploy/mlflow_modal_deploy-0.4.0-py3-none-any.whl/mlflow_modal/__init__.py
@@ -0,0 +1,21 @@
+"""
+MLflow Modal Deployment Plugin
+
+Deploy MLflow models to Modal's serverless infrastructure.
+See https://modal.com for more information.
+"""
+
+from mlflow_modal.deployment import (
+    SUPPORTED_GPUS,
+    ModalDeploymentClient,
+    run_local,
+    target_help,
+)
+
+__version__ = "0.4.0"
+__all__ = [
+    "ModalDeploymentClient",
+    "run_local",
+    "target_help",
+    "SUPPORTED_GPUS",
+]
