d98579b12174301bf0c83f724841ca5db858d06c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:15:25 +0000

    Add package odoo14-addon-ssi-lead-data-requirement/14.0.1.1.0/odoo14_addon_ssi_lead_data_requirement-14.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-lead-data-requirement/odoo14_addon_ssi_lead_data_requirement-14.0.1.1.0-py3-none-any.whl/odoo/addons/ssi_lead_data_requirement/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+{
+    "name": "Lead - Data Requirement Integration",
+    "version": "14.0.1.1.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_lead",
+        "ssi_data_requirement_mixin",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
---
+++ packages/odoo14-addon-ssi-lead-data-requirement/odoo14_addon_ssi_lead_data_requirement-14.0.1.1.0-py3-none-any.whl/odoo/addons/ssi_lead_data_requirement/models/crm_lead.py
@@ -0,0 +1,21 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import fields, models
+
+
+class CrmLead(models.Model):
+    _name = "crm.lead"
+    _inherit = [
+        "crm.lead",
+        "mixin.data_requirement",
+    ]
+    _data_requirement_create_page = True
+    _data_requirement_partner_field_name = "partner_id"
+
+    data_requirement_ids = fields.Many2many(
+        relation="rel_lead_2_data_requirement",
+        column1="lead_id",
+        column2="data_requirement_id",
+    )
