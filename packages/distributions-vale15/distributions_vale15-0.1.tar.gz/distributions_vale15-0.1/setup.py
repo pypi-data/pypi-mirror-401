a3a4b7a98aae451095c635acefbcd431ae895f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:50:51 +0000

    Add package distributions-vale15/0.1/distributions_vale15-0.1.tar.gz

---
+++ packages/distributions-vale15/distributions_vale15-0.1.tar.gz/distributions_vale15-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="distributions-vale15",
+    version="0.1",
+    description="Gaussian distributions",
+    packages=["distributions"],
+    zip_safe=False,
+)
