606b8f68a3d3dd9a8a6a5f6a8203c5678b849393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:14:47 +0000

    Add package osl-analysis/0.1.5/osl_analysis-0.1.5-py3-none-any.whl

---
+++ packages/osl-analysis/osl_analysis-0.1.5-py3-none-any.whl/OSL_ANALYSIS/__init__.py
@@ -0,0 +1,22 @@
+# OSL_ANALYSIS package initialization
+
+__version__ = "0.1.5"
+# Inicializamos con None para evitar errores al importar
+E1 = None
+E2 = None
+E3 = None
+def set_E_values():
+    global E1, E2, E3
+    E1 = float(input("Enter E1: "))
+    E2 = float(input("Enter E2: "))
+    E3 = float(input("Enter E3: "))
+
+# You can import the main module here if desired
+from . import modulo1
+from . import download
+from . import modulo2
+from . import modulo3
+from . import modulo4
+
+
+
