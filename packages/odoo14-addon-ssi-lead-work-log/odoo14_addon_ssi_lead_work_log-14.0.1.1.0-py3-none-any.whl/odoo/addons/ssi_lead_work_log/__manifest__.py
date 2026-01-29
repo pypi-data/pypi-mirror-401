6ab3a87b247dedfa35674aca9dc2b4880a4eb2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:15:25 +0000

    Add package odoo14-addon-ssi-lead-work-log/14.0.1.1.0/odoo14_addon_ssi_lead_work_log-14.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-lead-work-log/odoo14_addon_ssi_lead_work_log-14.0.1.1.0-py3-none-any.whl/odoo/addons/ssi_lead_work_log/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+{
+    "name": "Lead + Work Log Integration",
+    "version": "14.0.1.1.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_lead",
+        "ssi_work_log_mixin",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
