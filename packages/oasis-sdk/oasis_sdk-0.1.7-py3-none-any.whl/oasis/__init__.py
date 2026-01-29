b6b1f74a4ab1d611534a9b70c169f3bb81fc5faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 05:25:23 +0000

    Add package oasis-sdk/0.1.7/oasis_sdk-0.1.7-py3-none-any.whl

---
+++ packages/oasis-sdk/oasis_sdk-0.1.7-py3-none-any.whl/oasis/__init__.py
@@ -0,0 +1,12 @@
+from .core import ProviderCode, ProviderType, ModelType
+from .compatible import Oasis, AsyncOasis, ChatOasis, OasisEmbedding
+
+__all__ = [
+    "Oasis",
+    "AsyncOasis",
+    "ChatOasis",
+    "OasisEmbedding",
+    "ProviderCode",
+    "ProviderType",
+    "ModelType",
+]
