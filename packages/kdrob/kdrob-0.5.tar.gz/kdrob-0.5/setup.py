e05bebca0774e3893f341549351d95761ac067f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:50:47 +0000

    Add package kdrob/0.5/kdrob-0.5.tar.gz

---
+++ packages/kdrob/kdrob-0.5.tar.gz/kdrob-0.5/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="kdrob",
+    version="0.5",
+    packages=find_packages(),
+    author="Kalpesh",
+    description="Simple robotics utility library",
+)
