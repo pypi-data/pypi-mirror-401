e5da9e90ceb0fbe934588a765a5635bc37c6aea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:50:28 +0000

    Add package bmrb-sas/1.0.0/bmrb_sas-1.0.0-py3-none-any.whl

---
+++ packages/bmrb-sas/bmrb_sas-1.0.0-py3-none-any.whl/sas/__init__.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python -u
+#
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("bmrb-sas")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+#
+#
+class SasException( Exception ) :
+    def __init__( self, line = 0, msg = "SAS error", *args, **kwargs ) :
+        super( self.__class__, self ).__init__( *args, **kwargs )
+        self._line = line
+        self._msg = msg
+
+# simple timings
+#
+from contextlib import contextmanager
+import time
+import sys
+
+@contextmanager
+def timer( label ) :
+    start = time.time()
+    try :
+        yield
+    finally :
+        end = time.time()
+        sys.stdout.write( "%s: %0.3f\n" % (label,(end - start)) )
+
+#
+#
+#from ply.lex import LexError
+#
+#
+from .lexer import StarLexer
+from .handlers import ErrorHandler, ContentHandlerBase, ContentHandler, ContentHandler2, SasContentHandler
+from .parsebase import ParserBase
+from .nmrstar import SasParser, SansParser, Parser as SansParser2
+from .mmcif import CifParser
+from .ddl import DdlParser
+#from .quickcheck import QuickCheck
+
+# because of PLY's design I can't easily re-use lexer regexps elsewhere. so here they are again.
+# (use group for warnings: "keyword group(1) in value".)
+#
+import re
+KEYWORDS = (
+    re.compile( r"(?:^|\s)(global_)\s*.*$", re.IGNORECASE ),
+    re.compile( r"(?:^|\s)(data_\w+)\s*.*$", re.IGNORECASE ),
+    re.compile( r"(?:^|\s)(save_[^\s]*)\s*.*$", re.IGNORECASE ),
+    re.compile( r"(?:^|\s)(loop_)\s*.*$", re.IGNORECASE ),
+    re.compile( r"(?:^|\s)(stop_)\s*.*$", re.IGNORECASE ),
+    re.compile( r"(?:^|\s)(_\w[^\s]*)\s*.*$", re.IGNORECASE )
+)
+
+# value delimiter map: PLY token to what's passed by ``ContentHandler`` callback
+#
+TOKENS = {
+    "CHARACTERS"   : None,
+    "FRAMECODE"    : "$",
+    "SINGLESTART"  : "'",
+    "TSINGLESTART" : "'''",
+    "DOUBLESTART"  : '"',
+    "TDOUBLESTART" : '"""',
+    "SEMISTART"    : ";",
+    "SINGLEEND"    : "'",
+    "TSINGLEEND"   : "'''",
+    "DOUBLEEND"    : '"',
+    "TDOUBLEEND"   : '"""',
+    "SEMIEND"      : ";"
+}
+
+#
+__all__ = ["__version__", "TOKENS", "KEYWORDS", "SasException",
+    "ContentHandlerBase", "ParserBase",
+    "StarLexer",
+    "ErrorHandler", "ContentHandler", "ContentHandler2", "SasContentHandler",
+    "SasParser", "SansParser", "SansParser2",
+    "CifParser",
+    "DdlParser",
+#    "QuickCheck"
+    ]
+
+#
+#
+#
