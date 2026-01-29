ae1dd499955f34110e8fdac1291ec80c96e22c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 05:27:01 +0000

    Add package rosia/0.0.4/rosia-0.0.4.tar.gz

---
+++ packages/rosia/rosia-0.0.4.tar.gz/rosia-0.0.4/rosia/__init__.py
@@ -0,0 +1,9 @@
+from rosia.frontend import (
+    reaction as reaction,
+    Node as Node,
+    InputPort as InputPort,
+    OutputPort as OutputPort,
+    Rosia as Rosia,
+    InjectedRosia as InjectedRosia,
+)
+from rosia.coordinate import Coordinator as Coordinator
