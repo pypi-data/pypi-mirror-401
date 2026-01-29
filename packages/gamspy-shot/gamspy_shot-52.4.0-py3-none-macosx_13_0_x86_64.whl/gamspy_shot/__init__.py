7e5e045e948e8317e5020336e8009a284dd22194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:50:57 +0000

    Add package gamspy-shot/52.4.0/gamspy_shot-52.4.0-py3-none-macosx_13_0_x86_64.whl

---
+++ packages/gamspy-shot/gamspy_shot-52.4.0-py3-none-macosx_13_0_x86_64.whl/gamspy_shot/__init__.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+from .version import __version__
+directory = str(Path(__file__).resolve().parent)
+
+files = ['libipopt.dylib', 'libmkl_gams.dylib', 'libgurobi130.dylib', 'libiomp5.dylib', 'libshtcclib64.dylib']
+
+file_paths = [directory + os.sep + file for file in files]
+verbatim = 'SHOT 1001 5 00010203040506070809 1 0 2 MINLP MIQCP\ngmsgenus.run\ngmsgenux.out\nlibshtcclib64.dylib sht 1 1'
