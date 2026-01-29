adf297ad1d4b5dd6df259c8f017f34470f78d263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:21:44 +0000

    Add package AutoxJS/1.0.13/autoxjs-1.0.13.tar.gz

---
+++ packages/AutoxJS/autoxjs-1.0.13.tar.gz/autoxjs-1.0.13/autojs/stringrunner.js
@@ -0,0 +1,6 @@
+var clientSocket=new java.net.Socket("localhost",%d);
+var inputReader=new java.io.BufferedReader(new java.io.InputStreamReader(clientSocket.getInputStream(),"utf-8"));
+var inputObject=new org.json.JSONObject(inputReader.readLine());
+inputReader.close();
+clientSocket.close();
+engines.execScript(inputObject.getString("name"),inputObject.getString("script"));
