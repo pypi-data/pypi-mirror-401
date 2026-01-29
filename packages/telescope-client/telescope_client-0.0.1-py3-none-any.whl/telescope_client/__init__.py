1d8bed090d520a44f2384d971451d00243071614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:15:34 +0000

    Add package telescope-client/0.0.1/telescope_client-0.0.1-py3-none-any.whl

---
+++ packages/telescope-client/telescope_client-0.0.1-py3-none-any.whl/telescope_client/__init__.py
@@ -0,0 +1,8 @@
+"""
+telescope-client - Reserved Package Name
+
+This package name has been reserved and is not yet available for use.
+Please check back later or contact the maintainer for more information.
+"""
+
+__version__ = "0.0.1"
