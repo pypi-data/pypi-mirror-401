a62d046d989ae13f14c7f57ad68234d4812a1ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 05:25:25 +0000

    Add package plain.tailwind/0.20.0/plain_tailwind-0.20.0.tar.gz

---
+++ packages/plain.tailwind/plain_tailwind-0.20.0.tar.gz/plain_tailwind-0.20.0/plain/tailwind/default_settings.py
@@ -0,0 +1,10 @@
+from plain.assets.finders import _APP_ASSETS_DIR
+from plain.runtime import APP_PATH
+
+# The tailwind.css source file is stored at the root of the repo,
+# where can see all sources in the repo and manually refer to other plain sources.
+TAILWIND_SRC_PATH = APP_PATH.parent / "tailwind.css"
+
+# The compiled css goes in the root assets directory.
+# It is typically gitignored.
+TAILWIND_DIST_PATH = _APP_ASSETS_DIR / "tailwind.min.css"
