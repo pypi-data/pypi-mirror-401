526386dce49cee5a79500f3630a9b81ec13d6692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:15:43 +0000

    Add package tyc-engine/0.6.32/tyc_engine-0.6.32-py3-none-any.whl

---
+++ packages/tyc-engine/tyc_engine-0.6.32-py3-none-any.whl/TYC_ENGINE/assets/lib_assets/py_assets/core.py
@@ -0,0 +1,34 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+class Create:
+
+    def screen(self, wh, he, title, icon):
+        self.sc = pygame.display.set_mode((wh, he))
+        pygame.display.set_caption(title)
+        pygame.display.set_icon(icon)\
+
+    def fill(self, color):
+        self.sc.fill(color)
+
+    class draw:
+        def rect(self, color, wh, he, screen, radius):
+            pygame.draw.rect(screen, color, (wh, he))
+        def circle(self, color, wh, he, screen, radius):
+            pygame.draw.circle(screen, color, (wh, he), radius)
+        def line(self, color, wh, he, screen, radius):
+            pygame.draw.line(screen, color, (wh, he))
+
+
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+
+            clock = pygame.time.Clock()
+            pygame.display.update()
+            clock.tick(60)
