5a985d23154f2c18c14cc1c846eaed9835ce7c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:50:02 +0000

    Add package teval/0.1.2/teval-0.1.2-py3-none-any.whl

---
+++ packages/teval/teval-0.1.2-py3-none-any.whl/teval/__init__.py
@@ -0,0 +1,11 @@
+"""Trivial LLM eval - as simple as possible."""
+
+from teval.metrics import EvaluationRubric, MetricDefinition
+
+__version__ = "0.1.1"
+
+__all__ = [
+    "EvaluationRubric",
+    "MetricDefinition",
+    "__version__",
+]
