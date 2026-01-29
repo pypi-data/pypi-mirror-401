f578f9a017235de9fd9d52a028226057cd41bd4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:14:28 +0000

    Add package omoospace/0.2.5/omoospace-0.2.5.tar.gz

---
+++ packages/omoospace/omoospace-0.2.5.tar.gz/omoospace-0.2.5/src/omoospace/__init__.py
@@ -0,0 +1,36 @@
+from omoospace.utils import *
+from omoospace.validators import *
+from omoospace.functions import *
+from omoospace.items import *
+from omoospace.omoospace import *
+
+
+__all__ = [
+    "Omoospace",
+    "Subspace",
+    "Objective",
+    "ObjectiveTree",
+    "ObjectiveType",
+    "Maker",
+    "Tool",
+    "Work",
+    "MakerDict",
+    "ToolDict",
+    "WorkDict",
+    "Oset",
+    "Opath",
+    "create_omoospace",
+    "extract_pathname",
+    "extract_objective",
+    "make_path",
+    "copy_to_clipboard",
+    "normalize_name",
+    "is_ignore",
+    "is_recovered",
+    "is_buckup",
+    "is_autosave",
+    "is_version",
+    "is_url",
+    "is_email",
+    "is_number",
+]
