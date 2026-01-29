749b6c3250f9241daad774ee0a3aefff0f7b4b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:49:38 +0000

    Add package ghmd/1.0.1.post1/ghmd-1.0.1.post1-py3-none-any.whl

---
+++ packages/ghmd/ghmd-1.0.1.post1-py3-none-any.whl/ghmd/__main__.py
@@ -0,0 +1,85 @@
+import json
+import os
+import pkgutil
+import re
+import sys
+import requests
+
+
+def getShared(filename):
+    data = pkgutil.get_data("ghmd", f"shared/{filename}")
+    if data is None:
+        raise FileNotFoundError(f"ghmd shared file not found: {filename}")
+    return data.decode("utf-8")
+
+
+strings = json.loads(getShared("strings.json"))
+
+
+def main():
+    files = [argv for argv in sys.argv[1:] if not argv.startswith("--")]
+    options = [argv for argv in sys.argv[1:] if argv.startswith("--")]
+
+    theme = ""
+    embed_css = False
+    mode = "gfm"
+
+    for option in options:
+        if option == "--help":
+            return print("\n".join(strings["helpMessage"]))
+        elif option == "--dark":
+            theme = "-dark"
+        elif option == "--light":
+            theme = "-light"
+        elif option == "--embed-css":
+            embed_css = True
+        elif option == "--no-gfm":
+            mode = "markdown"
+        else:
+            raise Exception(strings["errors"]["invalidOption"].format(option=option))
+
+    if any(file.endswith(".html") for file in files):
+        raise Exception(strings["errors"]["htmlExtension"])
+
+    css_uri = strings["css"]["uri"].format(theme=theme)
+
+    if embed_css:
+        res = requests.get(css_uri)
+        if res.status_code != 200:
+            raise Exception(strings["errors"]["embedCss"])
+        css = f"<style>{res.text}</style>"
+    else:
+        css = strings["css"]["link"].format(uri=css_uri)
+
+    headers = {"Accept": "application/vnd.github+json"}
+    if github_token := os.environ.get("GITHUB_TOKEN"):
+        headers["Authorization"] = f"Bearer {github_token}"
+
+    for file in files:
+        file_content = open(file, "r", encoding="utf-8").read()
+
+        titleSearch = re.search(r"^# (.*)$", file_content, re.MULTILINE)
+        title = titleSearch.group(1) if titleSearch else ""
+
+        template = getShared("md-template.html")
+
+        res = requests.post(
+            "https://api.github.com/markdown",
+            headers=headers,
+            data=json.dumps({"text": file_content, "mode": mode}),
+        )
+
+        if res.status_code != 200:
+            raise Exception(strings["errors"]["markdownConversion"])
+
+        filename = ".".join(file.split(".")[:-1])
+        with open(f"{filename}.html", "w+", encoding="utf-8") as f:
+            f.write(
+                template.replace("{{ .CSS }}", css)
+                .replace("{{ .Title }}", title)
+                .replace("{{ .Content }}", res.text)
+            )
+
+
+if __name__ == "__main__":
+    main()
