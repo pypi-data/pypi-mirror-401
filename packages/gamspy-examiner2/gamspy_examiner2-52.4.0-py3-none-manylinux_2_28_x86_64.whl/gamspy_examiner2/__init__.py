354e0566f13d1320958c8bb2a70586d25797dea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:49:45 +0000

    Add package gamspy-examiner2/52.4.0/gamspy_examiner2-52.4.0-py3-none-manylinux_2_28_x86_64.whl

---
+++ packages/gamspy-examiner2/gamspy_examiner2-52.4.0-py3-none-manylinux_2_28_x86_64.whl/gamspy_examiner2/__init__.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+from .version import __version__
+directory = str(Path(__file__).resolve().parent)
+
+files = ['optexaminer2.def', 'optexaminer.def', 'libexmcclib64.so']
+
+file_paths = [directory + os.sep + file for file in files]
+verbatim = 'EXAMINER2 103011 5 00010203040506070809 0 1 2 LP MIP RMIP NLP MCP DNLP RMINLP MINLP QCP MIQCP RMIQCP\ngmsgenus.run optexaminer.def\ngmsgenux.out\nlibexmcclib64.so exm 1 0'
