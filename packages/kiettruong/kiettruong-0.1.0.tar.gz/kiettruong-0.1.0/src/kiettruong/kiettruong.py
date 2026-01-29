aab6dc402999dd513625881acadfa8630190e950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:15:02 +0000

    Add package kiettruong/0.1.0/kiettruong-0.1.0.tar.gz

---
+++ packages/kiettruong/kiettruong-0.1.0.tar.gz/kiettruong-0.1.0/src/kiettruong/kiettruong.py
@@ -0,0 +1,8 @@
+class Kiettruong:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def describe():
+        print(f'my name is {name}, my age is {age}')
+
+
