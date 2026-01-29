ad2d0664bc6f873fca3aa78256ab2352c3ffefcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:21:45 +0000

    Add package agentscope/1.0.12/agentscope-1.0.12.tar.gz

---
+++ packages/agentscope/agentscope-1.0.12.tar.gz/agentscope-1.0.12/src/agentscope/rag/_store/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""The vector database store abstraction in AgentScope RAG module."""
+
+from ._store_base import (
+    VDBStoreBase,
+)
+from ._qdrant_store import QdrantStore
+from ._milvuslite_store import MilvusLiteStore
+from ._alibabacloud_mysql_store import AlibabaCloudMySQLStore
+
+__all__ = [
+    "VDBStoreBase",
+    "QdrantStore",
+    "MilvusLiteStore",
+    "AlibabaCloudMySQLStore",
+]
