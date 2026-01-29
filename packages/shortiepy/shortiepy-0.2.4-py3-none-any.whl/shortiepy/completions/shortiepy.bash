1be22cd494cdac1ff9e3d5c631257d19c3a63ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:15:22 +0000

    Add package shortiepy/0.2.4/shortiepy-0.2.4-py3-none-any.whl

---
+++ packages/shortiepy/shortiepy-0.2.4-py3-none-any.whl/shortiepy/completions/shortiepy.bash
@@ -0,0 +1,29 @@
+_shortiepy_completion() {
+    local IFS=$'\n'
+    local response
+
+    response=$(env COMP_WORDS="${COMP_WORDS[*]}" COMP_CWORD=$COMP_CWORD _SHORTIEPY_COMPLETE=bash_complete $1)
+
+    for completion in $response; do
+        IFS=',' read type value <<< "$completion"
+
+        if [[ $type == 'dir' ]]; then
+            COMPREPLY=()
+            compopt -o dirnames
+        elif [[ $type == 'file' ]]; then
+            COMPREPLY=()
+            compopt -o default
+        elif [[ $type == 'plain' ]]; then
+            COMPREPLY+=($value)
+        fi
+    done
+
+    return 0
+}
+
+_shortiepy_completion_setup() {
+    complete -o nosort -F _shortiepy_completion shortiepy
+}
+
+_shortiepy_completion_setup;
+
