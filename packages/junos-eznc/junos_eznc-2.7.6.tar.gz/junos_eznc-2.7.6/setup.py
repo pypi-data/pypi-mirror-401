a5052c570433af8b2e2ec470d7cc9c3c26f182f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:14:56 +0000

    Add package junos-eznc/2.7.6/junos_eznc-2.7.6.tar.gz

---
+++ packages/junos-eznc/junos_eznc-2.7.6.tar.gz/junos_eznc-2.7.6/setup.py
@@ -0,0 +1,54 @@
+import os
+
+from setuptools import find_packages, setup
+
+import versioneer
+
+# parse requirements
+req_lines = [line.strip() for line in open("requirements.txt").readlines()]
+install_reqs = list(filter(None, req_lines))
+
+setup(
+    name="junos-eznc",
+    namespace_packages=["jnpr"],
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Jeremy Schulman, Nitin Kumar, Rick Sherman, Stacy Smith",
+    author_email="jnpr-community-netdev@juniper.net",
+    description=("Junos 'EZ' automation for non-programmers"),
+    license_files=["LICENSE"],
+    license="Apache 2.0",
+    keywords="Junos NETCONF networking automation",
+    url="http://www.github.com/Juniper/py-junos-eznc",
+    package_dir={"": "lib"},
+    packages=find_packages("lib"),
+    package_data={
+        "jnpr.junos.op": ["*.yml"],
+        "jnpr.junos.command": ["*.yml"],
+        "jnpr.junos.cfgro": ["*.yml"],
+        "jnpr.junos.resources": ["*.yml"],
+    },
+    python_requires=">=3.8",
+    install_requires=install_reqs,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Networking",
+        "Topic :: Text Processing :: Markup :: XML",
+    ],
+)
