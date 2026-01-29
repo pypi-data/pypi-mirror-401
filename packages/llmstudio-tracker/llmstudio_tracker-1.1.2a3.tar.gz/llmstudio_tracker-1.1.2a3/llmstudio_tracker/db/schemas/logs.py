ca643f93bb4ec7708038041fa722f383847c340f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:16:42 +0000

    Add package llmstudio-tracker/1.1.2a3/llmstudio_tracker-1.1.2a3.tar.gz

---
+++ packages/llmstudio-tracker/llmstudio_tracker-1.1.2a3.tar.gz/llmstudio_tracker-1.1.2a3/llmstudio_tracker/db/schemas/logs.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseModel
+
+
+# BaseModels
+class LogDefaultBase(BaseModel):
+    chat_input: str = None
+    chat_output: str = None
+    session_id: Optional[str] = None
+    context: List[Dict[str, Any]] = None
+    provider: str = None
+    model: str = None
+    deployment: str = None
+    parameters: dict = None
+    metrics: dict = None
+    extras: Optional[dict] = None
+
+
+class LogDefaultResponse(LogDefaultBase):
+    log_id: int
+    created_at: datetime
+
+
+class LogDefaultCreate(LogDefaultBase):
+    pass
+
+
+class DashboardMetrics(BaseModel):
+    request_by_provider: List[Dict[str, int]]
+    request_by_model: List[Dict[str, int]]
+    total_cost_by_provider: List[Dict]
+    total_cost_by_model: List[Dict]
+    average_latency: List[Dict]
+    average_ttft: List[Dict]
+    average_itl: List[Dict]
+    average_tps: List[Dict]
