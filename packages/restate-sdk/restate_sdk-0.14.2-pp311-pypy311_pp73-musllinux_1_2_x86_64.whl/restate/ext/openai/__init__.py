dd8af00d7004e57c62ca69c17a1d8d86d1148f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:50:37 +0000

    Add package restate-sdk/0.14.2/restate_sdk-0.14.2-pp311-pypy311_pp73-musllinux_1_2_x86_64.whl

---
+++ packages/restate-sdk/restate_sdk-0.14.2-pp311-pypy311_pp73-musllinux_1_2_x86_64.whl/restate/ext/openai/__init__.py
@@ -0,0 +1,49 @@
+#
+#  Copyright (c) 2023-2024 - Restate Software, Inc., Restate GmbH
+#
+#  This file is part of the Restate SDK for Python,
+#  which is released under the MIT license.
+#
+#  You can find a copy of the license in file LICENSE in the root
+#  directory of this repository or package, or at
+#  https://github.com/restatedev/sdk-typescript/blob/main/LICENSE
+#
+"""
+This module contains the optional OpenAI integration for Restate.
+"""
+
+import typing
+
+from restate import ObjectContext, Context
+from restate.server_context import current_context
+
+from .runner_wrapper import DurableRunner
+from .models import LlmRetryOpts
+from .functions import propagate_cancellation, raise_terminal_errors, durable_function_tool
+
+
+def restate_object_context() -> ObjectContext:
+    """Get the current Restate ObjectContext."""
+    ctx = current_context()
+    if ctx is None:
+        raise RuntimeError("No Restate context found.")
+    return typing.cast(ObjectContext, ctx)
+
+
+def restate_context() -> Context:
+    """Get the current Restate Context."""
+    ctx = current_context()
+    if ctx is None:
+        raise RuntimeError("No Restate context found.")
+    return ctx
+
+
+__all__ = [
+    "DurableRunner",
+    "LlmRetryOpts",
+    "restate_object_context",
+    "restate_context",
+    "raise_terminal_errors",
+    "propagate_cancellation",
+    "durable_function_tool",
+]
