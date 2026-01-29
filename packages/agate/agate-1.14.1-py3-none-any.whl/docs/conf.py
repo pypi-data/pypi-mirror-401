606956f3308e25b4c1dbf8d0efb71be64fa35126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:35:18 +0000

    Add package agate/1.14.1/agate-1.14.1-py3-none-any.whl

---
+++ packages/agate/agate-1.14.1-py3-none-any.whl/docs/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'agate'
+copyright = '2017, Christopher Groskopf'
+version = '1.14.1'
+release = version
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autosummary',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+
+htmlhelp_basename = 'agatedoc'
+
+autodoc_default_options = {
+    'members': None,
+    'member-order': 'bysource',
+    'show-inheritance': True,
+}
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'leather': ('https://leather.readthedocs.io/en/latest/', None)
+}
