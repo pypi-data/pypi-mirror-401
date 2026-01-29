fa1433025b4f2dfe37c2e1a0075c7d603a271426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:50:46 +0000

    Add package funmap/0.2.2/funmap-0.2.2-py3-none-any.whl

---
+++ packages/funmap/funmap-0.2.2-py3-none-any.whl/funmap/data_urls.py
@@ -0,0 +1,18 @@
+misc_urls = {
+    "reactome_gold_standard": "https://api.figshare.com/v2/file/download/38647601",
+    "reactome_gold_standard_md5": "671942763b6a7c32506cba1ed9900fe6",
+    "funmap_blacklist": "https://api.figshare.com/v2/file/download/9033977",
+    "mapping_file": "https://api.figshare.com/v2/file/download/39033971",
+}
+
+# the information about other networks is fixed for now
+network_info = {
+    "name": ["BioGRID", "BioPlex", "HI-union", "STRING"],
+    "type": ["BioGRID", "BioPlex", "HI", "STRING"],
+    "url": [
+        "https://api.figshare.com/v2/file/download/39125054",
+        "https://api.figshare.com/v2/file/download/39125051",
+        "https://api.figshare.com/v2/file/download/39125093",
+        "https://api.figshare.com/v2/file/download/39125090",
+    ],
+}
