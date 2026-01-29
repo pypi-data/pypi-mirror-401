c6889f5c08350844d09535f384ee78ea605d0d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:14:34 +0000

    Add package sendspy/1.0.1/sendspy-1.0.1.tar.gz

---
+++ packages/sendspy/sendspy-1.0.1.tar.gz/sendspy-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="sendspy",
+    version="1.0.1",
+    author="JA3FR",
+    author_email="hhvvm5@gmail.com",
+    description="مكتبة ارسال رسائل تليجرام بسيطة",
+    long_description="# sendspy\nمكتبة بسيطة",
+    long_description_content_type="text/markdown",
+    url="https://github.com/HHVVM1/sendpy",
+    py_modules=["sendspy"],
+    install_requires=["requests>=2.25.0"],
+)
