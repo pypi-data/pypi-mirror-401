2987b4864630e893b5f3ad523b7d23fcfa33ecd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:21:19 +0000

    Add package laboneq/26.1.0b4/laboneq-26.1.0b4-cp310-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/laboneq/laboneq-26.1.0b4-cp310-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/laboneq/compiler/common/signal_obj.py
@@ -0,0 +1,50 @@
+# Copyright 2022 Zurich Instruments AG
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
+
+from laboneq.core.types.enums.mixer_type import MixerType
+from laboneq.data.compilation_job import ParameterInfo, SignalRange
+
+if TYPE_CHECKING:
+    from laboneq.data.awg_info import AWGInfo
+
+
+@dataclass(init=True, repr=True, order=True)
+class SignalObj:
+    """A collection of a signal's properties relevant for code generation. The delay
+    fields are in seconds and their meaning is as follows:
+    - start_delay: the delay from the trigger to the start of the sequence (lead time),
+      realized as initial playZeros; includes lead time and precompensation
+    - delay_signal: user-defined additional delays, realized by adding to the initial
+      playZeros; rounded to the sequencer grid (sample_multiple)
+    - base_delay_signal: in case of an acquisition pulse, the delay_signal of the
+      corresponding measure pulse on the same AWG
+    - port_delay: port delay specified via the calibration; realized via the device node
+      in addition to potential on-device delays.
+    - base_port_delay: in case of an acquisition pulse, the port_delay of the
+      corresponding measure pulse on the same AWG
+    - automute: The signal output can be automatically muted when no waveforms
+        are played.
+    - signal_range: The selected input our output range of the signal.
+    """
+
+    id: str
+    start_delay: float
+    delay_signal: float
+    signal_type: str  # One of "iq" / "single" / "integration" - see SignalInfoType
+    base_delay_signal: float = 0.0
+    local_oscillator_frequency: float | None = None
+    channels: list[int] = field(default_factory=list)
+    channel_to_port: dict[int, str] = field(default_factory=dict)
+    awg: AWGInfo = None
+    port_delay: float | ParameterInfo = 0.0
+    base_port_delay: float = 0.0
+    mixer_type: MixerType | None = None
+    hw_oscillator: str | None = None
+    is_qc: bool | None = None
+    automute: bool = False
+    signal_range: SignalRange | None = None
