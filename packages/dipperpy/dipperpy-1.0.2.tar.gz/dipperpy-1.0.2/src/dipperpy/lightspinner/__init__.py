1dcfa76c011915cdc32d7eff3595d2fa86275691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:50:42 +0000

    Add package dipperpy/1.0.2/dipperpy-1.0.2.tar.gz

---
+++ packages/dipperpy/dipperpy-1.0.2.tar.gz/dipperpy-1.0.2/src/dipperpy/lightspinner/__init__.py
@@ -0,0 +1,23 @@
+from .fal import Falc82
+#from .rh_atoms import CaII_atom, H_6_atom, C_atom, MgII_atom
+from .atomic_set import RadiativeSet
+from .rh_method import Context
+from .background import Background
+from .rh_atoms_model import rh_atoms
+
+
+from .radtransfer_spectrum import atomicspectrum
+from .response_fn import iterate_temperature_perturbation
+
+
+#import matplotlib.pyplot as plt
+#import astropy.units as u
+#from copy import deepcopy
+
+#from astropy import units as u
+#import copy
+
+#import numpy as np
+
+
+
