de15a6c20ceb5c74df94b27e5a57326c286a7c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 05:24:04 +0000

    Add package antflow/0.7.2/antflow-0.7.2-py3-none-any.whl

---
+++ packages/antflow/antflow-0.7.2-py3-none-any.whl/antflow/__init__.py
@@ -0,0 +1,62 @@
+"""
+AntFlow: Async execution library with concurrent.futures-style API and advanced pipelines.
+"""
+
+from ._version import __version__
+from .exceptions import (
+    AntFlowError,
+    ExecutorShutdownError,
+    PipelineError,
+    StageValidationError,
+    TaskFailedError,
+)
+from .executor import AsyncExecutor, AsyncFuture, WaitStrategy
+from .pipeline import Pipeline, PipelineBuilder, Stage
+from .tracker import StatusEvent, StatusTracker
+from .types import (
+    DashboardProtocol,
+    DashboardSnapshot,
+    ErrorSummary,
+    FailedItem,
+    PipelineResult,
+    PipelineStats,
+    StatusType,
+    TaskEvent,
+    TaskEventType,
+    TaskFunc,
+    WorkerMetrics,
+    WorkerState,
+    WorkerStatus,
+)
+from .context import set_task_status
+
+__all__ = [
+    "__version__",
+    "AsyncExecutor",
+    "AsyncFuture",
+    "AntFlowError",
+    "DashboardProtocol",
+    "DashboardSnapshot",
+    "ErrorSummary",
+    "ExecutorShutdownError",
+    "FailedItem",
+    "PipelineResult",
+    "Pipeline",
+    "PipelineBuilder",
+    "PipelineError",
+    "PipelineStats",
+    "Stage",
+    "StageValidationError",
+    "StatusEvent",
+    "StatusTracker",
+    "StatusType",
+    "TaskEvent",
+    "TaskEventType",
+    "TaskFailedError",
+    "TaskFunc",
+    "WaitStrategy",
+    "WorkerMetrics",
+    "WorkerState",
+    "WorkerStatus",
+    "set_task_status",
+]
