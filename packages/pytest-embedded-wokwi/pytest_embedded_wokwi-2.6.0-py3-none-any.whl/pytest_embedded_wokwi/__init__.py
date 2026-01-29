874a0760abb00ba6f6468a27f20b10ca61ae17f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:15:44 +0000

    Add package pytest-embedded-wokwi/2.6.0/pytest_embedded_wokwi-2.6.0-py3-none-any.whl

---
+++ packages/pytest-embedded-wokwi/pytest_embedded_wokwi-2.6.0-py3-none-any.whl/pytest_embedded_wokwi/__init__.py
@@ -0,0 +1,14 @@
+"""Make pytest-embedded plugin work with the Wokwi CLI."""
+
+WOKWI_CLI_MINIMUM_VERSION = '0.10.1'
+
+from .dut import WokwiDut  # noqa
+from .wokwi import Wokwi  # noqa
+
+__all__ = [
+    'WOKWI_CLI_MINIMUM_VERSION',
+    'Wokwi',
+    'WokwiDut',
+]
+
+__version__ = '2.6.0'
