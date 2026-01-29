a0b5036135a0cf46f2e1bd3215be8db2cf5ea452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 05:23:43 +0000

    Add package zuban/0.4.2/zuban-0.4.2-py3-none-musllinux_1_2_i686.whl

---
+++ packages/zuban/zuban-0.4.2-py3-none-musllinux_1_2_i686.whl/zuban/third_party/typeshed/stubs/ttkthemes/ttkthemes/themed_tk.pyi
@@ -0,0 +1,77 @@
+import tkinter
+from collections.abc import Callable
+from typing import Any, Literal
+
+from ._widget import ThemedWidget
+
+class ThemedTk(tkinter.Tk, ThemedWidget):
+    def __init__(
+        self,
+        # non-keyword-only args copied from tkinter.Tk
+        screenName: str | None = ...,
+        baseName: str | None = ...,
+        className: str = ...,
+        useTk: bool = ...,
+        sync: bool = ...,
+        use: str | None = ...,
+        *,
+        theme: str | None = ...,
+        # fonts argument does nothing
+        toplevel: bool | None = ...,
+        themebg: bool | None = ...,
+        background: bool | None = ...,  # old alias for themebg
+        gif_override: bool = ...,
+    ) -> None: ...
+    def set_theme(self, theme_name: str, toplevel: bool | None = None, themebg: bool | None = None) -> None: ...
+    # Keep this in sync with tkinter.Tk
+    def config(  # type: ignore[override]
+        self,
+        kw: dict[str, Any] | None = None,
+        *,
+        themebg: bool | None = ...,
+        toplevel: bool | None = ...,
+        theme: str | None = ...,
+        background: str = ...,
+        bd: tkinter._ScreenUnits = ...,
+        bg: str = ...,
+        border: tkinter._ScreenUnits = ...,
+        borderwidth: tkinter._ScreenUnits = ...,
+        cursor: tkinter._Cursor = ...,
+        height: tkinter._ScreenUnits = ...,
+        highlightbackground: str = ...,
+        highlightcolor: str = ...,
+        highlightthickness: tkinter._ScreenUnits = ...,
+        menu: tkinter.Menu = ...,
+        padx: tkinter._ScreenUnits = ...,
+        pady: tkinter._ScreenUnits = ...,
+        relief: Literal["raised", "sunken", "flat", "ridge", "solid", "groove"] = ...,
+        takefocus: bool | Literal[0, 1, ""] | Callable[[str], bool | None] = ...,
+        width: tkinter._ScreenUnits = ...,
+    ) -> dict[str, tuple[str, str, str, Any, Any]] | None: ...
+    def cget(self, k: str) -> Any: ...
+    def configure(  # type: ignore[override]
+        self,
+        kw: dict[str, Any] | None = None,
+        *,
+        themebg: bool | None = ...,
+        toplevel: bool | None = ...,
+        theme: str | None = ...,
+        background: str = ...,
+        bd: tkinter._ScreenUnits = ...,
+        bg: str = ...,
+        border: tkinter._ScreenUnits = ...,
+        borderwidth: tkinter._ScreenUnits = ...,
+        cursor: tkinter._Cursor = ...,
+        height: tkinter._ScreenUnits = ...,
+        highlightbackground: str = ...,
+        highlightcolor: str = ...,
+        highlightthickness: tkinter._ScreenUnits = ...,
+        menu: tkinter.Menu = ...,
+        padx: tkinter._ScreenUnits = ...,
+        pady: tkinter._ScreenUnits = ...,
+        relief: Literal["raised", "sunken", "flat", "ridge", "solid", "groove"] = ...,
+        takefocus: bool | Literal[0, 1, ""] | Callable[[str], bool | None] = ...,
+        width: tkinter._ScreenUnits = ...,
+    ) -> dict[str, tuple[str, str, str, Any, Any]] | None: ...
+    def __getitem__(self, k: str) -> Any: ...
+    def __setitem__(self, k: str, v: Any) -> None: ...
