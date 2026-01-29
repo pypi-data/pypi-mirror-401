98de649947608ceee78a9cbda8db3e51f127e0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:17:18 +0000

    Add package agent1c-metrics/0.5.1/agent1c_metrics-0.5.1-py3-none-any.whl

---
+++ packages/agent1c-metrics/agent1c_metrics-0.5.1-py3-none-any.whl/agent1c_metrics/api/metrics.py
@@ -0,0 +1,73 @@
+from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
+from string import Template
+from agent1c_metrics.reader import get_data
+from agent1c_metrics import __version__
+
+router = APIRouter(prefix="/metrics", tags=["metrics"])
+
+@router.get('/', response_class=PlainTextResponse)
+async def metrics():
+    result = [
+        "# HELP agent1c_metrics_infobase_logsize Cumulative size of 1cv8log folder.",
+        "# TYPE agent1c_metrics_infobase_logsize gauge",
+        
+        "# HELP agent1c_metrics_infobase_changed_lastday A metric with a boolean value labeled with ibase information.",
+        "# TYPE agent1c_metrics_infobase_changed_lastday gauge",
+        
+        "# HELP agent1c_metrics_infobase_changed_lastweek A metric with a boolean value labeled with ibase information.",
+        "# TYPE agent1c_metrics_infobase_changed_lastweek gauge",
+        
+        "# HELP agent1c_metrics_infobase_changed_lastmonth A metric with a boolean value labeled with ibase information.",
+        "# TYPE agent1c_metrics_infobase_changed_lastmonth gauge",
+
+        "# HELP agent1c_metrics_infobase_block_sessions A metric with a boolean value labeled with ibase information.",
+        "# TYPE agent1c_metrics_infobase_block_sessions gauge",
+
+        "# HELP agent1c_metrics_infobase_block_tasks A metric with a boolean value labeled with ibase information.",
+        "# TYPE agent1c_metrics_infobase_block_tasks gauge",
+
+        "# HELP agent1c_metrics_infobase_inactivity A metric with a int value contains days of inactivity of the base calculated from log modification time.",
+        "# TYPE agent1c_metrics_infobase_inactivity gauge",
+    ]
+
+    #result.append("# TYPE logtype_by_infobase gauge")
+    templates = [
+        Template("agent1c_metrics_infobase_logsize{host=\"$host\",port=\"$port\",ibname=\"$name\",ibid=\"$id\",type=\"$logtype\",dbserver=\"$dbserver\",dbname=\"$dbname\"} $logsize"),
+        Template("agent1c_metrics_infobase_changed_lastday{host=\"$host\",port=\"$port\",ibname=\"$name\",ibid=\"$id\",type=\"$logtype\",dbserver=\"$dbserver\",dbname=\"$dbname\"} $mtime_lastday"),
+        Template("agent1c_metrics_infobase_changed_lastweek{host=\"$host\",port=\"$port\",ibname=\"$name\",ibid=\"$id\",type=\"$logtype\",dbserver=\"$dbserver\",dbname=\"$dbname\"} $mtime_lastweek"),
+        Template("agent1c_metrics_infobase_changed_lastmonth{host=\"$host\",port=\"$port\",ibname=\"$name\",ibid=\"$id\",type=\"$logtype\",dbserver=\"$dbserver\",dbname=\"$dbname\"} $mtime_lastmonth"),
+        Template("agent1c_metrics_infobase_block_sessions{host=\"$host\",port=\"$port\",ibname=\"$name\",ibid=\"$id\",type=\"$logtype\",dbserver=\"$dbserver\",dbname=\"$dbname\"} $block_sessions"),
+        Template("agent1c_metrics_infobase_block_tasks{host=\"$host\",port=\"$port\",ibname=\"$name\",ibid=\"$id\",type=\"$logtype\",dbserver=\"$dbserver\",dbname=\"$dbname\"} $block_tasks"),
+        Template("agent1c_metrics_infobase_inactivity{host=\"$host\",port=\"$port\",ibname=\"$name\",ibid=\"$id\",type=\"$logtype\",dbserver=\"$dbserver\",dbname=\"$dbname\"} $inactivity"),
+    ]
+
+    templates_cluster = [
+        Template("agent1c_metrics_cluster_info{host=\"$host\",port=\"$port\",name=\"$name\",id=\"$id\",cfgver=\"$cfgver\",cfgpath=\"$cfgpath\"} 1"),
+    ]
+    templates_files = [
+        Template("agent1c_metrics_file_exist{path1c=\"$path1c\",file=\"$file\"} $file_exists"),
+    ]
+    #t_logtype = Template("agent1c_metrics_logtype_by_infobase{host=\"$host\",port=\"$port\",ibname=\"$ibname\",ibid=\"$ibid\",type=\"$type\"} 1")
+    data = get_data()
+    result.append(f"agent1c_metrics_up{{version=\"{__version__}\"}} {1 if data['clusters'] else 0}")
+    #print('Data for metrics:',data)
+    for file_info in data['files']:
+        for t in templates_files:
+            file_prepared_info = dict(map(lambda k: (k,(1 if file_info[k] else 0) if type(file_info[k])==bool else file_info[k]),file_info))
+            result.append(t.safe_substitute(file_prepared_info))
+    for cluster_info in data['clusters']:
+        #print('Processing cluster:',cluster_info | {'data':''})
+        for t in templates_cluster:
+            cluster_prepared_info = { key:val for key, val in cluster_info.items() if key not in ['data'] }
+            result.append(t.safe_substitute(cluster_prepared_info))
+        for ib in cluster_info['data']['bases']:
+            for t in templates:
+                cluster_prepared_info = { key:val for key, val in cluster_info['data']['cluster'].items() if key not in ['id','name'] }
+                ib_prepared_data = dict(map(lambda k: (k,(1 if ib[k] else 0) if type(ib[k])==bool else ib[k]),ib))
+                ib_prepared_block_data = dict(map(lambda k: (k,(1 if ib_prepared_data['block'][k] else 0) if type(ib_prepared_data['block'][k])==bool else ib_prepared_data['block'][k]),ib_prepared_data['block']))
+                full_data = cluster_prepared_info | ib_prepared_data | ib_prepared_block_data
+                #print(full_data)
+                result.append(t.safe_substitute(full_data))
+    
+    return PlainTextResponse('\n'.join(result))
