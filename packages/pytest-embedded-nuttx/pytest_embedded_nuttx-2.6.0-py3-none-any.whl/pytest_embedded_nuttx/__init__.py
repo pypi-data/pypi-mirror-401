13d61ad2dd9884c473caf11b5959e9ed28cbd800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:15:45 +0000

    Add package pytest-embedded-nuttx/2.6.0/pytest_embedded_nuttx-2.6.0-py3-none-any.whl

---
+++ packages/pytest-embedded-nuttx/pytest_embedded_nuttx-2.6.0-py3-none-any.whl/pytest_embedded_nuttx/__init__.py
@@ -0,0 +1,26 @@
+"""Make pytest-embedded plugin work with NuttX."""
+
+import importlib
+
+from pytest_embedded.utils import lazy_load
+
+from .app import NuttxApp
+from .dut import NuttxDut, NuttxSerialDut
+
+__getattr__ = lazy_load(
+    importlib.import_module(__name__),
+    {
+        'NuttxApp': NuttxApp,
+        'NuttxDut': NuttxDut,
+        'NuttxSerialDut': NuttxSerialDut,
+    },
+    {
+        'NuttxSerial': '.serial',  # requires 'esp' service
+        'NuttxEspDut': '.serial',  # requires 'esp' service
+        'NuttxQemuDut': '.qemu',  # requires 'qemu' service
+    },
+)
+
+__all__ = ['NuttxApp', 'NuttxDut', 'NuttxEspDut', 'NuttxQemuDut', 'NuttxSerial', 'NuttxSerialDut']
+
+__version__ = '2.6.0'
---
+++ packages/pytest-embedded-nuttx/pytest_embedded_nuttx-2.6.0-py3-none-any.whl/pytest_embedded_nuttx/serial.py
@@ -0,0 +1,149 @@
+from typing import ClassVar
+
+import esptool
+from esptool.cmds import FLASH_MODES, LoadFirmwareImage
+from pytest_embedded_serial_esp.serial import EspSerial
+
+from .app import NuttxApp
+from .dut import NuttxSerialDut
+
+
+class NuttxSerial(EspSerial):
+    """
+    NuttX serial DUT class.
+    """
+
+    # Default offset for the primary MCUBoot slot across
+    # all Espressif devices on NuttX
+    MCUBOOT_PRIMARY_SLOT_OFFSET = 0x20000
+    VIRTUAL_EFUSE_OFFSET = 0x10000
+    SERIAL_BAUDRATE = 115200
+
+    binary_offsets: ClassVar[dict[str, int]] = {
+        'esp32': 0x1000,
+        'esp32c3': 0x0,
+        'esp32c5': 0x2000,
+        'esp32c6': 0x0,
+        'esp32h2': 0x0,
+        'esp32p4': 0x2000,
+        'esp32s2': 0x1000,
+        'esp32s3': 0x0,
+    }
+
+    def __init__(
+        self,
+        app: NuttxApp,
+        **kwargs,
+    ) -> None:
+        self.app = app
+        super().__init__(**kwargs)
+        self.flash_size = None
+        self.flash_freq = None
+        self.flash_mode = None
+        self._get_binary_target_info()
+
+    def _get_binary_target_info(self):
+        """Binary target should be in the format nuttx.merged.bin, where
+        the 'merged.bin' extension can be modified by the file_extension
+        argument.
+
+        Important note regarding MCUBoot:
+        If enabled, the magic number will be on the MCUBoot binary. In this
+        case, image_info should run on the mcuboot binary, not the NuttX one.
+        """
+
+        def get_key_from_value(dictionary, val):
+            """Get key from value in dictionary"""
+            for key, value in dictionary.items():
+                if value == val:
+                    return key
+            return None
+
+        binary_path = self.app.app_file
+        if self.app.bootloader_file:
+            binary_path = self.app.bootloader_file
+
+        # Load app image and retrieve flash information
+        image = LoadFirmwareImage(self.target, binary_path.as_posix())
+
+        # Flash Size
+        flash_s_bits = image.flash_size_freq & 0xF0
+        self.flash_size = get_key_from_value(image.ROM_LOADER.FLASH_SIZES, flash_s_bits)
+
+        # Flash Frequency
+        flash_fr_bits = image.flash_size_freq & 0x0F  # low four bits
+        self.flash_freq = get_key_from_value(image.ROM_LOADER.FLASH_FREQUENCY, flash_fr_bits)
+
+        # Flash Mode
+        self.flash_mode = get_key_from_value(FLASH_MODES, image.flash_mode)
+
+        # From esp-idf/components/esptool_py:
+        # We use esptool.py to flash bootloader in dio mode for QIO/QOUT, bootloader then
+        # upgrades itself to quad mode during initialization.
+        if self.flash_mode in ('qio', 'qout'):
+            self.flash_mode = 'dio'
+
+    @EspSerial.use_esptool()
+    def flash(
+        self,
+        virtual_efuse_offset: int = VIRTUAL_EFUSE_OFFSET,
+        mcuboot_primary_slot_offset: int = MCUBOOT_PRIMARY_SLOT_OFFSET,
+    ) -> None:
+        """Flash the binary files to the board."""
+
+        flash_files = []
+        if self.app.bootloader_file:
+            if self.app.vefuse_file:
+                flash_files.extend((str(virtual_efuse_offset), self.app.vefuse_file.as_posix()))
+            flash_files.extend((str(self.binary_offsets[self.target]), self.app.bootloader_file.as_posix()))
+            flash_files.extend((str(mcuboot_primary_slot_offset), self.app.app_file.as_posix()))
+        else:
+            flash_files.extend((str(self.binary_offsets[self.target]), self.app.app_file.as_posix()))
+
+        flash_settings = [
+            '--flash-mode',
+            self.flash_mode,
+            '--flash-size',
+            self.flash_size,
+            '--flash-freq',
+            self.flash_freq,
+        ]
+
+        print(f'Flash files: {flash_files}')
+        print(f'Flash settings: {flash_settings}')
+        print(f'Virtual E-Fuse offset: {hex(virtual_efuse_offset)}')
+        print(f'MCUBoot primary slot offset: {hex(mcuboot_primary_slot_offset)}')
+
+        esptool.main(
+            [
+                '--chip',
+                self.target,
+                '--port',
+                self.port,
+                '--baud',
+                str(self.esptool_baud),
+                'write-flash',
+                *flash_files,
+                *flash_settings,
+            ],
+            esp=self.esp,
+        )
+
+
+class NuttxEspDut(NuttxSerialDut):
+    """
+    DUT class for serial ports connected to Espressif boards which are
+    flashed with NuttX RTOS.
+    """
+
+    def __init__(
+        self,
+        app: NuttxApp,
+        **kwargs,
+    ) -> None:
+        super().__init__(app=app, **kwargs)
+
+    def reset(self) -> None:
+        """Resets the board."""
+        self.serial: NuttxSerial
+        self.serial.hard_reset()
