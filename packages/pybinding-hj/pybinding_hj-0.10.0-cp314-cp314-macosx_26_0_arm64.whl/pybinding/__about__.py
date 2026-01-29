ef1621aefcd7d8281161a42ac08fe882eca18e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:16:23 +0000

    Add package pybinding-hj/0.10.0/pybinding_hj-0.10.0-cp314-cp314-macosx_26_0_arm64.whl

---
+++ packages/pybinding-hj/pybinding_hj-0.10.0-cp314-cp314-macosx_26_0_arm64.whl/pybinding/__about__.py
@@ -0,0 +1,11 @@
+"""Modernized fork of pybinding for numerical tight-binding calculations in solid state physics"""
+__title__ = "pybinding-hj"
+__version__ = "0.10.0"
+__summary__ = "Modernized fork of pybinding with Python 3.14 and NumPy 2.x support"
+__url__ = "https://github.com/hyangminj/pybinding"
+
+__author__ = "Dean Moldovan"
+__maintainer__ = "HyangMin Jeong"
+__copyright__ = "2015-2025, " + __author__
+__email__ = "dean0x7d@gmail.com"
+__license__ = "BSD"
