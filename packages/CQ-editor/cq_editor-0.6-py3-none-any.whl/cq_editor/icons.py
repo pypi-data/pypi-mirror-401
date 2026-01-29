41a2df9c492493402eee52eb7cd5e6ff24b4f08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:16:17 +0000

    Add package CQ-editor/0.6/cq_editor-0.6-py3-none-any.whl

---
+++ packages/CQ-editor/cq_editor-0.6-py3-none-any.whl/cq_editor/icons.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 25 14:47:10 2018
+
+@author: adam
+"""
+
+from PyQt5.QtGui import QIcon
+
+from . import icons_res
+
+_icons = {"app": QIcon(":/images/icons/cadquery_logo_dark.svg")}
+
+import qtawesome as qta
+
+_icons_specs = {
+    "new": (("fa5.file",), {}),
+    "open": (("fa5.folder-open",), {}),
+    # borrowed from spider-ide
+    "autoreload": [
+        ("fa5s.redo-alt", "fa5.clock"),
+        {
+            "options": [
+                {"scale_factor": 0.75, "offset": (-0.1, -0.1)},
+                {"scale_factor": 0.5, "offset": (0.25, 0.25)},
+            ]
+        },
+    ],
+    "save": (("fa5.save",), {}),
+    "save_as": (
+        ("fa5.save", "fa5s.pencil-alt"),
+        {
+            "options": [
+                {
+                    "scale_factor": 1,
+                },
+                {"scale_factor": 0.8, "offset": (0.2, 0.2)},
+            ]
+        },
+    ),
+    "run": (("fa5s.play",), {}),
+    "debug": (("fa5s.bug",), {}),
+    "delete": (("fa5s.trash",), {}),
+    "delete-many": (
+        (
+            "fa5s.trash",
+            "fa5s.trash",
+        ),
+        {
+            "options": [
+                {"scale_factor": 0.8, "offset": (0.2, 0.2), "color": "gray"},
+                {"scale_factor": 0.8},
+            ]
+        },
+    ),
+    "help": (("fa5s.life-ring",), {}),
+    "about": (("fa5s.info",), {}),
+    "preferences": (("fa5s.cogs",), {}),
+    "inspect": (
+        ("fa5s.cubes", "fa5s.search"),
+        {"options": [{"scale_factor": 0.8, "offset": (0, 0), "color": "gray"}, {}]},
+    ),
+    "screenshot": (("fa5s.camera",), {}),
+    "screenshot-save": (
+        ("fa5.save", "fa5s.camera"),
+        {
+            "options": [
+                {"scale_factor": 0.8},
+                {"scale_factor": 0.8, "offset": (0.2, 0.2)},
+            ]
+        },
+    ),
+    "toggle-comment": (("fa5s.hashtag",), {}),
+    "search": (("fa5s.search",), {}),
+    "arrow-step-over": (("fa5s.step-forward",), {}),
+    "arrow-step-in": (("fa5s.angle-down",), {}),
+    "arrow-continue": (("fa5s.arrow-right",), {}),
+    "clear": (("fa5s.eraser",), {}),
+    "clear-2": (("fa5s.broom",), {}),
+}
+
+
+def icon(name):
+
+    if name in _icons:
+        return _icons[name]
+
+    args, kwargs = _icons_specs[name]
+
+    return qta.icon(*args, **kwargs)
---
+++ packages/CQ-editor/cq_editor-0.6-py3-none-any.whl/cq_editor/widgets/log.py
@@ -0,0 +1,76 @@
+import logbook as logging
+import re
+
+from PyQt5 import QtGui
+from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtWidgets import QPlainTextEdit, QAction
+
+from ..mixins import ComponentMixin
+
+from ..icons import icon
+
+
+def strip_escape_sequences(input_string):
+    # Regular expression pattern to match ANSI escape codes
+    escape_pattern = re.compile(r"\x1B\[[0-?]*[ -/]*[@-~]")
+
+    # Use re.sub to replace escape codes with an empty string
+    clean_string = re.sub(escape_pattern, "", input_string)
+
+    return clean_string
+
+
+class _QtLogHandlerQObject(QObject):
+    sigRecordEmit = pyqtSignal(str)
+
+
+class QtLogHandler(logging.Handler, logging.StringFormatterHandlerMixin):
+
+    def __init__(self, log_widget, *args, **kwargs):
+
+        super(QtLogHandler, self).__init__(*args, **kwargs)
+
+        log_format_string = (
+            "[{record.time:%H:%M:%S%z}] {record.level_name}: {record.message}"
+        )
+
+        logging.StringFormatterHandlerMixin.__init__(self, log_format_string)
+
+        self._qobject = _QtLogHandlerQObject()
+        self._qobject.sigRecordEmit.connect(log_widget.append)
+
+    def emit(self, record):
+        self._qobject.sigRecordEmit.emit(self.format(record) + "\n")
+
+
+class LogViewer(QPlainTextEdit, ComponentMixin):
+
+    name = "Log viewer"
+
+    def __init__(self, *args, **kwargs):
+
+        super(LogViewer, self).__init__(*args, **kwargs)
+        self._MAX_ROWS = 500
+
+        self._actions = {
+            "Run": [
+                QAction(icon("clear"), "Clear Log", self, triggered=self.clear),
+            ]
+        }
+
+        self.setReadOnly(True)
+        self.setMaximumBlockCount(self._MAX_ROWS)
+        self.setLineWrapMode(QPlainTextEdit.NoWrap)
+
+        self.handler = QtLogHandler(self)
+
+    def append(self, msg):
+        """Append text to the panel with ANSI escape sequences stipped."""
+        self.moveCursor(QtGui.QTextCursor.End)
+        self.insertPlainText(strip_escape_sequences(msg))
+
+    def clear_log(self):
+        """
+        Clear the log content.
+        """
+        self.clear()
