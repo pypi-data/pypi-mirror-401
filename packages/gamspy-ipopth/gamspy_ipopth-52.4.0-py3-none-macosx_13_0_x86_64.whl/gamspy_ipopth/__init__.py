b1c0575eb56e7968e0e70d48fc77dcc483c079a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:50:30 +0000

    Add package gamspy-ipopth/52.4.0/gamspy_ipopth-52.4.0-py3-none-macosx_13_0_x86_64.whl

---
+++ packages/gamspy-ipopth/gamspy_ipopth-52.4.0-py3-none-macosx_13_0_x86_64.whl/gamspy_ipopth/__init__.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+from .version import __version__
+directory = str(Path(__file__).resolve().parent)
+
+files = ['libipopt.dylib', 'libiomp5.dylib', 'libmkl_gams.dylib', 'optipopt.def', 'libipocclib64.dylib']
+
+file_paths = [directory + os.sep + file for file in files]
+verbatim = 'IPOPTH 11 5 IP 1 0 2 LP RMIP NLP CNS DNLP RMINLP QCP RMIQCP\ngmsgenus.run optipopt.def\ngmsgenux.out\nlibipocclib64.dylib ipo 1 1'
