35c6dae5beab8fc54a535c33f0fa29bfc75df89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:15:10 +0000

    Add package pertdb/2.0.0/pertdb-2.0.0-py2.py3-none-any.whl

---
+++ packages/pertdb/pertdb-2.0.0-py2.py3-none-any.whl/pertdb/__init__.py
@@ -0,0 +1,76 @@
+"""Basic pertdb entities [`source <https://github.com/laminlabs/pertdb/blob/main/pertdb/models.py>`__].
+
+Install and mount `pertdb` in a new instance:
+
+>>> pip install pertdb
+>>> lamin init --storage ./test-pertdb --modules bionty,pertdb
+
+Import the package:
+
+>>> import pertdb
+
+Create records:
+
+>>> biologic = pertdb.Biologic(name="IFNG").save()
+
+Perturbation registries:
+
+.. autosummary::
+   :toctree: .
+
+   GeneticPerturbation
+   Biologic
+   Compound
+   CompoundPerturbation
+   EnvironmentalPerturbation
+   CombinationPerturbation
+
+Auxiliary registries:
+
+.. autosummary::
+   :toctree: .
+
+   PerturbationTarget
+
+Types:
+
+.. autosummary::
+   :toctree: .
+
+   GeneticPerturbationSystem
+   BiologicType
+
+"""
+
+__version__ = "2.0.0"
+
+from lamindb_setup import _check_instance_setup
+
+from .types import BiologicType, GeneticPerturbationSystem
+
+_check_instance_setup(from_module="pertdb")
+
+
+from .models import (
+    Biologic,
+    CombinationPerturbation,
+    Compound,
+    CompoundPerturbation,
+    EnvironmentalPerturbation,
+    GeneticPerturbation,
+    PerturbationTarget,
+)
+
+__all__ = [
+    # registries
+    "Biologic",
+    "CombinationPerturbation",
+    "Compound",
+    "CompoundPerturbation",
+    "EnvironmentalPerturbation",
+    "GeneticPerturbation",
+    "PerturbationTarget",
+    # helper types
+    "BiologicType",
+    "GeneticPerturbationSystem",
+]
